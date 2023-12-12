3ef54df15afcf9f16915174d6b0176e6bbf20148
Travis Bloom <travisbloom@gmail.com>
2023-12-11 22:46:18 -0500

    prevent unexpected any's by using total-typescript/ts-reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Stand With Crypto Alliance',
-  description: 'Stand with Crypto Alliance is a non-profit organization dedicated to uniting global crypto advocates',
+  description:
+    'Stand with Crypto Alliance is a non-profit organization dedicated to uniting global crypto advocates',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
