dbbdc8656288bc6f8dfc01a77dc6c2cd7f72a288
Travis Bloom <travis.bloom@coinbase.com>
2023-12-15 18:07:12 -0500

    prettier format

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling-webpack'
+    '@storybook/addon-styling-webpack',
   ],
   framework: {
     name: '@storybook/nextjs',
--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,15 +1,7 @@
-import { cn } from "@/utils/web/cn"
+import { cn } from '@/utils/web/cn'
 
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
-    />
-  )
+function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />
 }
 
 export { Skeleton }
