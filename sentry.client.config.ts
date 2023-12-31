b7eafa929d959dddecc159c34146acfae43b9695
Travis Bloom <travis.bloom@coinbase.com>
2023-12-31 13:42:17 -0500

    remove profile sampling configs until we re-enable

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -12,13 +12,6 @@ Sentry.init({
   dsn,
   enabled: !!dsn,
   tracesSampleRate: environment === 'production' ? 0.001 : 1.0,
-  // Set profilesSampleRate to 1.0 to profile every transaction.
-  // Since profilesSampleRate is relative to tracesSampleRate,
-  // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
-  // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
-  // results in 25% of transactions being profiled (0.5*0.5=0.25)
-  profilesSampleRate: environment === 'production' ? 0.25 : 1.0,
-
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -18,13 +18,6 @@ Sentry.init({
     new Sentry.Integrations.Prisma({ client: prismaClient }),
   ],
   tracesSampleRate: environment === 'production' ? 0.001 : 1.0,
-  // Set profilesSampleRate to 1.0 to profile every transaction.
-  // Since profilesSampleRate is relative to tracesSampleRate,
-  // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
-  // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
-  // results in 25% of transactions being profiled (0.5*0.5=0.25)
-  profilesSampleRate: environment === 'production' ? 0.25 : 1.0,
-
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
   beforeSend: (event, hint) => {
