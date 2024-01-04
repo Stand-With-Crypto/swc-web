35947f4662fcd9a8bc82f34143b39f0b7cfc73ea
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-01-04 17:15:54 -0300

    revert: return to flex wrap based responsiveness

--- src/components/app/pageAbout/card.tsx
+++ src/components/app/pageAbout/card.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react'
 export const cardClassNames = twNoop('bg-muted px-4 py-6 rounded-xl')
 
 function CardGroup({ children }: PropsWithChildren) {
-  return <div className="grid grid-cols-1 gap-4 md:grid-cols-3">{children}</div>
+  return <div className="flex flex-wrap gap-4">{children}</div>
 }
 
 function CardHeading({ children }: PropsWithChildren) {
@@ -16,7 +16,11 @@ function CardDescription({ children }: PropsWithChildren) {
 }
 
 export function Card({ children, className }: PropsWithChildren<{ className?: string }>) {
-  return <div className={cn('flex flex-col gap-4', cardClassNames, className)}>{children}</div>
+  return (
+    <div className={cn('flex min-w-[256px] flex-1 flex-col gap-4', cardClassNames, className)}>
+      {children}
+    </div>
+  )
 }
 
 Card.Heading = CardHeading
