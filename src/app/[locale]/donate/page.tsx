64b2ce11b7c467e7a8731763c495f2044faade5d
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-01-05 15:43:39 -0300

    feat: add odometer component

--- src/app/[locale]/donate/page.tsx
+++ src/app/[locale]/donate/page.tsx
@@ -1,14 +1,18 @@
-import { PageTitle } from '@/components/ui/pageTitleText'
-import { PageProps } from '@/types'
+'use client'
+
+import { Button } from '@/components/ui/button'
+import { Odometer } from '@/components/ui/odometer/odometer'
+import { useState } from 'react'
 
 export const dynamic = 'error'
 
-type Props = PageProps
+export default function DonatePage() {
+  const [value, setValue] = useState(95832)
 
-export default async function DonatePage({ params }: Props) {
   return (
-    <div className="container">
-      <PageTitle>TODO</PageTitle>
+    <div className="container flex flex-col gap-8">
+      <Odometer value={value} format="(,ddd).dd" className="text-xl font-bold" />
+      <Button onClick={() => setValue(prev => prev + 321)}>ADD</Button>
     </div>
   )
 }
