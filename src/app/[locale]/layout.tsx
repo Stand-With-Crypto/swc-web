585bdc256ccb78b9643e658c55d915923df90040
Travis Bloom <travis.bloom@coinbase.com>
2024-01-09 13:56:47 -0500

    remove site manifest until we figure out why it's 401 in the browser

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -46,7 +46,7 @@ export const metadata: Metadata = {
     { url: '/logo/favicon-16x16.png', sizes: '16x16' },
     { url: '/logo/favicon-32x32.png', sizes: '32x32' },
   ],
-  manifest: '/site.webmanifest',
+  // manifest: '/site.webmanifest', // TODO figure out why we get 401s when we uncomment this
   appleWebApp: {
     title: 'Stand With Crypto',
     statusBarStyle: 'black-translucent',
