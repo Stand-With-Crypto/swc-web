c15698b966ddc243ae01678867c9388c3257fe3d
Travis Bloom <travis.bloom@coinbase.com>
2024-01-10 12:13:02 -0500

    fixes

--- src/bin/run.ts
+++ src/bin/run.ts
@@ -8,13 +8,6 @@ async function run() {
     This file is committed to the repo to allow engineers to easily run one off node logic leveraging our codebase. Add any code below to test what you need, than delete it before committing
     command for reference: npm run ts src/bin/run.ts
     */
-  prettyLog(
-    await getENSDataMapFromCryptoAddressesAndFailGracefully([
-      PopularCryptoAddress.BRIAN_ARMSTRONG,
-      PopularCryptoAddress.CHRIS_DIXON,
-      process.env.LOCAL_USER_CRYPTO_ADDRESS as any,
-    ]),
-  )
 }
 
 runBin(run)
