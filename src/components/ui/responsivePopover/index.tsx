68ce7db675bbef236fa300e4f0f5650de8d2cb65
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-01-11 16:43:39 -0300

    feat: memoize responsive popover

--- src/components/ui/responsivePopover/index.tsx
+++ src/components/ui/responsivePopover/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, useContext } from 'react'
+import React from 'react'
 
 import { useIsMobile } from '@/hooks/useIsMobile'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
@@ -15,10 +15,13 @@ import {
 export function useResponsivePopover() {
   const isMobile = useIsMobile()
 
-  return {
-    Popover: isMobile ? Drawer : Popover,
-    PopoverTrigger: isMobile ? DrawerTrigger : PopoverTrigger,
-    PopoverContent: isMobile ? DrawerContent : PopoverContent,
-    PopoverTitle: isMobile ? DrawerTitle : DrawerHeader,
-  }
+  return React.useMemo(
+    () => ({
+      Popover: isMobile ? Drawer : Popover,
+      PopoverTrigger: isMobile ? DrawerTrigger : PopoverTrigger,
+      PopoverContent: isMobile ? DrawerContent : PopoverContent,
+      PopoverTitle: isMobile ? DrawerTitle : DrawerHeader,
+    }),
+    [isMobile],
+  )
 }
