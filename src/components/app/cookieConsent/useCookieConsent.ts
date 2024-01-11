4153d51b8478f467421918e0c0a1dcce6fc55dc0
Travis Bloom <travis.bloom@coinbase.com>
2024-01-11 15:56:55 -0500

    fix cookie consent bug

--- src/components/app/cookieConsent/useCookieConsent.ts
+++ src/components/app/cookieConsent/useCookieConsent.ts
@@ -12,18 +12,6 @@ export function useCookieConsent() {
 
   const serializeConsentCookie = React.useCallback(
     (permissions: CookieConsentPermissions): string => {
-      /*
-      to be conservative, if someone opts out of functional or performance, we should assume
-      they don't want targeting either
-      */
-      if (isBrowser) {
-        // if (!permissions.functional || !permissions.performance || !permissions.targeting) {
-        //   mixpanel.opt_out_tracking()
-        // }
-        // if (permissions.functional && permissions.performance && permissions.targeting) {
-        //   mixpanel.opt_in_tracking()
-        // }
-      }
       return Object.entries({ required: true, ...permissions })
         .map(([key, value]) => `${key}:${String(value)}`)
         .join(',')
@@ -31,9 +19,23 @@ export function useCookieConsent() {
     [],
   )
 
+  const toggleProviders = React.useCallback((permissions: CookieConsentPermissions) => {
+    /*
+      to be conservative, if someone opts out of functional or performance, we should assume
+      they don't want targeting either
+      */
+    if (!permissions.functional || !permissions.performance || !permissions.targeting) {
+      mixpanel.opt_out_tracking()
+    }
+    if (permissions.functional && permissions.performance && permissions.targeting) {
+      mixpanel.opt_in_tracking()
+    }
+  }, [])
+
   const acceptSpecificCookies = React.useCallback(
     (consentCookie: CookieConsentPermissions): void => {
       setCookieConsentCookie(serializeConsentCookie(consentCookie))
+      toggleProviders(consentCookie)
     },
     [serializeConsentCookie, setCookieConsentCookie],
   )
@@ -60,10 +62,20 @@ export function useCookieConsent() {
 
   const rejectAllOptionalCookies = React.useCallback((): void => {
     setCookieConsentCookie(rejectAllCookieValue)
+    toggleProviders({
+      functional: false,
+      performance: false,
+      targeting: false,
+    })
   }, [setCookieConsentCookie, rejectAllCookieValue])
 
   const acceptAllCookies = React.useCallback((): void => {
     setCookieConsentCookie(acceptAllCookieValue)
+    toggleProviders({
+      functional: true,
+      performance: true,
+      targeting: true,
+    })
   }, [setCookieConsentCookie, acceptAllCookieValue])
 
   return {
@@ -71,8 +83,6 @@ export function useCookieConsent() {
     rejectAllOptionalCookies,
     acceptAllCookies,
     resetCookieConsent: removeCookieConsentCookie,
-    rejectAllCookieValue,
-    acceptAllCookieValue,
     acceptedCookies: !!cookieConsentCookie,
   }
 }
