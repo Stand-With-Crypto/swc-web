e35528d79d7d8d405b2c4014dd8cdd787331db0a
Travis Bloom <travis.bloom@coinbase.com>
2024-01-15 21:28:08 -0500

    maybeCreateMergeAlertAfterEmailVerification

---
+++ src/utils/server/mergeUsers/maybeCreateMergeAlertAfterEmailVerification.ts
@@ -0,0 +1,70 @@
+import { prismaClient } from '@/utils/server/prismaClient'
+import { getLogger } from '@/utils/shared/logger'
+import _ from 'lodash'
+
+const logger = getLogger(`maybeCreateMergeAlertAfterEmailVerification`)
+
+/*
+Logic that occurs anytime a users email is verified and the user previously existed
+*/
+// TODO integrate with logic that deals with verified emails being persisted to db
+
+export async function maybeCreateMergeAlertAfterEmailVerification(userEmailAddressId: string) {
+  const user = await prismaClient.user.findFirstOrThrow({
+    where: {
+      userEmailAddresses: { some: { id: userEmailAddressId } },
+    },
+    include: {
+      userEmailAddresses: true,
+      userMergeAlertUserA: true,
+      userMergeAlertUserB: true,
+    },
+  })
+  const userEmailAddress = user.userEmailAddresses.find(x => x.id !== userEmailAddressId)!
+  if (!userEmailAddress.isVerified) {
+    throw new Error(`userEmailAddress ${userEmailAddressId} is not verified`)
+  }
+  const maybeMatchUsers = await prismaClient.user.findMany({
+    where: {
+      id: { not: user.id },
+      userEmailAddresses: { some: { address: userEmailAddress.address, isVerified: true } },
+    },
+  })
+  if (!maybeMatchUsers.length) {
+    logger.info(`no additional users found with verified email address ${userEmailAddress.address}`)
+    return
+  }
+  logger.info(
+    `found ${maybeMatchUsers.length} users with verified email address ${userEmailAddress.address}`,
+  )
+  const existingMatchUserIds = _.uniq([
+    ...user.userMergeAlertUserA.map(x => x.userBId),
+    ...user.userMergeAlertUserB.map(x => x.userAId),
+  ])
+  logger.info(
+    `found ${
+      existingMatchUserIds.length
+    } other users with existing matches: ${existingMatchUserIds.join(`, `)}}`,
+  )
+  const mayBeMatchUsersWithoutExistingMatch = maybeMatchUsers.filter(
+    x => !existingMatchUserIds.includes(x.id),
+  )
+  if (!mayBeMatchUsersWithoutExistingMatch.length) {
+    logger.info(`no additional users found without existing matches`)
+    return
+  }
+  logger.info(
+    `found ${
+      mayBeMatchUsersWithoutExistingMatch.length
+    } users without existing matches: ${mayBeMatchUsersWithoutExistingMatch
+      .map(x => x.id)
+      .join(`, `)}`,
+  )
+  const userMergeAlerts = await prismaClient.userMergeAlert.createMany({
+    data: mayBeMatchUsersWithoutExistingMatch.map(x => ({
+      userAId: user.id,
+      userBId: x.id,
+    })),
+  })
+  return userMergeAlerts
+}
