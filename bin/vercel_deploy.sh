5529ca2227bfa08f609cb8d80a1354b7f2550081
Travis Bloom <travis.bloom@coinbase.com>
2024-01-17 16:27:43 -0500

    tweaks

--- bin/vercel_deploy.sh
+++ bin/vercel_deploy.sh
@@ -12,7 +12,7 @@ npm run lint
 NODE_ENV=test npm run test
 # If the branch is a db changes branch, we're going to skip building the preview because it's pointing to a database without the changes (testing)
 if [[ $VERCEL_GIT_COMMIT_REF == "main" || $VERCEL_GIT_COMMIT_REF == "deploy-web-production" ]]; then
-    if git diff --name-only HEAD^ | grep -q 'prisma/schema.prisma'; then
+    if git diff --name-only HEAD^ HEAD | grep -q 'prisma/schema.prisma'; then
         echo "Schema changes detected on a main branch, pushing changes to db. If there are breaking changes this command will fail"
         npx prisma db push --skip-generate
         echo "Building app"
@@ -23,7 +23,7 @@ if [[ $VERCEL_GIT_COMMIT_REF == "main" || $VERCEL_GIT_COMMIT_REF == "deploy-web-
     wait
     echo "frontend assets built"
 else
-    if git diff --name-only "origin/main" | grep -q 'prisma/schema.prisma'; then
+    if git diff --name-only HEAD^ HEAD | grep -q 'prisma/schema.prisma'; then
         echo "Schema changes detected on a preview branch, skipping deploy because this branch is pointing at the testing db which does not have the schema changes. The failure below is expected and is not an issue."
         exit 0;
         exit 1;
