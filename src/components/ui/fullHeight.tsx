6080931663e15b716496993886afa70a80c4c64c
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-01-18 12:45:01 -0300

    refactor: remove asChild prop from fullHeight

--- src/components/ui/fullHeight.tsx
+++ src/components/ui/fullHeight.tsx
@@ -1,27 +1,25 @@
 import { cn } from '@/utils/web/cn'
 import React from 'react'
 
-const Container = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { asChild?: boolean }
->(({ className, asChild: _asChild = false, ...props }, ref) => {
-  return <div className={cn('flex min-h-dvh flex-col', className)} ref={ref} {...props} />
-})
+const Container = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return <div className={cn('flex min-h-dvh flex-col', className)} ref={ref} {...props} />
+  },
+)
 Container.displayName = 'FullPageHeightContainer'
 
-const Content = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { asChild?: boolean }
->(({ className, asChild: _asChild = false, ...props }, ref) => {
-  return (
-    <main
-      className={cn('flex flex-col', className)}
-      style={{ flex: '1 0 auto' }}
-      ref={ref}
-      {...props}
-    />
-  )
-})
+const Content = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    return (
+      <main
+        className={cn('flex flex-col', className)}
+        style={{ flex: '1 0 auto' }}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
 Content.displayName = 'FullPageHeightContent'
 
 export const FullHeight = {
