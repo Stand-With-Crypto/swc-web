e734ce6448ef8139061ccdec660d59c910589cf0
Travis Bloom <153657370+travisbloom-cb@users.noreply.github.com>
2024-01-18 08:54:11 -0500

    ensure that we can view testing environment errors separately than preview branch errors in sentry (#109)
    * ensure that we can view testing environment errors separately than preview branch errors in sentry
    
    * fixes

--- src/utils/shared/sharedEnv.ts
+++ src/utils/shared/sharedEnv.ts
@@ -5,7 +5,17 @@ export const NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN = requiredEnv(
   'process.env.NEXT_PUBLIC_THIRDWEB_AUTH_DOMAIN',
 )
 
-export const NEXT_PUBLIC_ENVIRONMENT = requiredEnv(
+let initialEnv = requiredEnv(
   process.env.NEXT_PUBLIC_ENVIRONMENT,
   'process.env.NEXT_PUBLIC_ENVIRONMENT',
-) as 'local' | 'testing' | 'production'
+) as 'local' | 'testing' | 'production' | 'preview'
+if (initialEnv === 'testing') {
+  const NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF = requiredEnv(
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
+    'NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF',
+  )
+  if (NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF !== 'main') {
+    initialEnv = 'preview'
+  }
+}
+export const NEXT_PUBLIC_ENVIRONMENT = initialEnv
