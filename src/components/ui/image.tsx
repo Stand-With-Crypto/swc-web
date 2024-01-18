1d5fcf946d0ab4d5c95655495f9ad0650761e7dc
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-01-18 13:00:19 -0300

    refactor: remove omit from image

--- src/components/ui/image.tsx
+++ src/components/ui/image.tsx
@@ -1,4 +1,3 @@
-import { omit } from 'lodash'
 import NextImageComponent, { ImageProps as NextImageProps } from 'next/image'
 
 export const NextImage = NextImageComponent
@@ -13,21 +12,24 @@ export const MaybeNextImg: React.FC<MaybeNextImgProps> = props => {
     return <NextImage {...(props as NextImageProps)} />
   }
 
-  const { src, ...imgProps } = omit(props, [
-    'width',
-    'height',
-    'layout',
-    'placeholder',
-    'blurDataURL',
-    'loader',
-    'quality',
-    'priority',
-    'loading',
-    'unoptimized',
-    'objectFit',
-    'fill',
-    'objectPosition',
-  ])
+  const {
+    src,
+    width: _width,
+    height: _height,
+    layout: _layout,
+    placeholder: _placeholder,
+    blurDataURL: _blurDataURL,
+    loader: _loader,
+    quality: _quality,
+    priority: _priority,
+    loading: _loading,
+    unoptimized: _unoptimized,
+    objectFit: _objectFit,
+    fill: _fill,
+    objectPosition: _objectPosition,
+    ...imgProps
+  } = props
+
   // eslint-disable-next-line
   return <img {...{ ...imgProps, src: src as string }} />
 }
