c80cae4cc15d71711a8490fedc6a1be712fa20a2
Travis Bloom <travis.bloom@coinbase.com>
2024-01-18 07:32:27 -0500

    fixes

--- src/utils/shared/sharedEnv.ts
+++ src/utils/shared/sharedEnv.ts
@@ -9,7 +9,13 @@ let initialEnv = requiredEnv(
   process.env.NEXT_PUBLIC_ENVIRONMENT,
   'process.env.NEXT_PUBLIC_ENVIRONMENT',
 ) as 'local' | 'testing' | 'production' | 'preview'
-if (initialEnv === 'testing' && process.env.VERCEL_GIT_COMMIT_REF !== 'main') {
-  initialEnv = 'preview'
+if (initialEnv === 'testing') {
+  const NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF = requiredEnv(
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
+    'NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF',
+  )
+  if (NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF !== 'main') {
+    initialEnv = 'preview'
+  }
 }
 export const NEXT_PUBLIC_ENVIRONMENT = initialEnv
