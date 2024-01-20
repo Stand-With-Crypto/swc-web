a197044041d5a4a44ad0e7ebebb90115ea157e31
Travis Bloom <travis.bloom@coinbase.com>
2024-01-20 09:53:24 -0500

    get cypress working

---
+++ cypress/e2e/1-homepage/sanitycheck.cy.ts
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+it('sanity checks that the homepage is loading as expected', () => {
+  cy.visit('/')
+  // open the main watch CTA and verify the youtube video loads
+  cy.contains('Watch').click()
+  cy.get('iframe').should('have.attr', 'src').and('include', 'youtube.com')
+  cy.contains('Close').click()
+
+  // verify the donations tab shows top donors
+  cy.contains('Top donations').click()
+  cy.get('img[alt="position 1 medal"]')
+
+  // verify clicking one of the action cards opens a modal
+  cy.contains('Join Stand With Crypto').click()
+  cy.contains('Close').click()
+})
--- src/utils/shared/executionEnvironment.ts
+++ src/utils/shared/executionEnvironment.ts
@@ -1,1 +1,4 @@
 export const isBrowser = typeof window !== 'undefined'
+
+export const isCypress =
+  process.env.NEXT_PUBLIC_IS_CYPRESS || (isBrowser ? window.Cypress : process.env.CYPRESS)
