ef1445728788746d7e8028a534116a2bd16fd9b4
Travis Bloom <travis.bloom@coinbase.com>
2024-01-22 16:54:54 -0500

    ignore 'server-only' when running bin scripts

--- src/bin/mocks/fetchDTSISlugsForMocks.ts
+++ src/bin/mocks/fetchDTSISlugsForMocks.ts
@@ -1,4 +1,5 @@
 import { persistJSONToStaticContentFolder, runBin } from '@/bin/binUtils'
+import { queryDTSIAllPeople } from '@/data/dtsi/queries/queryDTSIAllPeople'
 import { getLogger } from '@/utils/shared/logger'
 import 'dotenv/config'
 
@@ -8,14 +9,9 @@ async function fetchDTSISlugsForMocks() {
   logger.info(
     `fetching testing DTSI slugs and persisting to local DB for usage when generating mocks`,
   )
-  // TODO uncomment after I figure out how to get `import 'server-only'` to work in bin scripts
-  // const { people } = await queryDTSIAllPeople()
-  // const slugs = people.map(person => person.slug)
-  await persistJSONToStaticContentFolder('mocks/dtsiSlugs.json', [
-    'sherrod---brown',
-    'scott---fitzgerald',
-    'marjorie---greene',
-  ])
+  const { people } = await queryDTSIAllPeople()
+  const slugs = people.map(person => person.slug)
+  await persistJSONToStaticContentFolder('mocks/dtsiSlugs.json', slugs)
 }
 
 runBin(fetchDTSISlugsForMocks)
