c69c745981d20b4f4eef08f210fd7c36f44388f2
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-23 13:46:50 -0500

    Update layout with navbar and footer

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,21 +1,20 @@
 'use client'
-import * as Sentry from '@sentry/nextjs'
-import { useEffect } from 'react'
-import NextError from 'next/error'
+import { ErrorPagesContent } from '@/components/app/errorPagesContent'
 import { logger } from '@/utils/shared/logger'
+import { useEffect } from 'react'
+import * as Sentry from '@sentry/nextjs'
 
-export default function RootErrorPage({ error }: { error: Error & { digest?: string } }) {
+export default function RootErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
   useEffect(() => {
     logger.info('RootErrorPage rendered with:', error)
     Sentry.captureException(error)
     Sentry.captureException(new Error('Root Error Page Displayed'))
   }, [error])
-  return (
-    <html lang="en">
-      <body>
-        {/* TODO graceful UI here */}
-        <NextError statusCode={undefined as any} />
-      </body>
-    </html>
-  )
+  return <ErrorPagesContent reset={reset} />
 }
