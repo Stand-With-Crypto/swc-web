75bfda71430425a12e9938c555b52be507933abb
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-01-23 15:29:47 -0300

    wip

--- src/mocks/models/mockUser.ts
+++ src/mocks/models/mockUser.ts
@@ -1,10 +1,11 @@
 import { fakerFields } from '@/mocks/fakerUtils'
 import { mockCommonDatetimes } from '@/mocks/mockCommonDatetimes'
 import { faker } from '@faker-js/faker'
-import { User } from '@prisma/client'
+import { User, UserInternalStatus } from '@prisma/client'
 
 export function mockUser(): User {
   const withData = faker.helpers.maybe(() => true, { probability: 0.5 })
+  const isVisible = faker.helpers.maybe(() => true, { probability: 0.9 })
   return {
     ...mockCommonDatetimes(),
     acquisitionCampaign: '',
@@ -22,5 +23,6 @@ export function mockUser(): User {
     hasOptedInToEmails: true,
     hasOptedInToMembership: false,
     hasOptedInToSms: false,
+    internalStatus: isVisible ? UserInternalStatus.VISIBLE : UserInternalStatus.MANUALLY_HIDDEN,
   }
 }
