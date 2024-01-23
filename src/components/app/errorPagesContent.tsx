c64795b74363d6941d6973ba7cf99602dac9b79b
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-23 13:45:50 -0500

    Update styling

--- src/components/app/errorPagesContent.tsx
+++ src/components/app/errorPagesContent.tsx
@@ -4,7 +4,7 @@ import { PageTitle } from '@/components/ui/pageTitleText'
 
 export function ErrorPagesContent({ reset }: { reset: () => void }) {
   return (
-    <div className="flex flex-col items-center justify-center gap-6">
+    <div className="flex flex-grow flex-col items-center justify-center gap-6">
       <MaybeNextImg src="/error_shield.svg" width={120} height={120} alt="" />
       <PageTitle>Something went wrong.</PageTitle>
       <Button className="mr-3" onClick={reset}>
