f699021c54fc592817dbf5d60b45361c7c2248c2
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-23 13:54:43 -0500

    Use shared error UI in global error

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,21 +1,20 @@
 'use client'
 import * as Sentry from '@sentry/nextjs'
 import { useEffect } from 'react'
-import NextError from 'next/error'
 import { logger } from '@/utils/shared/logger'
+import { ErrorPagesContent } from '@/components/app/errorPagesContent'
 
-export default function GlobalErrorPage({ error }: { error: Error & { digest?: string } }) {
+export default function GlobalErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
   useEffect(() => {
     logger.info('GlobalErrorPage rendered with:', error)
     Sentry.captureException(error)
     Sentry.captureException(new Error('Global Error Page Displayed'))
   }, [error])
-  return (
-    <html lang="en">
-      <body>
-        {/* TODO graceful UI here */}
-        <NextError statusCode={undefined as any} />
-      </body>
-    </html>
-  )
+  return <ErrorPagesContent reset={reset} />
 }
