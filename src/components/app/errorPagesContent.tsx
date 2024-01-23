b28cb7ea759d46a288a9d1d5515e7b74bb7e77ba
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-23 16:29:02 -0500

    remove trailing white space

--- src/components/app/errorPagesContent.tsx
+++ src/components/app/errorPagesContent.tsx
@@ -4,7 +4,7 @@ import { PageTitle } from '@/components/ui/pageTitleText'
 
 export function ErrorPagesContent({ reset }: { reset: () => void }) {
   return (
-    <div className=" container flex flex-grow flex-col items-center justify-center space-y-7">
+    <div className="container flex flex-grow flex-col items-center justify-center space-y-7">
       <MaybeNextImg src="/error_shield.svg" width={120} height={120} alt="" />
       <PageTitle size="sm">Something went wrong.</PageTitle>
       <Button className="mr-3" onClick={reset}>
