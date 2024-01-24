1660933533ab3a9ba52f73ef725abe0b3a479f82
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-24 13:53:09 -0500

    Add navbar and header to root not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,17 @@
+import '@/globals.css'
+
+import { TopLevelClientLogic } from '@/app/[locale]/topLevelClientLogic'
+import { CookieConsent } from '@/components/app/cookieConsent'
+import { Footer } from '@/components/app/footer'
+import { Navbar } from '@/components/app/navbar'
 import { NotFoundPagesContent } from '@/components/app/notFoundPagesContent'
-import { cn } from '@/utils/web/cn'
+import { FullHeight } from '@/components/ui/fullHeight'
+import { DEFAULT_LOCALE } from '@/intl/locales'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Inter } from 'next/font/google'
+import NextTopLoader from 'nextjs-toploader'
+import { Toaster } from 'sonner'
 
 // TODO replace with font we want
 const inter = Inter({ subsets: ['latin'] })
@@ -9,10 +20,21 @@ export const dynamic = 'error'
 export default function NotFound() {
   return (
     <html lang="en">
-      <body className={cn(inter.className, 'flex h-screen content-center items-center')}>
-        <main className="prose-sm mx-auto mt-10 w-full max-w-xl p-4">
-          <NotFoundPagesContent skipTracking />
-        </main>
+      <body className={inter.className}>
+        <NextTopLoader />
+        <TopLevelClientLogic locale={DEFAULT_LOCALE}>
+          <FullHeight.Container>
+            <FullHeight.Content>
+              <Navbar locale={DEFAULT_LOCALE} />
+              <NotFoundPagesContent />
+            </FullHeight.Content>
+            <Footer locale={DEFAULT_LOCALE} />
+          </FullHeight.Container>
+        </TopLevelClientLogic>
+        <Toaster />
+        <CookieConsent locale={DEFAULT_LOCALE} />
+        <Analytics debug={false} />
+        <SpeedInsights debug={false} />
       </body>
     </html>
   )
