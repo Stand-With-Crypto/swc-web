ca25cbbd41b80454c1d09d7442d12dfc1f49edbd
Kaerah Lopez <108414827+kl-cb@users.noreply.github.com>
2024-01-24 11:30:41 -0500

    [trivial] Add app error and global error page content (#159)
    * Create shared global error UI
    
    * Update styling
    
    * Update layout with navbar and footer
    
    * Use shared error UI in global error
    
    * Add back html and body tags in global error
    
    * Move root special files to [locale] folder
    
    * Update styling
    
    * Add error and global-error file to [locale] folder
    
    * Revert "Move root special files to [locale] folder"
    
    This reverts commit 2cec52438c591278c8b774ce1b19d72a31bab386.
    
    * Fix files
    
    * remove trailing white space
    
    * Revert layout
    
    * Fix global error styling
    
    * Add event tracking
    
    * Add metadata
    
    * Update event tracking

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,10 +1,21 @@
 'use client'
 import * as Sentry from '@sentry/nextjs'
 import { useEffect } from 'react'
-import NextError from 'next/error'
 import { logger } from '@/utils/shared/logger'
+import { ErrorPagesContent } from '@/components/app/errorPagesContent'
+import { Inter } from 'next/font/google'
+import { cn } from '@/utils/web/cn'
 
-export default function GlobalErrorPage({ error }: { error: Error & { digest?: string } }) {
+// TODO replace with font we want
+const inter = Inter({ subsets: ['latin'] })
+
+export default function GlobalErrorPage({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
   useEffect(() => {
     logger.info('GlobalErrorPage rendered with:', error)
     Sentry.captureException(error)
@@ -12,9 +23,8 @@ export default function GlobalErrorPage({ error }: { error: Error & { digest?: s
   }, [error])
   return (
     <html lang="en">
-      <body>
-        {/* TODO graceful UI here */}
-        <NextError statusCode={undefined as any} />
+      <body className={cn(inter.className, 'flex h-screen content-center items-center')}>
+        <ErrorPagesContent reset={reset} />
       </body>
     </html>
   )
