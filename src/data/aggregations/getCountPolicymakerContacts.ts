f7bf34a21ff283c3fe741e9cfe176f4973c1f332
Travis Bloom <travis.bloom@coinbase.com>
2024-01-25 14:53:18 -0500

    readd unclear todo

--- src/data/aggregations/getCountPolicymakerContacts.ts
+++ src/data/aggregations/getCountPolicymakerContacts.ts
@@ -3,6 +3,7 @@ import { UserActionType } from '@prisma/client'
 import 'server-only'
 
 export const getCountPolicymakerContacts = async () => {
+  // TODO verify what we mean when we say "policymaker contacts".
   const [countUserActionEmailRecipients, countUserActionCalls] = await Promise.all([
     prismaClient.userActionEmailRecipient.count(),
     prismaClient.userAction.count({ where: { actionType: UserActionType.CALL } }),
