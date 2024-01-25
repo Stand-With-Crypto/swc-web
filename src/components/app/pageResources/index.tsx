c469b3d1b9833fe8bab267aa6374cc503b0e3b0e
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-25 16:12:54 -0500

    Add EventCards to events sections

--- src/components/app/pageResources/index.tsx
+++ src/components/app/pageResources/index.tsx
@@ -1,3 +1,4 @@
+import { EventCard } from '@/components/app/pageResources/eventCard'
 import { UserActionRowCTAsListWithApi } from '@/components/app/userActionRowCTA/userActionRowCTAsListWithApi'
 import { PageSubTitle } from '@/components/ui/pageSubTitle'
 import { PageTitle } from '@/components/ui/pageTitleText'
@@ -23,6 +24,26 @@ export function PageResources() {
           Events
         </PageTitle>
         <p>Learn more about recent and upcoming events to mobilize the crypto community.</p>
+        <div className="grid grid-flow-col grid-rows-3  gap-4 md:grid-rows-1">
+          <EventCard
+            imageUrl="/resources/eventCards/stand_with_crypto_day_dc.webp"
+            title="Stand With Crypto Day (Washington DC)"
+            cta="Watch"
+            link="https://www.youtube.com/watch?v=MzrUzOhgA6I"
+          />
+          <EventCard
+            imageUrl="/resources/eventCards/atlanta_advocate_town_hall.webp"
+            title="Atlanta Advocate Town Hall"
+            cta="Watch"
+            link="https://www.youtube.com/watch?v=ETq_UZ1aRGQ"
+          />
+          <EventCard
+            imageUrl="/resources/eventCards/crypto_presidential_forum_nh_townhall.webp"
+            title="Crypto Presidential Forum (New Hampshire)"
+            cta="Watch"
+            link="https://www.youtube.com/watch?v=refW193w3Xw"
+          />
+        </div>
       </section>
 
       <section className="mb-16 space-y-4 text-center">
