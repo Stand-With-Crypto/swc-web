fe7cdb427f91d8e1322a5134a79f3c30b8831533
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-25 11:13:50 -0500

    Revert global-error

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,13 +1,14 @@
 'use client'
-import * as Sentry from '@sentry/nextjs'
-import { useEffect } from 'react'
-import { logger } from '@/utils/shared/logger'
 import { ErrorPagesContent } from '@/components/app/errorPagesContent'
-import { Inter } from 'next/font/google'
+import { logger } from '@/utils/shared/logger'
 import { cn } from '@/utils/web/cn'
+import * as Sentry from '@sentry/nextjs'
+import { Inter } from 'next/font/google'
+import { useEffect } from 'react'
 
 // TODO replace with font we want
 const inter = Inter({ subsets: ['latin'] })
+export const dynamic = 'error'
 
 export default function GlobalErrorPage({
   error,
@@ -17,14 +18,14 @@ export default function GlobalErrorPage({
   reset: () => void
 }) {
   useEffect(() => {
-    logger.info('GlobalErrorPage rendered with:', error)
+    logger.info(`Global Error Page rendered with:`, error)
     Sentry.captureException(error)
     Sentry.captureException(new Error('Global Error Page Displayed'))
   }, [error])
   return (
     <html lang="en">
       <body className={cn(inter.className, 'flex h-screen content-center items-center')}>
-        <ErrorPagesContent reset={reset} skipTracking />
+        <ErrorPagesContent reset={reset} />
       </body>
     </html>
   )
