4cdf24c88c08c03052e3d5cec3712c0169ed08cd
Yann Druffin <yann.druffin@coinbase.com>
2024-01-25 15:55:38 -0800

    * PR feedbacks

--- src/mocks/models/mockNFTMint.ts
+++ src/mocks/models/mockNFTMint.ts
@@ -13,7 +13,10 @@ import NFTMintStatus = $Enums.NFTMintStatus
 export function mockNFTMint(): NFTMint {
   const status = faker.helpers.arrayElement(Object.values(NFTMintStatus))
   const transactionHash = status === NFTMintStatus.CLAIMED ? faker.git.commitSha() : ''
-  const nft = faker.helpers.arrayElement([SWCShieldThirdWebNFT, CallYourRepresentativeSept11ThirdWebNFT])
+  const nft = faker.helpers.arrayElement([
+    SWCShieldThirdWebNFT,
+    CallYourRepresentativeSept11ThirdWebNFT,
+  ])
   const costAtMint = new Decimal(faker.number.float({ min: 0.01, max: 0.2, precision: 0.01 }))
   return {
     ...mockCommonDatetimes(),
