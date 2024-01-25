957783180a1127a2a037d570c71e7f1bc913fc6a
Travis Bloom <travis.bloom@coinbase.com>
2024-01-25 08:30:19 -0500

    ensure we see all extra metadata passed to sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -15,6 +15,7 @@ Sentry.init({
   dsn,
   integrations: [new ExtraErrorData({ depth: 10 })],
   tracesSampleRate: NEXT_PUBLIC_ENVIRONMENT === 'production' ? 0.001 : 1.0,
+  normalizeDepth: 10,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
