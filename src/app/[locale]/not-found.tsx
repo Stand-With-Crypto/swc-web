7db98ff03e9ea77342fcda1079070f58e4b77301
Kaerah Lopez <108414827+kl-cb@users.noreply.github.com>
2024-01-25 13:40:55 -0500

    [trivial] Add not found pages UI (#164)
    * Add not found content
    
    * Add use client to error pages content
    
    * Display not found UI in the not found pages
    
    * Skip tracking in global error page
    
    * Remove unnecessary wrapping div
    
    * Remove duplicate main tag in root layout'
    
    * Add navbar and header to root not-found
    
    * Update button to link in not found UI
    
    * Create shared DefaultLocaleLayout UI
    
    * Update error pages content
    
    * Revert global-error
    
    * Revert backticks

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,10 +1,7 @@
+import { NotFoundPagesContent } from '@/components/app/notFoundPagesContent'
+
 export const dynamic = 'error'
 
 export default function NotFound() {
-  return (
-    <div>
-      <h1>not found1</h1>
-      {/* TODO */}
-    </div>
-  )
+  return <NotFoundPagesContent />
 }
