585075206c11076257ff6d12d4588aba8a41f6c7
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-26 14:28:07 -0500

    Add TrackedExternalLink ui component

---
+++ src/components/ui/trackedExternalLink.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { ExternalLink } from '@/components/ui/link'
+import { AnalyticProperties } from '@/utils/shared/sharedAnalytics'
+import { trackExternalLink } from '@/utils/web/clientAnalytics'
+import React from 'react'
+
+export const TrackedExternalLink = React.forwardRef<
+  React.ElementRef<typeof ExternalLink>,
+  React.ComponentPropsWithoutRef<typeof ExternalLink> & { eventProperties?: AnalyticProperties }
+>(({ eventProperties, ...props }, ref) => {
+  const onClick = React.useCallback(() => {
+    trackExternalLink(eventProperties)
+  }, [eventProperties])
+  return <ExternalLink ref={ref} onClick={onClick} {...props} />
+})
+TrackedExternalLink.displayName = 'TrackedExternalLink'
