aaee289dd388abf02f8a86dbc7296a771522a8fa
Benson Budiman <benson.budiman@coinbase.com>
2024-01-29 15:15:23 -0800

    works locally...

--- cypress/e2e/2-page-homepage.cy.ts
+++ cypress/e2e/2-page-homepage.cy.ts
@@ -7,10 +7,12 @@ it('page - homepage interactions', () => {
   // verify that there is an iframe on the page with youtube.com in the src
   cy.get('iframe[allowfullscreen]').should('have.attr', 'src').and('include', 'youtube.com')
   // wait so the iframe can fully load
-  cy.wait(1000)
+  cy.wait(2000)
   cy.contains('Close').click()
 
   // verify the donations tab shows top donors
   cy.contains('Top donations').click()
+  // wait so donations can fully load
+  cy.wait(2000)
   cy.get('img[alt="position 1 medal"]')
 })
