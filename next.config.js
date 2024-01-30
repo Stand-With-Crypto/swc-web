1b99bda1d5d4d32bfa070d69eb4a1df0507cd955
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-01-30 16:23:41 -0500

    Update nextjs config

--- next.config.js
+++ next.config.js
@@ -3,18 +3,153 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-// TODO bring over old configs
+const isProd = process.env.NODE_ENV === 'production' && process.env.CORS_ENV_PREFIX;
+
+const standWithCryptoDomain = isLocalDevelopment
+  ? 'http://localhost:*'
+  : process.env.CORS_ENV_PREFIX;
+
+const contentSecurityPolicy = {
+  'default-src': ["'self'", 'blob:'],
+  'style-src': [
+    "'self'",
+    "'unsafe-inline'", // NextJS requires 'unsafe-inline'
+    standWithCryptoDomain,
+  ],
+  'script-src': [
+    "'self'",
+    isLocalDevelopment ? "'unsafe-eval' 'unsafe-inline'" : '', // NextJS requires 'unsafe-eval' in dev (faster source maps)
+    standWithCryptoDomain,
+    isLocalDevelopment || process.env.TARGET_ENV === 'dev'
+      ? ''
+      : 'https://static.ads-twitter.com/uwt.js',
+    'https://*.googleapis.com',
+    'https://*.gstatic.com',
+    '*.google.com',
+    'https://static.ads-twitter.com/uwt.js',
+  ],
+  'img-src': [
+    "'self'",
+    'blob: data:',
+    'https://res.cloudinary.com/',
+    'https://*.walletconnect.com/',
+    'https://euc.li/',
+    standWithCryptoDomain,
+    'https://*.googleapis.com',
+    'https://*.gstatic.com',
+    '*.google.com',
+  ],
+  'connect-src': [
+    "'self'",
+    'ws: wss:',
+    isLocalDevelopment ? 'ws://localhost:*' : process.env.CORS_ENV_PREFIX,
+    'https://cloudflare-eth.com',
+    'https://base.rpc.thirdweb.com/',
+    'https://polygon.rpc.thirdweb.com/',
+    'https://ipfs.io/ipfs/',
+    'https://*.walletconnect.com/',
+    'https://developer-access-mainnet.base.org/',
+    'https://*.googleapis.com',
+    'https://*.gstatic.com',
+    '*.google.com',
+  ],
+  'frame-src': ['*.google.com'],
+}
+
+const cspObjectToString = Object.entries(contentSecurityPolicy).reduce((acc, [key, value]) => {
+  return `${acc}${key} ${value.join(' ')};`
+}, '')
+
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=15552000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: cspObjectToString,
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: false,
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ipfs.io',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'euc.li',
+        port: '',
+      },
       // dotheysupportit image cdn
       { protocol: 'https', hostname: 'db0prh5pvbqwd.cloudfront.net' },
     ],
   },
+  env: {
+    CIVIC_API: process.env.NEXT_PUBLIC_GOOGLE_CIVIC_API,
+    CIVIC_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_CIVIC_API_KEY,
+    PLACES_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY,
+    LOGGED_OUT_CONTENT_PREVIEW_API_KEY:
+      process.env.NEXT_PUBLIC_LOGGED_OUT_CONTENT_PREVIEW_API_KEY,
+    LOGGED_OUT_CONTENT_DELIVERY_API_KEY:
+      process.env.NEXT_PUBLIC_LOGGED_OUT_CONTENT_DELIVERY_API_KEY,
+    NEXT_PUBLIC_DONATIONS_ID: process.env.NEXT_PUBLIC_DONATIONS_ID,
+    NEXT_PUBLIC_ALLIANCE_DETAILS_URL: process.env.NEXT_PUBLIC_ALLIANCE_DETAILS_URL,
+    NEXT_PUBLIC_DONATIONS_URL: isProd
+      ? 'https://commerce.coinbase.com/checkout/396fc233-3d1f-4dd3-8e82-6efdf78432ad'
+      : 'https://commerce.coinbase.com/checkout/4aac12b1-a0d3-40e1-8b20-62c6cf5e8cfe',
+    RECAPTCHA_API_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_API_KEY,
+    TARGET_ENV: process.env.TARGET_ENV,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        basePath: false,
+        headers: securityHeaders,
+      },
+    ]
+  },
   async redirects() {
     return [
       // v1 redirects
+      {
+        permanent: false,
+        destination: '/?action=call-your-representative',
+        source: '/call',
+      },
+      {
+        permanent: false,
+        destination: '/?modal=nh-event',
+        source: '/events/nh-potus',
+      },
+      {
+        permanent: false,
+        destination: '/?modal=member-join',
+        source: '/member-join',
+      },
       {
         source: '/politicians/senate',
         destination: '/politicians',
@@ -25,7 +160,7 @@ const nextConfig = {
         destination: '/politicians',
         permanent: true,
       },
-    ]
+    ],
   },
 }
 /** @type {import('@sentry/nextjs').SentryWebpackPluginOptions} */
