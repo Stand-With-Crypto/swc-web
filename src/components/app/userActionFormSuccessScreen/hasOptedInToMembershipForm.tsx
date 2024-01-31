fe6ab58ac6f523a051f3d1b7bfec2fbc51ac7af6
Travis Bloom <travis.bloom@coinbase.com>
2024-01-30 20:06:11 -0500

    remove-console

--- src/components/app/userActionFormSuccessScreen/hasOptedInToMembershipForm.tsx
+++ src/components/app/userActionFormSuccessScreen/hasOptedInToMembershipForm.tsx
@@ -58,7 +58,6 @@ export function HasOptedInToMembershipForm({
               },
               () => actionUpdateUserHasOptedInToMembership(),
             )
-            console.log('asasddasd')
             setFormState('default')
             onSuccess()
           }}
