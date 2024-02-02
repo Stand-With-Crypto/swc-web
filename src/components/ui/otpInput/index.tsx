ec7af868e6b762c63d26ef25432a67c5b9422ee1
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-02-02 17:23:51 -0300

    feat: add memoization to otp input value

--- src/components/ui/otpInput/index.tsx
+++ src/components/ui/otpInput/index.tsx
@@ -1,8 +1,9 @@
 'use client'
 
+import React from 'react'
+
 import { Input } from '@/components/ui/input'
 import { cn } from '@/utils/web/cn'
-import * as React from 'react'
 
 interface OTPInputProps
   extends Omit<
@@ -32,9 +33,12 @@ export function OTPInput({
   const [activeInput, setActiveInput] = React.useState(0)
   const inputRefs = React.useRef<Array<HTMLInputElement | null>>([])
 
-  const getOTPValue = () => (otpValue ? otpValue.toString().split('') : [])
+  const otpValueArray = React.useMemo(
+    () => (otpValue ? otpValue.toString().split('') : []),
+    [otpValue],
+  )
 
-  const isInputNum = type === 'number' || type === 'tel'
+  const isInputNum = React.useMemo(() => type === 'number' || type === 'tel', [type])
 
   React.useEffect(() => {
     inputRefs.current = inputRefs.current.slice(0, numInputs)
@@ -46,10 +50,13 @@ export function OTPInput({
     }
   }, [autoFocus])
 
-  const isInputValueValid = (inputValue: string) => {
-    const isTypeValid = isInputNum ? !isNaN(Number(inputValue)) : typeof inputValue === 'string'
-    return isTypeValid && inputValue.trim().length === 1
-  }
+  const isInputValueValid = React.useCallback(
+    (inputValue: string) => {
+      const isTypeValid = isInputNum ? !isNaN(Number(inputValue)) : typeof inputValue === 'string'
+      return isTypeValid && inputValue.trim().length === 1
+    },
+    [isInputNum],
+  )
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value: newValue } = event.target
@@ -69,92 +76,114 @@ export function OTPInput({
     setActiveInput(activeInput - 1)
   }
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    onKeyDown?.(event)
+  const focusInput = React.useCallback(
+    (index: number) => {
+      const currentActiveInput = Math.max(Math.min(numInputs - 1, index), 0)
 
-    const otp = getOTPValue()
-    if ([event.code, event.key].includes('Backspace')) {
-      event.preventDefault()
-      changeCodeAtFocus('')
-      focusInput(activeInput - 1)
-    } else if (event.code === 'Delete') {
-      event.preventDefault()
-      changeCodeAtFocus('')
-    } else if (event.code === 'ArrowLeft') {
-      event.preventDefault()
-      focusInput(activeInput - 1)
-    } else if (event.code === 'ArrowRight') {
-      event.preventDefault()
-      focusInput(activeInput + 1)
-    }
-
-    // React does not trigger onChange when the same value is entered
-    // again. So we need to focus the next input manually in this case.
-    else if (event.key === otp[activeInput]) {
-      event.preventDefault()
-      focusInput(activeInput + 1)
-    } else if (
-      event.code === 'Spacebar' ||
-      event.code === 'Space' ||
-      event.code === 'ArrowUp' ||
-      event.code === 'ArrowDown'
-    ) {
-      event.preventDefault()
-    } else if (isInputNum && !isInputValueValid(event.key)) {
-      event.preventDefault()
-    }
-  }
-
-  const focusInput = (index: number) => {
-    const currentActiveInput = Math.max(Math.min(numInputs - 1, index), 0)
-
-    if (inputRefs.current[currentActiveInput]) {
-      inputRefs.current[currentActiveInput]?.focus()
-      inputRefs.current[currentActiveInput]?.select()
-      setActiveInput(currentActiveInput)
-    }
-  }
-
-  const changeCodeAtFocus = (newValue: string) => {
-    const otp = getOTPValue()
-    otp[activeInput] = newValue[0]
-    handleOTPChange(otp)
-  }
-
-  const handleOTPChange = (otp: Array<string>) => {
-    const newOtpValue = otp.join('')
-    setOTPValue(newOtpValue)
-    onChange?.(newOtpValue)
-  }
-
-  const handlePaste = (event: React.ClipboardEvent<HTMLInputElement>) => {
-    event.preventDefault()
-
-    const otp = getOTPValue()
-    let nextActiveInput = activeInput
-
-    // Get pastedData in an array of max size (num of inputs - current position)
-    const pastedData = event.clipboardData
-      .getData('text/plain')
-      .slice(0, numInputs - activeInput)
-      .split('')
-
-    // Prevent pasting if the clipboard data contains non-numeric values for number inputs
-    if (isInputNum && pastedData.some(v => isNaN(Number(v)))) {
-      return
-    }
-
-    // Paste data from focused input onwards
-    for (let pos = 0; pos < numInputs; ++pos) {
-      if (pos >= activeInput && pastedData.length > 0) {
-        otp[pos] = pastedData.shift() ?? ''
-        nextActiveInput++
+      if (inputRefs.current[currentActiveInput]) {
+        inputRefs.current[currentActiveInput]?.focus()
+        inputRefs.current[currentActiveInput]?.select()
+        setActiveInput(currentActiveInput)
       }
-    }
+    },
+    [numInputs],
+  )
 
-    focusInput(nextActiveInput)
-    handleOTPChange(otp)
-  }
+  const handleOTPChange = React.useCallback(
+    (otp: Array<string>) => {
+      const newOtpValue = otp.join('')
+      setOTPValue(newOtpValue)
+      onChange?.(newOtpValue)
+    },
+    [onChange],
+  )
+
+  const changeCodeAtFocus = React.useCallback(
+    (newValue: string) => {
+      const otp = [...otpValueArray]
+      otp[activeInput] = newValue[0]
+      handleOTPChange(otp)
+    },
+    [activeInput, handleOTPChange, otpValueArray],
+  )
+
+  const handleKeyDown = React.useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      onKeyDown?.(event)
+
+      if ([event.code, event.key].includes('Backspace')) {
+        event.preventDefault()
+        changeCodeAtFocus('')
+        focusInput(activeInput - 1)
+      } else if (event.code === 'Delete') {
+        event.preventDefault()
+        changeCodeAtFocus('')
+      } else if (event.code === 'ArrowLeft') {
+        event.preventDefault()
+        focusInput(activeInput - 1)
+      } else if (event.code === 'ArrowRight') {
+        event.preventDefault()
+        focusInput(activeInput + 1)
+      }
+
+      // React does not trigger onChange when the same value is entered
+      // again. So we need to focus the next input manually in this case.
+      else if (event.key === otpValueArray[activeInput]) {
+        event.preventDefault()
+        focusInput(activeInput + 1)
+      } else if (
+        event.code === 'Spacebar' ||
+        event.code === 'Space' ||
+        event.code === 'ArrowUp' ||
+        event.code === 'ArrowDown'
+      ) {
+        event.preventDefault()
+      } else if (isInputNum && !isInputValueValid(event.key)) {
+        event.preventDefault()
+      }
+    },
+    [
+      activeInput,
+      changeCodeAtFocus,
+      focusInput,
+      isInputNum,
+      isInputValueValid,
+      onKeyDown,
+      otpValueArray,
+    ],
+  )
+
+  const handlePaste = React.useCallback(
+    (event: React.ClipboardEvent<HTMLInputElement>) => {
+      event.preventDefault()
+
+      const otp = [...otpValueArray]
+      let nextActiveInput = activeInput
+
+      // Get pastedData in an array of max size (num of inputs - current position)
+      const pastedData = event.clipboardData
+        .getData('text/plain')
+        .slice(0, numInputs - activeInput)
+        .split('')
+
+      // Prevent pasting if the clipboard data contains non-numeric values for number inputs
+      if (isInputNum && pastedData.some(v => isNaN(Number(v)))) {
+        return
+      }
+
+      // Paste data from focused input onwards
+      for (let pos = 0; pos < numInputs; ++pos) {
+        if (pos >= activeInput && pastedData.length > 0) {
+          otp[pos] = pastedData.shift() ?? ''
+          nextActiveInput++
+        }
+      }
+
+      focusInput(nextActiveInput)
+      handleOTPChange(otp)
+    },
+    [activeInput, focusInput, handleOTPChange, isInputNum, numInputs, otpValueArray],
+  )
 
   return (
     <div className="flex gap-2">
@@ -163,7 +192,7 @@ export function OTPInput({
           key={i}
           id={`${id ?? ''}-${i}`}
           name={`${name ?? ''}-${i}`}
-          value={getOTPValue()[i] ?? ''}
+          value={otpValueArray[i] ?? ''}
           placeholder={placeholder}
           ref={element => (inputRefs.current[i] = element)}
           onChange={handleChange}
