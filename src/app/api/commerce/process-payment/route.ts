40c879c0250b403fde29152c293d5d1543135b95
Benson Budiman <benson.budiman@coinbase.com>
2024-02-01 19:34:48 -0800

    refactoring

--- src/app/api/commerce/process-payment/route.ts
+++ src/app/api/commerce/process-payment/route.ts
@@ -1,38 +1,92 @@
 import { NextRequest, NextResponse } from 'next/server'
-import * as crypto from 'crypto'
-import { requiredEnv } from '@/utils/shared/requiredEnv'
-import { headers } from 'next/headers'
 import { prettyLog } from '@/utils/shared/prettyLog'
+import { authenticatePaymentRequest } from '@/utils/server/coinbaseCommerce/authenticatePaymentRequest'
+import * as Sentry from '@sentry/nextjs'
 
-const COINBASE_COMMERCE_WEBHOOK_SECRET = requiredEnv(
-  process.env.COINBASE_COMMERCE_WEBHOOK_SECRET,
-  'process.env.COINBASE_COMMERCE_WEBHOOK_SECRET',
-)
+// Final interfaces are TBD - this is currently all available (known) fields.
+interface CoinbaseCommercePaymentDetails {
+  network: string
+  transaction_id: string
+  status?: string
+  detected_at?: string
+  value?: {
+    local: { amount: string; currency: string }
+    crypto: { amount: string; currency: string }
+  }
+  block?: {
+    height: number
+    hash: string
+    confirmations_accumulated: number
+    confirmations_required: number
+  }
+}
+
+interface CoinbaseCommercePaymentTimeline {
+  time: string
+  status: string
+  payment?: CoinbaseCommercePaymentDetails
+}
+
+interface CoinbaseCommercePaymentEventData {
+  code: string
+  id: string
+  resource: string
+  name: string
+  description: string
+  hosted_url: string
+  created_at: string
+  confirmed_at?: string
+  expires_at: string
+  support_email: string
+  timeline: CoinbaseCommercePaymentTimeline[]
+  payment_threshold: {
+    overpayment_absolute_threshold: { amount: string; currency: string }
+    overpayment_relative_threshold: string
+    underpayment_absolute_threshold: { amount: string; currency: string }
+    underpayment_relative_threshold: string
+  }
+  pricing: {
+    local: { amount: string; currency: string }
+  }
+  pricing_type: string
+  payments: CoinbaseCommercePaymentDetails[]
+  exchange_rates: Record<string, string>
+  local_exchange_rates: Record<string, string>
+  pwcb_only: boolean
+  offchain_eligible: boolean
+  coinbase_managed_merchant: boolean
+  collected_email: boolean
+  fee_rate: number
+}
+
+interface CoinbaseCommercePaymentEvent {
+  id: string
+  resource: string
+  type: string
+  api_version: string
+  created_at: string
+  data: CoinbaseCommercePaymentEventData
+}
+
+export interface CoinbaseCommercePayment {
+  id: string
+  scheduled_for: string
+  attempt_number: number
+  event: CoinbaseCommercePaymentEvent
+}
 
 export async function POST(request: NextRequest) {
-  // Performing signature verification here because both signature verification and subsequent mutations require the
-  // request body to be consumed as a stream, and we can't consume the request body more than once.
-  const requestBody = await request.json()
-  const headerSignature = headers().get('x-cc-webhook-signature')
-  if (!headerSignature) {
-    throw new Error('no signature header provided')
-  }
-  const constructedSignature = crypto
-    .createHmac('sha256', COINBASE_COMMERCE_WEBHOOK_SECRET)
-    .update(JSON.stringify(requestBody), 'utf8')
-    .digest('hex')
-  if (!crypto.timingSafeEqual(Buffer.from(constructedSignature), Buffer.from(headerSignature))) {
-    return new NextResponse('unauthorized', { status: 401 })
-  }
-
+  const rawRequestBody = await request.json()
+  authenticatePaymentRequest(rawRequestBody)
   try {
-    prettyLog(requestBody)
-
+    const body = rawRequestBody as CoinbaseCommercePayment
+    prettyLog(body)
     // TODO (Benson): Record donation in database.
   } catch (error) {
-    console.log('Error processing commerce webhook:', error)
+    Sentry.captureException('internal error processing request body', {
+      extra: { id: (rawRequestBody as CoinbaseCommercePayment).id },
+    })
     return new NextResponse('internal error', { status: 500 })
   }
-
   return new NextResponse('success', { status: 200 })
 }
