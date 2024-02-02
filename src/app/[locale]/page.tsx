63815805db8beca7db1422c9b873293d00253ec8
Travis Bloom <travis.bloom@coinbase.com>
2024-02-02 17:28:01 -0500

    see if dynamic pages work

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,8 +2,8 @@ import { PageHome } from '@/components/app/pageHome'
 import { getHomepageData } from '@/data/pageSpecific/getHomepageData'
 import { PageProps } from '@/types'
 
-export const revalidate = 5
-export const dynamic = 'error'
+// export const revalidate = 5
+// export const dynamic = 'error'
 
 export default async function Home({ params }: PageProps) {
   const asyncProps = await getHomepageData()
