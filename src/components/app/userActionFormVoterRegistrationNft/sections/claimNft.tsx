5a96cebf1b6336fa34f1e93cdfeedf6775a3de84
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-02-06 13:31:38 -0500

    Add UI for claim NFT step

--- src/components/app/userActionFormVoterRegistrationNft/sections/claimNft.tsx
+++ src/components/app/userActionFormVoterRegistrationNft/sections/claimNft.tsx
@@ -1,13 +1,22 @@
 import { SectionNames } from '@/components/app/userActionFormVoterRegistrationNft/constants'
+import { UserActionFormVoterRegistrationNftLayout } from '@/components/app/userActionFormVoterRegistrationNft/sections/layout'
+import { Button } from '@/components/ui/button'
 import { UseSectionsReturn } from '@/hooks/useSections'
 
 interface ClaimNftProps extends UseSectionsReturn<SectionNames> {}
 
 export function ClaimNft({ goToSection }: ClaimNftProps) {
   return (
-    <div className="flex flex-col">
-      TODO: Claim NFT
-      <button onClick={() => goToSection(SectionNames.SUCCESS)}>Go to success</button>
-    </div>
+    <UserActionFormVoterRegistrationNftLayout>
+      <UserActionFormVoterRegistrationNftLayout.Container>
+        <UserActionFormVoterRegistrationNftLayout.Heading
+          title="Get your “I Registered” NFT"
+          subtitle='You can get get a free "I Registered" NFT'
+        />
+      </UserActionFormVoterRegistrationNftLayout.Container>
+      <UserActionFormVoterRegistrationNftLayout.Footer>
+        <Button onClick={() => goToSection(SectionNames.SUCCESS)}>Claim NFT</Button>
+      </UserActionFormVoterRegistrationNftLayout.Footer>
+    </UserActionFormVoterRegistrationNftLayout>
   )
 }
