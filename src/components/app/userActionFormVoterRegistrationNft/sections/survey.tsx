3e4c58fd8e3f0ddb712beb46ad6d852b4386a03b
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-02-06 14:03:46 -0500

    Fix spacing

--- src/components/app/userActionFormVoterRegistrationNft/sections/survey.tsx
+++ src/components/app/userActionFormVoterRegistrationNft/sections/survey.tsx
@@ -11,40 +11,42 @@ export function Survey({ goToSection }: SurveyProps) {
   return (
     <UserActionFormVoterRegistrationNftLayout>
       <UserActionFormVoterRegistrationNftLayout.Container>
-        <UserActionFormVoterRegistrationNftLayout.Heading
-          title="Are you registered to vote?"
-          subtitle='Register to vote or check your voter registration and get a free "I Registered" NFT'
-        />
-        <div className="flex flex-col content-center items-center gap-3 lg:flex-row">
-          <Button
-            onClick={() => goToSection(SectionNames.CLAIM_NFT)}
-            variant="secondary"
-            className="w-[190px]"
-          >
-            Yes
-          </Button>
-          <Button
-            onClick={() => goToSection(SectionNames.VOTER_REGISTRATION_FORM)}
-            variant="secondary"
-            className="w-[190px]"
-          >
-            No
-          </Button>
-          <Button
-            onClick={() => goToSection(SectionNames.CHECK_REGISTRATION_FORM)}
-            variant="secondary"
-            className="w-[190px]"
-          >
-            I’m not sure
-          </Button>
+        <div className="flex flex-col gap-24">
+          <UserActionFormVoterRegistrationNftLayout.Heading
+            title="Are you registered to vote?"
+            subtitle='Register to vote or check your voter registration and get a free "I Registered" NFT'
+          />
+          <div className="flex flex-col content-center items-center gap-3 lg:flex-row">
+            <Button
+              onClick={() => goToSection(SectionNames.CLAIM_NFT)}
+              variant="secondary"
+              className="w-[190px]"
+            >
+              Yes
+            </Button>
+            <Button
+              onClick={() => goToSection(SectionNames.VOTER_REGISTRATION_FORM)}
+              variant="secondary"
+              className="w-[190px]"
+            >
+              No
+            </Button>
+            <Button
+              onClick={() => goToSection(SectionNames.CHECK_REGISTRATION_FORM)}
+              variant="secondary"
+              className="w-[190px]"
+            >
+              I’m not sure
+            </Button>
+          </div>
+          <p className="text-center">
+            Personal information subject to{' '}
+            <PrivacyPolicyDialog>
+              <span className="cursor-pointer text-blue-600">Stand with Crypto Privacy Policy</span>
+            </PrivacyPolicyDialog>
+            .
+          </p>
         </div>
-        <p className="text-center">
-          Personal information subject to{' '}
-          <PrivacyPolicyDialog>
-            <span className="cursor-pointer text-blue-600">Stand with Crypto Privacy Policy</span>
-          </PrivacyPolicyDialog>
-          .
-        </p>
       </UserActionFormVoterRegistrationNftLayout.Container>
     </UserActionFormVoterRegistrationNftLayout>
   )
