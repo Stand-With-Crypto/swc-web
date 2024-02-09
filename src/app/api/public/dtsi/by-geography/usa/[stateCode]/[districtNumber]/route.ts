61eeacbf96ffbd5228f3a8451c4e7288ec2ab029
Travis Bloom <travis.bloom@coinbase.com>
2024-02-09 17:04:53 -0500

    fixes

--- src/app/api/public/dtsi/by-geography/usa/[stateCode]/[districtNumber]/route.ts
+++ src/app/api/public/dtsi/by-geography/usa/[stateCode]/[districtNumber]/route.ts
@@ -6,11 +6,7 @@ import { z } from 'zod'
 import { SECONDS_DURATION } from '@/utils/shared/seconds'
 
 export const dynamic = 'error'
-<<<<<<< Updated upstream
-export const revalidate = 60 * 24
-=======
 export const revalidate = SECONDS_DURATION.WEEK
->>>>>>> Stashed changes
 
 const zodParams = z.object({
   districtNumber: z.string().pipe(z.coerce.number().int().gte(0).lt(1000)),
