d1f25767f4d451b02158631cfc1380ddf38609b5
Travis Bloom <travis.bloom@coinbase.com>
2024-02-09 17:05:40 -0500

    revert

--- src/app/api/public/dtsi/by-geography/usa/[stateCode]/[districtNumber]/route.ts
+++ src/app/api/public/dtsi/by-geography/usa/[stateCode]/[districtNumber]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { z } from 'zod'
 
 export const dynamic = 'error'
-export const revalidate = 60 * 60 * 24
+export const revalidate = 60 * 24
 
 const zodParams = z.object({
   districtNumber: z.string().pipe(z.coerce.number().int().gte(0).lt(1000)),
