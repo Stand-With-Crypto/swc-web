ed1768629d57caf996eaa941821d4f129476aff9
Travis Bloom <153657370+travisbloom-cb@users.noreply.github.com>
2024-02-12 15:05:32 -0500

    Minor v2 fixes backlog (#293)
    * https://github.com/orgs/Stand-With-Crypto/projects/1/views/6?pane=issue&itemId=52611507
    
    * https://github.com/orgs/Stand-With-Crypto/projects/1/views/6?pane=issue&itemId=52611663
    
    * https://github.com/orgs/Stand-With-Crypto/projects/1/views/6?pane=issue&itemId=52612374
    
    * https://github.com/orgs/Stand-With-Crypto/projects/1/views/6?pane=issue&itemId=52612758
    
    * https://github.com/orgs/Stand-With-Crypto/projects/1/views/6?pane=issue&itemId=52612879
    
    * fixes

--- src/components/app/pageHome/delayedRecentActivity.tsx
+++ src/components/app/pageHome/delayedRecentActivity.tsx
@@ -5,18 +5,29 @@ import { useInView } from 'framer-motion'
 
 import { RecentActivityAndLeaderboardTabs } from '@/components/app/pageHome/recentActivityAndLeaderboardTabs'
 import { RecentActivityRowAnimatedContainer } from '@/components/app/recentActivityRow/recentActivityRowAnimatedContainer'
+import { Button } from '@/components/ui/button'
+import { InternalLink } from '@/components/ui/link'
 import { PublicRecentActivity } from '@/data/recentActivity/getPublicRecentActivity'
 import { useApiRecentActivity } from '@/hooks/useApiRecentActivity'
+import { useIntlUrls } from '@/hooks/useIntlUrls'
 
 export function DelayedRecentActivity(props: { actions: PublicRecentActivity }) {
   const actions = useApiRecentActivity(props.actions, { limit: 10 }).data
   const ref = useRef(null)
   const isInVew = useInView(ref, { margin: '-50%', once: true })
   const visibleActions = actions.slice(isInVew ? 0 : 1, actions.length)
+  const urls = useIntlUrls()
   return (
     <TabsContent ref={ref} value={RecentActivityAndLeaderboardTabs.RECENT_ACTIVITY}>
-      <div className="mt-2 h-8 lg:h-10" />
       <RecentActivityRowAnimatedContainer actions={visibleActions} />
+      <div className="mt-7 space-x-4 text-center">
+        <Button asChild>
+          <InternalLink href={urls.donate()}>Donate</InternalLink>
+        </Button>
+        <Button asChild variant="secondary">
+          <InternalLink href={urls.leaderboard()}>View all</InternalLink>
+        </Button>
+      </div>
     </TabsContent>
   )
 }
