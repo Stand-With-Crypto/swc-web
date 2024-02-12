3b5980f4fe2357df1f68e934b16a4375a7c269e4
Travis Bloom <travis.bloom@coinbase.com>
2024-02-12 17:48:28 -0500

    fix post action success page

--- src/hooks/useApiResponseForUserPerformedUserActionTypes.tsx
+++ src/hooks/useApiResponseForUserPerformedUserActionTypes.tsx
@@ -1,15 +1,21 @@
 'use client'
 import useSWR from 'swr'
+import { FullConfiguration } from 'swr/_internal'
 
 import { GetUserPerformedUserActionTypesResponse } from '@/app/api/identified-user/performed-user-action-types/route'
 import { fetchReq } from '@/utils/shared/fetchReq'
 import { apiUrls } from '@/utils/shared/urls'
 
 // TODO determine if we want to be more conservative about revalidation https://swr.vercel.app/docs/revalidation
-export function useApiResponseForUserPerformedUserActionTypes() {
-  return useSWR(apiUrls.userPerformedUserActionTypes(), url =>
-    fetchReq(url)
-      .then(res => res.json())
-      .then(data => data as GetUserPerformedUserActionTypesResponse),
+export function useApiResponseForUserPerformedUserActionTypes(
+  config?: Pick<FullConfiguration, 'revalidateOnMount'>,
+) {
+  return useSWR(
+    apiUrls.userPerformedUserActionTypes(),
+    url =>
+      fetchReq(url)
+        .then(res => res.json())
+        .then(data => data as GetUserPerformedUserActionTypesResponse),
+    config,
   )
 }
