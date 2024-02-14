90bf326cf158687e847cb7166abcab593616c9e8
Travis Bloom <travis.bloom@coinbase.com>
2024-02-14 08:24:20 -0500

    animation fixes for mobile

--- src/components/app/userActionRowCTA/userActionRowCTAsAnimatedList.tsx
+++ src/components/app/userActionRowCTA/userActionRowCTAsAnimatedList.tsx
@@ -5,6 +5,7 @@ import { UserActionType } from '@prisma/client'
 import { motion } from 'framer-motion'
 
 import { UserActionRowCTA } from '@/components/app/userActionRowCTA'
+import { useIsDesktop } from '@/hooks/useIsDesktop'
 import { cn } from '@/utils/web/cn'
 
 import { ORDERED_USER_ACTION_ROW_CTA_INFO } from './constants'
@@ -18,6 +19,7 @@ export function UserActionRowCTAsAnimatedList({
   performedUserActionTypes?: UserActionType[]
   excludeUserActionTypes?: UserActionType[]
 }) {
+  const isDesktop = useIsDesktop({ defaultState: false })
   const filteredActions = useMemo(
     () =>
       !excludeUserActionTypes
@@ -33,11 +35,22 @@ export function UserActionRowCTAsAnimatedList({
         <motion.div
           // we apply individual pb to the elements instead of space-y-7 to ensure that there's no jank in the animation as the height transitions in
           className={cn(index !== 0 && 'pt-4')}
-          initial={{ opacity: 0, transform: index % 2 ? 'translateX(-60px)' : 'translateX(60px)' }}
-          key={actionType}
+          initial={{
+            opacity: 0,
+            /*
+            On iOS mobile, if we do the sideways animation the entire viewport will have an overscroll (because the content if past the screen until it animates in)
+            To prevent this, we want a simpler, vertical animation on screen sizes smaller than desktop
+            */
+            transform: isDesktop
+              ? index % 2
+                ? `translateX(-60px)`
+                : `translateX(60px)`
+              : `translateY(60px)`,
+          }}
+          key={`${actionType}-${isDesktop.toString()}`}
           transition={{ duration: 0.8 }}
           viewport={{ once: true, margin: '-150px' }}
-          whileInView={{ opacity: 1, transform: 'translateX(0)' }}
+          whileInView={{ opacity: 1, transform: isDesktop ? `translateX(0)` : `translateY(0)` }}
         >
           <UserActionRowCTA
             state={
