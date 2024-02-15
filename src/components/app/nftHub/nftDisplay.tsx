8a74d610b4e77484997b1dc151f693b8d65f5487
Angely Rodriguez <angely.rodriguez@coinbase.com>
2024-02-15 17:22:12 -0500

    getting image name + img url

--- src/components/app/nftHub/nftDisplay.tsx
+++ src/components/app/nftHub/nftDisplay.tsx
@@ -1,26 +1,41 @@
-import { SensitiveDataClientUserAction } from "@/clientModels/clientUserAction/sensitiveDataClientUserAction";
-
+import { SensitiveDataClientUserAction } from '@/clientModels/clientUserAction/sensitiveDataClientUserAction'
+import { NFTSlug } from '@/utils/shared/nft'
+import { NFT_CLIENT_METADATA } from '@/utils/web/nft'
 
 type NFTDisplayProps = {
-    userActions: SensitiveDataClientUserAction[];
+  userActions: SensitiveDataClientUserAction[]
 }
 
-export function NFTDisplay( { userActions }: NFTDisplayProps){
-    console.log('USER: ', userActions)
-    //iterate through userActions and find the ones that are NFTs
-    //display the NFT images in a grid 
-    //if there is not an NFT, render a empty grey div 
-    const userNFTSlugs = userActions.reduce((acc: string[], action: SensitiveDataClientUserAction): string[] =>{
-        const nft = action.nftMint?.nftSlug;
-        if(nft){
-          acc.push(nft)
-        }
-        return acc
-    }, [])
+const retrieveNFTEnumKey = (nftSlug: string) => {
+  const nftEnumKey = null
+  for (const key in NFTSlug) {
+    return NFTSlug[key as keyof typeof NFTSlug] === nftSlug ? key : null
+  }
+  return nftEnumKey
+}
 
-    console.log(userNFTSlugs)
-    return (
-        <div>
-        </div>
-    );
+export function NFTDisplay({ userActions }: NFTDisplayProps) {
+  console.log('USER: ', userActions)
+  //iterate through userActions and find the ones that are NFTs
+  //display the NFT images in a grid
+  //if there is not an NFT, render a empty grey div
+
+  const userNFTSlugs: string[][] = userActions.reduce(
+    (acc: string[][], action: SensitiveDataClientUserAction): string[][] => {
+      const nftSlug = action.nftMint?.nftSlug
+
+      if (nftSlug) {
+        const enumKey = retrieveNFTEnumKey(nftSlug)
+        if (enumKey) {
+          const { name, image } = NFT_CLIENT_METADATA[NFTSlug[enumKey as keyof typeof NFTSlug]]
+          acc.push([name, image.url])
+        }
+      }
+      return acc
+    },
+    [],
+  )
+
+  console.log(userNFTSlugs)
+  return <div></div>
 }
