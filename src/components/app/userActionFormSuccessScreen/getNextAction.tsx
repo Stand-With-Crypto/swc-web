3e733b8ce83a62d6554987a73af76d211fd0c22c
Travis Bloom <153657370+travisbloom-cb@users.noreply.github.com>
2024-02-15 10:36:21 -0500

    Allow for user actions without deeplinks (#312)
    * allow for user actions without deeplinks
    
    * refactor how we reference action priority

--- src/components/app/userActionFormSuccessScreen/getNextAction.tsx
+++ src/components/app/userActionFormSuccessScreen/getNextAction.tsx
@@ -1,16 +1,17 @@
 'use client'
 
 import { GetUserPerformedUserActionTypesResponse } from '@/app/api/identified-user/performed-user-action-types/route'
-import { ORDERED_USER_ACTION_ROW_CTA_INFO } from '@/components/app/userActionRowCTA/constants'
+import { USER_ACTION_ROW_CTA_INFO } from '@/components/app/userActionRowCTA/constants'
+import { USER_ACTION_TYPE_PRIORITY_ORDER } from '@/utils/web/userActionUtils'
 
 export function getNextAction(
   performedUserActionTypes: GetUserPerformedUserActionTypesResponse['performedUserActionTypes'],
 ) {
-  const action = ORDERED_USER_ACTION_ROW_CTA_INFO.find(
-    action => !performedUserActionTypes.includes(action.actionType),
+  const action = USER_ACTION_TYPE_PRIORITY_ORDER.find(
+    actionType => !performedUserActionTypes.includes(actionType),
   )
   if (action) {
-    const { WrapperComponent: _WrapperComponent, ...rest } = action
+    const { WrapperComponent: _WrapperComponent, ...rest } = USER_ACTION_ROW_CTA_INFO[action]
     return rest
   }
   return null
