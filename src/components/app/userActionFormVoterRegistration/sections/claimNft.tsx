85dd8f9dac642d1ef02ac6dcfb0e384ae03da647
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-02-16 14:28:54 -0500

    Update nft image and contract address

--- src/components/app/userActionFormVoterRegistration/sections/claimNft.tsx
+++ src/components/app/userActionFormVoterRegistration/sections/claimNft.tsx
@@ -5,8 +5,10 @@ import { useCallback } from 'react'
 import { SectionNames } from '@/components/app/userActionFormVoterRegistration/constants'
 import { UserActionFormVoterRegistrationLayout } from '@/components/app/userActionFormVoterRegistration/sections/layout'
 import { Button } from '@/components/ui/button'
+import { NextImage } from '@/components/ui/image'
 import { ExternalLink } from '@/components/ui/link'
 import { UseSectionsReturn } from '@/hooks/useSections'
+import { NFT_CLIENT_METADATA } from '@/utils/web/nft'
 
 interface ClaimNftProps extends UseSectionsReturn<SectionNames> {}
 
@@ -22,7 +24,13 @@ export function ClaimNft({ goToSection }: ClaimNftProps) {
           subtitle=""
           title={`Claim "I'm a Voter" NFT`}
         />
-        <div>
+        <div className="flex flex-row gap-8">
+          <NextImage
+            alt={NFT_CLIENT_METADATA['i-am-a-voter'].image.alt}
+            height={NFT_CLIENT_METADATA['i-am-a-voter'].image.height}
+            src={NFT_CLIENT_METADATA['i-am-a-voter'].image.url}
+            width={NFT_CLIENT_METADATA['i-am-a-voter'].image.width}
+          />
           <p className="text-fontcolor-muted">
             The “I'm a Voter” NFT was created by{' '}
             <ExternalLink className="text-fontcolor underline" href="https://pplplsr.com/About">
