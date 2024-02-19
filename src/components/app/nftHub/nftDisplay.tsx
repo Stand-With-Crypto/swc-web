c50ea3dba67ab43f80ae2a747c959bd4acf56772
Angely Rodriguez <angely.rodriguez@coinbase.com>
2024-02-19 13:29:32 -0500

    pr comment changes

--- src/components/app/nftHub/nftDisplay.tsx
+++ src/components/app/nftHub/nftDisplay.tsx
@@ -5,7 +5,6 @@ import { SensitiveDataClientUserAction } from '@/clientModels/clientUserAction/s
 import { USER_ACTION_ROW_CTA_INFO } from '@/components/app/userActionRowCTA/constants'
 import { Button } from '@/components/ui/button'
 import { NextImage } from '@/components/ui/image'
-import { NFTSlug } from '@/utils/shared/nft'
 import { NFT_CLIENT_METADATA } from '@/utils/web/nft'
 
 type NFTDisplayProps = {
@@ -19,27 +18,24 @@ type NFTImages = {
   height: number
 }
 
-// const retrieveNFTEnumKey = (nftSlug: string) => {
-//   const nftEnumKey = null
-//   for (const key in NFTSlug) {
-//     return NFTSlug[key as keyof typeof NFTSlug] === nftSlug ? key : null
-//   }
-//   return nftEnumKey
-// }
 const HEIGHT = '335px'
 const WIDTH = '335px'
 
 const ButtonWrapper = USER_ACTION_ROW_CTA_INFO[UserActionType.NFT_MINT].WrapperComponent
 
 export function NFTDisplay({ userActions }: NFTDisplayProps) {
+  const optInNftButton = true
   const userNfts: NFTImages[] = userActions.reduce(
     (acc: NFTImages[], action: SensitiveDataClientUserAction): NFTImages[] => {
       const nftSlug = action.nftMint?.nftSlug
-      console.log('nftSlug', nftSlug)
+
       if (nftSlug) {
         const { name, image } = NFT_CLIENT_METADATA[nftSlug as keyof typeof NFT_CLIENT_METADATA]
         acc.push({ name, image: image.url, width: image.width, height: image.height })
       }
+
+      if (nftSlug === 'swc-shield') !optInNftButton
+
       return acc
     },
     [],
@@ -50,10 +46,7 @@ export function NFTDisplay({ userActions }: NFTDisplayProps) {
     return userNfts.map(nft => {
       const { name, height, width, image } = nft
       return (
-        <div
-          className="max-w-355 overflow-hidden rounded-3xl rounded-3xl bg-gray-100"
-          key={nft.name}
-        >
+        <div className="overflow-hidden rounded-3xl rounded-3xl bg-gray-100" key={nft.name}>
           <NextImage alt={name} height={height} src={image} width={width} />
         </div>
       )
@@ -73,14 +66,16 @@ export function NFTDisplay({ userActions }: NFTDisplayProps) {
 
   return (
     <>
-      <div className=" w-full flex-wrap items-center justify-center gap-4 lg:flex-row lg:justify-between xl:flex-row xl:justify-between">
+      <div className="max-w-355 flex-wrap items-center justify-center gap-4 lg:justify-between xl:justify-between">
         {renderNfts()}
         {renderEmptySpots()}
       </div>
       <div className="m-4 flex justify-center">
-        <ButtonWrapper>
-          <Button>Mint Stand With Crypto Supporter NFT</Button>
-        </ButtonWrapper>
+        {optInNftButton ?? (
+          <ButtonWrapper>
+            <Button>Mint Stand With Crypto Supporter NFT</Button>
+          </ButtonWrapper>
+        )}
       </div>
     </>
   )
