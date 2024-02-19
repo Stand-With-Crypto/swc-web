4908ce5f3e28877f9889cf8e6af20c256ab0db38
Travis Bloom <travis.bloom@coinbase.com>
2024-02-19 09:57:08 -0500

    make google place nullable on address field so we can gracefully fail

--- src/clientModels/clientAddress.ts
+++ src/clientModels/clientAddress.ts
@@ -2,12 +2,16 @@ import { Address } from '@prisma/client'
 
 import { ClientModel, getClientModel } from '@/clientModels/utils'
 
-export type ClientAddress = ClientModel<
-  Pick<Address, 'id' | 'googlePlaceId' | 'formattedDescription'>
->
+export type ClientAddress = ClientModel<Pick<Address, 'id' | 'formattedDescription'>> & {
+  googlePlaceId: string
+}
 
-export const getClientAddress = (record: Address): ClientAddress => {
+export const getClientAddress = (record: Address): ClientAddress | null => {
   const { id, googlePlaceId, formattedDescription } = record
+  // all addresses should have google places, but we want to gracefully fail if google starts hard-capping us for some reason
+  if (!googlePlaceId) {
+    return null
+  }
   return getClientModel({
     id,
     googlePlaceId,
