8d6efbe7a9d108287514c7f2b89206a93d048389
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-02-21 11:19:36 -0500

    Update variable name

--- src/app/[locale]/vote/page.tsx
+++ src/app/[locale]/vote/page.tsx
@@ -3,6 +3,6 @@ import { PageProps } from '@/types'
 
 export const dynamic = 'error'
 
-export default async function ResourcesPage({ params }: PageProps) {
+export default async function VotePage({ params }: PageProps) {
   return <PageVote params={params} />
 }
