ba4feca0a6ad707b11c6e305e1d7ef54ecd8382d
Travis Bloom <travis.bloom@coinbase.com>
2024-02-22 14:46:33 -0500

    reduce size of leaderboard temporarily to see if it helps builds complete

--- src/components/app/pageLeaderboard/constants.tsx
+++ src/components/app/pageLeaderboard/constants.tsx
@@ -1,6 +1,6 @@
 import { toBool } from '@/utils/shared/toBool'
 
-export const PAGE_LEADERBOARD_TOTAL_PAGES = 25
+export const PAGE_LEADERBOARD_TOTAL_PAGES = 10
 
 export const PAGE_LEADERBOARD_TOTAL_PRE_GENERATED_PAGES = toBool(
   process.env.MINIMIZE_PAGE_PRE_GENERATION,
