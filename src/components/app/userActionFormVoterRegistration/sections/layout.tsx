89bfe553628c053376eaf956cd024a8eb887bebd
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-02-23 17:12:25 -0500

    Remove padding in footer in voter registration modal

--- src/components/app/userActionFormVoterRegistration/sections/layout.tsx
+++ src/components/app/userActionFormVoterRegistration/sections/layout.tsx
@@ -52,7 +52,7 @@ function GoBackButton({ onClick }: { onClick: () => void }) {
 
 function Footer({ children }: React.PropsWithChildren) {
   return (
-    <div className="flex flex-row-reverse items-center justify-between space-y-4 px-8 md:space-y-8">
+    <div className="flex flex-row-reverse items-center justify-between space-y-4 md:space-y-8">
       {children}
     </div>
   )
