1327c15cfd0bd27ce9675898fac0b02ef92cfb32
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-02-23 20:38:53 -0300

    expanded the sumDonations params

--- src/data/aggregations/getSumDonationsByUser.ts
+++ src/data/aggregations/getSumDonationsByUser.ts
@@ -17,38 +17,42 @@ export type SumDonationsByUserConfig = {
   offset?: number
 }
 // If we ever have an nft mint action that is not a "donation", we'll need to refactor this logic
-export const getSumDonationsByUser = async ({ limit, offset }: SumDonationsByUserConfig) => {
+export const getSumDonationsByUser = async (
+  limit: SumDonationsByUserConfig['limit'],
+  offset: SumDonationsByUserConfig['offset'] = 0,
+) => {
+  console.log('EXECUTING', { limit, offset })
   // there might be a way of doing this better with https://www.prisma.io/docs/orm/prisma-client/queries/aggregation-grouping-summarizing
   // but nothing wrong with some raw sql for custom aggregations
   const total: {
     userId: string
     totalAmountUsd: Decimal
   }[] = await prismaClient.$queryRaw`
-    SELECT  
+    SELECT
       u.id AS userId,
       COALESCE(donation_total, 0) + COALESCE(mint_total, 0) AS totalAmountUsd
     FROM (
-      SELECT id 
+      SELECT id
       FROM user
       WHERE internal_status = 'VISIBLE'
     ) u
 
     LEFT JOIN (
-      SELECT  
+      SELECT
         ua.user_id,
         SUM(amount_usd) AS donation_total
       FROM user_action_donation
-      JOIN user_action ua USING(id)  
+      JOIN user_action ua USING(id)
       GROUP BY ua.user_id
     ) donation_totals ON u.id = donation_totals.user_id
 
     LEFT JOIN (
-      SELECT  
+      SELECT
         ua.user_id,
         SUM(cost_at_mint_usd) AS mint_total
       FROM nft_mint
       JOIN user_action ua ON ua.nft_mint_id = nft_mint.id
-      WHERE ua.action_type = 'NFT_MINT' 
+      WHERE ua.action_type = 'NFT_MINT'
       GROUP BY ua.user_id
     ) mint_totals ON u.id = mint_totals.user_id
 
@@ -94,10 +98,10 @@ export const getSumDonationsByUser = async ({ limit, offset }: SumDonationsByUse
 const getSumDonationsByUserCache = cache(getSumDonationsByUser)
 
 export async function getSumDonationsByUserWithBuildCache(config: SumDonationsByUserConfig) {
-  const results = await getSumDonationsByUserCache({
-    offset: 0,
-    limit: PAGE_LEADERBOARD_TOTAL_PAGES * PAGE_LEADERBOARD_ITEMS_PER_PAGE,
-  })
+  const results = await getSumDonationsByUserCache(
+    PAGE_LEADERBOARD_TOTAL_PAGES * PAGE_LEADERBOARD_ITEMS_PER_PAGE,
+    0,
+  )
   const offset = config.offset || 0
   return results.slice(offset, offset + config.limit)
 }
