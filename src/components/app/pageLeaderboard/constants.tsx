9b97494a0b9dbcabcbbd1ffc8d595c32d2e3c885
Travis Bloom <travis.bloom@coinbase.com>
2024-02-24 14:16:11 -0500

    reduce prebuild leaderboard pages

--- src/components/app/pageLeaderboard/constants.tsx
+++ src/components/app/pageLeaderboard/constants.tsx
@@ -6,6 +6,6 @@ export const PAGE_LEADERBOARD_TOTAL_PRE_GENERATED_PAGES = toBool(
   process.env.MINIMIZE_PAGE_PRE_GENERATION,
 )
   ? 1
-  : PAGE_LEADERBOARD_TOTAL_PAGES
+  : 3 // TODO set this back to all pages after we figure out why builds fail for these pages
 
 export const PAGE_LEADERBOARD_ITEMS_PER_PAGE = 30
