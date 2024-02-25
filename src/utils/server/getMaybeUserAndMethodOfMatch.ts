e59f97e3798765e4e88211dbf5477aaf526ad906
Travis Bloom <travis.bloom@coinbase.com>
2024-02-25 11:35:33 -0500

    split server functions in to items that need session and ones that dont

--- src/utils/server/getMaybeUserAndMethodOfMatch.ts
+++ src/utils/server/getMaybeUserAndMethodOfMatch.ts
@@ -4,34 +4,51 @@ import { GetFindResult } from '@prisma/client/runtime/library'
 import * as Sentry from '@sentry/nextjs'
 
 import { prismaClient } from '@/utils/server/prismaClient'
-import { getUserSessionId } from '@/utils/server/serverUserSessionId'
+import {
+  getUserSessionId,
+  getUserSessionIdThatMightNotExist,
+} from '@/utils/server/serverUserSessionId'
 import { appRouterGetAuthUser } from '@/utils/server/thirdweb/appRouterGetAuthUser'
 import { NEXT_PUBLIC_ENVIRONMENT } from '@/utils/shared/sharedEnv'
 
-export type UserAndMethodOfMatch<
-  I extends Omit<Prisma.UserFindFirstArgs, 'where'> = Omit<Prisma.UserFindFirstArgs, 'where'>,
-> =
+type PrismaBase = Omit<Prisma.UserFindFirstArgs, 'where'>
+
+type BaseUserAndMethodOfMatch<S extends string | undefined, I extends PrismaBase = PrismaBase> =
   | {
       user: GetFindResult<Prisma.$UserPayload, I>
       userCryptoAddress: UserCryptoAddress
     }
   | {
       user: GetFindResult<Prisma.$UserPayload, I> | null
-      sessionId: string
+      sessionId: S
     }
 
+export type UserAndMethodOfMatch<I extends PrismaBase = PrismaBase> = BaseUserAndMethodOfMatch<
+  string,
+  I
+>
+
+export type UserAndMethodOfMatchWithMaybeSession<I extends PrismaBase = PrismaBase> =
+  BaseUserAndMethodOfMatch<string | undefined, I>
 /*
 If you're wondering what all the prisma type signatures are for, this allows people to pass additional prismaClient.user.findFirst arguments in
 These arguments change the actual shape of the returned result (select and include for example) so we need to use generics to ensure we get the full type-safe result back 
 */
-export async function getMaybeUserAndMethodOfMatch<
-  I extends Omit<Prisma.UserFindFirstArgs, 'where'>,
+async function baseGetMaybeUserAndMethodOfMatch<
+  S extends string | undefined,
+  I extends PrismaBase,
 >({
-  include,
-  ...other
-}: Prisma.SelectSubset<I, Prisma.UserFindFirstArgs>): Promise<UserAndMethodOfMatch<I>> {
+  prisma: prismaConfig,
+  shouldThrowWithoutSession,
+}: {
+  shouldThrowWithoutSession: boolean
+  prisma?: Prisma.SelectSubset<I, Prisma.UserFindFirstArgs>
+}): Promise<BaseUserAndMethodOfMatch<S, I>> {
+  const { include, ...other } = prismaConfig || {}
   const authUser = await appRouterGetAuthUser()
-  const sessionId = getUserSessionId()
+  const sessionId = shouldThrowWithoutSession
+    ? getUserSessionId()
+    : getUserSessionIdThatMightNotExist()
   // PlanetScale currently has index issues when we query for both session and id in the same SQL statement
   // running them separately to make sure we hit our indexes.
   // if we can do this in a single query AND leverage the right indexes in the future this would be ideal.
@@ -48,16 +65,18 @@ export async function getMaybeUserAndMethodOfMatch<
           ...other,
         })
       : Promise.resolve(null),
-    prismaClient.user.findFirst({
-      where: {
-        userSessions: { some: { id: sessionId } },
-      },
-      include: {
-        ...((include || {}) as object),
-        userCryptoAddresses: true,
-      },
-      ...other,
-    }),
+    sessionId
+      ? prismaClient.user.findFirst({
+          where: {
+            userSessions: { some: { id: sessionId } },
+          },
+          include: {
+            ...((include || {}) as object),
+            userCryptoAddresses: true,
+          },
+          ...other,
+        })
+      : Promise.resolve(null),
   ])
   const userWithoutReturnTypes = authFoundUser || sessionUser
   const user = userWithoutReturnTypes as GetFindResult<Prisma.$UserPayload, I> | null
@@ -93,6 +112,23 @@ export async function getMaybeUserAndMethodOfMatch<
   }
   return {
     user,
-    sessionId,
+    sessionId: sessionId as S,
   }
+}
+
+export async function getMaybeUserAndMethodOfMatch<I extends PrismaBase>(
+  args: {
+    prisma?: Prisma.SelectSubset<I, Prisma.UserFindFirstArgs>
+  } = {},
+): Promise<BaseUserAndMethodOfMatch<string, I>> {
+  return baseGetMaybeUserAndMethodOfMatch({ ...args, shouldThrowWithoutSession: true })
+}
+
+// useful when you don't want to flag sentry errors for the select few clients who load the site without cookies enabled
+export async function getMaybeUserAndMethodOfMatchWithMaybeSession<I extends PrismaBase>(
+  args: {
+    prisma?: Prisma.SelectSubset<I, Prisma.UserFindFirstArgs>
+  } = {},
+): Promise<BaseUserAndMethodOfMatch<string | undefined, I>> {
+  return baseGetMaybeUserAndMethodOfMatch({ ...args, shouldThrowWithoutSession: true })
 }
