dfa3e4fb83677755882863903f8fae3f313385c7
Travis Bloom <travis.bloom@coinbase.com>
2024-02-25 19:45:34 -0500

    revert

--- src/bin/localDevelopment/simulateLiveActivityOnSite.ts
+++ src/bin/localDevelopment/simulateLiveActivityOnSite.ts
@@ -31,7 +31,7 @@ async function simulateLiveActivityOnSite() {
     const user = await createUser()
     await createAction(user)
     logger.info('waiting 3 seconds')
-    await sleep(1)
+    await sleep(3000)
   }
 }
 runBin(simulateLiveActivityOnSite)
