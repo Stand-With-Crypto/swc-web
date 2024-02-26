693682cdee31046edd2ee0374bc678cdd3c2bb62
Travis Bloom <travis.bloom@coinbase.com>
2024-02-26 06:31:34 -0500

    add context before throwing errors

--- src/app/[locale]/internal/debug-sentry-dynamic-server/page.tsx
+++ src/app/[locale]/internal/debug-sentry-dynamic-server/page.tsx
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/nextjs'
+
 import { prismaClient } from '@/utils/server/prismaClient'
 import { logger } from '@/utils/shared/logger'
 import { NEXT_PUBLIC_ENVIRONMENT } from '@/utils/shared/sharedEnv'
@@ -17,6 +19,12 @@ export default async function DebugServerSentry() {
   console.log('page debug-sentry-dynamic-server log')
   const randomDatabaseQuery = await prismaClient.authenticationNonce.findFirst()
   logger.info('randomDatabaseQuery', { randomDatabaseQuery })
+  const scope = Sentry.getCurrentScope()
+  scope.setExtras({
+    debugSentry: 'debug-sentry-dynamic-server-value',
+    now: new Date().toISOString(),
+  })
+  scope.setTags({ debugSentry: 'debug-sentry-dynamic-server-value' })
   const val = await mockError()
   return <div>This will never render {val}</div>
 }
