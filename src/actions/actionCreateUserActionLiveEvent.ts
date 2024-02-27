69a8968dc8e932bfdb19037cf5461c417402aa99
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-02-27 15:25:26 -0500

    Create server action

---
+++ src/actions/actionCreateUserActionLiveEvent.ts
@@ -0,0 +1,222 @@
+'use server'
+import 'server-only'
+
+import { User, UserAction, UserActionType, UserInformationVisibility } from '@prisma/client'
+import * as Sentry from '@sentry/nextjs'
+import { nativeEnum, object, z } from 'zod'
+
+import { getClientUser } from '@/clientModels/clientUser/clientUser'
+import {
+  getMaybeUserAndMethodOfMatch,
+  UserAndMethodOfMatch,
+} from '@/utils/server/getMaybeUserAndMethodOfMatch'
+import { claimNFT } from '@/utils/server/nft/claimNFT'
+import { prismaClient } from '@/utils/server/prismaClient'
+import { throwIfRateLimited } from '@/utils/server/ratelimit/throwIfRateLimited'
+import { getServerAnalytics, getServerPeopleAnalytics } from '@/utils/server/serverAnalytics'
+import {
+  mapLocalUserToUserDatabaseFields,
+  parseLocalUserFromCookies,
+} from '@/utils/server/serverLocalUser'
+import { getUserSessionId } from '@/utils/server/serverUserSessionId'
+import { withServerActionMiddleware } from '@/utils/server/withServerActionMiddleware'
+import { mapPersistedLocalUserToAnalyticsProperties } from '@/utils/shared/localUser'
+import { getLogger } from '@/utils/shared/logger'
+import { generateReferralId } from '@/utils/shared/referralId'
+import { UserActionLiveEventCampaignName } from '@/utils/shared/userActionCampaigns'
+
+const logger = getLogger(`actionCreateUserActionLiveEvent`)
+
+const createActionLiveEventInputValidationSchema = object({
+  campaignName: nativeEnum(UserActionLiveEventCampaignName),
+})
+
+export type CreateActionLiveEventInput = z.infer<typeof createActionLiveEventInputValidationSchema>
+
+interface SharedDependencies {
+  localUser: ReturnType<typeof parseLocalUserFromCookies>
+  sessionId: ReturnType<typeof getUserSessionId>
+  analytics: ReturnType<typeof getServerAnalytics>
+  peopleAnalytics: ReturnType<typeof getServerPeopleAnalytics>
+}
+
+export const actionCreateUserActionLiveEvent = withServerActionMiddleware(
+  'actionCreateUserActionLiveEvent',
+  _actionCreateUserActionLiveEvent,
+)
+
+const LA_CRYPTO_VOTES_START_TIME = new Date('2024-03-04')
+
+async function _actionCreateUserActionLiveEvent(input: CreateActionLiveEventInput) {
+  logger.info('triggered')
+
+  const validatedInput = createActionLiveEventInputValidationSchema.safeParse(input)
+  if (!validatedInput.success) {
+    return {
+      errors: validatedInput.error.flatten().fieldErrors,
+    }
+  }
+
+  const currentTime = Date.now()
+  if (currentTime < LA_CRYPTO_VOTES_START_TIME.getTime()) {
+    return {
+      errors: {
+        campaignName: ['The campaign is not active'],
+      },
+    }
+  }
+
+  const localUser = parseLocalUserFromCookies()
+  const sessionId = getUserSessionId()
+
+  const userMatch = await getMaybeUserAndMethodOfMatch({
+    prisma: { include: { primaryUserCryptoAddress: true, address: true } },
+  })
+  await throwIfRateLimited()
+
+  const user = userMatch.user || (await createUser({ localUser, sessionId }))
+
+  const peopleAnalytics = getServerPeopleAnalytics({
+    localUser,
+    userId: user.id,
+  })
+  const analytics = getServerAnalytics({
+    userId: user.id,
+    localUser,
+  })
+
+  const recentUserAction = await getRecentUserActionByUserId(user.id, validatedInput)
+  if (recentUserAction) {
+    logSpamActionSubmissions({
+      validatedInput,
+      userAction: recentUserAction,
+      userId: user.id,
+      sharedDependencies: { analytics },
+    })
+    return { user: getClientUser(user) }
+  }
+
+  const { userAction } = await createAction({
+    user,
+    isNewUser: !userMatch.user,
+    validatedInput: validatedInput.data,
+    userMatch,
+    sharedDependencies: { sessionId, analytics, peopleAnalytics },
+  })
+
+  if (user.primaryUserCryptoAddress !== null) {
+    await claimNFT(userAction, user.primaryUserCryptoAddress)
+  }
+
+  return { user: getClientUser(user) }
+}
+
+async function createUser(sharedDependencies: Pick<SharedDependencies, 'localUser' | 'sessionId'>) {
+  const { localUser, sessionId } = sharedDependencies
+  const createdUser = await prismaClient.user.create({
+    data: {
+      informationVisibility: UserInformationVisibility.ANONYMOUS,
+      userSessions: { create: { id: sessionId } },
+      hasOptedInToEmails: false,
+      hasOptedInToMembership: false,
+      hasOptedInToSms: false,
+      referralId: generateReferralId(),
+      ...mapLocalUserToUserDatabaseFields(localUser),
+    },
+    include: {
+      primaryUserCryptoAddress: true,
+      address: true,
+    },
+  })
+  logger.info('created user')
+
+  if (localUser?.persisted) {
+    getServerPeopleAnalytics({ localUser, userId: createdUser.id }).setOnce(
+      mapPersistedLocalUserToAnalyticsProperties(localUser.persisted),
+    )
+  }
+
+  return createdUser
+}
+
+async function getRecentUserActionByUserId(
+  userId: User['id'],
+  validatedInput: z.SafeParseSuccess<CreateActionLiveEventInput>,
+) {
+  return prismaClient.userAction.findFirst({
+    where: {
+      actionType: UserActionType.LIVE_EVENT,
+      campaignName: validatedInput.data.campaignName,
+      userId: userId,
+    },
+  })
+}
+
+function logSpamActionSubmissions({
+  validatedInput,
+  userAction,
+  userId,
+  sharedDependencies,
+}: {
+  validatedInput: z.SafeParseSuccess<CreateActionLiveEventInput>
+  userAction: UserAction
+  userId: User['id']
+  sharedDependencies: Pick<SharedDependencies, 'analytics'>
+}) {
+  sharedDependencies.analytics.trackUserActionCreatedIgnored({
+    actionType: UserActionType.LIVE_EVENT,
+    campaignName: validatedInput.data.campaignName,
+    reason: 'Too Many Recent',
+    userState: 'Existing',
+  })
+  Sentry.captureMessage(
+    `duplicate ${UserActionType.LIVE_EVENT} user action for campaign ${validatedInput.data.campaignName} submitted`,
+    {
+      extra: { validatedInput: validatedInput.data, userAction },
+      user: { id: userId },
+    },
+  )
+}
+
+async function createAction<U extends User>({
+  user,
+  validatedInput,
+  userMatch,
+  sharedDependencies,
+  isNewUser,
+}: {
+  user: U
+  isNewUser: boolean
+  validatedInput: CreateActionLiveEventInput
+  userMatch: UserAndMethodOfMatch
+  sharedDependencies: Pick<SharedDependencies, 'sessionId' | 'analytics' | 'peopleAnalytics'>
+}) {
+  const userAction = await prismaClient.userAction.create({
+    data: {
+      user: { connect: { id: user.id } },
+      actionType: UserActionType.LIVE_EVENT,
+      campaignName: validatedInput.campaignName,
+      ...('userCryptoAddress' in userMatch
+        ? {
+            userCryptoAddress: { connect: { id: userMatch.userCryptoAddress.id } },
+          }
+        : { userSession: { connect: { id: sharedDependencies.sessionId } } }),
+      userActionLiveEvent: {
+        create: {},
+      },
+    },
+    include: {
+      userActionLiveEvent: true,
+    },
+  })
+
+  logger.info('created user action')
+
+  sharedDependencies.analytics.trackUserActionCreated({
+    actionType: UserActionType.LIVE_EVENT,
+    campaignName: validatedInput.campaignName,
+    userState: isNewUser ? 'New' : 'Existing',
+  })
+
+  return { userAction }
+}
