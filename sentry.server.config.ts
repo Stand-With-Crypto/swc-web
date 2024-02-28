4c194233ea69ef4580d76d7901fdcf66350df19f
Travis Bloom <travis.bloom@coinbase.com>
2024-02-28 08:58:37 -0500

    ramp up sentry tracing

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -19,7 +19,7 @@ Sentry.init({
     new ExtraErrorData({ depth: 10 }),
     new Sentry.Integrations.Prisma({ client: prismaClient }),
   ],
-  tracesSampleRate: NEXT_PUBLIC_ENVIRONMENT === 'production' ? 0.001 : 1.0,
+  tracesSampleRate: NEXT_PUBLIC_ENVIRONMENT === 'production' ? 0.02 : 1.0,
   normalizeDepth: 10,
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
