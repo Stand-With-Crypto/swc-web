7802c387eff41a64110bd6e7304cbdb1c9a1c477
Travis Bloom <travis.bloom@coinbase.com>
2024-02-28 16:56:15 -0500

    add missing characters

--- src/utils/shared/convertToOnlyEnglishCharacters.ts
+++ src/utils/shared/convertToOnlyEnglishCharacters.ts
@@ -6,10 +6,75 @@ export function convertToOnlyEnglishCharacters(word: string) {
   // Map of non-english characters to english
   const charMap: Record<string, string> = {
     á: 'a',
+    à: 'a',
+    â: 'a',
+    ä: 'a',
+    ã: 'a',
+
     é: 'e',
+    è: 'e',
+    ê: 'e',
+    ë: 'e',
+
     í: 'i',
+    ì: 'i',
+    î: 'i',
+    ï: 'i',
+
     ó: 'o',
+    ò: 'o',
+    ô: 'o',
+    ö: 'o',
+    õ: 'o',
+
     ú: 'u',
+    ù: 'u',
+    û: 'u',
+    ü: 'u',
+
+    ñ: 'n',
+
+    ç: 'c',
+
+    ā: 'a',
+    ē: 'e',
+    ī: 'i',
+    ō: 'o',
+    ū: 'u',
+
+    ă: 'a',
+    ĕ: 'e',
+    ŏ: 'o',
+    ŭ: 'u',
+
+    ċ: 'c',
+    ḋ: 'd',
+    ġ: 'g',
+    ḣ: 'h',
+
+    å: 'a',
+
+    ą: 'a',
+    ę: 'e',
+    į: 'i',
+    ų: 'u',
+
+    č: 'c',
+    ḑ: 'd',
+    ě: 'e',
+    ḟ: 'f',
+    ǧ: 'g',
+    ḧ: 'h',
+    ǐ: 'i',
+    ǰ: 'j',
+    ǩ: 'k',
+    ḷ: 'l',
+    ǹ: 'n',
+    ň: 'n',
+    ř: 'r',
+    š: 's',
+    ť: 't',
+    ž: 'z',
   }
 
   // Split name into array of characters
