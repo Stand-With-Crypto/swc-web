bcb21dbc632e6fdbee2ceafcae24f1ecfb78b625
Angely Rodriguez <angely.rodriguez@coinbase.com>
2024-02-28 19:20:23 -0500

    so much css, added getbyhexcolor fx, shield img

--- src/app/[locale]/politicians/person/[dtsiSlug]/opengraph-image.tsx
+++ src/app/[locale]/politicians/person/[dtsiSlug]/opengraph-image.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from 'next/og'
 
 import { getData } from '@/app/[locale]/politicians/person/[dtsiSlug]/getData'
+import { getBgHexColor } from '@/components/app/dtsiFormattedLetterGrade'
 import { dtsiPersonFullName } from '@/utils/dtsi/dtsiPersonUtils'
-import { convertDTSIStanceScoreToCryptoSupportLanguageSentence } from '@/utils/dtsi/dtsiStanceScoreUtils'
+import {
+  convertDTSIStanceScoreToCryptoSupportLanguageSentence,
+  convertDTSIStanceScoreToLetterGrade,
+} from '@/utils/dtsi/dtsiStanceScoreUtils'
 import { OPEN_GRAPH_IMAGE_DIMENSIONS } from '@/utils/server/generateOpenGraphImageUrl'
 import { SECONDS_DURATION } from '@/utils/shared/seconds'
-import { cn } from '@/utils/web/cn'
 
 export const dynamic = 'error'
 export const revalidate = SECONDS_DURATION.HOUR
@@ -20,7 +25,9 @@ export const contentType = 'image/png'
 
 export default async function Image({ params }: { params: { dtsiSlug: string } }) {
   const person = await getData(params.dtsiSlug)
-  console.log('person', person)
+  const imageData = await fetch(new URL('./shield.png', import.meta.url)).then(res =>
+    res.arrayBuffer(),
+  )
   if (!person) {
     return new ImageResponse(
       (
@@ -40,28 +47,82 @@ export default async function Image({ params }: { params: { dtsiSlug: string } }
   }
 
   const scoreLanguage = convertDTSIStanceScoreToCryptoSupportLanguageSentence(person)
+  const letterGrade = convertDTSIStanceScoreToLetterGrade(person)
   const dimensions = person.profilePictureUrlDimensions as
     | { width: number; height: number }
     | undefined
+  const circleColor = getBgHexColor(letterGrade)
+  const isLetterGrade = letterGrade ?? '?'
 
   return new ImageResponse(
     (
       <div
-        style={{ background: 'linear-gradient(180deg, #001C56 0%, #000 100%)' }}
-        tw="flex text-white p-8 w-full h-full flex-col justify-between items-center"
+        style={{ background: '#000' }}
+        tw="flex text-white p-8 w-full h-full flex-col justify-between items-center border border-solid border-gray-500"
       >
+        <div tw="flex p-8 w-full flex-col justify-between items-center border border-solid border-pink-500">
+          <img
+            src={imageData as any}
+            style={{ position: 'absolute', top: 0, right: 0 }}
+            tw="border border-solid border-orange-500"
+            width="48px"
+          />
+        </div>
         <div />
-        <div tw="flex flex-col items-center text-center">
+        <div tw="w-full flex flex-col text-center justify-center items-center border border-solid border-yellow-500">
           {person.profilePictureUrl && dimensions ? (
-            /* eslint-disable-next-line */
-            <img
-              height="256"
-              src={person.profilePictureUrl}
-              tw={cn('rounded-full object-none')}
-              width="256"
-            />
+            <div
+              style={{
+                display: 'flex',
+                paddingLeft: '60px',
+              }}
+              tw="border border-solid border-blue-500"
+            >
+              <img
+                alt={dtsiPersonFullName(person)}
+                src={person.profilePictureUrl}
+                style={{
+                  borderRadius: '50%',
+                  overflow: 'hidden',
+                  objectFit: 'cover',
+                  width: '200px',
+                  height: '200px',
+                }}
+                tw="border border-solid border-orange-500"
+              />
+              <div
+                style={{
+                  display: 'flex',
+                  position: 'relative',
+                  width: '60px',
+                  height: '60px',
+                  borderRadius: '50%',
+                  background: `${circleColor}`,
+                  top: '140px',
+                  right: '80px',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+                tw="border border-solid border-purple-500"
+              >
+                <p
+                  style={{
+                    textAlign: 'center',
+                    alignItems: 'center',
+                    position: 'absolute',
+                    fontSize: '50px',
+                    fontWeight: 'bold',
+                    lineHeight: 1,
+                    color: 'white',
+                  }}
+                  tw="border border-solid border-orange-500"
+                >
+                  {isLetterGrade}
+                </p>
+              </div>
+            </div>
           ) : null}
-          <div tw="flex-col text-xl mb-2 mt-8 flex text-gray-400">
+          <div tw="flex-col text-xl mb-2 mt-8 flex text-gray-400 justify-center items-center">
             <span tw="text-white mx-2 text-[40px] leading-[48px] pb-4">
               {dtsiPersonFullName(person)}
             </span>
