1f2c6a1efc09d6453c387c4a23426983c89d4d2f
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-02-29 16:03:55 -0300

    use metadata constants in nft mint

--- src/components/app/userActionFormNFTMint/constants.ts
+++ src/components/app/userActionFormNFTMint/constants.ts
@@ -1,10 +1,12 @@
+import { NFT_SLUG_BACKEND_METADATA } from '@/utils/server/nft/constants'
 import { toBigNumber } from '@/utils/shared/bigNumber'
+import { NFTSlug } from '@/utils/shared/nft'
 
 export const ETH_NFT_DONATION_AMOUNT = toBigNumber('0.00435')
 
 export const MINT_NFT_CONTRACT_ADDRESS =
   process.env.NEXT_PUBLIC_OVERRIDE_MINT_NFT_CONTRACT_ADDRESS ??
-  '0x741B334b0690de44Bce6c926a1F74Ca69C95c80c'
+  NFT_SLUG_BACKEND_METADATA[NFTSlug.SWC_SHIELD]
 
 export enum UserActionFormNFTMintSectionNames {
   INTRO = 'intro',
