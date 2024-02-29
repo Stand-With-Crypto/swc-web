aeeb7766cd0ed169c8f34017e7e618f93d846966
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-02-29 11:25:17 -0500

    Update to use ActiveClientUserActionType

--- src/components/app/userActionRowCTA/index.tsx
+++ src/components/app/userActionRowCTA/index.tsx
@@ -1,14 +1,14 @@
 'use client'
 import React from 'react'
-import { UserActionType } from '@prisma/client'
 import { ChevronRight } from 'lucide-react'
 
 import { NextImage } from '@/components/ui/image'
 import { Skeleton } from '@/components/ui/skeleton'
+import { ActiveClientUserActionType } from '@/utils/shared/activeUserAction'
 import { cn } from '@/utils/web/cn'
 
 export interface UserActionRowCTAProps {
-  actionType: UserActionType
+  actionType: ActiveClientUserActionType
   state: 'unknown' | 'complete' | 'incomplete' | 'hidden'
   image: string
   text: string
