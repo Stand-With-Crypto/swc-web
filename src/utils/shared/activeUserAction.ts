784b3bbff431a55b47b3545a3e5342f5de17fb6a
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-02-29 13:41:00 -0500

    Explicitly declare ACTIVE_CLIENT_USER_ACTION_TYPES

--- src/utils/shared/activeUserAction.ts
+++ src/utils/shared/activeUserAction.ts
@@ -1,4 +1,12 @@
 import { UserActionType } from '@prisma/client'
 
-export type ActiveClientUserActionType = UserActionType
-export const ACTIVE_CLIENT_USER_ACTION_TYPES = Object.values(UserActionType)
+export const ACTIVE_CLIENT_USER_ACTION_TYPES = [
+  UserActionType.CALL,
+  UserActionType.EMAIL,
+  UserActionType.DONATION,
+  UserActionType.OPT_IN,
+  UserActionType.TWEET,
+  UserActionType.NFT_MINT,
+  UserActionType.VOTER_REGISTRATION,
+] as const
+export type ActiveClientUserActionType = (typeof ACTIVE_CLIENT_USER_ACTION_TYPES)[number]
