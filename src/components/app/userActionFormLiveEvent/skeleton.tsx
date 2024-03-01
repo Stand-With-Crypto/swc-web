468e3d133df94267e0573f1b442b2e918ae81c2e
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-03-01 16:00:22 -0500

    Update loading state

--- src/components/app/userActionFormLiveEvent/skeleton.tsx
+++ src/components/app/userActionFormLiveEvent/skeleton.tsx
@@ -2,11 +2,14 @@ import { UserActionFormLayout } from '@/components/app/userActionFormCommon'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
 
+const NFT_IMAGE_SIZE_PX = 160
+
 export function UserActionFormLiveEventSkeleton() {
   return (
     <UserActionFormLayout>
       <UserActionFormLayout.Container>
         <div className="flex h-full  flex-col items-center justify-center gap-4">
+          <Skeleton className={`h-[${NFT_IMAGE_SIZE_PX}px] w-[${NFT_IMAGE_SIZE_PX}px]`} />
           <UserActionFormLayout.HeadingSkeleton subtitle="dummy title" title="dummy subtitle" />
           <Skeleton>
             <Button>Sign in to claim</Button>
