8156aba980f92e8a119f3343ddb0ff141ab83230
Travis Bloom <travis.bloom@coinbase.com>
2024-03-01 09:41:12 -0500

    Don't trigger vercel builds unless user is core contributor

---
+++ bin/ignore_vercel_build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+author=$(echo $VERCEL_GIT_COMMIT_AUTHOR_LOGIN)
+
+users_file="./bin/vercel-users.txt"
+
+author_found=false
+
+while IFS= read -r line; do
+  echo "Checking $author against $line"
+  if [[ "$line" == "$author" ]]; then
+    author_found=true
+    break
+  fi
+done < "$users_file"
+
+if $author_found; then
+  echo "Author $author found in $users_file"
+  exit 0
+else
+  echo "Author $author not found in $users_file"
+  exit 1 
+fi
