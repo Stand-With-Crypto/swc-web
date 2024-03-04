9feaceff2ba89ee48c28173b8f95f5748a5121af
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-03-04 12:39:54 -0300

    fix: apply wait for ens rule in logged in popover

--- src/components/app/navbar/navbarLoggedInButton/index.tsx
+++ src/components/app/navbar/navbarLoggedInButton/index.tsx
@@ -1,14 +1,12 @@
 'use client'
 
 import React from 'react'
-import { UserInformationVisibility } from '@prisma/client'
-import { useENS } from '@thirdweb-dev/react'
 
 import { Button } from '@/components/ui/button'
 import { useResponsivePopover } from '@/components/ui/responsivePopover'
 import { useApiResponseForUserFullProfileInfo } from '@/hooks/useApiResponseForUserFullProfileInfo'
 import { useDialog } from '@/hooks/useDialog'
-import { appendENSHookDataToUser } from '@/utils/web/appendENSHookDataToUser'
+import { useUserWithMaybeENSData } from '@/hooks/useUserWithMaybeEnsData'
 import { getSensitiveDataUserDisplayName } from '@/utils/web/userUtils'
 
 import { NavbarLoggedInPopoverContent } from './navbarLoggedInPopoverContent'
@@ -16,31 +14,21 @@ import { NavbarLoggedInPopoverContent } from './navbarLoggedInPopoverContent'
 export function NavbarLoggedInButton({ onOpenChange }: { onOpenChange: (open: boolean) => void }) {
   const { Popover, PopoverContent, PopoverTrigger } = useResponsivePopover()
   const dialogProps = useDialog({ analytics: 'Navbar Logged In Button' })
-  const ensData = useENS()
   const { data } = useApiResponseForUserFullProfileInfo()
   const user = data?.user
 
-  const displayName = React.useMemo(() => {
-    const shouldWaitForEnsData =
-      user?.informationVisibility === UserInformationVisibility.CRYPTO_INFO_ONLY
-
-    if (!user) {
-      return
-    }
-
-    if (shouldWaitForEnsData && ensData.isLoading) {
-      return
-    }
-
-    return getSensitiveDataUserDisplayName(
-      appendENSHookDataToUser(user, shouldWaitForEnsData ? ensData.data : null),
-    )
-  }, [ensData.data, ensData.isLoading, user])
+  const userWithMaybeEnsData = useUserWithMaybeENSData({ user: data?.user })
+  const displayName = userWithMaybeEnsData
+    ? getSensitiveDataUserDisplayName(userWithMaybeEnsData)
+    : null
 
   return (
     <Popover
       {...dialogProps}
       onOpenChange={open => {
+        if (!displayName) {
+          return
+        }
         dialogProps.onOpenChange(open)
         onOpenChange?.(open)
       }}
