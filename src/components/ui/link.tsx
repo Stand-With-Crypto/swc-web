2d820881be1760d7405b258c623f75a54c40209e
Travis Bloom <travis.bloom@coinbase.com>
2024-03-04 11:49:03 -0800

    font-tweaks

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -7,7 +7,9 @@ export const InternalLink = React.forwardRef<
   HTMLAnchorElement,
   React.ComponentProps<typeof NextLink>
 >(({ className, ...props }, ref) => {
-  return <NextLink className={cn('text-primary hover:underline', className)} ref={ref} {...props} />
+  return (
+    <NextLink className={cn('text-primary-cta hover:underline', className)} ref={ref} {...props} />
+  )
 })
 InternalLink.displayName = 'InternalLink'
 
@@ -17,7 +19,7 @@ export const ExternalLink = React.forwardRef<
 >(({ className, ...props }, ref) => {
   return (
     <a
-      className={cn('text-primary hover:underline', className)}
+      className={cn('text-primary-cta hover:underline', className)}
       ref={ref}
       target="_blank"
       {...props}
