d5de40b395357f0417990a2e1af0aff207f8c48a
victor.frolov <victor.frolov@coinbase.com>
2024-03-06 21:01:08 -0800

    Adds metatags for twitter.'

--- src/app/[locale]/campaign/shield/page.tsx
+++ src/app/[locale]/campaign/shield/page.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 const videoUrl =
   'https://fgrsqtudn7ktjmlh.public.blob.vercel-storage.com/public/shield/stand-with-crypto-h8EMnIjlCFnMREravQ2irnktkh6egS.mp4'
 
+const siteUrl = 'https://swc-hxs2qx3vs-stand-with-crypto.vercel.app/campaign/shield'
+
 export default function ShieldCampaign() {
   return (
     <div>
@@ -16,9 +18,22 @@ export default function ShieldCampaign() {
         <meta content={videoUrl} property="og:video:secure_url" />
         <meta content="video/mp4" property="og:video:type" />
         <meta content="Stand with Crypto" property="og:description" />
+
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="player" name="twitter:card" />
+        <meta content="@StandWithCrypto" name="twitter:site" />
+        <meta content="Stand With Crypto" name="twitter:title" />
+        <meta content="Test Content" name="twitter:description" />
+        <meta content="https://yoursite.com/example.png" name="twitter:image" />
+        <meta content={siteUrl} name="twitter:player" />
+        <meta content="480" name="twitter:player:width" />
+        <meta content="480" name="twitter:player:height" />
       </Head>
 
-      <video controls src={videoUrl} />
+      <video controls>
+        <source src={videoUrl} type="video/mp4" />
+        Your browser does not support video
+      </video>
     </div>
   )
 }
