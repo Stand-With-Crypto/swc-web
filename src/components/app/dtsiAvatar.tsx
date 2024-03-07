4ed6cd3b151cee02f10249ec471958b571eeb993
Travis Bloom <travis.bloom@coinbase.com>
2024-03-07 15:54:02 -0500

    fixes

--- src/components/app/dtsiAvatar.tsx
+++ src/components/app/dtsiAvatar.tsx
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/nextjs'
+
 import { ImageAvatar, ImageAvatarProps } from '@/components/ui/imageAvatar'
 import { InitialsAvatar } from '@/components/ui/initialsAvatar'
 import { DTSI_Person } from '@/data/dtsi/generated'
@@ -26,10 +28,15 @@ export const DTSIAvatar: React.FC<
       />
     )
   }
+  // https://stand-with-crypto.sentry.io/issues/5012094116/?environment=production&project=4506490717470720&query=is%3Aunresolved+timesSeen%3A%3E%3D10&referrer=issue-stream&statsPeriod=30d&stream_index=17
+  // this should never happen, adding additional debug info to figure out whats going on
+  if (!person.firstNickname && !person.firstName) {
+    Sentry.captureMessage('DTSIAvatar: Found person without first name', { extra: { person } })
+  }
   return (
     <InitialsAvatar
       className="rounded-md"
-      firstInitial={(person.firstNickname || person.firstName).slice(0, 1)}
+      firstInitial={(person.firstNickname || person.firstName || '').slice(0, 1)}
       lastInitial={person.lastName.slice(0, 1)}
       {...props}
     />
