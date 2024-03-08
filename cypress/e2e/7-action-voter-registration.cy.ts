ec060ef706f941fb99ff4e7f97f9a9f900713f6e
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-03-08 14:13:32 -0500

    Add test cases

--- cypress/e2e/7-action-voter-registration.cy.ts
+++ cypress/e2e/7-action-voter-registration.cy.ts
@@ -1,12 +1,44 @@
 /// <reference types="cypress" />
-
-it('action - voter registration - yes flow', () => {
+it('Yes flow', () => {
   cy.visit('/')
 
   cy.contains('Check your voter registration and get a free NFT').click()
   cy.get('[role="dialog"]')
 
   cy.contains('Yes').click()
+
+  cy.contains("Claim “I'm a Voter” NFT")
+
+  cy.contains('Claim NFT').click()
+
+  // waiting for Inngest to consume job
+  cy.contains('Nice work!')
+
+  // validate database
+  cy.queryDb('SELECT * FROM user_action WHERE action_type="VOTER_REGISTRATION"').then(
+    (result: any) => {
+      expect(result.length, 'user_action to exist in database').to.equal(1)
+    },
+  )
+  cy.clearDb()
+})
+
+it('No flow', () => {
+  cy.visit('/')
+
+  cy.contains('Check your voter registration and get a free NFT').click()
+  cy.get('[role="dialog"]')
+
+  cy.contains('No').click()
+
+  cy.contains("Register to vote and get a free “I'm a Voter” NFT")
+
+  cy.get('button[data-testid="state-filter-trigger"]').click()
+  cy.contains('New Jersey').click()
+
+  cy.get('a[data-testid="voter-registration-link"]').click()
+
+  cy.contains('Claim NFT').click()
 
   cy.contains("Claim “I'm a Voter” NFT")
 
@@ -21,4 +53,38 @@ it('action - voter registration - yes flow', () => {
       expect(result.length, 'user_action to exist in database').to.equal(1)
     },
   )
+  cy.clearDb()
+})
+
+it("I'm not sure flow", () => {
+  cy.visit('/')
+
+  cy.contains('Check your voter registration and get a free NFT').click()
+  cy.get('[role="dialog"]')
+
+  cy.contains('I’m not sure').click()
+
+  cy.contains("Check your registration and get a free “I'm a Voter” NFT")
+
+  cy.get('button[data-testid="state-filter-trigger"]').click()
+  cy.contains('Alabama').click()
+
+  cy.get('a[data-testid="voter-registration-link"]').click()
+
+  cy.contains('Claim NFT').click()
+
+  cy.contains("Claim “I'm a Voter” NFT")
+
+  cy.contains('Claim NFT').click()
+
+  // waiting for Inngest to consume job
+  cy.contains('Nice work!')
+
+  // validate database
+  cy.queryDb('SELECT * FROM user_action WHERE action_type="VOTER_REGISTRATION"').then(
+    (result: any) => {
+      expect(result.length, 'user_action to exist in database').to.equal(1)
+    },
+  )
+  cy.clearDb()
 })
