7c57d083d38aaee08d2dfeb14e0816c399c97a90
Kaerah Lopez <kaerah.lopez@coinbase.com>
2024-03-08 12:30:14 -0500

    Add test for yes flow

---
+++ cypress/e2e/7-action-voter-registration.cy.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+it('action - voter registration - yes flow', () => {
+  cy.visit('/')
+
+  cy.contains('Check your voter registration and get a free NFT').click()
+  cy.get('[role="dialog"]')
+
+  cy.contains('Yes').click()
+
+  cy.contains("Claim “I'm a Voter” NFT")
+
+  cy.contains('Claim NFT').click()
+
+  // waiting for Inngest to consume job
+  cy.contains('Nice work! You earned a new NFT.')
+
+  // validate database
+  cy.queryDb('SELECT * FROM user_action WHERE action_type="VOTER_REGISTRATION"').then(
+    (result: any) => {
+      expect(result.length, 'user_action to exist in database').to.equal(1)
+    },
+  )
+})
