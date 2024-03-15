2e2bd55119c0cee533e35f8ba13f10c1e4dd3c9e
Benson Budiman <benson.budiman@coinbase.com>
2024-03-14 19:49:35 -0700

    more comments

--- src/inngest/functions/mergeBackfilledUsers/logic.ts
+++ src/inngest/functions/mergeBackfilledUsers/logic.ts
@@ -24,6 +24,8 @@ export async function mergeBackfilledUsers(parameters: z.infer<typeof zodMergeBa
    * This implies that the emails were all from the V1 --> V2 backfill and that there has been no log-in attempt from that user.
    * (The data creation would be `BY_USER` if the user had inputted the email in V2. In those cases, we ignore those duplicate emails).
    *
+   * Simply put, we want to find users where they share the same email address, and the email address is not an V2 input.
+   *
    * "Why are you using the email addresses to determine duplicate users instead of user rows themselves?"
    * - There is no good way to determine duplicate users from the backfill from the users table:
    *   - Email addresses can be the same, but have different IDs - the users table will show the different IDs for different users, so that does not help.
@@ -33,14 +35,15 @@ export async function mergeBackfilledUsers(parameters: z.infer<typeof zodMergeBa
    *   - If they have logged in with email, then the email addresses should have already been merged (and hence no duplicate users).
    *   - If they have logged in with a crypto address, then there might still be duplicate email addresses.
    *     - However, we should not touch those users because it is intentional that email users are different than crypto users.
-   *       - There are only about a few hundred of these cases as far as I can tell.
+   *       - There are only about a few hundred of these cases as far as I can tell. We should still consider them as separate users.
    *
    * "What about crypto addresses?"
-   * By my investigation, if there are any duplicate crypto addresses, then that actually means that there are 2+ users
-   *   that use the same crypto address but use different email addresses. A common case for this is for couples who
-   *   share the same crypto address in SWC V1. I believe that this is not a problem and that we should not merge these users.
-   *   Also, if there is a case there are duplicate users that have the same email address and the same crypto address, the query
-   *   below will still catch those duplicate users anyways.
+   * - By my investigation, I determine that if there are any duplicate crypto addresses, then that usually means that there are 2+ users
+   *   that use the same crypto address but use different email addresses.
+   *   - A common case for this is for couples who share the same crypto address in SWC V1.
+   *   - I believe that this is not a problem and that we should not merge these users.
+   * - Also, if there is a case there are duplicate users that have the same email address and the same crypto address, the script
+   *   below will still catch those duplicate users anyways based on email address and merge properly.
    */
   const userEmailAddressGroupings = await prismaClient.userEmailAddress.groupBy({
     by: ['emailAddress'],
