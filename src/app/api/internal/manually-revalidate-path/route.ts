6a992cb3d1326e4d46d2c555c6674da30ac34798
Travis Bloom <travis.bloom@coinbase.com>
2024-03-18 16:40:00 -0400

    comments

--- src/app/api/internal/manually-revalidate-path/route.ts
+++ src/app/api/internal/manually-revalidate-path/route.ts
@@ -11,10 +11,10 @@ const zodPayload = z.object({
   secret: z.string(),
 })
 
+// Note because this secret is widely shared internally, do NOT use it for anything sensitive
 const SWC_INTERNAL_ENDPOINTS_SECRET = process.env.SWC_INTERNAL_ENDPOINTS_SECRET
 
 // example of url that uses this endpoint https://standwithcrypto.org/api/internal/manually-revalidate-path?secret=[OUR SECRET]&paths=/api/public/dtsi/by-geography/usa/ca/1
-
 export async function GET(request: NextRequest) {
   if (!SWC_INTERNAL_ENDPOINTS_SECRET) {
     return NextResponse.json({ error: 'SWC_INTERNAL_ENDPOINTS_SECRET not set up' }, { status: 401 })
