3c74ed4ad8674af59723a49020e183d8192a41c1
Benson Budiman <benson.budiman@coinbase.com>
2024-03-19 15:35:21 -0700

    stop job when wallet funds are low

--- src/inngest/functions/backfillNFTCronJob/index.ts
+++ src/inngest/functions/backfillNFTCronJob/index.ts
@@ -2,17 +2,16 @@ import { chunk } from 'lodash-es'
 
 import { inngest } from '@/inngest/inngest'
 import { onScriptFailure } from '@/inngest/onScriptFailure'
+import { getBaseETHBalances } from '@/utils/server/basescan/getBaseETHBalances'
 import { actionsWithNFT } from '@/utils/server/nft/actionsWithNFT'
 import { claimNFT } from '@/utils/server/nft/claimNFT'
+import { LEGACY_NFT_DEPLOYER_WALLET, SWC_DOT_ETH_WALLET } from '@/utils/server/nft/constants'
 import { prismaClient } from '@/utils/server/prismaClient'
 import { fetchAirdropTransactionFee } from '@/utils/server/thirdweb/fetchCurrentClaimTransactionFee'
 import { getLogger } from '@/utils/shared/logger'
 import { NEXT_PUBLIC_ENVIRONMENT } from '@/utils/shared/sharedEnv'
 
 const logger = getLogger('backfillNFTCronJob')
-
-// This is the date when SWC went live. We do not care about user actions before this date.
-const GO_LIVE_DATE = new Date('2024-02-25 00:00:00.000')
 
 // This is the milliseconds to wait before processing the next batch of user actions.
 const BACKFILL_NFT_INNGEST_CRON_JOB_AIRDROP_SLEEP_INTERVAL =
@@ -24,10 +23,15 @@ const BACKFILL_NFT_INNGEST_CRON_JOB_AIRDROP_BATCH_SIZE =
 
 // This is the ETH threshold in which we will stop the cron job if the current transaction fee exceeds the threshold.
 const AIRDROP_NFT_ETH_TRANSACTION_FEE_THRESHOLD =
-  Number(process.env.AIRDROP_NFT_ETH_TRANSACTION_FEE_THRESHOLD) || 0.00005
+  Number(process.env.AIRDROP_NFT_ETH_TRANSACTION_FEE_THRESHOLD) || 0.00006
+
+// This is the date when SWC went live. We do not care about user actions before this date.
+const GO_LIVE_DATE = new Date('2024-02-25 00:00:00.000')
+
+const LOW_ETH_BALANCE_THRESHOLD = 0.01
+const ETH_BASE_UNIT_WEI = 10 ** 18
 
 const BACKFILL_NFT_INNGEST_CRON_JOB_AIRDROP_TIMEFRAME = 9 * 60 * 1000 // 9 minutes timeframe to backfill the records, leaving 1 minute before the next run.
-
 const BACKFILL_NFT_INNGEST_CRON_JOB_SCHEDULE = '*/10 * * * *' // Every 10 minutes.
 const BACKFILL_NFT_INNGEST_CRON_JOB_FUNCTION_ID = 'script.backfill-nft-cron-job'
 const BACKFILL_NFT_INNGEST_CRON_JOB_EVENT_NAME = 'script/backfill.nft.cron.job'
@@ -82,15 +86,42 @@ export const backfillNFTInngestCronJob = inngest.createFunction(
 
     // Process the user action batches.
     for (const userActionBatch of userActionBatches) {
-      // Determine if we should break the loop.
+      // If there are no more user actions to backfill, stop the cron job.
       if (userActionBatch.length === 0) {
         logger.info(`No more user actions to backfill - stopping the cron job`)
         break
       }
+
+      // Check if the current timestamp has passed the timeframe.
       if (new Date().getTime() > currentTime + BACKFILL_NFT_INNGEST_CRON_JOB_AIRDROP_TIMEFRAME) {
         logger.info(`Current timestamp has passed the timeframe - stopping the cron job`)
         break
       }
+
+      // Check if the current wallet balances are low.
+      const walletsWithLowBalances = await step.run(
+        `script.fetch-current-wallet-balances-${batchNum}`,
+        async () => {
+          const baseETHBalances = await getBaseETHBalances([
+            SWC_DOT_ETH_WALLET,
+            LEGACY_NFT_DEPLOYER_WALLET,
+          ])
+          return baseETHBalances.result.filter(
+            cryptoAddress =>
+              Number(cryptoAddress.balance) / ETH_BASE_UNIT_WEI < LOW_ETH_BALANCE_THRESHOLD,
+          )
+        },
+      )
+      if (walletsWithLowBalances && walletsWithLowBalances.length > 0) {
+        logger.warn(
+          `Low Base ETH balance detected for ${walletsWithLowBalances
+            .map(wallet => wallet.account)
+            .join(', ')} - please fund as soon as possible - stopping the cron job`,
+        )
+        break
+      }
+
+      // Check if the current airdrop transaction fee exceeds the threshold.
       const currentAirdropTransactionFee = await step.run(
         `script.fetch-airdrop-transaction-fee-${batchNum}`,
         async () => {
