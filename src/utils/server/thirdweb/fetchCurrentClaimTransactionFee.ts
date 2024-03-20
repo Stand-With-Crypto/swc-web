eed8602a3bf72227912ff864c3af6a6231e1c832
Benson Budiman <benson.budiman@coinbase.com>
2024-03-20 14:28:51 -0700

    reduce gas limit

--- src/utils/server/thirdweb/fetchCurrentClaimTransactionFee.ts
+++ src/utils/server/thirdweb/fetchCurrentClaimTransactionFee.ts
@@ -19,7 +19,7 @@ declare module globalThis {
 // This is our SWC testing NFT contract address, but because its claim conditions are public, we can use this contract address to check the current transaction fees of the Base network.
 const AIRDROP_FEE_ESTIMATION_CONTRACT_ADDRESS = '0xd9C77be238F858b3d08eF87202c624D520663920'
 
-const CLAIM_GAS_LIMIT_OE721_CONTRACT = 231086
+const AIRDROP_GAS_LIMIT_OE721_CONTRACT = 190000 // This is based on the used gas limit of existing transactions from our airdropping wallets.
 
 export async function fetchAirdropTransactionFee(): Promise<number> {
   globalThis.TW_SKIP_FETCH_SETUP = true
@@ -28,7 +28,7 @@ export async function fetchAirdropTransactionFee(): Promise<number> {
   })
   const contract = await sdk.getContract(AIRDROP_FEE_ESTIMATION_CONTRACT_ADDRESS)
   const transaction = await contract.erc721.claim.prepare(1)
-  transaction.updateOverrides({ gasLimit: CLAIM_GAS_LIMIT_OE721_CONTRACT })
+  transaction.updateOverrides({ gasLimit: AIRDROP_GAS_LIMIT_OE721_CONTRACT })
   const gasCost = await transaction.estimateGasCost()
   return Number(gasCost.ether)
 }
