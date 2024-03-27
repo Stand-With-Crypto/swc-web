97c4157cb322589c71fce059590dc23c61f2ce66
Benson Budiman <benson.budiman@coinbase.com>
2024-03-27 16:44:21 -0700

    handle another edge case

--- src/inngest/functions/cleanupNFTMints/index.ts
+++ src/inngest/functions/cleanupNFTMints/index.ts
@@ -20,6 +20,41 @@ export const cleanupNFTMintsWithInngest = inngest.createFunction(
     const payload = event.data as ScriptPayload
 
     // Get `nft_mint` rows where:
+    // - `nft_slug` = 'swc-shield'
+    // - `contract_address` = '0x741B334b0690de44Bce6c926a1F74Ca69C95c80c'
+    const invalidShieldRows = await step.run('script.get-invalid-shield-nft-mints', async () => {
+      return prismaClient.nFTMint.findMany({
+        select: {
+          id: true,
+        },
+        where: {
+          nftSlug: NFTSlug.SWC_SHIELD,
+          contractAddress: '0x741B334b0690de44Bce6c926a1F74Ca69C95c80c',
+        },
+      })
+    })
+
+    let updatedInvalidShieldRows = { count: 0 }
+    if (payload.persist) {
+      // Update the `nft_slug` to `'stand-with-crypto-supporter'` for the selected rows.
+      updatedInvalidShieldRows = await step.run(
+        'script.update-invalid-shield-nft-mints',
+        async () => {
+          return prismaClient.nFTMint.updateMany({
+            where: {
+              id: {
+                in: invalidShieldRows.map(row => row.id),
+              },
+            },
+            data: {
+              nftSlug: NFTSlug.STAND_WITH_CRYPTO_SUPPORTER,
+            },
+          })
+        },
+      )
+    }
+
+    // Get `nft_mint` rows where:
     // - `nft_slug` is in `('swc-shield', 'i-am-a-voter', 'call-representative-sept-11', 'la-crypto-event-2024-03-04')`
     // - `cost_at_mint` is greater than 0
     const mintRows = await step.run('script.get-relevant-nft-mints', async () => {
@@ -43,33 +78,30 @@ export const cleanupNFTMintsWithInngest = inngest.createFunction(
       })
     })
 
-    if (!payload.persist) {
-      return {
-        dryRun: !payload.persist,
-        mintRowsCount: mintRows.length,
-        updatedMintRowsCount: 0,
-      }
-    }
-
-    // Update the `cost_at_mint` and `cost_at_mint_usd` to 0 for the selected rows.
-    const updatedMintRows = await step.run('script.update-nft-mints', async () => {
-      return prismaClient.nFTMint.updateMany({
-        where: {
-          id: {
-            in: mintRows.map(row => row.id),
+    let updatedMintRows = { count: 0 }
+    if (payload.persist) {
+      // Update the `cost_at_mint` and `cost_at_mint_usd` to 0 for the selected rows.
+      updatedMintRows = await step.run('script.update-nft-mints', async () => {
+        return prismaClient.nFTMint.updateMany({
+          where: {
+            id: {
+              in: mintRows.map(row => row.id),
+            },
           },
-        },
-        data: {
-          costAtMint: 0,
-          costAtMintUsd: 0,
-        },
+          data: {
+            costAtMint: 0,
+            costAtMintUsd: 0,
+          },
+        })
       })
-    })
+    }
 
     return {
       dryRun: payload.persist,
       mintRowsCount: mintRows.length,
       updatedMintRowsCount: updatedMintRows.count,
+      invalidShieldRowsCount: invalidShieldRows.length,
+      updatedInvalidShieldRowsCount: updatedInvalidShieldRows.count,
     }
   },
 )
