afd9034215fc88fd8b5733a3656af8c03b50811a
Benson Budiman <135282747+benson-budiman-cb@users.noreply.github.com>
2024-03-29 12:04:25 -0700

    [feature] update aggregation calculations to use `totalDonationAmountUsd` and implement auditing cron job (#729)
    * cron job implementation
    
    * remove unused
    
    * change aggregation queries
    
    * remove logs

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -3,16 +3,20 @@ import { serve } from 'inngest/next'
 import { airdropNFTWithInngest } from '@/inngest/functions/airdropNFT'
 import { backfillNFTWithInngest } from '@/inngest/functions/backfillNFT'
 import { backfillNFTInngestCronJob } from '@/inngest/functions/backfillNFTCronJob'
-import {
-  backfillUsersTotalDonationAmountUsdInngest,
-  backfillUsersTotalDonationAmountUsdInngestUpdateBatchOfUsers,
-} from '@/inngest/functions/backfillUsersTotalDonationAmountUsd'
 import { cleanupNFTMintsWithInngest } from '@/inngest/functions/cleanupNFTMints'
 import { cleanupPostalCodesWithInngest } from '@/inngest/functions/cleanupPostalCodes'
 import { emailRepViaCapitolCanaryWithInngest } from '@/inngest/functions/emailRepViaCapitolCanary'
 import { monitorBaseETHBalances } from '@/inngest/functions/monitorBaseETHBalances'
 import { setPrimaryCryptoAddressOfUserWithInngest } from '@/inngest/functions/setPrimaryCryptoAddressOfUser'
 import { upsertAdvocateInCapitolCanaryWithInngest } from '@/inngest/functions/upsertAdvocateInCapitolCanary'
+import {
+  auditUsersTotalDonationAmountUsdInngest,
+  auditUsersTotalDonationAmountUsdInngestAuditBatchOfUsers,
+} from '@/inngest/functions/usersTotalDonationAmountUsd/audit'
+import {
+  backfillUsersTotalDonationAmountUsdInngest,
+  backfillUsersTotalDonationAmountUsdInngestUpdateBatchOfUsers,
+} from '@/inngest/functions/usersTotalDonationAmountUsd/backfill'
 import { inngest } from '@/inngest/inngest'
 
 export const maxDuration = 180 // 3 minutes
@@ -31,5 +35,7 @@ export const { GET, POST, PUT } = serve({
     cleanupNFTMintsWithInngest,
     backfillUsersTotalDonationAmountUsdInngest,
     backfillUsersTotalDonationAmountUsdInngestUpdateBatchOfUsers,
+    auditUsersTotalDonationAmountUsdInngest,
+    auditUsersTotalDonationAmountUsdInngestAuditBatchOfUsers,
   ],
 })
--- src/data/aggregations/getSumDonationsByUser.ts
+++ src/data/aggregations/getSumDonationsByUser.ts
@@ -9,7 +9,6 @@ import { COMMUNITY_PAGINATION_DATA } from '@/components/app/pageLeaderboard/cons
 import { getENSDataMapFromCryptoAddressesAndFailGracefully } from '@/data/web3/getENSDataFromCryptoAddress'
 import { prismaClient } from '@/utils/server/prismaClient'
 import { redis } from '@/utils/server/redis'
-import { getLogger } from '@/utils/shared/logger'
 import { SECONDS_DURATION } from '@/utils/shared/seconds'
 import { NEXT_PUBLIC_ENVIRONMENT } from '@/utils/shared/sharedEnv'
 
@@ -18,57 +17,30 @@ export type SumDonationsByUserConfig = {
   offset?: number
 }
 
-const logger = getLogger('getSumDonationsByUser')
-
 async function getSumDonationsByUserQuery({ limit, offset }: SumDonationsByUserConfig) {
-  logger.info(
-    `triggering getSumDonationsByUser directly without cache: limit=${limit}, offset=${offset || 'undefined'}`,
-  )
-  // there might be a way of doing this better with https://www.prisma.io/docs/orm/prisma-client/queries/aggregation-grouping-summarizing
-  // but nothing wrong with some raw sql for custom aggregations
   const total: {
-    userId: string
-    totalAmountUsd: Decimal
-  }[] = await prismaClient.$queryRaw`
-    SELECT
-      u.id AS userId,
-      COALESCE(donation_total, 0) + COALESCE(mint_total, 0) AS totalAmountUsd
-    FROM (
-      SELECT id
-      FROM user
-      WHERE internal_status = 'VISIBLE'
-    ) u
-
-    LEFT JOIN (
-      SELECT
-        ua.user_id,
-        SUM(amount_usd) AS donation_total
-      FROM user_action_donation
-      JOIN user_action ua USING(id)
-      GROUP BY ua.user_id
-    ) donation_totals ON u.id = donation_totals.user_id
-
-    LEFT JOIN (
-      SELECT
-        ua.user_id,
-        SUM(cost_at_mint_usd) AS mint_total
-      FROM nft_mint
-      JOIN user_action ua ON ua.nft_mint_id = nft_mint.id
-      WHERE ua.action_type = 'NFT_MINT'
-      GROUP BY ua.user_id
-    ) mint_totals ON u.id = mint_totals.user_id
-
-    GROUP BY u.id
-    HAVING totalAmountUsd > 0
-    ORDER BY totalAmountUsd DESC
-
-    -- don't worry, prisma $queryRaw sanitizes the input
-    LIMIT ${limit}
-    OFFSET ${offset || 0}
-  `
-  return total.map(({ userId, totalAmountUsd }) => ({
-    userId,
-    totalAmountUsd: totalAmountUsd.toNumber(),
+    id: string
+    totalDonationAmountUsd: Decimal
+  }[] = await prismaClient.user.findMany({
+    select: {
+      id: true,
+      totalDonationAmountUsd: true,
+    },
+    where: {
+      internalStatus: 'VISIBLE',
+      totalDonationAmountUsd: {
+        gt: 0,
+      },
+    },
+    orderBy: {
+      totalDonationAmountUsd: 'desc',
+    },
+    take: limit,
+    ...(offset ? { skip: offset } : {}),
+  })
+  return total.map(({ id, totalDonationAmountUsd }) => ({
+    userId: id,
+    totalAmountUsd: totalDonationAmountUsd.toNumber(),
   }))
 }
 
