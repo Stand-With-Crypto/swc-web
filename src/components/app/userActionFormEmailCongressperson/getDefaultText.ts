0e0fa2a26015ccea7cad9251c9693af7d337a578
Travis Bloom <travis.bloom@coinbase.com>
2024-04-16 09:42:10 -0400

    temp changes until we're ready for final text

--- src/components/app/userActionFormEmailCongressperson/getDefaultText.ts
+++ src/components/app/userActionFormEmailCongressperson/getDefaultText.ts
@@ -1,15 +1,17 @@
+const ORIGINAL_TEXT = `The House Financial Services Committee and the House Agriculture Committee in the U.S. House of Representatives passed historic, bipartisan legislation in July. I am asking you to support the bill when it comes to the floor for a full House vote.
+
+The Financial Innovation and Technology for the 21st Century Act ("FIT21") addresses a pressing need for regulatory clarity in the United States for crypto. A vote for this bill is a vote to protect customers, promote job opportunities, and bolster national security.
+
+As your constituent, I am asking you to vote for FIT21 to safeguard consumers and promote responsible innovation. Thank you.`
+
 export function getDefaultText(dtsiPersonSlugs: string[]) {
   if (dtsiPersonSlugs.some(slug => HOUSE_LEADERSHIP_SLUGS.includes(slug))) {
-    return 'PHASE 1 TEXT TODO'
+    return ORIGINAL_TEXT // TODO replace once we have new text
   }
   if (dtsiPersonSlugs.some(slug => KEY_HOUSE_REP_SLUGS.includes(slug))) {
-    return 'PHASE 1.5 TEXT TODO'
+    return ORIGINAL_TEXT // TODO replace once we have new text
   }
-  return `The House Financial Services Committee and the House Agriculture Committee in the U.S. House of Representatives passed historic, bipartisan legislation in July. I am asking you to support the bill when it comes to the floor for a full House vote.
-
-  The Financial Innovation and Technology for the 21st Century Act ("FIT21") addresses a pressing need for regulatory clarity in the United States for crypto. A vote for this bill is a vote to protect customers, promote job opportunities, and bolster national security.
-  
-  As your constituent, I am asking you to vote for FIT21 to safeguard consumers and promote responsible innovation. Thank you.`
+  return ORIGINAL_TEXT
 }
 
 const KEY_HOUSE_REP_SLUGS = [
