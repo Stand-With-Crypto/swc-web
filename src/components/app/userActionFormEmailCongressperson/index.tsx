f146cb702e8be119241e44ed6b8efea8540b4ccb
Travis Bloom <travis.bloom@coinbase.com>
2024-04-16 11:03:28 -0400

    fixes

--- src/components/app/userActionFormEmailCongressperson/index.tsx
+++ src/components/app/userActionFormEmailCongressperson/index.tsx
@@ -316,7 +316,7 @@ export function UserActionFormEmailCongressperson({
           </div>
         </ScrollArea>
         <div
-          className="z-10 flex flex-1 flex-col items-center justify-end border border-t p-6 sm:flex-row md:px-12"
+          className="z-10 flex flex-1 flex-col items-center justify-center border border-t p-6 sm:flex-row md:px-12"
           style={{ boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 6px 0px' }}
         >
           <Button
