f2c23243c036a30ee4cdffcd5e8586591217330b
Lucas Pessone <155585835+lucasrmp-cb@users.noreply.github.com>
2024-04-17 17:12:59 -0300

    fix: add support for international phone numbers on zodPhoneNumber schema (#782)

--- src/validation/fields/zodPhoneNumber.ts
+++ src/validation/fields/zodPhoneNumber.ts
@@ -1,5 +1,5 @@
 import { string } from 'zod'
 
-const phoneRegex = new RegExp(/^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
+import { PHONE_NUMBER_REGEX } from '@/utils/shared/phoneNumber'
 
-export const zodPhoneNumber = string().regex(phoneRegex, 'Please enter a valid phone number')
+export const zodPhoneNumber = string().regex(PHONE_NUMBER_REGEX, 'Please enter a valid phone number')
