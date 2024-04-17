8c4ff788572e82ba9f489beb394f4d7707d64a5c
Benson Budiman <135282747+benson-budiman-cb@users.noreply.github.com>
2024-04-17 16:24:39 -0700

    [bug] fix "mint NFT" flow when "logged-in" as session user (#783)
    * fix logged in session mint
    
    * prettier

--- src/validation/fields/zodPhoneNumber.ts
+++ src/validation/fields/zodPhoneNumber.ts
@@ -2,4 +2,7 @@ import { string } from 'zod'
 
 import { PHONE_NUMBER_REGEX } from '@/utils/shared/phoneNumber'
 
-export const zodPhoneNumber = string().regex(PHONE_NUMBER_REGEX, 'Please enter a valid phone number')
+export const zodPhoneNumber = string().regex(
+  PHONE_NUMBER_REGEX,
+  'Please enter a valid phone number',
+)
