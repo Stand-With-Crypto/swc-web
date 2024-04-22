d38da6b5625821b4c07ad20e6635f490ffeefa60
Eduardo Nunes Picolo <eduardo.picolo@coinbase.com>
2024-04-22 13:52:25 -0300

    Fix politicians page e2e test

--- cypress/e2e/3-page-politicians.cy.ts
+++ cypress/e2e/3-page-politicians.cy.ts
@@ -5,21 +5,35 @@ it('page - politicians interactions', () => {
 
   cy.get('h1').contains('politicians')
   cy.get('tbody').find('tr').should('have.length', 100)
-  // filter the table by state
 
-  // TODO: fix this test
-  // // find the element with the data-e2e attribute of "state-filter-trigger"
-  // cy.get('[data-testid="state-filter-trigger"]').click()
-  // cy.get('[role="option"]').contains('AK').click()
-  // cy.get('tbody').find('tr').should('have.length', 3)
-  // cy.get('[data-testid="state-filter-trigger"]').click()
-  // cy.get('[role="option"]').contains('All').click()
-  // cy.get('tbody').find('tr').should('have.length', 100)
-  // // filter the table by name
-  // cy.get('input[placeholder="Search by name"]').type('Cynthia Lummis')
-  // cy.get('tbody').find('tr').should('have.length', 1)
-  // cy.get('input[placeholder="Search by name"]').clear()
-  // cy.get('tbody').find('tr').should('have.length', 100)
+  // filter the table by state
+  /**
+   * find the element with the data-e2e attribute of "state-filter-trigger".
+   *
+   * When rendering in a small screen Cypress will define the element as not visible
+   * due to the parent having `position: fixed`, so we need to scroll it into view.
+   *
+   * NOTE: If the table rerenders too much the scroll state will reset.
+   */
+  cy.get('[data-testid="state-filter-trigger"]').as('stateFilterTrigger').scrollIntoView()
+  cy.get('@stateFilterTrigger').should('be.visible').click()
+
+  cy.get('[role="option"]').contains('div', 'AK').as('stateOption')
+  cy.get('@stateOption').should('be.visible').click({
+    // force: true // Bypass visibility checks, not ideal
+  })
+  cy.get('tbody').find('tr').should('have.length', 4)
+
+  cy.get('@stateFilterTrigger').click()
+  cy.get('[role="option"]').contains('div', 'All').click()
+  cy.get('tbody').find('tr').should('have.length', 100)
+
+  // filter the table by name
+  cy.get('input[placeholder="Search by name or state"]').as('nameFilter').should('be.visible')
+  cy.get('@nameFilter').type('Cynthia Lummis')
+  cy.get('tbody').find('tr').should('have.length', 1)
+  cy.get('@nameFilter').clear()
+  cy.get('tbody').find('tr').should('have.length', 100)
 
   // enter your address and see your rep
   cy.selectFromComboBox({
