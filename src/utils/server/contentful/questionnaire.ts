ab82c2c37e51b3c56c8f3013e5cf461022913673
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-04-25 10:31:22 -0300

    feat: completes contentful questionnaire integration

--- src/utils/server/contentful/questionnaire.ts
+++ src/utils/server/contentful/questionnaire.ts
@@ -1,8 +1,28 @@
 import * as Sentry from '@sentry/nextjs'
 import { EntryFieldTypes } from 'contentful'
+import { array, boolean, number, object, string } from 'zod'
 
 import { contentfulClient } from '@/utils/server/contentful/client'
 import { getLogger } from '@/utils/shared/logger'
+
+const zodQuestionnaireSchemaValidation = object({
+  total: number().gte(1),
+  items: array(
+    object({
+      fields: object({
+        slug: string(),
+        q1ExperienceUsingBlockchainTechnology: boolean().optional(),
+        q2BlockchainWillPlayMajorRoleNextInnoWave: boolean().optional(),
+        q3AmerCryptoIsDrivingEconomicGrowth: boolean().optional(),
+        q4UsCompAtRiskIfDigitalAssetsPushedOverse: boolean().optional(),
+        q5UsModernizeRegulatoryEnvironmentForCrypto: boolean().optional(),
+        q6WouldYouVoteInFavorOfLegislation: boolean().optional(),
+        q7VoteInFavorOfLegisToPaymentStablecoins: boolean().optional(),
+        q8ShareAnyOtherOpinionsOnCrypto: string().optional(),
+      }),
+    }),
+  ).nonempty(),
+})
 
 export type QuestionnaireEntrySkeleton = {
   contentTypeId: 'swcQuestionnaire'
@@ -22,18 +42,17 @@ export type QuestionnaireEntrySkeleton = {
 const logger = getLogger(`contentfulQuestionnaire`)
 export async function getQuestionnaire(DTSISlug: string) {
   try {
-    const entries = await contentfulClient.getEntries({
+    const entries = await contentfulClient.getEntries<QuestionnaireEntrySkeleton>({
       content_type: 'swcQuestionnaire',
       'fields.slug': DTSISlug,
     })
-    console.log({ entries })
-    // if (entries.total > 0) {
-    //   return entries.items[0]
-    // } else {
-    //   return null
-    // }
 
-    return null
+    const isValidResponse = zodQuestionnaireSchemaValidation.safeParse(entries)
+
+    // Using safeParse to prevent logging errors when a politician hasn't answered the questionnaire yet.
+    if (!isValidResponse.success) return null
+
+    return entries.items[0]
   } catch (e) {
     logger.error('error getting questionnaire entry:' + e)
     Sentry.captureException(e, {
