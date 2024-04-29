e9c6deaf7d10857abc040fccf9420f60fb80ae96
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-04-29 11:52:27 -0300

    refactor: improvements on questionnaire accordion

--- src/components/app/pagePoliticianDetails/questionnaireAccordion.tsx
+++ src/components/app/pagePoliticianDetails/questionnaireAccordion.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import { isNil } from 'lodash-es'
 
 import {
@@ -11,11 +11,14 @@ import {
 } from '@/components/ui/accordion'
 import { PageTitle } from '@/components/ui/pageTitleText'
 import { useUrlHash } from '@/hooks/useUrlHash'
-import { SWCQuestionnaireAnswers } from '@/utils/server/builderIO/swc-questionnaire'
+import {
+  QUESTION_ANSWER_OPTIONS,
+  SWCQuestionnaireAnswers,
+} from '@/utils/server/builderIO/swc-questionnaire'
 import { twNoop } from '@/utils/web/cn'
 
 interface QuestionnaireAccordionProps {
-  questionnaire: SWCQuestionnaireAnswers | null
+  questionnaire: SWCQuestionnaireAnswers
 }
 
 const QUESTIONNAIRE_HASH_KEY = 'questionnaire'
@@ -25,13 +28,27 @@ export function QuestionnaireAccordion({ questionnaire }: QuestionnaireAccordion
   const accordionDefaultValue =
     urlHash === QUESTIONNAIRE_HASH_KEY ? QUESTIONNAIRE_HASH_KEY : isNil(urlHash) ? null : ''
   const questionnaireRef = useRef<HTMLDivElement>(null)
-  const answersAmount = Object.keys(questionnaire?.data ?? {}).length - 1
+
+  const answersAmount = useMemo(() => {
+    const totalAnsweredFields = Object.entries(questionnaire?.data ?? {}).reduce(
+      (acc, [key, value]) => {
+        if (key === 'q8ShareAnyOtherOpinionsOnCrypto' && value.length > 0) return (acc += 1)
+
+        return value !== QUESTION_ANSWER_OPTIONS['Not answered'] ? (acc += 1) : acc
+      },
+      0,
+    )
+
+    // subtracting one because dtsiSlug is returned inside data and it doesn't represent an answer
+    return totalAnsweredFields - 1
+  }, [questionnaire.data])
+
   useEffect(() => {
     if (!questionnaireRef.current || !accordionDefaultValue) return
     questionnaireRef.current?.scrollIntoView({ behavior: 'smooth' })
   }, [questionnaireRef, accordionDefaultValue])
 
-  if (!questionnaire || isNil(accordionDefaultValue)) return null
+  if (!questionnaire || isNil(accordionDefaultValue) || answersAmount === -1) return null
 
   return (
     <div className="mb-10 flex scroll-mt-20 flex-col" id="questionnaire" ref={questionnaireRef}>
@@ -45,61 +62,46 @@ export function QuestionnaireAccordion({ questionnaire }: QuestionnaireAccordion
 
           <AccordionContent className="pb-0">
             <div className="px-6 last:*:border-none">
-              {!isNil(questionnaire.data?.q1ExperienceUsingBlockchainTechnology) && (
-                <QuestionnaireItem
-                  answer={questionnaire.data?.q1ExperienceUsingBlockchainTechnology}
-                  question="Do you have experience buying, selling, or using blockchain technology assets or investment tools?"
-                />
-              )}
-              {!isNil(questionnaire.data?.q2BlockchainWillPlayMajorRoleNextInnoWave) && (
-                <QuestionnaireItem
-                  answer={questionnaire.data?.q2BlockchainWillPlayMajorRoleNextInnoWave}
-                  question="Do you believe blockchain technology and digital assets, including cryptocurrency like Bitcoin, will play a major role in the next wave of technological innovation globally?"
-                />
-              )}
+              <QuestionnaireItem
+                answer={questionnaire.data?.q1ExperienceUsingBlockchainTechnology}
+                question="Do you have experience buying, selling, or using blockchain technology assets or investment tools?"
+              />
 
-              {!isNil(questionnaire.data?.q3AmerCryptoIsDrivingEconomicGrowth) && (
-                <QuestionnaireItem
-                  answer={questionnaire.data?.q3AmerCryptoIsDrivingEconomicGrowth}
-                  question="Do you believe the American cryptocurrency and digital asset industry is driving economic growth and supporting millions of jobs across the country?"
-                />
-              )}
+              <QuestionnaireItem
+                answer={questionnaire.data?.q2BlockchainWillPlayMajorRoleNextInnoWave}
+                question="Do you believe blockchain technology and digital assets, including cryptocurrency like Bitcoin, will play a major role in the next wave of technological innovation globally?"
+              />
 
-              {!isNil(questionnaire.data?.q4UsCompAtRiskIfDigitalAssetsPushedOverse) && (
-                <QuestionnaireItem
-                  answer={questionnaire.data?.q4UsCompAtRiskIfDigitalAssetsPushedOverse}
-                  question="Do you believe US competitiveness and American national security are at risk if the digital asset industry is pushed overseas?"
-                />
-              )}
+              <QuestionnaireItem
+                answer={questionnaire.data?.q3AmerCryptoIsDrivingEconomicGrowth}
+                question="Do you believe the American cryptocurrency and digital asset industry is driving economic growth and supporting millions of jobs across the country?"
+              />
 
-              {!isNil(questionnaire.data?.q5UsModernizeRegulatoryEnvironmentForCrypto) && (
-                <QuestionnaireItem
-                  answer={questionnaire.data?.q5UsModernizeRegulatoryEnvironmentForCrypto}
-                  question="Do you believe it is important for the United States to modernize the regulatory environment for crypto and digital assets to ensure proper consumer protection while also fostering responsible innovation?"
-                />
-              )}
+              <QuestionnaireItem
+                answer={questionnaire.data?.q4UsCompAtRiskIfDigitalAssetsPushedOverse}
+                question="Do you believe US competitiveness and American national security are at risk if the digital asset industry is pushed overseas?"
+              />
 
-              {!isNil(questionnaire.data?.q6WouldYouVoteInFavorOfLegislation) && (
-                <QuestionnaireItem
-                  answer={questionnaire.data?.q6WouldYouVoteInFavorOfLegislation}
-                  question="If you are currently a Member of Congress or are elected to Congress, would you vote in favor of legislation that creates a comprehensive regulatory framework for digital assets like HR 4763, the “Financial Innovation and Technology for the 21st Century Act”, a bipartisan bill?"
-                />
-              )}
+              <QuestionnaireItem
+                answer={questionnaire.data?.q5UsModernizeRegulatoryEnvironmentForCrypto}
+                question="Do you believe it is important for the United States to modernize the regulatory environment for crypto and digital assets to ensure proper consumer protection while also fostering responsible innovation?"
+              />
 
-              {!isNil(questionnaire.data?.q7VoteInFavorOfLegisToPaymentStablecoins) && (
-                <QuestionnaireItem
-                  answer={questionnaire.data?.q7VoteInFavorOfLegisToPaymentStablecoins}
-                  question="If you are currently a Member of Congress or are elected to Congress, would you vote in favor of legislation to create clear rules for payment stablecoins (i.e., digital assets that are redeemable for U.S. dollars 1:1) like HR 4766, “Clarity for Payment Stablecoins Act of 2023”, a bipartisan bill?"
-                />
-              )}
+              <QuestionnaireItem
+                answer={questionnaire.data?.q6WouldYouVoteInFavorOfLegislation}
+                question="If you are currently a Member of Congress or are elected to Congress, would you vote in favor of legislation that creates a comprehensive regulatory framework for digital assets like HR 4763, the “Financial Innovation and Technology for the 21st Century Act”, a bipartisan bill?"
+              />
 
-              {!isNil(questionnaire.data?.q8ShareAnyOtherOpinionsOnCrypto) && (
-                <QuestionnaireItem
-                  answer={questionnaire.data?.q8ShareAnyOtherOpinionsOnCrypto}
-                  isBooleanQuestion={false}
-                  question="Please share any other positions or opinions that you have on how crypto and digital assets should be regulated?"
-                />
-              )}
+              <QuestionnaireItem
+                answer={questionnaire.data?.q7VoteInFavorOfLegisToPaymentStablecoins}
+                question="If you are currently a Member of Congress or are elected to Congress, would you vote in favor of legislation to create clear rules for payment stablecoins (i.e., digital assets that are redeemable for U.S. dollars 1:1) like HR 4766, “Clarity for Payment Stablecoins Act of 2023”, a bipartisan bill?"
+              />
+
+              <QuestionnaireItem
+                answer={questionnaire.data?.q8ShareAnyOtherOpinionsOnCrypto ?? null}
+                isBooleanQuestion={false}
+                question="Please share any other positions or opinions that you have on how crypto and digital assets should be regulated?"
+              />
             </div>
           </AccordionContent>
         </AccordionItem>
@@ -110,18 +112,30 @@ export function QuestionnaireAccordion({ questionnaire }: QuestionnaireAccordion
 
 interface QuestionnaireItemProps {
   question: string
-  answer: boolean | string
+  answer: QUESTION_ANSWER_OPTIONS | string | null
   isBooleanQuestion?: boolean
 }
 
 function QuestionnaireItem({ answer, question, isBooleanQuestion = true }: QuestionnaireItemProps) {
-  const parsedAnswer = !isBooleanQuestion ? answer : answer === true ? 'Yes' : 'No'
+  const parsedAnswer = !isBooleanQuestion
+    ? answer
+    : answer === QUESTION_ANSWER_OPTIONS['Yes']
+      ? 'Yes'
+      : 'No'
 
   function getAnswerStyles() {
-    if (!isBooleanQuestion) return ''
+    if (!isBooleanQuestion || answer === QUESTION_ANSWER_OPTIONS['Not answered']) return ''
 
-    return answer ? twNoop('text-green-600') : twNoop('text-red-600')
+    return answer === QUESTION_ANSWER_OPTIONS['Yes']
+      ? twNoop('text-green-600')
+      : twNoop('text-red-600')
   }
+
+  if (
+    answer === QUESTION_ANSWER_OPTIONS['Not answered'] ||
+    (!isBooleanQuestion && answer?.length === 0)
+  )
+    return null
 
   return (
     <div className="last-of-type:div:bg-red-500 flex flex-col gap-4 border-b-[1px] border-[#5B616E33]  py-6 text-base text-fontcolor-muted">
