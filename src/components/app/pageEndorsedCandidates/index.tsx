49026cf326c62dc559dc0e9fb60f61b02df936a2
Travis Bloom <travis.bloom@coinbase.com>
2024-05-02 12:16:42 -0400

    tweaks to ux

--- src/components/app/pageEndorsedCandidates/index.tsx
+++ src/components/app/pageEndorsedCandidates/index.tsx
@@ -1,40 +1,61 @@
+import Balancer from 'react-wrap-balancer'
+
+import { DarkHeroSection } from '@/components/app/darkHeroSection'
 import { DTSIPersonHeroCard } from '@/components/app/dtsiPersonHeroCard'
 import { DTSIPersonHeroCardRow } from '@/components/app/dtsiPersonHeroCard/dtsiPersonHeroCardRow'
 import { PACFooter } from '@/components/app/pacFooter'
 import { Button } from '@/components/ui/button'
+import { InternalLink } from '@/components/ui/link'
 import { PageTitle } from '@/components/ui/pageTitleText'
 import { DTSI_EndorsedCandidatesQuery } from '@/data/dtsi/generated'
 import { SupportedLocale } from '@/intl/locales'
+import { getIntlUrls } from '@/utils/shared/urls'
 
 interface LocationStateSpecificProps extends DTSI_EndorsedCandidatesQuery {
   locale: SupportedLocale
 }
 
 export function PageEndorsedCandidates({ people, locale }: LocationStateSpecificProps) {
+  const urls = getIntlUrls(locale)
   return (
-    <div className="standard-spacing-from-navbar space-y-20">
-      <section className="container">
-        <PageTitle as="h1">Stand With Crypto PAC 2024 House and Senate Endorsements</PageTitle>
-        <h2 className="mx-auto mt-6 space-y-4 text-center text-fontcolor-muted">
-          <p>
-            2024 will be a monumental election year and Congress holds the power to shape the future
-            of crypto in the U.S.
-          </p>
+    <div className="space-y-20">
+      <DarkHeroSection>
+        <div className="container text-center">
+          <h2 className={'mb-4'}>
+            <InternalLink className="text-gray-400" href={urls.locationUnitedStates()}>
+              United States
+            </InternalLink>{' '}
+            / <span>Endorsements</span>
+          </h2>
+          <PageTitle as="h1" className="mb-4" size="md">
+            Stand With Crypto PAC 2024 House and Senate Endorsements
+          </PageTitle>
+          <h3 className="mt-4 space-y-4 font-mono text-xl font-light">
+            <p>
+              <Balancer>
+                2024 will be a monumental election year and Congress holds the power to shape the
+                future of crypto in the U.S.
+              </Balancer>
+            </p>
 
-          <p>
-            Stand With Crypto is committed to supporting pro-crypto candidates and we are proud to
-            endorse the candidates below.
-          </p>
+            <p>
+              <Balancer>
+                Stand With Crypto is committed to supporting pro-crypto candidates and we are proud
+                to endorse the candidates below.
+              </Balancer>
+            </p>
 
-          {/*
+            {/*
           Uncomment once we have a PAC donate button
            <p>
           You can support the candidates directly, or click the donate button below to support the
           SWC PAC that will disburse funds to all the endorsed candidates below.
         </p> 
         */}
-        </h2>
-      </section>
+          </h3>
+        </div>
+      </DarkHeroSection>
+
       <DTSIPersonHeroCardRow>
         {people.map(person => (
           <DTSIPersonHeroCard
