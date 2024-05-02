be19f9f6ea2e20bf2652bf4b9f2af592117daef6
Eduardo Nunes Picolo <eduardo.picolo@coinbase.com>
2024-05-02 13:37:32 -0300

    Replace Modal with deep link

--- src/components/app/pageHome/heroCTA.tsx
+++ src/components/app/pageHome/heroCTA.tsx
@@ -1,15 +1,17 @@
 'use client'
 import { LoginDialogWrapper } from '@/components/app/authentication/loginDialogWrapper'
 import { OPEN_UPDATE_USER_PROFILE_FORM_QUERY_PARAM_KEY } from '@/components/app/updateUserProfileForm/queryParamConfig'
-import { UserActionFormEmailCongresspersonDialog } from '@/components/app/userActionFormEmailCongressperson/dialog'
 import { Button } from '@/components/ui/button'
 import { InternalLink } from '@/components/ui/link'
 import { useApiResponseForUserFullProfileInfo } from '@/hooks/useApiResponseForUserFullProfileInfo'
 import { useIntlUrls } from '@/hooks/useIntlUrls'
-import { useSession } from '@/hooks/useSession'
+import { useLocale } from '@/hooks/useLocale'
+import { USER_ACTION_DEEPLINK_MAP } from '@/utils/shared/urlsDeeplinkUserActions'
 import { hasCompleteUserProfile } from '@/utils/web/hasCompleteUserProfile'
 
 export function HeroCTA() {
+  const locale = useLocale()
+
   const profileReq = useApiResponseForUserFullProfileInfo()
 
   const urls = useIntlUrls()
@@ -23,21 +25,23 @@ export function HeroCTA() {
   return (
     <LoginDialogWrapper
       authenticatedContent={
-        profileReq.data?.user && hasCompleteUserProfile(profileReq.data?.user) ? (
-          <UserActionFormEmailCongresspersonDialog>
-            <Button size="lg" variant="primary-cta">
+        <Button asChild size="lg" variant="primary-cta">
+          {profileReq.data?.user && hasCompleteUserProfile(profileReq.data?.user) ? (
+            <InternalLink
+              href={USER_ACTION_DEEPLINK_MAP.EMAIL.getDeeplinkUrl({
+                locale,
+              })}
+            >
               Email your Rep
-            </Button>
-          </UserActionFormEmailCongresspersonDialog>
-        ) : (
-          <Button asChild size="lg" variant="primary-cta">
+            </InternalLink>
+          ) : (
             <InternalLink
               href={`${urls.profile()}?${OPEN_UPDATE_USER_PROFILE_FORM_QUERY_PARAM_KEY}=true`}
             >
               Finish your profile
             </InternalLink>
-          </Button>
-        )
+          )}
+        </Button>
       }
       loadingFallback={unauthenticatedContent}
     >
