9fa962ba472c261ca414b1a03038577845b4398f
Travis Bloom <travis.bloom@coinbase.com>
2024-05-02 22:56:04 -0400

    ux fixes for homepage

--- src/components/app/pageHome/heroImage.tsx
+++ src/components/app/pageHome/heroImage.tsx
@@ -6,24 +6,29 @@ import { UserActionFormEmailCongresspersonDialog } from '@/components/app/userAc
 import { Button } from '@/components/ui/button'
 import { NextImage } from '@/components/ui/image'
 import { LinkBox, linkBoxLinkClassName } from '@/components/ui/linkBox'
+import { cn } from '@/utils/web/cn'
 
 interface HeroImageProps {
   children: ReactNode
+  className?: string
 }
 
-const HeroImage = ({ children }: HeroImageProps) => {
+const HeroImage = ({ children, className }: HeroImageProps) => {
   return (
     <LinkBox className="relative h-[320px] overflow-hidden md:rounded-xl lg:h-[400px]">
       <NextImage
-        alt="Email your Rep"
+        alt="sign up"
         className="h-full w-full object-cover"
         fill
         priority
-        sizes={'500px'}
+        sizes={'(max-width: 400px) 375px, 500px'}
         src="/homepageHero.gif"
       />
       <div
-        className="absolute bottom-0 flex w-full items-center justify-between gap-4 p-4 text-sm text-white"
+        className={cn(
+          'absolute bottom-0 flex w-full items-center justify-between gap-4 p-4 text-sm text-white',
+          className,
+        )}
         style={{
           background:
             'linear-gradient(to top, hsla(0, 0%, 0%, 0.8) 10%, hsla(0, 0%, 0%, 0.4) 70%,  transparent 100%)',
@@ -39,7 +44,7 @@ const unauthenticatedContent = (
   <HeroImage>
     <p>Join Stand With Crypto and help us defend your right to own crypto in America.</p>
     <Button className={linkBoxLinkClassName} data-link-box-subject variant="secondary">
-      Register
+      Join
       <ArrowUpRight />
     </Button>
   </HeroImage>
@@ -47,12 +52,16 @@ const unauthenticatedContent = (
 
 const authenticatedContent = (
   <UserActionFormEmailCongresspersonDialog>
-    <HeroImage>
+    <HeroImage className="flex-col sm:flex-row">
       <p>
         FIT21 is a bipartisan crypto bill being considered for a vote in Congress. Ask your Rep to
         support its clear, commonsense rules for crypto today.
       </p>
-      <Button className={linkBoxLinkClassName} data-link-box-subject variant="secondary">
+      <Button
+        className={cn('max-sm:w-full', linkBoxLinkClassName)}
+        data-link-box-subject
+        variant="secondary"
+      >
         Email your Rep <ArrowUpRight />
       </Button>
     </HeroImage>
