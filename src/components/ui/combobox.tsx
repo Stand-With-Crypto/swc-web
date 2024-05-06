b2831b1912d4e0ce4be81d9f3a8b68131cacd6eb
Matheus Mendes <167922524+matheusmendes-cb@users.noreply.github.com>
2024-05-06 19:10:36 -0300

    iOS keyboard offsetting the address input dialog (#821)
    * fix: ios keyboard offsets the address input dialog
    
    * fix(usePreventMobileKeyboardOffset): check for typeof sessionStorage
    
    * refactor(usePreventMobileKeyboardOffset): improve hook code quality
    
    * fix(usePreventMobileKeyboardOffset): improve constant name
    
    * feat(usePreventMobileKeyboardOffset): comments to add context to this issue
    
    * fix(usePreventMobileKeyboardOffset): scroll reset sending to wrong Y position on iOS

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -12,6 +12,7 @@ import {
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { useIsMobile } from '@/hooks/useIsMobile'
+import { usePreventMobileKeyboardOffset } from '@/hooks/usePreventMobileKeyboardOffset'
 import { useResizeObserver } from '@/hooks/useResizeObserver'
 import { trackClientAnalytic } from '@/utils/web/clientAnalytics'
 import { cn } from '@/utils/web/cn'
@@ -51,6 +52,7 @@ export function Combobox<T>({
   setOpen,
   ...inputProps
 }: ComboBoxProps<T>) {
+  usePreventMobileKeyboardOffset(open)
   const parentRef = React.useRef<HTMLButtonElement>(null)
   const isMobile = useIsMobile({ defaultState: false })
   const size = useResizeObserver(parentRef)
