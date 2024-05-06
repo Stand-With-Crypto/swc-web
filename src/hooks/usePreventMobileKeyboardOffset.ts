7ee9d6a1744fe10318de33e5c99dccc8f655ba95
Matheus Mendes <matheushenrique.araujomendes@coinbase.com>
2024-05-06 13:34:49 -0300

    feat(usePreventMobileKeyboardOffset): comments to add context to this issue

--- src/hooks/usePreventMobileKeyboardOffset.ts
+++ src/hooks/usePreventMobileKeyboardOffset.ts
@@ -3,6 +3,8 @@ import * as React from 'react'
 
 import { useIsMobile } from '@/hooks/useIsMobile'
 
+// This constant is the difference between the scrollY position saved and the actual height of the page
+// If we use just the scrollY position saved, the page will actually scroll more than it should.
 const DIFF_SCROLL_Y_TO_ACTUAL_HEIGHT = 148
 
 function getScrollHeight() {
@@ -25,6 +27,20 @@ function resetScrollPosition() {
   return
 }
 
+// This hook "fixes" a known issue on iOS devices where when you open a modal with the keyboard
+// opened, the keyboard will push the entire page up.
+
+// This happens because if the keyboard is already open the component will no resize to fit the
+// screen height when using 100dvh but instead it behaves like 100vh, causing the keyboard to
+//  offset the page to the top.
+
+// To fix this, this hook will save the current scrollY position when it is rendered and scroll
+// the entire page to the top. Once the hook is unmounted, it will scroll the page back to the
+// original position.
+
+// References:
+// https://stackoverflow.com/questions/13820088/how-to-prevent-keyboard-push-up-webview-at-ios-app-using-phonegap
+// https://stackoverflow.com/questions/38619762/how-to-prevent-ios-keyboard-from-pushing-the-view-off-screen-with-css-or-js
 export function usePreventMobileKeyboardOffset() {
   const isMobile = useIsMobile()
 
