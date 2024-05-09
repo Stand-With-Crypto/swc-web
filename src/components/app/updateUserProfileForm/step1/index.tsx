fc168e476b0846dcd1b6bdf0e67c1327d94b05f9
Matheus Mendes <matheushenrique.araujomendes@coinbase.com>
2024-05-08 21:07:47 -0300

    fix: render control form if user has completed profile or already has nft

--- src/components/app/updateUserProfileForm/step1/index.tsx
+++ src/components/app/updateUserProfileForm/step1/index.tsx
@@ -1,11 +1,19 @@
 import { ExperimentsTesting } from '@/components/ui/experimentsTesting'
+import { useApiResponseForUserClaimedOptInNft } from '@/hooks/useApiResponseForUserClaimedOptInNft'
 import { hasCompleteUserProfile } from '@/utils/web/hasCompleteUserProfile'
 
 import { UpdateUserProfileForm as Control } from './control'
 import { UpdateUserProfileForm as Variant } from './variant'
 
 export function UpdateUserProfileFormExperimentTesting(props: Parameters<typeof Control>[0]) {
-  if (hasCompleteUserProfile(props.user)) return <Control {...props} />
+  const { data: claimedOptInNft } = useApiResponseForUserClaimedOptInNft({ suspense: true })
+
+  // We must check if the user has already claimed the opt-in NFT or has a complete user profile because there
+  // are cases where the NFT is not minted right away because the transaction fee is higher than the threshold
+
+  // If the user is in the variant of the experiment and has not claimed the opt-in NFT because of it
+  // we should still render the Control Form if he has already completed his profile
+  if (claimedOptInNft || hasCompleteUserProfile(props.user)) return <Control {...props} />
 
   return (
     <ExperimentsTesting
