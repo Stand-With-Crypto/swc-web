4010cca0a741736004ebdab913a9f210373ea912
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-05-09 10:30:17 -0300

    feat: adds new activity icon for tweet at person

--- src/components/app/recentActivityRow/activityAvatar.tsx
+++ src/components/app/recentActivityRow/activityAvatar.tsx
@@ -14,6 +14,7 @@ export const ACTIVITY_TYPE_TO_ICON_URL: Record<UserActionType, string> = {
   [UserActionType.VOTER_REGISTRATION]: '/activityFeedIcons/vote.svg',
   [UserActionType.TWEET]: '/activityFeedIcons/share-x.svg',
   [UserActionType.LIVE_EVENT]: '/activityFeedIcons/event.svg',
+  [UserActionType.TWEET_AT_PERSON]: '/activityFeedIcons/share-x.svg',
 }
 
 type ActivityAvatarProps = {
