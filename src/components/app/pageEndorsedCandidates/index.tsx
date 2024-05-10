b3eef8edc38f151e847272d423b700cdb7cca4cf
Eduardo Nunes Picolo <eduardo.picolo@coinbase.com>
2024-05-10 19:34:18 -0300

    Fix button position

--- src/components/app/pageEndorsedCandidates/index.tsx
+++ src/components/app/pageEndorsedCandidates/index.tsx
@@ -19,28 +19,28 @@ export function PageEndorsedCandidates({ people, locale }: LocationStateSpecific
   return (
     <div className="space-y-20">
       <DarkHeroSection>
-        <div className="container text-center">
-          <h2 className={'mb-4'}>
+        <div className="container space-y-4 text-center">
+          <h2>
             <InternalLink className="text-gray-400" href={urls.locationUnitedStates()}>
               United States
             </InternalLink>{' '}
             / <span>Endorsements</span>
           </h2>
-          <PageTitle as="h1" className="mb-4" size="md">
+          <PageTitle as="h1" size="md">
             Stand With Crypto PAC 2024 House and Senate Endorsements
           </PageTitle>
-          <h3 className="mt-4 space-y-4 font-mono text-xl font-light">
+          <h3 className="font-mono text-xl font-light">
             2024 will be a monumental election year and Congress holds the power to shape the future
             of crypto in the US. SWC is committed to supporting pro-crypto candidates and we are
             proud to endorse the candidates below. You can support the candidates directly by
             clicking on their links.
           </h3>
+
+          <MaybeDonateButton donationUrl="#" variant="secondary">
+            Donate to all candidates
+          </MaybeDonateButton>
         </div>
       </DarkHeroSection>
-
-      <div className="mx-auto flex">
-        <MaybeDonateButton donationUrl="#">Donate to all candidates</MaybeDonateButton>
-      </div>
 
       <DTSIPersonHeroCardRow className="mx-auto max-w-4xl xl:max-w-6xl">
         {people.map(person => (
