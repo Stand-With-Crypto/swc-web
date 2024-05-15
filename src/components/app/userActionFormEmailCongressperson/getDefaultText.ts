531a48d43ae7d8fc66f3e3809c8167abcdcaf292
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-05-15 18:09:58 -0300

    feat: change email body

--- src/components/app/userActionFormEmailCongressperson/getDefaultText.ts
+++ src/components/app/userActionFormEmailCongressperson/getDefaultText.ts
@@ -24,9 +24,9 @@ export function getDefaultText({
 
 ${maybeDistrictIntro} and recently learned that the Financial Innovation and Technology for the 21st Century Act [FIT21 H.R. 4763] is being considered for a floor vote in the House.
 
-I am asking you to vote YES on the FIT21 Act because it sets foundational rules for crypto that are pro-consumer and pro-job creation. It will protect 52 million Americans who own crypto, unlock the creation of millions of jobs in the U.S., and ensure America remains a global leader in technology.  That’s good for our economic and national security.  
+I am asking you to vote YES on the FIT21 Act because it sets foundational rules for crypto that are pro-consumer and pro-job creation. It will protect 52 million Americans who own crypto, unlock the creation of millions of jobs in the U.S., and ensure America remains a global leader in technology. That’s good for our economic and national security.
 
 We have been waiting patiently for years for our lawmakers to protect us, and strengthen America’s role in building the future of money and the internet.
 
-I am excited the House is going to vote on FIT21, and I am asking you to vote YES for the Bill.${fullNameSignOff}`
+I am excited the House is going to vote on FIT21, and I am asking you to vote YES on the Act.${fullNameSignOff}`
 }
