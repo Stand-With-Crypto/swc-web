2cc2c81760930c42253b678ea04ceed2e393597b
Travis Bloom <travis.bloom@coinbase.com>
2024-05-17 10:37:16 -0400

    lint fix

--- src/bin/reports/emailActionsByDTSISlug.ts
+++ src/bin/reports/emailActionsByDTSISlug.ts
@@ -1,12 +1,12 @@
 import { UserActionType } from '@prisma/client'
 import { subDays } from 'date-fns'
+import { groupBy } from 'lodash-es'
 import xlsx from 'xlsx'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
 import { runBin } from '@/bin/runBin'
 import { prismaClient } from '@/utils/server/prismaClient'
-import { groupBy } from 'lodash-es'
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
 
 const params = yargs(hideBin(process.argv)).option('daysOfData', {
   type: 'number',
@@ -32,13 +32,13 @@ async function emailActionsByDTSISlug() {
     action => action.userActionEmail?.userActionEmailRecipients || [],
   )
   const actionsByDTSISlug = groupBy(recipients, x => x.dtsiSlug)
-  const countRows = Object.entries(actionsByDTSISlug).map(([dtsiSlug, recipients]) => ({
+  const countRows = Object.entries(actionsByDTSISlug).map(([dtsiSlug, rows]) => ({
     dtsiSlug,
-    count: recipients.length,
+    count: rows.length,
   }))
   const workbook = xlsx.utils.book_new()
 
-  let worksheet = xlsx.utils.json_to_sheet(countRows)
+  const worksheet = xlsx.utils.json_to_sheet(countRows)
   xlsx.utils.book_append_sheet(workbook, worksheet, `Emails By DTSI Slug - ${daysOfData} Days`)
   await xlsx.writeFile(workbook, './src/bin/localCache/emailActionsByDTSISlug.xlsx')
 }
