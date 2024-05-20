aeec22c970c4aa87bd8bd88c74b347b42ac878dd
Paulo Victor <165944245+paulovictor-cb@users.noreply.github.com>
2024-05-20 13:23:35 -0300

    feat: removes async code to remove layout shift (#881)
    * feat: removes async code to remove layout shift
    
    * feat: adds window check
    
    * feat: updates window condition
    
    * feat: changes hash logic

--- src/hooks/useUrlHash.ts
+++ src/hooks/useUrlHash.ts
@@ -1,16 +1,7 @@
-import { useEffect, useState } from 'react'
+'use client'
 
 export function useUrlHash() {
-  const [urlHash, setUrlHash] = useState<string | null>()
-
-  useEffect(() => {
-    const hash =
-      typeof window !== 'undefined'
-        ? decodeURIComponent(window.location.hash.replace('#', ''))
-        : null
-
-    setUrlHash(hash)
-  }, [])
-
-  return { urlHash }
+  return typeof window !== 'undefined'
+    ? decodeURIComponent(window.location.hash.replace('#', ''))
+    : null
 }
