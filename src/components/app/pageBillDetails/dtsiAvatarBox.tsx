f7d0e182e620a1f4a458abd0f61fc921c30dfbef
Travis Bloom <travis.bloom@coinbase.com>
2024-05-22 22:34:59 -0400

    add political affiliation to bills page

--- src/components/app/pageBillDetails/dtsiAvatarBox.tsx
+++ src/components/app/pageBillDetails/dtsiAvatarBox.tsx
@@ -4,14 +4,17 @@ import { LinkBox, linkBoxLinkClassName } from '@/components/ui/linkBox'
 import { DTSI_Person, DTSI_PersonRole, Maybe } from '@/data/dtsi/generated'
 import { SupportedLocale } from '@/intl/locales'
 import { getDTSIPersonRoleCategoryDisplayName } from '@/utils/dtsi/dtsiPersonRoleUtils'
-import { dtsiPersonFullName } from '@/utils/dtsi/dtsiPersonUtils'
+import {
+  dtsiPersonFullName,
+  dtsiPersonPoliticalAffiliationCategoryAbbreviation,
+} from '@/utils/dtsi/dtsiPersonUtils'
 import { getIntlUrls } from '@/utils/shared/urls'
 import { cn } from '@/utils/web/cn'
 
 interface DTSIAvatarBoxProps extends DTSIAvatarProps {
   locale: SupportedLocale
   person: DTSIAvatarProps['person'] &
-    Pick<DTSI_Person, 'slug'> & {
+    Pick<DTSI_Person, 'slug' | 'politicalAffiliationCategory'> & {
       primaryRole: Maybe<
         Pick<DTSI_PersonRole, 'roleCategory' | 'title' | 'status' | 'primaryState'>
       >
@@ -21,7 +24,9 @@ interface DTSIAvatarBoxProps extends DTSIAvatarProps {
 
 export const DTSIAvatarBox = (props: DTSIAvatarBoxProps) => {
   const { person, locale, prefetch = false, ...avatarProps } = props
-
+  const politicalAffiliationCategoryAbbr =
+    person.politicalAffiliationCategory &&
+    dtsiPersonPoliticalAffiliationCategoryAbbreviation(person.politicalAffiliationCategory)
   return (
     <LinkBox className="flex w-fit flex-col items-center gap-2 text-center">
       <DTSIAvatar className="rounded-full" person={person} {...avatarProps} />
@@ -33,6 +38,7 @@ export const DTSIAvatarBox = (props: DTSIAvatarBoxProps) => {
           prefetch={prefetch}
         >
           {dtsiPersonFullName(person)}
+          {!!politicalAffiliationCategoryAbbr && ` (${politicalAffiliationCategoryAbbr})`}
         </InternalLink>
         <p className="text-sm text-fontcolor-muted">
           {person.primaryRole ? getDTSIPersonRoleCategoryDisplayName(person.primaryRole) : '-'}
