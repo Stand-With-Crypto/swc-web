8f8cd8aa97df39344fc62b9c5b903731f96035ef
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-05-29 16:19:25 -0300

    fix: outlook error spikes

--- src/hooks/useHandlePageError.ts
+++ src/hooks/useHandlePageError.ts
@@ -17,10 +17,47 @@ export function useHandlePageError({
     const isIntentionalError = window.location.pathname.includes('debug-sentry')
     logger.info(`${humanReadablePageName} Error Page rendered with:`, error)
     Sentry.captureException(error, { tags: { domain } })
+
+    if (checkIfErrorIsCausedByOutlook(error)) return
+
     const message = isIntentionalError
       ? `Testing Sentry Triggered ${humanReadablePageName} Error Page`
       : `${humanReadablePageName} Error Page Displayed`
     Sentry.captureMessage(message, { fingerprint: [`fingerprint-${message}`] })
     trackClientAnalytic('Error Page Visible', { Category: humanReadablePageName })
   }, [domain, error, humanReadablePageName])
+}
+
+// we are not sure what causes outlook users to trigger an anti-fingerprint error when accessing
+// SWC using the parsed safe link from outlook. This is a fix to prevent errors spikes
+// from showing up in Sentry and Mixpanel when new email campaigns are sent out.
+// You can find more information about this issue here: https://github.com/Stand-With-Crypto/swc-web/issues/848
+const OUTLOOK_BOT_ERROR_MESSAGE =
+  'Non-Error promise rejection captured with value: Object Not Found'
+function checkIfErrorIsCausedByOutlook(error: Error & { digest?: string }) {
+  if (error?.message.includes(OUTLOOK_BOT_ERROR_MESSAGE)) {
+    return true
+  }
+
+  if (error?.name.includes(OUTLOOK_BOT_ERROR_MESSAGE)) {
+    return true
+  }
+
+  if (error?.digest && error?.digest.includes(OUTLOOK_BOT_ERROR_MESSAGE)) {
+    return true
+  }
+
+  if (
+    error?.cause &&
+    typeof error?.cause === 'string' &&
+    error?.cause.includes(OUTLOOK_BOT_ERROR_MESSAGE)
+  ) {
+    return true
+  }
+
+  if (error?.stack && error?.stack.includes(OUTLOOK_BOT_ERROR_MESSAGE)) {
+    return true
+  }
+
+  return false
 }
