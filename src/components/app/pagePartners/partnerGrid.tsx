83b259461d1651510dde6509477afc7308d995a4
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-05-31 13:45:52 -0300

    fix: attempt to fix partner images jank

--- src/components/app/pagePartners/partnerGrid.tsx
+++ src/components/app/pagePartners/partnerGrid.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react'
+
 import { NextImage } from '@/components/ui/image'
 import { ExternalLink } from '@/components/ui/link'
 import { INDUSTRY_PARTNERS } from '@/utils/shared/industryPartners'
@@ -8,20 +10,28 @@ export function PartnerGrid() {
       {INDUSTRY_PARTNERS.map(({ title, url, imageSrc }) => {
         return (
           <ExternalLink href={url} key={title}>
-            <div className="relative flex h-40 w-40 items-center rounded-lg border-[12px] border-secondary bg-secondary transition hover:drop-shadow-lg sm:h-56 sm:w-56 sm:border-[36px]">
-              <NextImage
-                alt={`${title} logo`}
-                className="object-contain"
-                fill
-                priority
-                quality={100}
-                sizes={'(max-width: 768px) 160px, 224px'}
-                src={imageSrc}
-              />
-            </div>
+            <MemoizedPartnerImage imageSrc={imageSrc} title={title} />
           </ExternalLink>
         )
       })}
     </div>
   )
 }
+
+const MemoizedPartnerImage = memo(({ title, imageSrc }: { title: string; imageSrc: string }) => {
+  return (
+    <div className="relative flex h-40 w-40 items-center rounded-lg border-[12px] border-secondary bg-secondary transition hover:drop-shadow-lg sm:h-56 sm:w-56 sm:border-[36px]">
+      <NextImage
+        alt={`${title} logo`}
+        className="object-contain"
+        fill
+        priority
+        quality={100}
+        sizes={'(max-width: 768px) 160px, 224px'}
+        src={imageSrc}
+      />
+    </div>
+  )
+})
+
+MemoizedPartnerImage.displayName = 'MemoizedPartnerImage'
