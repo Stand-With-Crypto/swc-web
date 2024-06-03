7085b8701bcdaf64acfaeca4628dd09b4c0f5060
Eduardo Nunes Picolo <eduardo.picolo@coinbase.com>
2024-06-03 17:02:19 -0300

    Trigger capitol canary on sms opt in

--- src/actions/actionUpdateUserHasOptedInSMS.ts
+++ src/actions/actionUpdateUserHasOptedInSMS.ts
@@ -1,10 +1,18 @@
 'use server'
 import 'server-only'
 
+import { Address, User, UserCryptoAddress } from '@prisma/client'
 import { z } from 'zod'
 
 import { getClientUser } from '@/clientModels/clientUser/clientUser'
+import { CAPITOL_CANARY_UPSERT_ADVOCATE_INNGEST_EVENT_NAME } from '@/inngest/functions/capitolCanary/upsertAdvocateInCapitolCanary'
+import { inngest } from '@/inngest/inngest'
 import { appRouterGetAuthUser } from '@/utils/server/authentication/appRouterGetAuthUser'
+import {
+  CapitolCanaryCampaignName,
+  getCapitolCanaryCampaignID,
+} from '@/utils/server/capitolCanary/campaigns'
+import { UpsertAdvocateInCapitolCanaryPayloadRequirements } from '@/utils/server/capitolCanary/payloadRequirements'
 import { prismaClient } from '@/utils/server/prismaClient'
 import { throwIfRateLimited } from '@/utils/server/ratelimit/throwIfRateLimited'
 import { getServerPeopleAnalytics } from '@/utils/server/serverAnalytics'
@@ -60,7 +68,29 @@ async function _actionUpdateUserHasOptedInToSMS(data: UpdateUserHasOptedInToSMSP
     },
   })
 
+  await handleCapitolCanarySMSUpdate(updatedUser)
+
   return {
     user: getClientUser(updatedUser),
   }
+}
+
+async function handleCapitolCanarySMSUpdate(
+  updatedUser: User & { address: Address | null } & {
+    primaryUserCryptoAddress: UserCryptoAddress | null
+  },
+) {
+  const payload: UpsertAdvocateInCapitolCanaryPayloadRequirements = {
+    campaignId: getCapitolCanaryCampaignID(CapitolCanaryCampaignName.DEFAULT_SUBSCRIBER),
+    user: updatedUser,
+    opts: {
+      isSmsOptin: updatedUser.hasOptedInToSms,
+      // shouldSendSmsOptinConfirmation: true,
+    },
+  }
+
+  await inngest.send({
+    name: CAPITOL_CANARY_UPSERT_ADVOCATE_INNGEST_EVENT_NAME,
+    data: payload,
+  })
 }
