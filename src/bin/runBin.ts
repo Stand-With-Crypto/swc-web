06a00a6f3f4a5fa92c01eb9320ae072efc8cdbd3
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-06-04 15:11:29 -0300

    feat: adds sentry integration to runBin

--- src/bin/runBin.ts
+++ src/bin/runBin.ts
@@ -2,8 +2,36 @@
 import * as Sentry from '@sentry/nextjs'
 
 import { prismaClient } from '@/utils/server/prismaClient'
+import { NEXT_PUBLIC_ENVIRONMENT } from '@/utils/shared/sharedEnv'
+import { toBool } from '@/utils/shared/toBool'
+
+const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN
 
 export const runBin = async (fn: (...args: any[]) => Promise<any>) => {
+  Sentry.init({
+    environment: NEXT_PUBLIC_ENVIRONMENT,
+    dsn,
+    enabled: !!dsn,
+    integrations: [Sentry.prismaIntegration()],
+    tracesSampleRate: NEXT_PUBLIC_ENVIRONMENT === 'production' ? 0.1 : 1.0,
+    normalizeDepth: 10,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+    beforeSend: (event, hint) => {
+      if (NEXT_PUBLIC_ENVIRONMENT === 'local') {
+        const shouldSuppress = toBool(process.env.SUPPRESS_SENTRY_ERRORS_ON_LOCAL) || !dsn
+        console.error(
+          `${shouldSuppress ? 'Suppressed ' : ''}Sentry`,
+          hint?.originalException || hint?.syntheticException,
+        )
+        if (shouldSuppress) {
+          return null
+        }
+      }
+      return event
+    },
+  })
+
   return fn()
     .then(async () => {
       await prismaClient.$disconnect()
