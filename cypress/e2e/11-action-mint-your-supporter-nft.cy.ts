acab5eeee88c7e438907ad15bba2b2a77c42826e
Olavo Parno <olavo.parno@coinbase.com>
2024-06-04 13:59:10 -0300

    test: add mint nft action cypress
    #569

---
+++ cypress/e2e/11-action-mint-your-supporter-nft.cy.ts
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+
+import { mockRandomUser, mockWallet } from 'cypress/fixture/mocks'
+
+it('action - mint your supporter NFT', () => {
+  cy.visit('/')
+
+  cy.contains('Mint your Supporter NFT').click()
+
+  cy.get('[role="dialog"]')
+
+  cy.get('button[data-testid="signin-button"]').should('be.visible').click()
+
+  cy.get('button[data-test="continue-as-guest-button"]').should('be.visible').click()
+
+  // type random password
+  cy.get('input[data-test="new-password"][type="password"]')
+    .should('be.visible')
+    .type(mockWallet.password)
+  cy.get('input[data-test="confirm-password"][type="password"]')
+    .should('be.visible')
+    .type(mockWallet.password)
+
+  // click create new wallet
+  cy.contains('Create new wallet').click()
+
+  // wait until there's Finish your profile on the screen
+  cy.contains('Finish your profile').should('be.visible')
+
+  // type first name
+  cy.get('input[placeholder="First name"').should('be.visible').type(mockRandomUser.firstName)
+
+  // type last name
+  cy.get('input[placeholder="Last name"').should('be.visible').type(mockRandomUser.lastName)
+
+  // type address
+  cy.selectFromComboBox({
+    trigger: cy.get('input[placeholder="Street address"]'),
+    searchText: mockRandomUser.address,
+  })
+
+  // type email
+  const emailAddressInput = cy.get('input[placeholder="Your email"')
+
+  emailAddressInput.should('be.visible')
+  emailAddressInput.clear()
+  emailAddressInput.type(mockRandomUser.email)
+
+  cy.contains('Next').click()
+
+  cy.contains('Submit').click()
+
+  cy.contains('Continue').click()
+
+  // cy.get('button[role="checkbox"][data-state="unchecked"]').click()
+  cy.contains('Mock the minting transaction').click()
+
+  cy.contains('Mint now - Mocked').click()
+
+  cy.contains('Transaction complete').should('be.visible')
+
+  // validate database
+  cy.queryDb('SELECT * FROM user_action WHERE nft_mint_id is not NULL').then((result: any) => {
+    expect(result.length, 'user_action to exist in database').to.equal(1)
+  })
+  cy.clearDb()
+})
---
+++ cypress/fixture/mocks.ts
@@ -0,0 +1,10 @@
+export const mockWallet = {
+  password: Math.random().toString(36).substring(7),
+}
+
+export const mockRandomUser = {
+  email: `johndoe${Math.random().toString(36).substring(7)}@email.com`,
+  firstName: 'John',
+  lastName: 'Doe',
+  address: '350 Fifth Avenue New York, NY 10118',
+}
