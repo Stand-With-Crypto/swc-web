82424997d294599992cfd8960cb0b5ed14ff4204
Paulo Victor <165944245+paulovictor-cb@users.noreply.github.com>
2024-06-05 09:17:29 -0300

    Upgrades Sentry to V8 (#932)
    * feat: upgrades sentry to version 8
    
    * feat: removes sentry server config file import in runBin
    
    * feat: adds sentry integration to runBin

--- next.config.js
+++ next.config.js
@@ -177,6 +177,9 @@ const V1_ACTION_REDIRECTS = ACTION_REDIRECTS.map(({ destination, queryKey, query
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    instrumentationHook: true,
+  },
   images: {
     minimumCacheTTL: 60 * 60 * 24 * 7, // 7 days
     unoptimized: false,
@@ -319,5 +322,8 @@ const userSentryOptions = {
 }
 // Injected content via Sentry wizard below
 module.exports = withBundleAnalyzer(
-  withSentryConfig(nextConfig, sentryWebpackPluginOptions, userSentryOptions),
+  withSentryConfig(nextConfig, {
+    ...sentryWebpackPluginOptions,
+    ...userSentryOptions,
+  }),
 )
