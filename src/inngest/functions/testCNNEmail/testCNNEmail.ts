048796a649475c563f210a68c125fc3c4ac49b74
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-06-13 08:59:36 -0300

    fix: duplicate event name

--- src/inngest/functions/testCNNEmail/testCNNEmail.ts
+++ src/inngest/functions/testCNNEmail/testCNNEmail.ts
@@ -2,8 +2,8 @@ import { testCNNEmailSend } from '@/inngest/functions/testCNNEmail/logic'
 import { inngest } from '@/inngest/inngest'
 import { onScriptFailure } from '@/inngest/onScriptFailure'
 
-const TEST_CNN_EMAIL_INNGEST_EVENT_NAME = 'script/cleanup-postal-codes'
-const TEST_CNN_EMAIL_INNGEST_FUNCTION_ID = 'script.cleanup-postal-codes'
+const TEST_CNN_EMAIL_INNGEST_EVENT_NAME = 'script/test-cnn-email'
+const TEST_CNN_EMAIL_INNGEST_FUNCTION_ID = 'script.test-cnn-email'
 export const testCNNEmail = inngest.createFunction(
   {
     id: TEST_CNN_EMAIL_INNGEST_FUNCTION_ID,
