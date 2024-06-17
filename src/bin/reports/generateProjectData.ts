8f1fa662c9f0c371892cc81e26e9106366f603ec
Travis Bloom <travis.bloom@coinbase.com>
2024-06-16 21:31:53 -0400

    tweaks

--- src/bin/reports/generateProjectData.ts
+++ src/bin/reports/generateProjectData.ts
@@ -9,12 +9,16 @@ const GITHUB_API_URL = 'https://api.github.com/graphql'
 
 interface GithubProjectDataIssueNode {
   state: string
+  stateReason: string
   url: string
   updatedAt: string
   closedAt: string
   createdAt: string
   repository: {
     name: string
+  }
+  milestone?: {
+    title: string
   }
   assignees: {
     nodes: {
@@ -27,6 +31,9 @@ interface GithubProjectDataIssueNode {
     nodes?: [
       {
         effort?: {
+          value: string
+        }
+        status?: {
           value: string
         }
         impact?: {
@@ -63,9 +70,13 @@ export const GITHUB_PROJECT_DATA_QUERY = /* GraphQL */ `
           url
           updatedAt
           closedAt
+          stateReason
           createdAt
           repository {
             name
+          }
+          milestone {
+            title
           }
           assignees(first: 5, last: null) {
             nodes {
@@ -75,6 +86,11 @@ export const GITHUB_PROJECT_DATA_QUERY = /* GraphQL */ `
           }
           projectItems(first: 100) {
             nodes {
+              status: fieldValueByName(name: "Status") {
+                ... on ProjectV2ItemFieldSingleSelectValue {
+                  value: name
+                }
+              }
               effort: fieldValueByName(name: "Effort") {
                 ... on ProjectV2ItemFieldSingleSelectValue {
                   value: name
@@ -126,7 +142,6 @@ async function getGithubProjectData(
   const { data } = (await response.json()) as GithubProjectDataResponse
   const repository = data.repository
   const repositoryIssues = repository?.issues
-  console.log(repositoryIssues)
   if (repositoryIssues) {
     issues.push(...repositoryIssues.nodes)
 
@@ -142,122 +157,49 @@ async function generateProjectData() {
   await getGithubProjectData(issues, 'swc-web')
   await getGithubProjectData(issues, 'swc-internal')
 
-  await generateProjectDataOverview(issues)
-  await generateProjectDataByAssignee(issues)
+  await writeProjectData(issues)
 }
 
 void runBin(generateProjectData)
 
-async function generateProjectDataOverview(issues: GithubProjectDataIssueNode[]) {
+async function writeProjectData(issues: GithubProjectDataIssueNode[]) {
   const workbook = xlsx.utils.book_new()
 
-  const closedIssues = issues.filter(issue => issue.state === 'CLOSED')
-  const closedIssuesByMonth = closedIssues.reduce(
-    (acc, issue) => {
-      const closedAtKey = format(new Date(issue.closedAt), 'yyyy-MM (MMMM)')
-
-      if (!acc[closedAtKey]) {
-        acc[closedAtKey] = []
-      }
-
-      acc[closedAtKey].push(issue)
-
-      return acc
-    },
-    {} as Record<string, GithubProjectDataIssueNode[]>,
-  )
-  const closedIssuesByMonthOrderedDesc = Object.entries(closedIssuesByMonth).sort(([a], [b]) => {
-    return new Date(b).getTime() - new Date(a).getTime()
+  const issuesOrderedDesc = issues.sort((a, b) => {
+    return new Date(b.closedAt).getTime() - new Date(a.closedAt).getTime()
   })
 
-  closedIssuesByMonthOrderedDesc.forEach(([month, _issues]) => {
-    const worksheet = xlsx.utils.json_to_sheet(
-      _issues.map((item: GithubProjectDataIssueNode) => {
-        const days = differenceInDays(new Date(item.closedAt), new Date(item.createdAt))
-        const hours = differenceInHours(new Date(item.closedAt), new Date(item.createdAt)) % 24
-        const minutes = differenceInMinutes(new Date(item.closedAt), new Date(item.createdAt)) % 60
+  const worksheet = xlsx.utils.json_to_sheet(
+    issuesOrderedDesc.map((item: GithubProjectDataIssueNode) => {
+      const days = differenceInDays(new Date(item.closedAt), new Date(item.createdAt))
+      const hours = differenceInHours(new Date(item.closedAt), new Date(item.createdAt)) % 24
+      const minutes = differenceInMinutes(new Date(item.closedAt), new Date(item.createdAt)) % 60
 
-        const timePassed = []
-        if (days > 0) timePassed.push(`${days} days`)
-        if (hours > 0) timePassed.push(`${hours} hours`)
-        if (minutes > 0) timePassed.push(`${minutes} minutes`)
-
-        return {
-          ['Issue Title']: item.title,
-          ['Assignee']: item.assignees.nodes[0]?.name ?? item.assignees.nodes[0]?.login ?? '---',
-          ['Effort']: item?.projectItems?.nodes?.[0]?.effort?.value ?? '---',
-          ['Impact']: item?.projectItems?.nodes?.[0]?.impact?.value ?? '---',
-          ['Issue Type']: item?.projectItems?.nodes?.[0]?.issueType?.value ?? '---',
-          ['Repo']: item?.repository?.name ?? '---',
-          ['Url']: item.url,
-          ['Created']: format(new Date(item.createdAt), 'yyyy-MM-dd'),
-          ['Closed']: format(new Date(item.closedAt), 'yyyy-MM-dd'),
-          ['Time to Close']: timePassed.join(', '),
-          state: item.state === 'CLOSED' ? 'Closed' : 'Open',
-        }
-      }),
-    )
-
-    xlsx.utils.book_append_sheet(workbook, worksheet, month)
-  })
-
-  await xlsx.writeFile(workbook, './src/bin/localCache/projectReportByMonth.xlsx')
-}
-
-async function generateProjectDataByAssignee(issues: GithubProjectDataIssueNode[]) {
-  const workbook = xlsx.utils.book_new()
-
-  const closedIssues = issues.filter(issue => issue.state === 'CLOSED')
-
-  const issuesByAssignee = closedIssues.reduce(
-    (acc, issue) => {
-      const assignee = issue.assignees.nodes[0]
-      if (!assignee) return acc
-
-      if (!acc[assignee.login]) {
-        acc[assignee.login] = []
+      const timePassed = []
+      if (days > 0) timePassed.push(`${days} days`)
+      if (hours > 0) timePassed.push(`${hours} hours`)
+      if (minutes > 0) timePassed.push(`${minutes} minutes`)
+      return {
+        ['Issue Title']: item.title,
+        ['Assignee']: item.assignees.nodes[0]?.name ?? item.assignees.nodes[0]?.login,
+        ['Effort']: item?.projectItems?.nodes?.[0]?.effort?.value,
+        ['Impact']: item?.projectItems?.nodes?.[0]?.impact?.value,
+        ['Status']: item?.projectItems?.nodes?.[0]?.status?.value,
+        ['Issue Type']: item?.projectItems?.nodes?.[0]?.issueType?.value,
+        ['Repo']: item?.repository?.name,
+        ['Url']: item.url,
+        ['Created']: format(new Date(item.createdAt), 'yyyy-MM-dd'),
+        ['Closed']: format(new Date(item.closedAt), 'yyyy-MM-dd'),
+        ['Time to Close']: timePassed.join(', '),
+        ['Milestone']: item.milestone?.title,
+        State:
+          item.state === 'CLOSED' && item.stateReason === 'NOT_PLANNED'
+            ? 'CLOSED_NOT_PLANNED'
+            : item.state,
       }
-
-      acc[assignee.login].push(issue)
-
-      return acc
-    },
-    {} as Record<string, GithubProjectDataIssueNode[]>,
+    }),
   )
 
-  Object.entries(issuesByAssignee).forEach(([assignee, _issues]) => {
-    const issuesOrderedDesc = _issues.sort((a, b) => {
-      return new Date(b.closedAt).getTime() - new Date(a.closedAt).getTime()
-    })
-
-    const worksheet = xlsx.utils.json_to_sheet(
-      issuesOrderedDesc.map((item: GithubProjectDataIssueNode) => {
-        const days = differenceInDays(new Date(item.closedAt), new Date(item.createdAt))
-        const hours = differenceInHours(new Date(item.closedAt), new Date(item.createdAt)) % 24
-        const minutes = differenceInMinutes(new Date(item.closedAt), new Date(item.createdAt)) % 60
-
-        const timePassed = []
-        if (days > 0) timePassed.push(`${days} days`)
-        if (hours > 0) timePassed.push(`${hours} hours`)
-        if (minutes > 0) timePassed.push(`${minutes} minutes`)
-
-        return {
-          ['Issue Title']: item.title,
-          ['Assignee']: item.assignees.nodes[0]?.name ?? item.assignees.nodes[0]?.login ?? '---',
-          ['Effort']: item?.projectItems?.nodes?.[0]?.effort?.value ?? '---',
-          ['Impact']: item?.projectItems?.nodes?.[0]?.impact?.value ?? '---',
-          ['Issue Type']: item?.projectItems?.nodes?.[0]?.issueType?.value ?? '---',
-          ['Repo']: item?.repository?.name ?? '---',
-          ['Url']: item.url,
-          ['Created']: format(new Date(item.createdAt), 'yyyy-MM-dd'),
-          ['Closed']: format(new Date(item.closedAt), 'yyyy-MM-dd'),
-          ['Time to Close']: timePassed.join(', '),
-          state: item.state === 'CLOSED' ? 'Closed' : 'Open',
-        }
-      }),
-    )
-
-    xlsx.utils.book_append_sheet(workbook, worksheet, assignee)
-  })
-  await xlsx.writeFile(workbook, './src/bin/localCache/projectReportByAssignee.xlsx')
+  xlsx.utils.book_append_sheet(workbook, worksheet, 'Main')
+  await xlsx.writeFile(workbook, './src/bin/localCache/projectReport.xlsx')
 }
