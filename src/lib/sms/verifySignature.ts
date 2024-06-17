6c781d2ee8fc7c422d7ad41ad47251318eb93dca
Lucas Bernardi <lucas.bernardi@coinbase.com>
2024-06-17 13:41:05 -0300

    test: sms events webhook

---
+++ src/lib/sms/verifySignature.ts
@@ -0,0 +1,19 @@
+import twilio from 'twilio'
+
+const authToken = process.env.TWILIO_AUTH_TOKEN
+
+export async function verifySignature(request: Request) {
+  if (!authToken) {
+    throw new Error('TWILIO_AUTH_TOKEN is not set')
+  }
+
+  const signature = request.headers.get('X-Twilio-Signature')
+  const url = new URL(request.url)
+  const params = new URLSearchParams(await request.text())
+
+  if (!signature) {
+    throw new Error('Missing verification headers')
+  }
+
+  return twilio.validateRequest(authToken, signature, url.href, params)
+}
