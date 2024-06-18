baec79c5e568e94ca150a008e806e9b9047adc86
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-06-18 13:55:52 -0300

    test: log incoming request

--- src/lib/sms/verifySignature.ts
+++ src/lib/sms/verifySignature.ts
@@ -7,6 +7,8 @@ const authToken = process.env.TWILIO_AUTH_TOKEN
 const logger = getLogger('verifySignature')
 
 export async function verifySignature(request: Request) {
+  logger.info(request)
+
   if (!authToken) {
     throw new Error('TWILIO_AUTH_TOKEN is not set')
   }
