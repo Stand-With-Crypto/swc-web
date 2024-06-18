f84f38caa39d29d7b99b5bbf35d7397c002d9830
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-06-18 14:44:33 -0300

    test: add request body to log

--- src/lib/sms/verifySignature.ts
+++ src/lib/sms/verifySignature.ts
@@ -7,14 +7,15 @@ const authToken = process.env.TWILIO_AUTH_TOKEN
 const logger = getLogger('verifySignature')
 
 export async function verifySignature(request: Request) {
-  logger.info(JSON.stringify(request))
-
   if (!authToken) {
     throw new Error('TWILIO_AUTH_TOKEN is not set')
   }
 
   const signature = request.headers.get('X-Twilio-Signature')
   const params = Object.fromEntries(new URL(request.url).searchParams)
+  const body = await request.text()
+  const bodyParams = Object.fromEntries(new URLSearchParams(body))
+  const formData = await request.formData()
   const url = request.url
 
   if (!signature) {
@@ -25,8 +26,11 @@ export async function verifySignature(request: Request) {
     signature,
     url,
     params,
-    expectedSignature: twilio.getExpectedTwilioSignature(authToken, url, params),
+    expectedSignature: twilio.getExpectedTwilioSignature(authToken, url, bodyParams),
+    bodyParams,
+    body,
+    formData,
   })
 
-  return twilio.validateRequest(authToken, signature, url, params)
+  return twilio.validateRequest(authToken, signature, url, bodyParams)
 }
