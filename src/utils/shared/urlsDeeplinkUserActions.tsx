4c7ab8e2b2beda46d1429a70dfc8473c5fd4d048
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-06-20 14:02:40 -0400

    CNN Email improvements (#997)
    * chore: updates cnn email constant
    
    * feat: shows campaigns on internal deeplinks page

--- src/utils/shared/urlsDeeplinkUserActions.tsx
+++ src/utils/shared/urlsDeeplinkUserActions.tsx
@@ -3,12 +3,24 @@ import { UserActionType } from '@prisma/client'
 import { SupportedLocale } from '@/intl/locales'
 import { ActiveClientUserActionType } from '@/utils/shared/activeUserAction'
 import { getIntlPrefix } from '@/utils/shared/urls'
+import {
+  USER_ACTION_TO_CAMPAIGN_NAME_DEFAULT_MAP,
+  UserActionCampaigns,
+  UserActionEmailCampaignName,
+} from '@/utils/shared/userActionCampaigns'
 
 const parseQueryString = (queryString?: string) => {
   if (!queryString) return ''
   if (queryString.startsWith('?')) return queryString
   return `?${queryString}`
 }
+
+export type DeeplinkConfig = {
+  locale: SupportedLocale
+  queryString?: string
+}
+
+type DeeplinkFunction = (config: DeeplinkConfig) => string
 
 export const USER_ACTION_DEEPLINK_MAP: Omit<
   {
@@ -49,5 +61,43 @@ export const USER_ACTION_DEEPLINK_MAP: Omit<
     },
   },
 }
-
 export type UserActionTypesWithDeeplink = keyof typeof USER_ACTION_DEEPLINK_MAP
+
+export const USER_ACTION_WITH_CAMPAIGN_DEEPLINK_MAP: {
+  [key in ActiveClientUserActionType]?: {
+    [campaign in UserActionCampaigns[key]]?: DeeplinkFunction
+  }
+} = {
+  [UserActionType.EMAIL]: {
+    [UserActionEmailCampaignName.CNN_PRESIDENTIAL_DEBATE_2024]: ({ locale }) => {
+      return `${getIntlPrefix(locale)}/action/email-cnn`
+    },
+  },
+}
+
+export type UserActionWithCampaignDeeplink =
+  (typeof USER_ACTION_WITH_CAMPAIGN_DEEPLINK_MAP)[UserActionTypesWithDeeplink]
+
+type GetUserActionDeeplinkArgs<ActionType extends UserActionTypesWithDeeplink> = {
+  actionType: ActionType
+  config: DeeplinkConfig
+  campaign?: UserActionCampaigns[ActionType]
+}
+
+export const getUserActionDeeplink = <
+  ActionType extends UserActionTypesWithDeeplink = UserActionTypesWithDeeplink,
+>({
+  actionType,
+  config,
+  campaign,
+}: GetUserActionDeeplinkArgs<ActionType>) => {
+  if (!campaign || campaign === USER_ACTION_TO_CAMPAIGN_NAME_DEFAULT_MAP[actionType]) {
+    return USER_ACTION_DEEPLINK_MAP[actionType].getDeeplinkUrl(config)
+  }
+
+  if (USER_ACTION_WITH_CAMPAIGN_DEEPLINK_MAP[actionType]?.[campaign]) {
+    return USER_ACTION_WITH_CAMPAIGN_DEEPLINK_MAP[actionType]?.[campaign]?.(config)
+  }
+
+  throw new Error(`No deeplink found for actionType: ${actionType} and campaign: ${campaign}`)
+}
