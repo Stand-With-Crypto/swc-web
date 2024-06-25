881d7957432fc9510059da8d4868cb33d21f8b1e
Matheus Mendes <matheushenrique.araujomendes@coinbase.com>
2024-06-25 22:55:16 +0100

    feat: cnn debate page metadata

--- src/app/[locale]/events/cnn-debate/page.tsx
+++ src/app/[locale]/events/cnn-debate/page.tsx
@@ -1,4 +1,20 @@
+import { Metadata } from 'next'
+
 import { CNNDebateEventPage } from '@/components/app/pageEvents/CNNDebate'
+import { generateMetadataDetails } from '@/utils/server/metadataUtils'
+
+export const dynamic = 'force-static'
+
+const title = 'Presidential debate watch parties'
+const description =
+  'Crypto advocates are hosting watch parties across the country to make sure our Presidential candidates stand with crypto.'
+
+export const metadata: Metadata = {
+  ...generateMetadataDetails({
+    title,
+    description,
+  }),
+}
 
 export default function CNNDebatePage() {
   return <CNNDebateEventPage />
