8d5359149a38361785c2eb164512e03a8eb6aca6
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-06-25 12:15:58 -0400

    Changes caching logic from route to fetch req in all dtsi api route (#1004)
    * feat: changes cachign from route to fetch req
    
    * fix: spreading props

--- src/app/api/public/dtsi/all-people/route.ts
+++ src/app/api/public/dtsi/all-people/route.ts
@@ -3,10 +3,9 @@ import 'server-only'
 import { NextResponse } from 'next/server'
 
 import { queryDTSIAllPeople } from '@/data/dtsi/queries/queryDTSIAllPeople'
-import { SECONDS_DURATION } from '@/utils/shared/seconds'
 
 export const dynamic = 'error'
-export const revalidate = SECONDS_DURATION['10_MINUTES']
+export const revalidate = 0
 
 export async function GET() {
   const data = await queryDTSIAllPeople()
