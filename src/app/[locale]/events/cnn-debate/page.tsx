bdfe298bbd188ad9dee69a0ea9a8996aa0dcd819
Matheus Mendes <matheushenrique.araujomendes@coinbase.com>
2024-06-26 12:02:22 +0100

    fix: page title equal to page header

--- src/app/[locale]/events/cnn-debate/page.tsx
+++ src/app/[locale]/events/cnn-debate/page.tsx
@@ -5,7 +5,7 @@ import { generateMetadataDetails } from '@/utils/server/metadataUtils'
 
 export const dynamic = 'force-static'
 
-const title = 'Presidential debate watch parties'
+const title = 'Presidential Debate watch parties'
 const description =
   'Crypto advocates are hosting watch parties across the country to make sure our Presidential candidates stand with crypto.'
 
