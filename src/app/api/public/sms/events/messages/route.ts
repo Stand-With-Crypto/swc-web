5356036cc70c2100ec7df67407d363e834de5a16
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-06-26 10:02:45 -0300

    feat: reply to incoming messages with twiml

--- src/app/api/public/sms/events/messages/route.ts
+++ src/app/api/public/sms/events/messages/route.ts
@@ -1,10 +1,21 @@
 import 'server-only'
 
+import { SMSStatus } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
+import twilio from 'twilio'
 
+import { prismaClient } from '@/utils/server/prismaClient'
 import { getLogger } from '@/utils/shared/logger'
+import { requiredEnv } from '@/utils/shared/requiredEnv'
 
 import { verifySignature } from '@/lib/sms'
+import * as messages from '@/lib/sms/messages'
+
+const SWC_STOP_SMS_KEYWORD = requiredEnv(process.env.SWC_STOP_SMS_KEYWORD, 'SWC_STOP_SMS_KEYWORD')
+const SWC_UNSTOP_SMS_KEYWORD = requiredEnv(
+  process.env.SWC_UNSTOP_SMS_KEYWORD,
+  'SWC_UNSTOP_SMS_KEYWORD',
+)
 
 const logger = getLogger('sms-events')
 
@@ -44,7 +55,58 @@ export async function POST(request: NextRequest) {
     )
   }
 
-  logger.info(JSON.stringify(body))
+  const response = new twilio.twiml.MessagingResponse()
 
-  return NextResponse.json({ ok: true })
+  logger.info('body', JSON.stringify(body))
+
+  switch (body.Body.toUpperCase()) {
+    case 'STOP':
+      await optOutUser(body.From)
+      response.message(messages.GOODBYE_MESSAGE)
+      break
+    case SWC_STOP_SMS_KEYWORD:
+      await optOutUser(body.From)
+      response.message(messages.GOODBYE_MESSAGE)
+      break
+    case 'YES':
+    case 'START':
+    case 'CONTINUE':
+    case SWC_UNSTOP_SMS_KEYWORD:
+      await prismaClient.user.updateMany({
+        data: {
+          smsStatus: SMSStatus.OPTED_IN,
+        },
+        where: {
+          phoneNumber: body.From,
+        },
+      })
+      response.message('')
+      break
+    default:
+      response.message('')
+  }
+
+  logger.info('response', response.toString())
+
+  const headers = new Headers()
+  headers.set('Content-Type', 'text/xml')
+
+  return new Response(response.toString(), {
+    headers,
+    status: 200,
+  })
+}
+
+async function optOutUser(phoneNumber: string) {
+  await prismaClient.user.updateMany({
+    data: {
+      smsStatus: SMSStatus.OPTED_OUT,
+    },
+    where: {
+      phoneNumber,
+    },
+  })
+
+  // TODO: create communication journey
+  // TODO: create communication
 }
--- src/lib/sms/messages.ts
+++ src/lib/sms/messages.ts
@@ -1,1 +1,3 @@
 export const WELCOME_MESSAGE = `Thanks for subscribing to Stand With Crypto. Msg & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`
+
+export const GOODBYE_MESSAGE = `<goodbye message>Reply HELP for help or CONTINUE to opt back in.`
