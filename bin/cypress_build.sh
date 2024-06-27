77f210a6b77b9179de8c18e84ec158cb20fa367f
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-06-27 18:17:31 -0300

    test: increase memory allocation to 6gb

--- bin/cypress_build.sh
+++ bin/cypress_build.sh
@@ -4,4 +4,4 @@ npx prisma generate
 npm run codegen
 npm run intl:extract-compile
 SEED_SIZE=SM npm run db:seed
-NODE_OPTIONS="--max-old-space-size=4096" npm run build
+NODE_OPTIONS="--max-old-space-size=6144" npm run build
