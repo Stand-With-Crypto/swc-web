0c369130b61e4cf0497ca804b5a0f9ccdbe436d1
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-07-08 09:56:40 -0300

    chore: add comment explaining placeholder messages

--- src/utils/server/sms/messages.ts
+++ src/utils/server/sms/messages.ts
@@ -1,8 +1,9 @@
-const SWC_STOP_SMS_KEYWORD = process.env.SWC_STOP_SMS_KEYWORD
+// This is just a placeholder, messages will be changed later.
+
 const SWC_UNSTOP_SMS_KEYWORD = process.env.SWC_UNSTOP_SMS_KEYWORD
 
-export const WELCOME_MESSAGE = `Thanks for subscribing to Stand With Crypto. Msg & data rates may apply. Message frequency varies. Reply HELP for help or ${SWC_STOP_SMS_KEYWORD ?? 'STOP'} to opt out.`
+export const WELCOME_MESSAGE = `Thanks for subscribing to Stand With Crypto. Msg & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`
 
 export const GOODBYE_MESSAGE = `<goodbye message>. Reply HELP for help or ${SWC_UNSTOP_SMS_KEYWORD ?? 'CONTINUE'} to opt back in.`
 
-export const UNSTOP_CONFIRMATION_MESSAGE = `<unstop confirmation message>. Reply HELP for help or ${SWC_STOP_SMS_KEYWORD ?? 'STOP'} to opt out.`
+export const UNSTOP_CONFIRMATION_MESSAGE = `<unstop confirmation message>. Reply HELP for help or STOP to opt out.`
