4624e8feb90a804675ef6dd9b1fbfec440db4597
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-07-09 14:40:52 -0400

    Creates the `SAB 121 veto override` vote banner (#1050)
    * feat: creates the `SAB 121 veto override` vote banner
    
    * feat: animates close button on mobile
    
    * refactor: removes unnecessary condition
    
    * feat: removes date logic and banner height
    
    * fix: lint

--- src/components/app/navbar/index.tsx
+++ src/components/app/navbar/index.tsx
@@ -1,8 +1,10 @@
 'use client'
 
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
+import Balancer from 'react-wrap-balancer'
+import { AnimatePresence, motion } from 'framer-motion'
 import { capitalize } from 'lodash-es'
-import { Menu } from 'lucide-react'
+import { Menu, X } from 'lucide-react'
 
 import { LoginDialogWrapper } from '@/components/app/authentication/loginDialogWrapper'
 import { NavbarLoggedInButton } from '@/components/app/navbar/navbarLoggedInButton'
@@ -56,6 +58,8 @@ export function Navbar({ locale }: { locale: SupportedLocale }) {
     }
   }, [dialogProps])
 
+  const [isSAB121ContentOpen, setIsSAB121ContentOpen] = useState(false)
+
   const hasEnvironmentBar = NEXT_PUBLIC_ENVIRONMENT !== 'production'
   const loginButton = (
     <LoginDialogWrapper
@@ -83,6 +87,69 @@ export function Navbar({ locale }: { locale: SupportedLocale }) {
           </div>
         </div>
       )}
+
+      <div className="relative bg-primary-cta py-4 text-center">
+        <div className="container flex flex-col items-center text-sm text-background antialiased max-sm:text-center sm:text-base">
+          <div className="flex flex-col items-center justify-between gap-4 lg:flex-row">
+            <p>
+              <b>KEY VOTE:</b> SAB 121 VETO OVERRIDE
+            </p>
+
+            <Button
+              className={cn('max-sm:w-full lg:block', isSAB121ContentOpen && 'hidden')}
+              onClick={() => setIsSAB121ContentOpen(oldState => !oldState)}
+              size="sm"
+              variant="secondary"
+            >
+              {isSAB121ContentOpen ? <X className="h-4 w-4" /> : 'Learn More'}
+            </Button>
+          </div>
+
+          <AnimatePresence>
+            {isSAB121ContentOpen && (
+              <motion.div
+                animate={{ opacity: 1, height: 'auto', paddingTop: '1rem' }}
+                exit={{ opacity: 0, height: 0, paddingTop: 0 }}
+                initial={{ opacity: 0, height: 0, paddingTop: 0 }}
+                transition={{ ease: 'easeInOut' }}
+              >
+                <p className="text-sm font-normal tracking-normal">
+                  <Balancer>
+                    The House is voting on whether to override President Biden's veto of SAB 121, a
+                    bipartisan bill that allows banks to custody digital assets like they would
+                    other assets.
+                  </Balancer>
+                </p>
+                <p className="text-sm font-normal tracking-normal">
+                  <Balancer>
+                    In order to override the veto, the House needs a 2/3rds majority.
+                  </Balancer>
+                </p>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
+          <AnimatePresence>
+            {isSAB121ContentOpen && (
+              <motion.div
+                animate={{ opacity: 1 }}
+                className="mt-2 lg:hidden"
+                exit={{ opacity: 0 }}
+                initial={{ opacity: 0 }}
+                transition={{ delay: 0.1, ease: 'easeIn' }}
+              >
+                <Button
+                  onClick={() => setIsSAB121ContentOpen(oldState => !oldState)}
+                  size="sm"
+                  variant="secondary"
+                >
+                  <X className="h-4 w-4" />
+                </Button>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+      </div>
 
       <nav
         className={
