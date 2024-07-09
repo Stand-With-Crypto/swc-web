63a17b0306a8eabe57ef8b217e48b2437c49857b
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-07-09 14:46:41 -0300

    refactor: removes unnecessary condition

--- src/components/app/navbar/index.tsx
+++ src/components/app/navbar/index.tsx
@@ -147,7 +147,7 @@ export function Navbar({ locale }: { locale: SupportedLocale }) {
                     size="sm"
                     variant="secondary"
                   >
-                    {isSAB121ContentOpen ? <X className="h-4 w-4" /> : 'Learn More'}
+                    <X className="h-4 w-4" />
                   </Button>
                 </motion.div>
               )}
