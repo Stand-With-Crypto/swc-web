00353853260f645a7d1e18af7455a42e9507bb41
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-07-16 18:15:14 -0300

    fix: remove twilio's phone number validation

--- src/inngest/functions/sms/shared/validatePhoneNumber.ts
+++ src/inngest/functions/sms/shared/validatePhoneNumber.ts
@@ -1,6 +1,5 @@
 import { NonRetriableError } from 'inngest'
 
-import { messagingClient } from '@/utils/server/sms'
 import { PHONE_NUMBER_REGEX } from '@/utils/shared/phoneNumber'
 
 export async function validatePhoneNumber(phoneNumber: string) {
@@ -16,18 +15,4 @@ export async function validatePhoneNumber(phoneNumber: string) {
   if (!phoneNumber.startsWith('+1')) {
     throw new NonRetriableError('Phone number not from US or CA')
   }
-
-  const lookup = await messagingClient.lookups.v2.phoneNumbers(phoneNumber).fetch()
-
-  if (!lookup.valid) {
-    throw new NonRetriableError(`Invalid phone number: ${errorStr(lookup.validationErrors)}`)
-  }
-}
-
-function errorStr(errors: string[]) {
-  return errors
-    .map(err => {
-      return err.replaceAll('_', ' ').toLowerCase()
-    })
-    .join(', ')
 }
