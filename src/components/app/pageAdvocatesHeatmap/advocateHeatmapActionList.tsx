6c59233ae3ea1d7dd915da32a084b29b6812f221
Olavo Parno <olavoparno@gmail.com>
2024-07-17 13:20:05 -0300

    style: adjust className with cn

--- src/components/app/pageAdvocatesHeatmap/advocateHeatmapActionList.tsx
+++ src/components/app/pageAdvocatesHeatmap/advocateHeatmapActionList.tsx
@@ -2,13 +2,19 @@
 
 import { ADVOCATES_ACTIONS } from '@/components/app/pageAdvocatesHeatmap/constants'
 import { useIsMobile } from '@/hooks/useIsMobile'
+import { cn } from '@/utils/web/cn'
 
 export function AdvocateHeatmapActionList({ isEmbedded }: { isEmbedded?: boolean }) {
   const isMobile = useIsMobile()
 
   return (
     <div
-      className={`flex  flex-row justify-around gap-3  md:justify-between ${isEmbedded ? 'w-full md:w-[auto] md:flex-col' : 'w-full items-center rounded-[24px] bg-[#FBF8FF] px-10 py-8'}`}
+      className={cn(
+        'flex  flex-row justify-around gap-3  md:justify-between',
+        isEmbedded
+          ? 'w-full md:w-[auto] md:flex-col'
+          : 'w-full items-center rounded-[24px] bg-[#FBF8FF] px-10 py-8',
+      )}
     >
       {!isEmbedded && <strong>Key</strong>}
       {Object.entries(ADVOCATES_ACTIONS).map(([key, action]) => {
@@ -16,7 +22,10 @@ export function AdvocateHeatmapActionList({ isEmbedded }: { isEmbedded?: boolean
 
         return (
           <div
-            className={`flex flex-col items-center gap-3 font-sans text-base md:flex-row ${isEmbedded ? 'text-white' : 'text-black'}`}
+            className={cn(
+              'flex flex-col items-center gap-3 font-sans text-base md:flex-row',
+              isEmbedded ? 'text-white' : 'text-black',
+            )}
             key={key}
           >
             <ActionIcon className="w-8 md:w-10" />
