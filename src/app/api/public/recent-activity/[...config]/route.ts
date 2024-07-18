a48444631f5d25b44bb444c2aca2dfcbc3459168
Olavo Parno <olavoparno@gmail.com>
2024-07-18 12:41:41 -0300

    refactor: adjust zod params for recent activity

--- src/app/api/public/recent-activity/[...config]/route.ts
+++ src/app/api/public/recent-activity/[...config]/route.ts
@@ -10,14 +10,15 @@ export const dynamic = 'error'
 export const revalidate = SECONDS_DURATION['30_SECONDS']
 
 const zodParams = z.object({
-  config: z.array(z.string().pipe(z.coerce.number().int().gte(0).lt(100)), z.string().optional()),
+  config: z.tuple([z.string().pipe(z.coerce.number().int().gte(0).lt(100)), z.string().optional()]),
 })
 
 export async function GET(
   _request: NextRequest,
   { params }: { params: { config: [string, string?] } },
 ) {
-  const [limit, restrictToUS] = zodParams.parse(params).config
+  const { config } = zodParams.parse(params)
+  const [limit, restrictToUS] = config
   const data = await getPublicRecentActivity({
     limit,
     restrictToUS: !!restrictToUS,
