5df53b2d82c261abf829c9b217200a658750e078
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-07-22 17:41:03 -0300

    fix: update bulk sms query to fetch only valid phone numbers

--- src/inngest/functions/sms/bulkSMSCommunicationJourney.ts
+++ src/inngest/functions/sms/bulkSMSCommunicationJourney.ts
@@ -320,6 +320,7 @@ async function getPhoneNumberList(
     by: ['phoneNumber', 'datetimeCreated'],
     where: {
       ...args.where,
+      hasValidPhoneNumber: true,
       smsStatus: {
         in: [
           SMSStatus.OPTED_IN,
