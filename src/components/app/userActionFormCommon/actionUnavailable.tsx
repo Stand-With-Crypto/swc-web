fed2b50c50ba312104b97ee9910080efd26b50e6
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2024-07-22 16:44:55 -0300

    Add analytics to UserActionUnavailable

--- src/components/app/userActionFormCommon/actionUnavailable.tsx
+++ src/components/app/userActionFormCommon/actionUnavailable.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react'
+
 import { Button } from '@/components/ui/button'
 import { InternalLink } from '@/components/ui/link'
 import { PageSubTitle } from '@/components/ui/pageSubTitle'
 import { PageTitle } from '@/components/ui/pageTitleText'
+import { useEffectOnce } from '@/hooks/useEffectOnce'
+import { AnalyticActionType, AnalyticComponentType } from '@/utils/shared/sharedAnalytics'
+import { trackClientAnalytic } from '@/utils/web/clientAnalytics'
+import { trackPrimitiveComponentAnalytics } from '@/utils/web/primitiveComponentAnalytics'
+
+const ANALYTICS_NAME_USER_ACTION_FORM_UNAVAILABLE = 'User Action Form Unavailable'
 
 interface UserActionFormActionUnavailableProps {
   onConfirm?: () => void
@@ -9,6 +17,19 @@ interface UserActionFormActionUnavailableProps {
 
 export const UserActionFormActionUnavailable = (props: UserActionFormActionUnavailableProps) => {
   const { onConfirm } = props
+
+  useEffectOnce(() => {
+    trackPrimitiveComponentAnalytics(
+      ({ properties }) => {
+        trackClientAnalytic('User Action Unavailable', {
+          action: AnalyticActionType.view,
+          component: AnalyticComponentType.text,
+          ...properties,
+        })
+      },
+      { args: undefined, analytics: ANALYTICS_NAME_USER_ACTION_FORM_UNAVAILABLE },
+    )
+  })
 
   return (
     <div className="flex min-h-[500px] flex-col items-center justify-center space-y-8">
