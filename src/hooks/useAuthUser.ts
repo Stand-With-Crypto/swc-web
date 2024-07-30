f29219d560d57c5380e00f67a13688b1c977cdf8
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2024-07-30 12:41:49 -0300

    fix: session error

--- src/hooks/useAuthUser.ts
+++ src/hooks/useAuthUser.ts
@@ -7,18 +7,21 @@ import { parseThirdwebAddress } from '@/hooks/useThirdwebAddress/parseThirdwebAd
 import { THIRDWEB_AUTH_TOKEN_COOKIE_PREFIX } from '@/utils/shared/thirdwebAuthToken'
 
 export function useThirdwebAuthUser() {
-  const connectionStatus = useActiveWalletConnectionStatus()
+  // We are not using the return value here because what we want is to trigger
+  // a rerender when the wallet connection status changes.
+  // This way, we can update the user data when the user connects or disconnects
+  useActiveWalletConnectionStatus()
+
+  const token = Cookies.get(THIRDWEB_AUTH_TOKEN_COOKIE_PREFIX)
 
   const { userId, address } = useMemo(() => {
-    const token = Cookies.get(THIRDWEB_AUTH_TOKEN_COOKIE_PREFIX)
     const decodedToken = token
       ? jwtDecode<{ ctx?: { userId?: string; address?: string } }>(token)
       : null
     const { userId, address } = decodedToken?.ctx ?? {}
 
     return { userId, address }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [connectionStatus])
+  }, [token])
 
   return {
     isLoggedIn: !!userId,
