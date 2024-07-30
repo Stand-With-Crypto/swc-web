1d1f2883cabc1319ac00704b66c8e189badc307d
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2024-07-30 16:57:00 -0300

    Remove profile visibility step from onboarding flow

--- src/components/app/updateUserProfileForm/index.tsx
+++ src/components/app/updateUserProfileForm/index.tsx
@@ -1,20 +1,16 @@
 'use client'
-import { useEffect, useState } from 'react'
-import { ArrowLeft } from 'lucide-react'
+import { useEffect } from 'react'
 
 import { ClientAddress } from '@/clientModels/clientAddress'
 import { SensitiveDataClientUserWithENSData } from '@/clientModels/clientUser/sensitiveDataClientUser'
 import { ANALYTICS_NAME_UPDATE_USER_PROFILE_FORM } from '@/components/app/updateUserProfileForm/constants'
 import { UpdateUserProfileFormExperimentTesting } from '@/components/app/updateUserProfileForm/step1'
-import { UpdateUserInformationVisibilityForm } from '@/components/app/updateUserProfileForm/step2'
-import { dialogButtonStyles } from '@/components/ui/dialog/styles'
 import { useSections } from '@/hooks/useSections'
 import { trackSectionVisible } from '@/utils/web/clientAnalytics'
-import { cn } from '@/utils/web/cn'
 
 enum Sections {
   Profile = 'Profile',
-  InformationVisibility = 'Information Visibility',
+  InformationVisibility = 'Information Visibility', // Currently not used
 }
 
 export function UpdateUserProfileFormContainer({
@@ -36,32 +32,9 @@ export function UpdateUserProfileFormContainer({
     })
   }, [])
 
-  // we need to leverage the data submitted in the first step in the second step (whether we show the option to use first/last name)
-  const [statefulUser, setStatefulUser] = useState(user)
-
   if (sections.currentSection === Sections.Profile) {
-    return (
-      <UpdateUserProfileFormExperimentTesting
-        onSuccess={newFields => {
-          setStatefulUser({ ...user, ...newFields })
-          sections.goToSection(Sections.InformationVisibility)
-        }}
-        user={user}
-      />
-    )
+    return <UpdateUserProfileFormExperimentTesting onSuccess={onSuccess} user={user} />
   }
-  if (sections.currentSection === Sections.InformationVisibility) {
-    return (
-      <>
-        <div
-          className={cn('left-2', dialogButtonStyles)}
-          onClick={() => sections.goToSection(Sections.Profile)}
-          role="button"
-        >
-          <ArrowLeft size={20} />
-        </div>
-        <UpdateUserInformationVisibilityForm onSuccess={onSuccess} user={statefulUser} />
-      </>
-    )
-  }
+
+  return null
 }
