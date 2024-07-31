ffebe3c9ac36dc64d8a79fb5c1bfd580a52d9180
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-07-31 13:57:49 -0300

    fix: conflict when using sendgrid prod on local env

--- src/utils/server/email/index.ts
+++ src/utils/server/email/index.ts
@@ -1,6 +1,6 @@
 export type { EmailEvent } from './constants'
 export { EmailEventName, EVENT_NAME_TO_HUMAN_READABLE_STRING } from './constants'
 export type { SendMailPayload } from './sendMail'
-export { sendMail, sendMultipleMails } from './sendMail'
+export { sendMail } from './sendMail'
 export { parseEventsWebhookRequest } from './utils/parseEventsWebhookRequest'
 export { verifySignature } from './utils/verifySignature'
