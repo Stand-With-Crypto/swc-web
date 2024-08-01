4811ec80cac97a66b1efa9507b02f110706ab7d5
Olavo Parno <olavo.parno@coinbase.com>
2024-08-01 15:04:43 -0300

    style: adjust legalese font size and dialog maxh

--- src/components/app/authentication/thirdwebLoginContent.tsx
+++ src/components/app/authentication/thirdwebLoginContent.tsx
@@ -72,7 +72,7 @@ export function ThirdwebLoginContent({
   const LegaleseDisclaimer =
     currentExperiment === 'variant'
       ? () => (
-          <span className="text-[9px]">
+          <span className="text-[10px]">
             By signing up with my phone number, you consent to receive recurring texts from Stand
             with Crypto. You can reply STOP to stop receiving texts. Message and data rates may
             apply. You understand that Stand With Crypto and its vendors may collect and use your
