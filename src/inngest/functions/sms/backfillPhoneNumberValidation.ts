411c1c8af5f37a327aeed408ad28e10477174755
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-08-05 18:15:23 -0300

    fix: remove unnecessary logic

--- src/inngest/functions/sms/backfillPhoneNumberValidation.ts
+++ src/inngest/functions/sms/backfillPhoneNumberValidation.ts
@@ -1,13 +1,10 @@
-import { Prisma } from '@prisma/client'
-import { parseISO } from 'date-fns'
+import { uniq } from 'lodash-es'
 
 import { inngest } from '@/inngest/inngest'
 import { prismaClient } from '@/utils/server/prismaClient'
-import { getLogger } from '@/utils/shared/logger'
+import { validatePhoneNumber } from '@/utils/shared/phoneNumber'
 
-import { fetchPhoneNumbers, flagInvalidPhoneNumbers, validatePhoneNumbers } from './utils'
-
-const logger = getLogger('backfill-phone-validation')
+import { flagInvalidPhoneNumbers } from './utils'
 
 const BACKFILL_PHONE_NUMBER_VALIDATION_INNGEST_FUNCTION_ID =
   'script.backfill-phone-number-validation'
@@ -32,91 +29,45 @@ export const backfillPhoneNumberValidation = inngest.createFunction(
   async ({ step, event }) => {
     const { persist } = event.data as BackfillPhoneNumberValidationPayload
 
-    let hasNumbersLest = true
-    let outerCursor: Date | undefined
-    let iterations = 0
-    let phoneNumbersValidated = 0
-    let totalInvalidPhoneNumbers = 0
-    while (hasNumbersLest) {
-      logger.info(`Iteration ${iterations}`)
-      logger.info(`Fetching...`)
-      // Fetches all phone numbers within the database query limit range
-      const [phoneNumberChunks, newCursor, length] = await step.run('fetching-phone-numbers', () =>
-        fetchPhoneNumbers(
-          (take, cursor = outerCursor) =>
-            getPhoneNumberList({
-              take,
-              where: {
-                datetimeCreated: {
-                  gt: cursor,
-                },
-              },
-            }),
-          {
-            // fetchPhoneNumbers wont exceed the query limit if we pass it as max length
-            maxLength: DATABASE_QUERY_LIMIT,
+    let allPhoneNumbers: string[] = []
+    let cursor: Date | undefined
+    let hasNumbersLeft = true
+
+    while (hasNumbersLeft) {
+      const phoneNumbers = await prismaClient.user.groupBy({
+        by: ['phoneNumber', 'datetimeCreated'],
+        where: {
+          datetimeCreated: {
+            gte: cursor,
           },
-        ),
-      )
-
-      outerCursor = parseISO(newCursor)
-      phoneNumbersValidated += length
-
-      logger.info(`Validating...`)
-
-      const invalidPhoneNumbers = await step.run('validating-phone-numbers', async () => {
-        let allInvalidPhoneNumbers: string[] = []
-
-        for (const phoneNumberBatch of phoneNumberChunks) {
-          const { invalid } = await validatePhoneNumbers(phoneNumberBatch)
-
-          allInvalidPhoneNumbers = allInvalidPhoneNumbers.concat(invalid)
-        }
-
-        return allInvalidPhoneNumbers
+          // By default phones are considered valid because they passed the basic validation
+          hasValidPhoneNumber: true,
+        },
+        take: DATABASE_QUERY_LIMIT,
+        orderBy: [{ datetimeCreated: 'asc' }, { phoneNumber: 'asc' }],
       })
 
-      logger.info(`Found ${invalidPhoneNumbers.length} invalid phone numbers`)
+      cursor = phoneNumbers.at(-1)?.datetimeCreated
+      allPhoneNumbers = allPhoneNumbers.concat(phoneNumbers.map(({ phoneNumber }) => phoneNumber))
 
-      totalInvalidPhoneNumbers += invalidPhoneNumbers.length
-
-      if (persist && invalidPhoneNumbers.length > 0) {
-        logger.info('Persisting...')
-
-        await step.run('updating-invalid-phone-numbers', () =>
-          flagInvalidPhoneNumbers(invalidPhoneNumbers),
-        )
+      if (!DATABASE_QUERY_LIMIT || phoneNumbers.length < DATABASE_QUERY_LIMIT) {
+        hasNumbersLeft = false
       }
-
-      logger.info(`Finished iteration ${iterations}`)
-
-      if ((DATABASE_QUERY_LIMIT && length < DATABASE_QUERY_LIMIT) || !DATABASE_QUERY_LIMIT) {
-        hasNumbersLest = false
-      }
-
-      iterations += 1
     }
 
-    return {
-      iterations,
-      phoneNumbersValidated,
-      totalInvalidPhoneNumbers,
+    // Using uniq here to not send multiple messages to the same phone number
+    const uniquePhoneNumbers = uniq(allPhoneNumbers)
+
+    const invalidPhoneNumbers = uniquePhoneNumbers.filter(
+      phoneNumber => !validatePhoneNumber(phoneNumber),
+    )
+
+    if (persist) {
+      await step.run('flag-invalid-phone-numbers', () =>
+        flagInvalidPhoneNumbers(invalidPhoneNumbers),
+      )
     }
+
+    return `Found ${invalidPhoneNumbers.length} invalid phone numbers`
   },
 )
-
-async function getPhoneNumberList(args?: Omit<Prisma.UserGroupByArgs, 'by' | 'orderBy'>) {
-  return prismaClient.user.groupBy({
-    ...args,
-    by: ['phoneNumber', 'datetimeCreated'],
-    where: {
-      ...args?.where,
-      // By default phones are considered valid because they passed the basic validation
-      hasValidPhoneNumber: true,
-      phoneNumber: {
-        not: '',
-      },
-    },
-    orderBy: [{ datetimeCreated: 'asc' }, { phoneNumber: 'asc' }],
-  })
-}
