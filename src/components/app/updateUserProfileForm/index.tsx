5a8d630e2a0bb0bb50d61069f103e9f078690d69
eduardopicolo-cb <167582510+eduardopicolo-cb@users.noreply.github.com>
2024-08-06 12:33:52 -0300

    Fix profile form showing stale data after submit (#1144)
    * Fix profile form showing stale data after submit
    
    * Update src/hooks/useSections/index.tsx
    
    Co-authored-by: Lucas Bernardi <45859300+LucasdRossi@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lucas Bernardi <45859300+LucasdRossi@users.noreply.github.com>

--- src/components/app/updateUserProfileForm/index.tsx
+++ src/components/app/updateUserProfileForm/index.tsx
@@ -1,14 +1,20 @@
 'use client'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { ArrowLeft } from 'lucide-react'
+import { useSWRConfig } from 'swr'
 
 import { ClientAddress } from '@/clientModels/clientAddress'
 import { SensitiveDataClientUserWithENSData } from '@/clientModels/clientUser/sensitiveDataClientUser'
 import { ANALYTICS_NAME_UPDATE_USER_PROFILE_FORM } from '@/components/app/updateUserProfileForm/constants'
 import { UpdateUserProfileFormExperimentTesting } from '@/components/app/updateUserProfileForm/step1'
+import { UpdateUserInformationVisibilityForm } from '@/components/app/updateUserProfileForm/step2'
+import { dialogButtonStyles } from '@/components/ui/dialog/styles'
 import { useSections } from '@/hooks/useSections'
+import { apiUrls } from '@/utils/shared/urls'
 import { trackSectionVisible } from '@/utils/web/clientAnalytics'
+import { cn } from '@/utils/web/cn'
 
-enum Sections {
+export enum UserProfileFormSections {
   Profile = 'Profile',
   InformationVisibility = 'Information Visibility', // Currently not used
 }
@@ -16,24 +22,58 @@ enum Sections {
 export function UpdateUserProfileFormContainer({
   user,
   onSuccess,
+  skipSections,
 }: {
   user: SensitiveDataClientUserWithENSData & { address: ClientAddress | null }
   onSuccess: () => void
+  skipSections?: UserProfileFormSections[]
 }) {
   const sections = useSections({
-    sections: [Sections.Profile, Sections.InformationVisibility],
-    initialSectionId: Sections.Profile,
+    sections: [UserProfileFormSections.Profile, UserProfileFormSections.InformationVisibility],
+    initialSectionId: UserProfileFormSections.Profile,
     analyticsName: ANALYTICS_NAME_UPDATE_USER_PROFILE_FORM,
+    skipSections,
   })
   useEffect(() => {
     trackSectionVisible({
       sectionGroup: ANALYTICS_NAME_UPDATE_USER_PROFILE_FORM,
-      section: Sections.Profile,
+      section: UserProfileFormSections.Profile,
     })
   }, [])
 
-  if (sections.currentSection === Sections.Profile) {
-    return <UpdateUserProfileFormExperimentTesting onSuccess={onSuccess} user={user} />
+  const { mutate } = useSWRConfig()
+
+  // we need to leverage the data submitted in the first step in the second step (whether we show the option to use first/last name)
+  const [statefulUser, setStatefulUser] = useState(user)
+
+  if (sections.currentSection === UserProfileFormSections.Profile) {
+    return (
+      <UpdateUserProfileFormExperimentTesting
+        onSuccess={newFields => {
+          setStatefulUser({ ...user, ...newFields })
+          void mutate(apiUrls.userFullProfileInfo())
+          sections.goToSection(UserProfileFormSections.InformationVisibility)
+          if (skipSections?.includes(UserProfileFormSections.InformationVisibility)) {
+            onSuccess()
+          }
+        }}
+        user={user}
+      />
+    )
+  }
+  if (sections.currentSection === UserProfileFormSections.InformationVisibility) {
+    return (
+      <>
+        <div
+          className={cn('left-2', dialogButtonStyles)}
+          onClick={() => sections.goToSection(UserProfileFormSections.Profile)}
+          role="button"
+        >
+          <ArrowLeft size={20} />
+        </div>
+        <UpdateUserInformationVisibilityForm onSuccess={onSuccess} user={statefulUser} />
+      </>
+    )
   }
 
   return null
