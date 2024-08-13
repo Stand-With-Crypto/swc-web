3bd7bb6d64335501a72722a4ab63f3e681549b11
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2024-08-13 18:30:12 -0300

    Refactor thirwebRPCClient

--- src/utils/server/thirdweb/thirdwebRPCClients.ts
+++ src/utils/server/thirdweb/thirdwebRPCClients.ts
@@ -1,5 +1,8 @@
+import { createThirdwebClient } from 'thirdweb'
+import { viemAdapter } from 'thirdweb/adapters/viem'
+import { base } from 'thirdweb/chains'
 import { createPublicClient, http } from 'viem'
-import { base, mainnet } from 'viem/chains'
+import { mainnet } from 'viem/chains'
 
 import { THIRD_WEB_CLIENT_SECRET } from '@/utils/server/thirdweb/thirdwebClientSecret'
 
@@ -15,14 +18,11 @@ export const thirdwebRPCClient = createPublicClient({
   }),
 })
 
-export const thirdwebBaseRPCClient = createPublicClient({
+export const thirdwebServerClient = createThirdwebClient({
+  secretKey: THIRD_WEB_CLIENT_SECRET,
+})
+
+export const thirdwebBaseRPCClient = viemAdapter.publicClient.toViem({
+  client: thirdwebServerClient,
   chain: base,
-  transport: http('https://base.rpc.thirdweb.com', {
-    batch: {
-      wait: 100,
-    },
-    fetchOptions: {
-      headers: { 'x-secret-key': THIRD_WEB_CLIENT_SECRET },
-    },
-  }),
 })
