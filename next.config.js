492a0f9adc0b43065216639b0918a7f97d704e28
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-08-16 18:55:43 -0300

    fix: redirect

--- next.config.js
+++ next.config.js
@@ -337,6 +337,12 @@ const nextConfig = {
         permanent: false,
       },
       {
+        source: '/az/1./n/nThank',
+        destination:
+          'https://americalovescryptoaz.splashthat.com/?utm_source=swc&utm_medium=sms&utm_campaign=az_1&utm_id=sst&utm_content=v1',
+        permanent: false,
+      },
+      {
         source: '/az/2',
         destination:
           'https://americalovescryptoaz.splashthat.com/?utm_source=swc&utm_medium=sms&utm_campaign=az_1&utm_id=sst&utm_content=v2',
