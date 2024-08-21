71f64538c71dddafc9eaa573dcf2e63d34809c5e
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-08-21 13:28:46 -0300

    fix: remove unnecessary async

--- src/utils/server/sms/utils/isPhoneNumberSupported.ts
+++ src/utils/server/sms/utils/isPhoneNumberSupported.ts
@@ -2,7 +2,7 @@ import parsePhoneNumber, { CountryCode } from 'libphonenumber-js'
 
 const supportedCountries: CountryCode[] = ['US', 'CA']
 
-export const isPhoneNumberSupported = async (phoneNumber: string) => {
+export const isPhoneNumberSupported = (phoneNumber: string) => {
   const parsedPhoneNumber = parsePhoneNumber(phoneNumber)
 
   if (!parsedPhoneNumber?.country) {
