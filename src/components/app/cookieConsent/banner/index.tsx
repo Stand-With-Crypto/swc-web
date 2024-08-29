8ca04c2663a0b168fa747063922a4052b268a691
Olavo Parno <olavo.parno@coinbase.com>
2024-08-29 13:42:48 -0300

    refactor: small changes to cookies and privacy

--- src/components/app/cookieConsent/banner/index.tsx
+++ src/components/app/cookieConsent/banner/index.tsx
@@ -42,8 +42,9 @@ export function CookieConsentBanner({
           </button>
           <p className={cn('max-w-2xl text-justify text-xs text-muted-foreground md:text-left')}>
             We use our own and third-party cookies on our website to enhance your experience,
-            analyze traffic, and for security. You may opt out of the cookies that are not strictly
-            necessary by choosing from the below options. For more information, visit our{' '}
+            analyze traffic, for marketing, and for security. You may opt out of the cookies that
+            are not strictly necessary by choosing from the below options. For more information,
+            visit our{' '}
             <InternalLink className="underline" href={urls.privacyPolicy()}>
               Privacy Policy
             </InternalLink>
