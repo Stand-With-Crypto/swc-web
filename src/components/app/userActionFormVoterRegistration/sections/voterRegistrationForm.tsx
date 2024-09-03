132cda4b77fe647969506fe68710068b5b108011
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-09-03 11:05:43 -0300

    feat: rework the voter registration action flow

--- src/components/app/userActionFormVoterRegistration/sections/voterRegistrationForm.tsx
+++ src/components/app/userActionFormVoterRegistration/sections/voterRegistrationForm.tsx
@@ -1,13 +1,13 @@
-import { Dispatch, SetStateAction, useCallback, useMemo, useState } from 'react'
-import { ArrowUpRight } from 'lucide-react'
+import { Dispatch, SetStateAction, useCallback } from 'react'
 
 import { UserActionFormLayout } from '@/components/app/userActionFormCommon'
 import {
   REGISTRATION_URLS_BY_STATE,
+  RegistrationStatusAnswer,
   SectionNames,
 } from '@/components/app/userActionFormVoterRegistration/constants'
+import { useVoterRegistrationAction } from '@/components/app/userActionFormVoterRegistration/useVoterRegistrationAction'
 import { Button } from '@/components/ui/button'
-import { ExternalLink } from '@/components/ui/link'
 import {
   Select,
   SelectContent,
@@ -21,181 +21,119 @@ import {
   US_STATE_CODE_TO_DISPLAY_NAME_MAP,
   USStateCode,
 } from '@/utils/shared/usStateUtils'
+import { toastGenericError } from '@/utils/web/toastUtils'
 
 const STATE_CODES = Object.keys(US_STATE_CODE_TO_DISPLAY_NAME_MAP)
 
-const COPY = {
-  register: {
-    title: "Register to vote and get a free “I'm a Voter” NFT",
-    subtitle: 'Register now to be ready to vote in your state this year.',
-    step2: 'Go and register to vote',
-    step2Cta: 'Register',
-  },
-  checkRegistration: {
-    title: "Check your registration and get a free “I'm a Voter” NFT",
-    subtitle: 'Check your voter registration in your state this year.',
-    step2: 'Check voter registration',
-    step2Cta: 'Check',
-  },
-  registrationConfirm: {
-    title: 'Awesome, thank you for being registered',
+const COPY: Record<
+  RegistrationStatusAnswer,
+  {
+    title: string
+    subtitle: string
+    cta: string
+  }
+> = {
+  yes: {
+    title: 'Thanks for being registered!',
     subtitle: "Let's double check your information to make sure you have no issues voting.",
-    step2: 'Check voter registration',
-    step2Cta: 'Check',
+    cta: 'Check my registration status',
   },
-} as const
+  no: {
+    title: 'Register to vote',
+    subtitle: "You can register to vote online. It won't take more than 5 minutes.",
+    cta: 'Register to vote',
+  },
+  'not-sure': {
+    title: 'Check your registration status',
+    subtitle: "Make sure you're registered to vote in your state.",
+    cta: 'Check my registration status',
+  },
+}
 
 const WY_DISCLAIMER =
-  'Wyoming does not provide online voter registration checks. But you can still get the free NFT.'
+  'Wyoming does not provide online voter registration checks. But you can still complete the action.'
 const ND_DISCLAIMER =
-  'North Dakota does not require voter registration. But you can still get the free NFT.'
+  'North Dakota does not require voter registration. But you can still complete the action.'
 
-const LIST_ITEM_STYLE = 'flex flex-row items-center gap-4 py-4'
-
-function Step1Svg() {
-  return (
-    <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M7.86 4.84003L5.54 6.36003V4.15003L8.18 2.28003H10.45V13.73H7.86V4.84003Z"
-        fill="#5B616E"
-      />
-    </svg>
-  )
-}
-
-function Step2Svg() {
-  return (
-    <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M3.68 13.39L7.87 8.95995C8.97 7.78995 9.68 6.77995 9.68 5.73995C9.68 4.79995 9.15 4.18995 8.16 4.18995C7.15 4.18995 6.51 4.77995 6.34 6.28995H3.98C4.11 3.63995 5.71 2.19995 8.27 2.19995C10.8 2.19995 12.24 3.60995 12.24 5.66995C12.24 7.18995 11.36 8.43995 10.03 9.71995L7.87 11.83H12.32V13.8H3.68V13.39Z"
-        fill="#5B616E"
-      />
-    </svg>
-  )
-}
-
-function Step3Svg() {
-  return (
-    <svg fill="none" height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M3.53 10.08H5.87C5.97 11.18 6.56 11.99 7.93 11.99C9.23 11.99 9.83 11.37 9.83 10.34C9.83 9.25 9.22 8.68999 7.99 8.68999H7.1V6.96H7.93C9.13 6.96 9.67 6.42 9.67 5.44C9.67 4.62 9.14 4.02 8.09 4.02C7 4.02 6.33 4.68 6.2 5.75H3.92C4.05 3.65 5.54 2.12 8.15 2.12C10.66 2.12 12.13 3.43 12.13 5.22C12.13 6.5 11.38 7.29999 10.24 7.7C11.57 7.99 12.46 8.93 12.46 10.39C12.46 12.54 10.81 13.9 8.03 13.9C5.12 13.89 3.63 12.4 3.53 10.08Z"
-        fill="#5B616E"
-      />
-    </svg>
-  )
-}
-
-function disclaimer(stateCode: USStateCode | undefined) {
+function getDisclaimerByState(stateCode: USStateCode | undefined) {
   if (stateCode === 'ND') return ND_DISCLAIMER
   if (stateCode === 'WY') return WY_DISCLAIMER
-  return 'Complete registration at step 2 to claim NFT'
+  return null
 }
 
-interface VoterRegistrationFormProps extends UseSectionsReturn<SectionNames> {
-  checkRegistration?: boolean
-  registrationConfirm?: boolean
+export interface VoterRegistrationFormProps extends UseSectionsReturn<SectionNames> {
+  registrationAnswer: RegistrationStatusAnswer
   stateCode?: USStateCode
-  setStateCode: Dispatch<SetStateAction<USStateCode | undefined>>
+  onChangeStateCode: Dispatch<SetStateAction<USStateCode | undefined>>
 }
 
 export function VoterRegistrationForm({
-  checkRegistration,
-  registrationConfirm,
+  registrationAnswer = 'not-sure',
   goToSection,
+  goBackSection,
   stateCode,
-  setStateCode,
+  onChangeStateCode,
 }: VoterRegistrationFormProps) {
-  const [completeStep2, setCompleteStep2] = useState(false)
-
-  const { title, subtitle, step2, step2Cta } = useMemo(() => {
-    if (registrationConfirm) return COPY.registrationConfirm
-
-    return checkRegistration ? COPY.checkRegistration : COPY.register
-  }, [checkRegistration, registrationConfirm])
+  const { title, subtitle, cta } = COPY[registrationAnswer]
+  const { createAction, isCreatingAction } = useVoterRegistrationAction()
 
   const link = stateCode
     ? REGISTRATION_URLS_BY_STATE[stateCode][
-        checkRegistration ? 'checkRegistrationUrl' : 'registerUrl'
+        registrationAnswer === 'no' ? 'registerUrl' : 'checkRegistrationUrl'
       ]
     : undefined
 
   const handleOnValueChange = useCallback(
     (value: string) => {
       if (STATE_CODES.includes(value)) {
-        setCompleteStep2(false)
-        setStateCode(value as USStateCode)
+        onChangeStateCode(value as USStateCode)
       }
     },
-    [setStateCode],
+    [onChangeStateCode],
   )
 
-  const handleStep2Cta = useCallback(() => {
-    setCompleteStep2(true)
-  }, [])
-
   const handleClaimNft = useCallback(async () => {
-    if (!stateCode) return
+    if (!stateCode || !link) {
+      return toastGenericError()
+    }
 
-    goToSection(SectionNames.CLAIM_NFT)
-  }, [goToSection, stateCode])
-
-  const handleOnBack = useCallback(() => goToSection(SectionNames.SURVEY), [goToSection])
+    void createAction({
+      stateCode,
+      onSuccess: () => goToSection(SectionNames.SUCCESS),
+    })
+    window.open(link)
+  }, [createAction, goToSection, link, stateCode])
 
   return (
-    <UserActionFormLayout onBack={handleOnBack}>
+    <UserActionFormLayout onBack={goBackSection}>
       <UserActionFormLayout.Container>
         <UserActionFormLayout.Heading subtitle={subtitle} title={title} />
-        <ol className="flex flex-col justify-self-center">
-          <li className={LIST_ITEM_STYLE}>
-            <Step1Svg />
-            <div className="flex flex-grow flex-row items-center justify-between gap-2">
-              Choose your state
-              <Select onValueChange={handleOnValueChange} value={stateCode}>
-                <SelectTrigger
-                  className="w-[195px] flex-shrink-0"
-                  data-testid="state-filter-trigger"
-                >
-                  <SelectValue placeholder="State" />
-                </SelectTrigger>
-                <SelectContent>
-                  {Object.entries(US_STATE_CODE_TO_DISPLAY_NAME_MAP).map(([key]) => (
-                    <SelectItem key={key} value={key}>
-                      {getUSStateNameFromStateCode(key)}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-          </li>
-          <div className={stateCode === 'ND' || stateCode === 'WY' ? 'invisible' : ''}>
-            <li className={LIST_ITEM_STYLE}>
-              <Step2Svg />
-              <div className="flex flex-grow flex-row items-center justify-between gap-2">
-                {step2}
-                <Button asChild disabled={!link} onClick={handleStep2Cta} variant="secondary">
-                  <ExternalLink data-testid="step2-cta-anchor" href={link}>
-                    <div className="flex flex-row items-center justify-center gap-2">
-                      {step2Cta} <ArrowUpRight />
-                    </div>
-                  </ExternalLink>
-                </Button>
-              </div>
-            </li>
-            <li className={LIST_ITEM_STYLE}>
-              <Step3Svg />
-              Return here to claim your free NFT
-            </li>
-          </div>
-        </ol>
+        <div className="mx-auto w-full max-w-64">
+          <Select disabled={isCreatingAction} onValueChange={handleOnValueChange} value={stateCode}>
+            <SelectTrigger className="w-full flex-shrink-0" data-testid="state-filter-trigger">
+              <SelectValue placeholder="State" />
+            </SelectTrigger>
+            <SelectContent>
+              {Object.entries(US_STATE_CODE_TO_DISPLAY_NAME_MAP).map(([key]) => (
+                <SelectItem key={key} value={key}>
+                  {getUSStateNameFromStateCode(key)}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
       </UserActionFormLayout.Container>
       <UserActionFormLayout.Footer>
-        <div className="flex flex-grow flex-row items-center justify-between gap-8">
-          <span className="w-2/3 text-sm text-fontcolor-muted">{disclaimer(stateCode)}</span>
+        <div className="mx-auto flex w-full max-w-96 flex-col items-center gap-4">
+          <p className="text-center text-sm text-fontcolor-muted">
+            {getDisclaimerByState(stateCode)}
+          </p>
           <Button
-            disabled={stateCode !== 'WY' && stateCode !== 'ND' && !completeStep2}
+            className="w-full max-w-64"
+            disabled={!link || isCreatingAction}
             onClick={handleClaimNft}
           >
-            Claim NFT
+            {isCreatingAction ? 'Loading...' : cta}
           </Button>
         </div>
       </UserActionFormLayout.Footer>
