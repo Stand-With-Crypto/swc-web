9ee742f1a0743edfbfa4664e18a0f9ec354d4e99
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-09-05 12:49:42 -0300

    ABC Debate email campaign (#1329)
    * feat: creates `ABC debate email campaign`
    
    * .
    
    * fix: email copy spacing
    
    * refactor: replaces `ABC` statements to `Debate` to make the action generic for the future
    
    * feat: updates CC campaign and ABC email
    
    * feat: removes ABC action card
    
    * fix: lint
    
    * fix: line breaks in email message
    
    * feat: requested change
    
    * feat: requested change
    
    * style: fix width
    
    * feat: changes skeleton dialog title

---
+++ src/components/app/userActionFormEmailDebate/homepageDialogDeeplinkWrapper.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import React, { Suspense, useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { UserActionFormEmailDebate } from '@/components/app/userActionFormEmailDebate'
+import { ANALYTICS_NAME_USER_ACTION_FORM_EMAIL_DEBATE } from '@/components/app/userActionFormEmailDebate/constants'
+import { UserActionFormEmailDebateSkeleton } from '@/components/app/userActionFormEmailDebate/skeleton'
+import { UserActionFormEmailDebateSuccess } from '@/components/app/userActionFormEmailDebate/success'
+import { UserActionEmailDebateFormFields } from '@/components/app/userActionFormEmailDebate/types'
+import { UserActionFormSuccessScreen } from '@/components/app/userActionFormSuccessScreen'
+import { dialogContentPaddingStyles } from '@/components/ui/dialog/styles'
+import { trackDialogOpen } from '@/components/ui/dialog/trackDialogOpen'
+import { useApiResponseForUserFullProfileInfo } from '@/hooks/useApiResponseForUserFullProfileInfo'
+import { useEncodedInitialValuesQueryParam } from '@/hooks/useEncodedInitialValuesQueryParam'
+import { useLocale } from '@/hooks/useLocale'
+import { usePreventOverscroll } from '@/hooks/usePreventOverscroll'
+import { getIntlUrls } from '@/utils/shared/urls'
+import { cn } from '@/utils/web/cn'
+
+function UserActionFormEmailDebateDeeplinkWrapperContent() {
+  usePreventOverscroll()
+
+  const fetchUser = useApiResponseForUserFullProfileInfo()
+  const router = useRouter()
+  const locale = useLocale()
+  const urls = getIntlUrls(locale)
+  const [state, setState] = useState<'form' | 'success'>('form')
+  const { user } = fetchUser.data || { user: null }
+  const [initialValues, loadingParams] =
+    useEncodedInitialValuesQueryParam<UserActionEmailDebateFormFields>({
+      email: '',
+      firstName: '',
+      lastName: '',
+      address: {
+        description: '',
+        place_id: '',
+      },
+    })
+  useEffect(() => {
+    trackDialogOpen({ open: true, analytics: ANALYTICS_NAME_USER_ACTION_FORM_EMAIL_DEBATE })
+  }, [])
+
+  return fetchUser.isLoading || loadingParams ? (
+    <UserActionFormEmailDebateSkeleton />
+  ) : state === 'form' ? (
+    <UserActionFormEmailDebate
+      initialValues={initialValues}
+      onCancel={() => router.replace(urls.home())}
+      onSuccess={() => setState('success')}
+      user={user}
+    />
+  ) : (
+    <div className={cn(dialogContentPaddingStyles, 'h-full')}>
+      <UserActionFormSuccessScreen onClose={() => router.replace(urls.home())}>
+        <UserActionFormEmailDebateSuccess />
+      </UserActionFormSuccessScreen>
+    </div>
+  )
+}
+
+export function UserActionFormEmailDebateDeeplinkWrapper() {
+  return (
+    <Suspense fallback={<UserActionFormEmailDebateSkeleton />}>
+      <UserActionFormEmailDebateDeeplinkWrapperContent />
+    </Suspense>
+  )
+}
