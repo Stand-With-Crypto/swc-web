54f8b0d433f807f281f13aa700a968b075f8fb39
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-09-09 08:56:14 -0300

    Creates past events section in events page (#1364)
    * feat: creates past events section
    
    * refactor: changes var name to uppercase
    
    * style: changes slideshow width on mobile
    
    * fix: promoted events past calculation
    
    * fix: responsiveness
    
    * style: fix responsiveness and overlay
    
    * style: changes description styles

--- src/components/app/pageEvents/components/featuredPastEvents.tsx
+++ src/components/app/pageEvents/components/featuredPastEvents.tsx
@@ -1,5 +1,7 @@
-import { format, isBefore } from 'date-fns'
+import { isBefore } from 'date-fns'
+import { ArrowRight } from 'lucide-react'
 
+import { EventDialog } from '@/components/app/pageEvents/components/eventDialog'
 import { NextImage } from '@/components/ui/image'
 import { SWCEvents } from '@/utils/shared/getSWCEvents'
 
@@ -26,33 +28,40 @@ export function FeaturedPastEvents({ events }: FeaturedPastEventsProps) {
 
       <div className="grid gap-4 lg:grid-cols-3">
         {pastFeaturedEvents.map(event => (
-          <div className="group relative" key={event.data.slug}>
-            <div className="relative h-[222px] min-w-[345px] lg:h-[271px] lg:min-w-[271px]">
-              <NextImage
-                alt={event.data.name}
-                className="object-cover object-center"
-                fill
-                src={event.data.image}
-              />
-            </div>
-            <EventOverlay eventDate={event.data.date} eventName={event.data.name} />
-          </div>
+          <EventDialog
+            event={event.data}
+            key={event.data.slug}
+            trigger={
+              <div className="group relative" key={event.data.slug}>
+                <div className="relative h-[222px] min-w-[345px] lg:h-[271px] lg:min-w-[271px]">
+                  <NextImage
+                    alt={event.data.name}
+                    className="object-cover object-center"
+                    fill
+                    src={event.data.image}
+                  />
+                </div>
+                <EventOverlay eventName={event.data.name} />
+              </div>
+            }
+            triggerClassName="w-full"
+          />
         ))}
       </div>
     </section>
   )
 }
 
-function EventOverlay({ eventName, eventDate }: { eventName: string; eventDate: string }) {
-  const formattedDate = format(new Date(eventDate), 'MMMM dd, yyyy')
-
+function EventOverlay({ eventName }: { eventName: string }) {
   return (
-    <div className="absolute inset-0 flex items-center justify-center rounded-lg opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/60 opacity-75" />
+    <div className="absolute inset-0 flex items-center justify-center rounded-lg opacity-100 transition-opacity duration-300 ease-in-out group-hover:opacity-100 lg:opacity-0">
+      <div className="absolute inset-0 bg-black/80 opacity-75" />
 
-      <div className="relative z-10 flex h-full w-full cursor-pointer flex-col items-start justify-end p-4 text-left text-white">
-        <h2 className="text-xl font-bold">{eventName}</h2>
-        <p className="text-lg">{formattedDate}</p>
+      <div className="relative z-10 flex h-full w-full cursor-pointer flex-col justify-between p-4 text-left text-white">
+        <strong className="font-sans text-xl">{eventName}</strong>
+        <p className="flex items-center gap-1 self-end text-sm">
+          View details <ArrowRight size={16} />
+        </p>
       </div>
     </div>
   )
