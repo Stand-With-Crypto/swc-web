9b845a6fe80e4313394d9ec6fd5f400aa7f610f2
Olavo Parno <olavo.parno@coinbase.com>
2024-09-12 17:51:43 -0300

    refactor: reorder items into vote page

--- src/components/app/pageVoterGuide/index.tsx
+++ src/components/app/pageVoterGuide/index.tsx
@@ -8,6 +8,16 @@ import { PageTitle } from '@/components/ui/pageTitleText'
 export function PageVoterGuide() {
   return (
     <div className="standard-spacing-from-navbar container flex flex-col items-center gap-16">
+      <div className="space-y-2">
+        <PageTitle size="xs">Voter guide</PageTitle>
+        <PageSubTitle>
+          This year's election is critical to the future of crypto. Here's how to become an informed
+          voter.
+        </PageSubTitle>
+      </div>
+
+      <VoterJourneyStepList className="w-full" />
+
       <div className="flex flex-col items-center justify-center gap-10">
         <Badge
           className="p-2.5 font-bold antialiased md:text-base lg:px-6 lg:py-3.5"
@@ -18,16 +28,6 @@ export function PageVoterGuide() {
 
         <Countdown targetDate={new Date('2024-11-05T00:00:00')} />
       </div>
-
-      <div className="space-y-2">
-        <PageTitle size="xs">Voter guide</PageTitle>
-        <PageSubTitle>
-          This year's election is critical to the future of crypto. Here's how to become an informed
-          voter.
-        </PageSubTitle>
-      </div>
-
-      <VoterJourneyStepList className="w-full" />
 
       <TurbovoteDisclaimer />
     </div>
