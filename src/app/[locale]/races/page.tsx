03948cdf70ffc734b05623db9ac0bb3d0c0ba54c
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2024-10-01 20:05:39 -0300

    Fetch initial data

--- src/app/[locale]/races/page.tsx
+++ src/app/[locale]/races/page.tsx
@@ -1,15 +1,19 @@
 import { Metadata } from 'next'
 
 import { LocationUnitedStatesLiveResults } from '@/components/app/pageLocationKeyRaces/locationUnitedStatesLiveResults'
+import { organizePeople } from '@/components/app/pageLocationKeyRaces/locationUnitedStatesLiveResults/organizePeople'
+import { fetchRacesData } from '@/data/decisionDesk/services'
+import { GetRacesResponse } from '@/data/decisionDesk/types'
 import { queryDTSILocationUnitedStatesInformation } from '@/data/dtsi/queries/queryDTSILocationUnitedStatesInformation'
 import { PageProps } from '@/types'
+import { normalizeDTSIDistrictId } from '@/utils/dtsi/dtsiPersonRoleUtils'
 import { generateMetadataDetails } from '@/utils/server/metadataUtils'
 import { SECONDS_DURATION } from '@/utils/shared/seconds'
 import { toBool } from '@/utils/shared/toBool'
 
 export const dynamic = 'error'
 export const dynamicParams = toBool(process.env.MINIMIZE_PAGE_PRE_GENERATION)
-export const revalidate = SECONDS_DURATION['10_MINUTES']
+export const revalidate = SECONDS_DURATION['5_MINUTES']
 
 type LocationUnitedStatesPageProps = PageProps
 
@@ -27,5 +31,31 @@ export default async function LocationUnitedStatesPage({ params }: LocationUnite
   const { locale } = params
   const [dtsiResults] = await Promise.all([queryDTSILocationUnitedStatesInformation()])
 
-  return <LocationUnitedStatesLiveResults {...dtsiResults} {...{ locale }} />
+  const races = organizePeople(dtsiResults)
+
+  const racesDataMap: Record<string, GetRacesResponse> = {}
+
+  const racePromises = Object.entries(races.keyRaces).flatMap(([state, keyRaces]) =>
+    keyRaces.map(async race => {
+      const primaryDistrict = race[0].runningForSpecificRole.primaryDistrict
+        ? normalizeDTSIDistrictId(race[0].runningForSpecificRole)
+        : undefined
+
+      const officeId = primaryDistrict ? '3' : '4'
+      const key = `${state}_${primaryDistrict?.toString() || 'undefined'}_${officeId}`
+
+      const raceData = await fetchRacesData({
+        state,
+        district: primaryDistrict?.toString(),
+        office_id: officeId,
+      })
+      racesDataMap[key] = raceData
+    }),
+  )
+
+  await Promise.all(racePromises)
+
+  return (
+    <LocationUnitedStatesLiveResults ddhqResults={racesDataMap} locale={locale} races={races} />
+  )
 }
