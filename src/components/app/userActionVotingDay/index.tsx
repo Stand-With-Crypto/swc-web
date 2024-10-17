b2cb7bcc5007e152ae7f62529fc8dca9b8ab425a
Olavo Parno <olavo.parno@coinbase.com>
2024-10-17 17:49:29 -0300

    refactor: adjust after login screen for voting day action

--- src/components/app/userActionVotingDay/index.tsx
+++ src/components/app/userActionVotingDay/index.tsx
@@ -29,7 +29,7 @@ export function UserActionVotingDay({ onClose }: { onClose: () => void }) {
         )
       case SectionNames.SUCCESS:
         return (
-          <UserActionFormSuccessScreen onClose={onClose} skipNextActions>
+          <UserActionFormSuccessScreen isVotingDay onClose={onClose}>
             <UserActionIVotedSuccess />
           </UserActionFormSuccessScreen>
         )
