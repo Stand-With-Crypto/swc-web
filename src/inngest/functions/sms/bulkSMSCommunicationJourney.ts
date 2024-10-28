a5111579ecc630690b342553e8789d3bbc590856
Lucas Bernardi <rossi.lucas06@gmail.com>
2024-10-28 16:58:27 -0300

    feat: check message status when querying bulk-sms phone numbers

--- src/inngest/functions/sms/bulkSMSCommunicationJourney.ts
+++ src/inngest/functions/sms/bulkSMSCommunicationJourney.ts
@@ -1,4 +1,9 @@
-import { Prisma, SMSStatus, UserCommunicationJourneyType } from '@prisma/client'
+import {
+  CommunicationMessageStatus,
+  Prisma,
+  SMSStatus,
+  UserCommunicationJourneyType,
+} from '@prisma/client'
 import { addDays, addHours, addSeconds, differenceInMilliseconds, startOfDay } from 'date-fns'
 import { NonRetriableError } from 'inngest'
 import { chunk, merge, uniq, uniqBy, update } from 'lodash-es'
@@ -114,6 +119,11 @@ export const bulkSMSCommunicationJourney = inngest.createFunction(
               ? {
                   some: {
                     journeyType: UserCommunicationJourneyType.WELCOME_SMS,
+                    userCommunications: {
+                      some: {
+                        status: CommunicationMessageStatus.DELIVERED,
+                      },
+                    },
                     datetimeCreated: {
                       gt: SHORT_CODE_GO_LIVE_DATE,
                     },
@@ -131,6 +141,16 @@ export const bulkSMSCommunicationJourney = inngest.createFunction(
                         journeyType: UserCommunicationJourneyType.WELCOME_SMS,
                         datetimeCreated: {
                           lt: SHORT_CODE_GO_LIVE_DATE,
+                        },
+                      },
+                      {
+                        journeyType: UserCommunicationJourneyType.WELCOME_SMS,
+                        userCommunications: {
+                          every: {
+                            status: {
+                              not: CommunicationMessageStatus.DELIVERED,
+                            },
+                          },
                         },
                       },
                     ],
