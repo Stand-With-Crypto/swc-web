a5bd22cb18cdedad4e5a63d3d24515276324b5a6
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-10-28 11:11:44 -0300

    fix: add distinct to the count

--- src/data/aggregations/getCountVoterActions.ts
+++ src/data/aggregations/getCountVoterActions.ts
@@ -5,16 +5,14 @@ import { UserActionType } from '@prisma/client'
 import { prismaClient } from '@/utils/server/prismaClient'
 
 export async function getCountVoterActions() {
-  return prismaClient.userAction.count({
-    where: {
-      actionType: {
-        in: [
-          UserActionType.VOTER_REGISTRATION,
-          UserActionType.VOTER_ATTESTATION,
-          UserActionType.VOTING_INFORMATION_RESEARCHED,
-          UserActionType.VIEW_KEY_RACES,
-        ],
-      },
-    },
-  })
+  // `prismaClient.count` doesn't support distinct, that's why we're using a raw query here
+  // see https://github.com/prisma/prisma/issues/4228
+  return prismaClient.$queryRaw`
+    select count(DISTINCT user_id) from user_action where action_type in (
+      ${UserActionType.VOTER_REGISTRATION},
+      ${UserActionType.VOTER_ATTESTATION},
+      ${UserActionType.VOTING_INFORMATION_RESEARCHED},
+      ${UserActionType.VIEW_KEY_RACES},
+    )
+  `
 }
