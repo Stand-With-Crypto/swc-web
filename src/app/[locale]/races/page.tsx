d53871064dad8502e25e7aafcfe9f60dcbe75b38
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-10-29 13:50:42 -0300

    feat: changes `Key Races` page cache time to `30 minutes`

--- src/app/[locale]/races/page.tsx
+++ src/app/[locale]/races/page.tsx
@@ -10,7 +10,7 @@ import { toBool } from '@/utils/shared/toBool'
 
 export const dynamic = 'error'
 export const dynamicParams = toBool(process.env.MINIMIZE_PAGE_PRE_GENERATION)
-export const revalidate = SECONDS_DURATION['10_MINUTES']
+export const revalidate = SECONDS_DURATION['30_MINUTES']
 
 type LocationUnitedStatesPageProps = PageProps
 
