cf08cf149fd133a3f7ee4668540afb19c249769b
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2024-10-29 16:14:55 -0300

    Update ddhq logo

--- src/components/app/ddhqFooter.tsx
+++ src/components/app/ddhqFooter.tsx
@@ -6,8 +6,8 @@ export function DDHQFooter({ className }: { className?: string }) {
   return (
     <div className={cn('relative top-20 mx-auto max-w-3xl', className)}>
       <p>Election data sourced from</p>
-      <ExternalLink href="https://decisiondeskhq.com">
-        <div className="mx-auto mt-4">
+      <div className="mx-auto mt-4">
+        <ExternalLink href="https://decisiondeskhq.com">
           <NextImage
             alt="Decision Desk logo"
             className="object-contain"
@@ -16,8 +16,8 @@ export function DDHQFooter({ className }: { className?: string }) {
             src="/partners/ddhq.png"
             width={100}
           />
-        </div>
-      </ExternalLink>
+        </ExternalLink>
+      </div>
     </div>
   )
 }
