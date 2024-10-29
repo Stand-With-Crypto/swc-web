4558fe916f9fdb58bfce6cf31d73ef62075483e1
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2024-10-29 16:43:58 -0300

    Fix image center

--- src/components/app/ddhqFooter.tsx
+++ src/components/app/ddhqFooter.tsx
@@ -7,7 +7,7 @@ export function DDHQFooter({ className }: { className?: string }) {
     <div className={cn('relative top-20 mx-auto max-w-3xl', className)}>
       <p>Election data sourced from</p>
       <div className="mx-auto mt-4">
-        <ExternalLink href="https://decisiondeskhq.com">
+        <ExternalLink className="inline-block" href="https://decisiondeskhq.com">
           <NextImage
             alt="Decision Desk logo"
             className="object-contain"
