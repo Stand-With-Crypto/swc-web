efafc7f6ac59ac2bdddf4368fe2b94a9779916b4
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2024-10-29 15:19:08 -0300

    Add ddhq link

--- src/components/app/ddhqFooter.tsx
+++ src/components/app/ddhqFooter.tsx
@@ -1,17 +1,23 @@
 import { NextImage } from '@/components/ui/image'
+import { ExternalLink } from '@/components/ui/link'
 import { cn } from '@/utils/web/cn'
 
 export function DDHQFooter({ className }: { className?: string }) {
   return (
     <div className={cn('relative top-20 mx-auto max-w-3xl', className)}>
       <p>Election data sourced from</p>
-      <NextImage
-        alt="Decision Desk HQ"
-        className="mx-auto mt-4"
-        height={100}
-        src="/partners/ddhq.png"
-        width={100}
-      />
+      <ExternalLink href="https://decisiondeskhq.com">
+        <div className="mx-auto mt-4">
+          <NextImage
+            alt="Decision Desk logo"
+            className="object-contain"
+            height={100}
+            quality={100}
+            src="/partners/ddhq.png"
+            width={100}
+          />
+        </div>
+      </ExternalLink>
     </div>
   )
 }
