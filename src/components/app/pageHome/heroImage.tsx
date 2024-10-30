8d44fa1b1ff3d400177637f058a42f9fe093f1e1
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-10-30 15:40:43 -0300

    feat: updates homepage hero image

--- src/components/app/pageHome/heroImage.tsx
+++ src/components/app/pageHome/heroImage.tsx
@@ -1,10 +1,10 @@
+import Balancer from 'react-wrap-balancer'
 import { ArrowUpRight } from 'lucide-react'
 import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
 import { NextImage } from '@/components/ui/image'
 import { LinkBox, linkBoxLinkClassName } from '@/components/ui/linkBox'
-import { Video } from '@/components/ui/video'
 import { SupportedLocale } from '@/intl/locales'
 import { getIntlUrls } from '@/utils/shared/urls'
 
@@ -14,24 +14,20 @@ interface HeroImageProps {
 
 export function HeroImage({ locale }: HeroImageProps) {
   return (
-    <Link href={getIntlUrls(locale).voterGuide()}>
-      <LinkBox className="relative h-[320px] overflow-hidden md:rounded-xl lg:h-[400px]">
-        <Video
-          className={'absolute left-0 top-0 h-full w-full object-cover'}
-          fallback={
-            <NextImage
-              alt="sign up"
-              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOM8FqyAgAEOAHwiAoWHAAAAABJRU5ErkJggg=="
-              className="h-full w-full object-cover"
-              fill
-              placeholder="blur"
-              priority
-              sizes={'(max-width: 400px) 375px, 500px'}
-              src="/homepageHero.webp"
-            />
-          }
-          poster="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP0dpm3AgAD5gHXYQBQLgAAAABJRU5ErkJggg=="
-          src="https://fgrsqtudn7ktjmlh.public.blob.vercel-storage.com/heroImage.mp4"
+    <Link href={getIntlUrls(locale).locationUnitedStates()}>
+      <LinkBox
+        className="relative flex h-[320px] items-center justify-center overflow-hidden md:rounded-xl lg:h-[400px]"
+        style={{
+          background: 'radial-gradient(74.32% 74.32% at 50% 50%, #F0E8FF 8.5%, #6B28FF 89%);',
+        }}
+      >
+        <NextImage
+          alt="Election results"
+          height={400}
+          priority
+          quality={100}
+          src="/actionTypeIcons/votingResearched.png"
+          width={400}
         />
 
         <div
@@ -43,11 +39,9 @@ export function HeroImage({ locale }: HeroImageProps) {
               'linear-gradient(to top, hsla(0, 0%, 0%, 0.8) 10%, hsla(0, 0%, 0%, 0.4) 70%,  transparent 100%)',
           }}
         >
-          <p>
-            See where your politicians stand on crypto and prepare to vote with the SWC Voter Guide
-          </p>
+          <Balancer>Check out the election results and the role crypto played.</Balancer>
           <Button className={linkBoxLinkClassName} data-link-box-subject variant="secondary">
-            Explore
+            View results
             <ArrowUpRight />
           </Button>
         </div>
