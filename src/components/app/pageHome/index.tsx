889a0342c9f83593e15c08926e10a899723e519d
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2024-10-31 18:12:08 -0300

    Revert "revert top level metrics changes"
    This reverts commit 883f926447a154121ead1f0c6531783cfcf2ed01.

--- src/components/app/pageHome/index.tsx
+++ src/components/app/pageHome/index.tsx
@@ -26,7 +26,7 @@ export function PageHome({
   params,
   sumDonations,
   countUsers,
-  countPolicymakerContacts,
+  countVoterActions,
   actions,
   sumDonationsByUser,
   dtsiHomepagePeople,
@@ -59,7 +59,7 @@ export function PageHome({
         </div>
       </section>
       <div className="container">
-        <TopLevelMetrics {...{ sumDonations, locale, countUsers, countPolicymakerContacts }} />
+        <TopLevelMetrics {...{ sumDonations, locale, countUsers, countVoterActions }} />
 
         <section className="mb-16 md:mb-36">
           <PageTitle as="h3" className="mb-6 !text-[32px]">
