9870a29dcf8cca63cfb275f110acadb7f5352fee
Olavo Parno <olavoparno@gmail.com>
2024-11-19 11:33:11 -0300

    feat: add developer promotion on the console (#1607)
    Co-authored-by: Olavo Parno <olavo.parno@coinbase.com>

---
+++ src/hooks/usePromoteDevParticipation.ts
@@ -0,0 +1,66 @@
+import { useEffectOnce } from '@/hooks/useEffectOnce'
+
+export function usePromoteDevParticipation() {
+  useEffectOnce(() => {
+    const asciiShield = `                                                
+########################========================
+########################========================
+########################========================
+########################========================
+########################========================
+########################========================
+########################========================
+########################========================
+########################========================
+########################========================
+++++++++++++++++++++++++************************
+++++++++++++++++++++++++************************
+++++++++++++++++++++++++************************
+++++++++++++++++++++++++************************
+ +++++++++++++++++++++++***********************
+  ++++++++++++++++++++++**********************
+   +++++++++++++++++++++********************
+      ++++++++++++++++++*****************
+        ++++++++++++++++***************
+          ++++++++++++++*************
+                ++++++++*******
+                    ++++****
+`
+
+    const lines = asciiShield.trim().split('\n')
+
+    const stylesPerChar: Record<string, string> = {
+      '#': 'color: #6200ff;',
+      '*': 'color: #6200ff;',
+      '=': 'color: #C09AFF;',
+      '+': 'color: #C09AFF;',
+    }
+
+    const styledLines: string[] = []
+    const styles: string[] = []
+
+    styledLines.push('%cWELCOME, DEVELOPER!')
+    styles.push('color: #6200ff; background: #000000; font-size: 18px; font-weight: bold;')
+    styledLines.push(
+      `\n\n%cIf you'd like to collaborate or learn more, please check out at: \n\nwww.standwithcrypto.org/contribute\n\n`,
+    )
+    styles.push('color: #ffffff; background: #000000; font-size: 14px;')
+
+    lines.forEach(line => {
+      let styledLine = ''
+      const currentStyles: string[] = []
+
+      for (const char of line) {
+        styledLine += `%c${char}`
+        currentStyles.push(stylesPerChar[char])
+      }
+
+      styledLines.push(styledLine)
+      styles.push(...currentStyles)
+    })
+
+    console.log(styledLines.join('\n'), ...styles)
+  })
+
+  return null
+}
