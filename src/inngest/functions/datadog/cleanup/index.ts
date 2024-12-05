1071dffe7e65eab3ed39f34027c11b0afb56d678
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-12-05 15:33:12 -0300

    feat: improves datadog cleanup function and decreases cron job

--- src/inngest/functions/datadog/cleanup/index.ts
+++ src/inngest/functions/datadog/cleanup/index.ts
@@ -1,9 +1,14 @@
-import { UserActionType } from '@prisma/client'
+import { User, UserAction, UserActionType, UserEmailAddress } from '@prisma/client'
 import { uniqBy } from 'lodash-es'
 
 import { inngest } from '@/inngest/inngest'
 import { onScriptFailure } from '@/inngest/onScriptFailure'
 import { prismaClient } from '@/utils/server/prismaClient'
+
+type UserWithRelations = User & {
+  userActions: UserAction[]
+  userEmailAddresses: UserEmailAddress[]
+}
 
 const CLEANUP_DATADOG_SYNTHETIC_TESTS_FUNCTION_ID = 'script.cleanup-datadog-synthetic-tests'
 
@@ -15,10 +20,10 @@ export const cleanupDatadogSyntheticTestsWithInngest = inngest.createFunction(
     retries: 0,
     onFailure: onScriptFailure,
   },
-  { cron: 'TZ=America/New_York 0 3 * * *' }, // Every day - 3AM EST
+  { cron: 'TZ=America/New_York 0 * * * *' }, // Every hour
   async ({ step }) => {
-    const users = await step.run('script.getSyntheticTestUsers', () =>
-      prismaClient.user.findMany({
+    try {
+      const users = await prismaClient.user.findMany({
         where: {
           userEmailAddresses: {
             some: {
@@ -32,65 +37,67 @@ export const cleanupDatadogSyntheticTestsWithInngest = inngest.createFunction(
           userEmailAddresses: true,
           userActions: true,
         },
-      }),
-    )
+      })
 
-    const filteredUsers = uniqBy(users, user => user.id)
+      const filteredUsers = uniqBy(users, user => user.id)
 
-    if (filteredUsers.length === 0) {
-      return {
-        usersFound: 0,
+      if (filteredUsers.length === 0) {
+        return { usersFound: 0 }
       }
-    }
 
-    const userActionRemovalPromises = filteredUsers.map(user => {
-      const filteredUserActions = user.userActions.filter(
-        userAction => userAction.actionType !== UserActionType.OPT_IN,
+      await step.run('script.updateUsers', () => Promise.all(filteredUsers.map(updateUser)))
+
+      await step.run('script.removeUserActions', () =>
+        Promise.all(filteredUsers.map(removeUserActions)),
       )
 
-      const userActionIds = filteredUserActions.map(userAction => userAction.id)
+      await step.run('script.removeUserEmails', () =>
+        Promise.all(filteredUsers.map(removeUserEmails)),
+      )
 
-      return prismaClient.userAction.deleteMany({
-        where: {
-          id: {
-            in: userActionIds,
-          },
-        },
-      })
-    })
-
-    await step.run('script.removeUserActions', () => Promise.all(userActionRemovalPromises))
-
-    const userEmailRemovalPromises = filteredUsers.map(user => {
-      const userEmailIds = user.userEmailAddresses.map(userEmail => userEmail.id)
-
-      return prismaClient.userEmailAddress.deleteMany({
-        where: {
-          id: {
-            in: userEmailIds,
-          },
-        },
-      })
-    })
-
-    await step.run('script.removeUserEmails', () => Promise.all(userEmailRemovalPromises))
-
-    const usersToUpdatePromises = filteredUsers.map(user => {
-      return prismaClient.user.update({
-        where: {
-          id: user.id,
-        },
-        data: {
-          primaryUserEmailAddressId: null,
-        },
-      })
-    })
-
-    await step.run('script.updateUsers', () => Promise.all(usersToUpdatePromises))
-
-    return {
-      usersFound: filteredUsers.length,
-      usersIds: filteredUsers.map(user => user.id).join(', '),
+      return {
+        usersFound: filteredUsers.length,
+        usersIds: filteredUsers.map(user => user.id).join(', '),
+      }
+    } catch (error) {
+      console.error('Error processing users:', error)
+      throw error
     }
   },
 )
+
+async function updateUser(user: User) {
+  return prismaClient.user.update({
+    where: { id: user.id },
+    data: {
+      primaryUserEmailAddressId: undefined,
+      primaryUserEmailAddress: undefined,
+    },
+  })
+}
+
+async function removeUserActions(user: UserWithRelations) {
+  const filteredUserActions = user.userActions.filter(
+    userAction => userAction.actionType !== UserActionType.OPT_IN,
+  )
+  const userActionIds = filteredUserActions.map(userAction => userAction.id)
+
+  return prismaClient.userAction.deleteMany({
+    where: { id: { in: userActionIds } },
+  })
+}
+
+async function removeUserEmails(user: UserWithRelations) {
+  const userEmailIds = user.userEmailAddresses.map(userEmail => userEmail.id)
+
+  return prismaClient.userEmailAddress.deleteMany({
+    where: {
+      id: { in: userEmailIds },
+      datetimeCreated: {
+        // We only want to delete emails that were created more than 30 minutes ago
+        // This approach prevents any potential bugs from deleting emails while the synthetic tests are running.
+        gte: new Date(new Date().getTime() - 30 * 60 * 1000), // 30 minutes
+      },
+    },
+  })
+}
