0643e8aa1fd377aafed28a3cbd4d34d48602e393
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-12-06 13:25:51 -0300

    feat: improves datadog cleanup function and decreases cron job schedule (#1638)
    * feat: improves datadog cleanup function and decreases cron job
    
    * feat: requested change

--- src/inngest/functions/datadog/cleanup/index.ts
+++ src/inngest/functions/datadog/cleanup/index.ts
@@ -1,9 +1,14 @@
-import { UserActionType } from '@prisma/client'
-import { uniqBy } from 'lodash-es'
+import { User, UserAction, UserActionType, UserEmailAddress } from '@prisma/client'
+import { difference, flatten, uniqBy } from 'lodash-es'
 
 import { inngest } from '@/inngest/inngest'
 import { onScriptFailure } from '@/inngest/onScriptFailure'
 import { prismaClient } from '@/utils/server/prismaClient'
+
+type UserWithRelations = User & {
+  userActions: UserAction[]
+  userEmailAddresses: UserEmailAddress[]
+}
 
 const CLEANUP_DATADOG_SYNTHETIC_TESTS_FUNCTION_ID = 'script.cleanup-datadog-synthetic-tests'
 
@@ -15,10 +20,12 @@ export const cleanupDatadogSyntheticTestsWithInngest = inngest.createFunction(
     retries: 0,
     onFailure: onScriptFailure,
   },
-  { cron: 'TZ=America/New_York 0 3 * * *' }, // Every day - 3AM EST
+  { cron: 'TZ=America/New_York 0 * * * *' }, // Every hour
   async ({ step }) => {
-    const users = await step.run('script.getSyntheticTestUsers', () =>
-      prismaClient.user.findMany({
+    try {
+      const ThirtyMinutesAgo = new Date().getTime() - 30 * 60 * 1000
+
+      const users = await prismaClient.user.findMany({
         where: {
           userEmailAddresses: {
             some: {
@@ -32,65 +39,103 @@ export const cleanupDatadogSyntheticTestsWithInngest = inngest.createFunction(
           userEmailAddresses: true,
           userActions: true,
         },
-      }),
-    )
+      })
 
-    const filteredUsers = uniqBy(users, user => user.id)
+      const filteredUsers = uniqBy(users, user => user.id)
 
-    if (filteredUsers.length === 0) {
-      return {
-        usersFound: 0,
+      if (filteredUsers.length === 0) {
+        return { usersFound: 0 }
       }
-    }
 
-    const userActionRemovalPromises = filteredUsers.map(user => {
-      const filteredUserActions = user.userActions.filter(
-        userAction => userAction.actionType !== UserActionType.OPT_IN,
+      const emailAddressesCreatedInLast30MinutesIds = flatten(
+        filteredUsers.map(user => {
+          return user.userEmailAddresses
+            .filter(
+              userEmail =>
+                userEmail.emailAddress.includes(DATADOG_SYNTHETIC_TESTS_EMAIL_DOMAIN) &&
+                userEmail.datetimeCreated.getTime() > ThirtyMinutesAgo,
+            )
+            .map(userEmail => userEmail.id)
+        }),
       )
 
-      const userActionIds = filteredUserActions.map(userAction => userAction.id)
+      await step.run('script.updateUsers', () =>
+        Promise.all(
+          filteredUsers.map(user => updateUser(user, emailAddressesCreatedInLast30MinutesIds)),
+        ),
+      )
 
-      return prismaClient.userAction.deleteMany({
-        where: {
-          id: {
-            in: userActionIds,
-          },
-        },
-      })
-    })
+      await step.run('script.removeUserActions', () =>
+        Promise.all(filteredUsers.map(removeUserActions)),
+      )
 
-    await step.run('script.removeUserActions', () => Promise.all(userActionRemovalPromises))
+      await step.run('script.removeUserEmails', () =>
+        Promise.all(
+          filteredUsers.map(user =>
+            removeUserEmails(user, emailAddressesCreatedInLast30MinutesIds),
+          ),
+        ),
+      )
 
-    const userEmailRemovalPromises = filteredUsers.map(user => {
-      const userEmailIds = user.userEmailAddresses.map(userEmail => userEmail.id)
-
-      return prismaClient.userEmailAddress.deleteMany({
-        where: {
-          id: {
-            in: userEmailIds,
-          },
-        },
-      })
-    })
-
-    await step.run('script.removeUserEmails', () => Promise.all(userEmailRemovalPromises))
-
-    const usersToUpdatePromises = filteredUsers.map(user => {
-      return prismaClient.user.update({
-        where: {
-          id: user.id,
-        },
-        data: {
-          primaryUserEmailAddressId: null,
-        },
-      })
-    })
-
-    await step.run('script.updateUsers', () => Promise.all(usersToUpdatePromises))
-
-    return {
-      usersFound: filteredUsers.length,
-      usersIds: filteredUsers.map(user => user.id).join(', '),
+      return {
+        usersFound: filteredUsers.length,
+        usersIds: filteredUsers.map(user => user.id).join(', '),
+      }
+    } catch (error) {
+      console.error('Error processing users:', error)
+      throw error
     }
   },
 )
+
+async function updateUser(user: User, emailAddressesCreatedInLast30MinutesIds: string[]) {
+  const primaryUserEmailAddressId = user.primaryUserEmailAddressId
+
+  if (!primaryUserEmailAddressId) return
+
+  const isPrimaryEmailCreatedInLast30Minutes =
+    emailAddressesCreatedInLast30MinutesIds.includes(primaryUserEmailAddressId)
+
+  if (isPrimaryEmailCreatedInLast30Minutes) return
+
+  return prismaClient.user.update({
+    where: { id: user.id },
+    data: {
+      primaryUserEmailAddressId: undefined,
+      primaryUserEmailAddress: undefined,
+    },
+  })
+}
+
+async function removeUserActions(user: UserWithRelations) {
+  const filteredUserActions = user.userActions.filter(
+    userAction => userAction.actionType !== UserActionType.OPT_IN,
+  )
+  const userActionIds = filteredUserActions.map(userAction => userAction.id)
+
+  return prismaClient.userAction.deleteMany({
+    where: { id: { in: userActionIds } },
+  })
+}
+
+async function removeUserEmails(
+  user: UserWithRelations,
+  emailAddressesCreatedInLast30MinutesIds: string[],
+) {
+  const userEmailIds = user.userEmailAddresses.map(userEmail => userEmail.id)
+
+  const emailIdsToRemove = difference(userEmailIds, emailAddressesCreatedInLast30MinutesIds)
+
+  const ThirtyMinutesAgo = new Date(new Date().getTime() - 30 * 60 * 1000)
+
+  return prismaClient.userEmailAddress.deleteMany({
+    where: {
+      id: { in: emailIdsToRemove },
+      datetimeCreated: {
+        // We only want to delete emails that were created more than 30 minutes ago
+        // This approach prevents any potential bugs from deleting emails while the synthetic tests are running.
+        gte: ThirtyMinutesAgo, // 30 minutes
+      },
+    },
+  })
+}
