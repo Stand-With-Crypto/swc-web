5f79d62b3afe0e67ad6278907fb5763b103c4932
Olavo Parno <olavo.parno@coinbase.com>
2024-12-12 11:54:44 -0300

    refactor: add error boundary to deeplink actions

--- src/app/[locale]/(homepageDialogDeeplink)/action/email-debate/page.tsx
+++ src/app/[locale]/(homepageDialogDeeplink)/action/email-debate/page.tsx
@@ -1,15 +1,33 @@
+import { UserActionType } from '@prisma/client'
+
 import { HomepageDialogDeeplinkLayout } from '@/components/app/homepageDialogDeeplinkLayout'
 import { UserActionFormEmailDebateDeeplinkWrapper } from '@/components/app/userActionFormEmailDebate/homepageDialogDeeplinkWrapper'
 import { PageProps } from '@/types'
 import { SECONDS_DURATION } from '@/utils/shared/seconds'
+import { UserActionEmailCampaignName } from '@/utils/shared/userActionCampaigns'
+import { ErrorBoundary } from '@/utils/web/errorBoundary'
 
 export const revalidate = SECONDS_DURATION.HOUR
 export const dynamic = 'error'
 
 export default function UserActionEmailDebateDeepLink({ params }: PageProps) {
   return (
-    <HomepageDialogDeeplinkLayout pageParams={params}>
-      <UserActionFormEmailDebateDeeplinkWrapper />
-    </HomepageDialogDeeplinkLayout>
+    <ErrorBoundary
+      extras={{
+        action: {
+          isDeeplink: true,
+          actionType: UserActionType.EMAIL,
+          campaignName: UserActionEmailCampaignName.ABC_PRESIDENTIAL_DEBATE_2024,
+        },
+      }}
+      severityLevel="error"
+      tags={{
+        domain: 'UserActionEmailDebateDeepLink',
+      }}
+    >
+      <HomepageDialogDeeplinkLayout pageParams={params}>
+        <UserActionFormEmailDebateDeeplinkWrapper />
+      </HomepageDialogDeeplinkLayout>
+    </ErrorBoundary>
   )
 }
