c4040506791a909d1b56ca122583415f85af67ee
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-12-16 10:27:52 -0300

    fix: layout shift caused by top banner

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,10 +7,9 @@ import NextTopLoader from 'nextjs-toploader'
 import { TopLevelClientLogic } from '@/app/[locale]/topLevelClientLogic'
 import { CookieConsent } from '@/components/app/cookieConsent'
 import { Footer } from '@/components/app/footer'
-import { GeoLocationDisclaimerBanner } from '@/components/app/geoLocationDisclaimerBanner/geoLocationDisclaimerBanner'
+import { GeoLocationDisclaimerBanner } from '@/components/app/geoLocationDisclaimerBanner'
 import { GoogleTagManager } from '@/components/app/googleTagManager'
 import { Navbar } from '@/components/app/navbar'
-import { NavbarGlobalBanner } from '@/components/app/navbarGlobalBanner'
 import { OverrideGlobalLocalStorage } from '@/components/app/overrideGlobalLocalStorage'
 import { FullHeight } from '@/components/ui/fullHeight'
 import { Toaster } from '@/components/ui/sonner'
@@ -67,7 +66,6 @@ export default function Layout({ children, params }: PageProps & { children: Rea
         <TopLevelClientLogic locale={locale}>
           <FullHeight.Container>
             <GeoLocationDisclaimerBanner />
-            <NavbarGlobalBanner locale={locale} />
             <Navbar locale={locale} />
             <FullHeight.Content>{children}</FullHeight.Content>
             <Footer locale={locale} />
---
+++ src/components/app/geoLocationDisclaimerBanner/index.tsx
@@ -0,0 +1,9 @@
+import { GeoLocationDisclaimerBannerContent } from '@/components/app/geoLocationDisclaimerBanner/geoLocationDisclaimerBannerContent'
+
+export function GeoLocationDisclaimerBanner() {
+  return (
+    <div className="flex h-12 w-full items-center justify-center bg-primary-cta">
+      <GeoLocationDisclaimerBannerContent />
+    </div>
+  )
+}
