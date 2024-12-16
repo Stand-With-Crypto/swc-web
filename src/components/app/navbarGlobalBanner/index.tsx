e8dc67ad91d36fc9063aec5aff3290803218d0e4
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-12-16 11:41:37 -0300

    fix: navbar banner layout shift (#1657)
    * fix: navbar banner layout shift
    
    * fix: layout shift caused by top banner
    
    * style: decreases line height
    
    * fix: hydration error
    
    * refactor: renames component

---
+++ src/components/app/navbarGlobalBanner/index.tsx
@@ -0,0 +1,9 @@
+import { NavBarGlobalBannerContent } from '@/components/app/navbarGlobalBanner/navbarGlobalBannerContent'
+
+export function NavBarGlobalBanner() {
+  return (
+    <div className="flex h-12 w-full items-center justify-center bg-primary-cta">
+      <NavBarGlobalBannerContent />
+    </div>
+  )
+}
