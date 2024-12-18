0c495e1ae2dc51c71c143d8cb76af6e435d1df2f
Olavo Parno <olavo.parno@coinbase.com>
2024-12-18 15:00:35 -0300

    chore: adjust check db script

--- bin/vercel_deploy.sh
+++ bin/vercel_deploy.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
 set -e
-npm run db:check
+
+# Function to check the DB schema if the environment is production
+checkDbSchema() {
+  if [ "$NEXT_PUBLIC_ENVIRONMENT" = "production" ]; then
+    echo "Running db:check to check for differences in the schema."
+    
+    npm run db:check
+  else
+    echo "Skipping db:check because NEXT_PUBLIC_ENVIRONMENT is not production."
+  fi
+}
+
+checkDbSchema
+
 npx prisma generate
 npm run intl:extract-compile
 npm run codegen
