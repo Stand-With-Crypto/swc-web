04e43791ae7c60cf2d3d1774c92fdc930568e8cf
Paulo Victor <paulovictor.dasilva@coinbase.com>
2024-12-20 15:20:59 -0300

    refactor: update user action for email campaign
    - Changed the user action campaign name from 'SEC_COMMISSIONER_2024' to 'WELCOME_119_CONGRESS_2025' to align with the new outreach strategy.
    - Updated the corresponding email action URL to reflect the new campaign focus.

--- src/utils/shared/urlsDeeplinkUserActions.ts
+++ src/utils/shared/urlsDeeplinkUserActions.ts
@@ -89,7 +89,7 @@ const USER_ACTION_WITH_CAMPAIGN_DEEPLINK_MAP: {
     [UserActionEmailCampaignName.ABC_PRESIDENTIAL_DEBATE_2024]: ({ locale }) => {
       return `${getIntlPrefix(locale)}/action/email-debate`
     },
-    [UserActionEmailCampaignName.SEC_COMMISSIONER_2024]: ({ locale }) => {
+    [UserActionEmailCampaignName.WELCOME_119_CONGRESS_2025]: ({ locale }) => {
       return `${getIntlPrefix(locale)}/action/email`
     },
   },
