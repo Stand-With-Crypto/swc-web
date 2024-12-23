8387b475e8ec623633bc73a99791a238c9d90c84
Olavo Parno <olavo.parno@coinbase.com>
2024-12-23 11:15:39 -0300

    feat: add DataTableSkeleton component for improved loading state in DTSI client data table

--- src/components/app/dtsiClientPersonDataTable/dataTable.tsx
+++ src/components/app/dtsiClientPersonDataTable/dataTable.tsx
@@ -47,6 +47,8 @@ interface DataTableProps<TData extends Person = Person> extends Partial<TableOpt
   locale: SupportedLocale
 }
 
+type DataTableSkeletonProps<TData extends Person = Person> = Partial<TableOptions<TData>>
+
 export const SortableHeader = <TData extends Person = Person>({
   column,
   children,
@@ -217,6 +219,111 @@ export function DataTable<TData extends Person = Person>({
             <DataTablePagination table={table} />
           </div>
         )}
+      </div>
+    </div>
+  )
+}
+
+// create a skeleton for the table using DataTable as above but without the filter and pagination because we dont want to use Nextjs dynamic APIs such as searchParams
+
+export function DataTableSkeleton<TData extends Person = Person>({
+  columns = [],
+  data = [],
+  ...rest
+}: DataTableSkeletonProps<TData>) {
+  const table = useReactTable<TData>({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    initialState: {
+      pagination: {
+        pageSize: 100,
+      },
+      columnFilters: getGlobalFilterDefaults(),
+    },
+    filterFns: getPersonDataTableFilterFns(),
+    ...rest,
+  })
+
+  const tableRowModel = table.getRowModel()
+
+  return (
+    <div className="space-y-6">
+      <div className="container">
+        <div className="flex flex-col items-center justify-between gap-3 rounded-lg border p-6 md:flex-row">
+          <div className="max-w-[400px]">
+            <h3 className="text-2xl font-bold">Search for a politician</h3>
+            <p className="text-fontcolor-muted">
+              We have a database of over 1,000 politicians. Search any politician to see where they
+              stand on crypto.
+            </p>
+          </div>
+          <div className="w-full flex-shrink-0 md:max-w-96">
+            <InputWithIcons
+              className="rounded-full bg-gray-100 text-gray-600"
+              defaultValue={''}
+              leftIcon={<Search className="h-4 w-4 text-gray-500" />}
+              placeholder="Search by name or state"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="md:container">
+        <div className="md:min-h-[578px] md:rounded-md md:border-b md:border-l md:border-r">
+          <div className="sticky top-[72px] z-10 flex flex-col justify-between border-b border-t bg-white p-3 pl-3 lg:top-[84px] lg:flex-row lg:p-6">
+            <PageTitle className="text-left" size="md">
+              Politicians
+            </PageTitle>
+            <GlobalFilters columns={table.getAllColumns()} />
+          </div>
+
+          <div className="relative w-full">
+            <Table className="lg:table-fixed">
+              <TableHeader className="bg-secondary text-gray-400">
+                {table.getHeaderGroups().map(headerGroup => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map(header => {
+                      return (
+                        <TableHead key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(header.column.columnDef.header, header.getContext())}
+                        </TableHead>
+                      )
+                    })}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody>
+                {tableRowModel.rows?.length ? (
+                  tableRowModel.rows.map(row => (
+                    <TableRow
+                      className="cursor-pointer"
+                      data-state={row.getIsSelected() && 'selected'}
+                      key={row.id}
+                      role="button"
+                    >
+                      {row.getVisibleCells().map(cell => (
+                        <TableCell key={cell.id}>
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell className="h-24 text-center" colSpan={columns.length}>
+                      No results.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
       </div>
     </div>
   )
