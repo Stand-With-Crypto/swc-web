74857b5648413d76fe09f64120cee6e74af97095
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-01-03 15:03:49 -0300

    fix: replace DefaultErrorPage with notFound for 404 handling in RenderBuilderContent

--- src/components/app/builder.tsx
+++ src/components/app/builder.tsx
@@ -2,7 +2,7 @@
 
 import { ComponentProps } from 'react'
 import { BuilderComponent, useIsPreviewing } from '@builder.io/react'
-import DefaultErrorPage from 'next/error'
+import { notFound } from 'next/navigation'
 
 import { useHasHydrated } from '@/hooks/useHasHydrated'
 import { maybeInitCMSClient } from '@/utils/web/clientCMS'
@@ -13,22 +13,15 @@ maybeInitCMSClient()
 
 export function RenderBuilderContent(props: BuilderPageProps) {
   const hasHydrated = useHasHydrated()
-
-  // Call the useIsPreviewing hook to determine if
-  // the page is being previewed in Builder
   const isPreviewing = useIsPreviewing()
 
   if (!hasHydrated) {
     return null
   }
 
-  // If `content` has a value or the page is being previewed in Builder,
-  // render the BuilderComponent with the specified content and model props.
   if (props.content || isPreviewing) {
     return <BuilderComponent {...props} />
   }
-  // If the `content` is falsy and the page is
-  // not being previewed in Builder, render the
-  // DefaultErrorPage with a 404.
-  return <DefaultErrorPage statusCode={404} />
+
+  return notFound()
 }
