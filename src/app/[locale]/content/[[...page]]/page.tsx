970e50d36adf040a18cb909d02980173542ce421
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-01-07 14:07:11 -0300

    fix: generate page metadata on dynamic content pages

--- src/app/[locale]/content/[[...page]]/page.tsx
+++ src/app/[locale]/content/[[...page]]/page.tsx
@@ -1,5 +1,9 @@
+import { Content } from '@builder.io/react'
+import { Metadata } from 'next'
+
 import { RenderBuilderContent } from '@/components/app/builder'
 import { serverCMS } from '@/utils/server/builder/serverCMS'
+import { generateMetadataDetails } from '@/utils/server/metadataUtils'
 
 interface PageProps {
   params: {
@@ -8,35 +12,51 @@ interface PageProps {
 }
 
 const PAGE_PREFIX = '/content/'
+const PAGE_MODEL_IDENTIFIER = 'content'
 
 export default async function Page(props: PageProps) {
-  const model = 'content'
   const params = await props.params
 
-  const content = await serverCMS
-    // Get the page content from Builder with the specified options
-    .get('content', {
+  const content: Content | undefined = await serverCMS
+    .get(PAGE_MODEL_IDENTIFIER, {
       userAttributes: {
-        // Use the page path specified in the URL to fetch the content
         urlPath: PAGE_PREFIX + (params?.page?.join('/') || ''),
       },
       // Set prerender to false to return JSON instead of HTML
       prerender: false,
     })
-    // Convert the result to a promise
     .toPromise()
 
-  return (
-    <>
-      {/* Render the Builder page */}
-      <RenderBuilderContent content={content} model={model} />
-    </>
-  )
+  return <RenderBuilderContent content={content} model={PAGE_MODEL_IDENTIFIER} />
+}
+
+export async function generateMetadata(props: PageProps): Promise<Metadata> {
+  const params = await props.params
+
+  const content: Content | undefined = await serverCMS
+    .get(PAGE_MODEL_IDENTIFIER, {
+      userAttributes: {
+        urlPath: PAGE_PREFIX + (params?.page?.join('/') || ''),
+      },
+      fields: 'data',
+      // Set prerender to false to return JSON instead of HTML
+      prerender: false,
+    })
+    .toPromise()
+
+  if (!content?.data) {
+    return {}
+  }
+
+  return generateMetadataDetails({
+    title: content.data.title,
+    description: content.data.description,
+  })
 }
 
 export async function generateStaticParams() {
   const paths = await serverCMS
-    .getAll('content', { options: { noTargeting: true } })
+    .getAll(PAGE_MODEL_IDENTIFIER, { options: { noTargeting: true } })
     .then(res => res.map(({ data }) => data?.url))
 
   return paths.map((path: string) => {
