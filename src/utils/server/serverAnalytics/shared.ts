30b3cfaaf86e97ce693fddc54553cb57adcb8697
Lucas Bernardi <45859300+LucasdRossi@users.noreply.github.com>
2025-01-08 20:22:07 -0300

    fix: increase ANALYTICS_FLUSH_TIMEOUT_MS to 10000 (#1731)
    * feat: increase maxDuration on sms status webhook to 10
    
    * fix: remove maxDuration constant and increase ANALYTICS_FLUSH_TIMEOUT_MS to 10000

--- src/utils/server/serverAnalytics/shared.ts
+++ src/utils/server/serverAnalytics/shared.ts
@@ -9,4 +9,4 @@ const NEXT_PUBLIC_MIXPANEL_PROJECT_TOKEN = requiredEnv(
 
 export const mixpanel = mixpanelLib.init(NEXT_PUBLIC_MIXPANEL_PROJECT_TOKEN)
 
-export const ANALYTICS_FLUSH_TIMEOUT_MS = 2500
+export const ANALYTICS_FLUSH_TIMEOUT_MS = 10000
