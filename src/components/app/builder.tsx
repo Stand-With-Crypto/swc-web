386b7c6879347f2c0ca9b77d5411e19d716516e4
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-01-13 11:36:53 -0300

    fix: remove hydration check on builder component

--- src/components/app/builder.tsx
+++ src/components/app/builder.tsx
@@ -4,7 +4,6 @@ import { ComponentProps } from 'react'
 import { BuilderComponent, useIsPreviewing } from '@builder.io/react'
 import { notFound } from 'next/navigation'
 
-import { useHasHydrated } from '@/hooks/useHasHydrated'
 import { maybeInitCMSClient } from '@/utils/web/builder/clientCMS'
 
 type BuilderPageProps = ComponentProps<typeof BuilderComponent>
@@ -12,12 +11,7 @@ type BuilderPageProps = ComponentProps<typeof BuilderComponent>
 maybeInitCMSClient()
 
 export function RenderBuilderContent(props: BuilderPageProps) {
-  const hasHydrated = useHasHydrated()
   const isPreviewing = useIsPreviewing()
-
-  if (!hasHydrated) {
-    return null
-  }
 
   if (props.content || isPreviewing) {
     return <BuilderComponent {...props} />
