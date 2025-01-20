3138c4080d7c05f78f862803d8d41700cab5235b
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-01-20 17:37:02 -0300

    feat: apply margin to list item with text-indent to fix nested list item issue

--- src/components/ui/builder/text.builder.tsx
+++ src/components/ui/builder/text.builder.tsx
@@ -7,12 +7,28 @@ interface TextProps {
   text: string
 }
 
+function transformLi(tagName: string, attribs: Record<string, string>) {
+  if (attribs.style && attribs.style.includes('text-indent')) {
+    attribs.class = attribs.class ? `${attribs.class} ml-6` : 'ml-6'
+    attribs.style = attribs.style.replace(/text-indent:[^;]+;/, '')
+  }
+  return { tagName, attribs }
+}
+
 function Text(props: TextProps) {
   return (
     <div
       className="prose max-w-full"
       dangerouslySetInnerHTML={{
-        __html: sanitizeHtml(props.text),
+        __html: sanitizeHtml(props.text, {
+          allowedTags: sanitizeHtml.defaults.allowedTags,
+          allowedAttributes: {
+            '*': ['style', 'class'],
+          },
+          transformTags: {
+            li: transformLi,
+          },
+        }),
       }}
     />
   )
