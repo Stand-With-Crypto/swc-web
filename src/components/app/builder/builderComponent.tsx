df2399ad7d8f427d8020df2ea3e7b92a82ec833b
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-01-21 16:54:32 -0300

    feat: enhance Button component with link and authentication support

--- src/components/app/builder/builderComponent.tsx
+++ src/components/app/builder/builderComponent.tsx
@@ -4,13 +4,22 @@ import type { ComponentProps } from 'react'
 import { Builder, BuilderComponent, useIsPreviewing } from '@builder.io/react'
 import { notFound } from 'next/navigation'
 
+import { useSession } from '@/hooks/useSession'
+
 type BuilderPageProps = ComponentProps<typeof BuilderComponent>
 
 export function RenderBuilderContent(props: BuilderPageProps) {
+  const session = useSession()
+
   const isPreviewing = useIsPreviewing()
 
+  const builderData = {
+    ...props.data,
+    isAuthenticated: session.isLoggedIn,
+  }
+
   if (props.content || isPreviewing || Builder.isEditing) {
-    return <BuilderComponent {...props} />
+    return <BuilderComponent {...props} data={builderData} />
   }
 
   return notFound()
--- src/components/ui/button/button.builder.tsx
+++ src/components/ui/button/button.builder.tsx
@@ -1,5 +1,7 @@
 import { Builder, withChildren } from '@builder.io/react'
 
+import { LoginDialogWrapper } from '@/components/app/authentication/loginDialogWrapper'
+import { ExternalLink, InternalLink } from '@/components/ui/link'
 import type { BuilderComponentBaseProps } from '@/utils/web/builder'
 
 import { Button, buttonVariantsConfig } from '.'
@@ -7,14 +9,58 @@ import { Button, buttonVariantsConfig } from '.'
 interface BuilderButtonProps extends BuilderComponentBaseProps {
   variant: keyof typeof buttonVariantsConfig.variant
   size: keyof typeof buttonVariantsConfig.size
+  link?: {
+    href: string
+    type: 'internal' | 'external'
+  }
+  requireAuthentication?: boolean
 }
 
 Builder.registerComponent(
-  withChildren((props: BuilderButtonProps) => (
-    <Button size={props.size} variant={props.variant} {...props.attributes}>
-      {props.children}
-    </Button>
-  )),
+  withChildren((props: BuilderButtonProps) => {
+    const buttonProps = {
+      size: props.size,
+      variant: props.variant,
+      ...props.attributes,
+    }
+
+    let Comp = <Button {...buttonProps}>{props.children}</Button>
+
+    if (props.link) {
+      const { href, type } = props.link
+
+      if (type === 'external') {
+        Comp = (
+          <Button {...buttonProps} asChild>
+            <ExternalLink href={href}>{props.children}</ExternalLink>
+          </Button>
+        )
+      } else if (type === 'internal') {
+        Comp = (
+          <Button {...buttonProps} asChild>
+            <InternalLink href={href}>{props.children}</InternalLink>
+          </Button>
+        )
+      }
+    }
+
+    if (props.requireAuthentication) {
+      return (
+        <LoginDialogWrapper
+          authenticatedContent={Comp}
+          loadingFallback={
+            <Button {...buttonProps} disabled>
+              {props.children}
+            </Button>
+          }
+        >
+          <Button {...buttonProps}>{props.children}</Button>
+        </LoginDialogWrapper>
+      )
+    }
+
+    return Comp
+  }),
   {
     name: 'Button',
     canHaveChildren: true,
@@ -25,12 +71,45 @@ Builder.registerComponent(
         type: 'enum',
         defaultValue: 'default',
         enum: Object.keys(buttonVariantsConfig.variant),
+        helperText: 'The button variant',
       },
       {
         name: 'size',
         type: 'enum',
         defaultValue: 'default',
         enum: Object.keys(buttonVariantsConfig.size),
+        helperText: 'The button size',
+      },
+      {
+        name: 'requireAuthentication',
+        friendlyName: 'Require authentication',
+        type: 'boolean',
+        helperText:
+          'When enabled, the user will be prompted to login if they are not authenticated',
+      },
+      {
+        name: 'link',
+        type: 'object',
+        helperText: 'The link to navigate to when the button is clicked',
+        subFields: [
+          {
+            name: 'href',
+            type: 'string',
+            helperText: 'The URL to navigate to',
+            // should always start with / or http(s)://
+            regex: {
+              pattern: '^(/|https://)',
+              message: 'The URL must start with / or https://',
+            },
+          },
+          {
+            name: 'type',
+            type: 'enum',
+            enum: ['internal', 'external'],
+            defaultValue: 'internal',
+            helperText: 'Whether the link is internal or external',
+          },
+        ],
       },
     ],
     defaultChildren: [
