f472959b78656f9db40240f10fc8d56a9e131ba5
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2025-01-23 16:40:46 -0300

    fix: remove app dependency from mocks

--- src/mocks/fakerUtils.ts
+++ src/mocks/fakerUtils.ts
@@ -1,6 +1,5 @@
 import { faker } from '@faker-js/faker'
 
-import { mockValidPhoneNumber } from '@/mocks/mockValidPhoneNumber'
 import dtsiSlugs from '@/staticContent/dtsi/testingDtsiSlugs.json'
 import { SupportedCryptoCurrencyCodes, SupportedFiatCurrencyCodes } from '@/utils/shared/currency'
 
@@ -12,7 +11,7 @@ export const fakerFields = {
     faker.helpers.arrayElement(Object.values(SupportedFiatCurrencyCodes)),
   supportedCryptoCurrencyCode: () =>
     faker.helpers.arrayElement(Object.values(SupportedCryptoCurrencyCodes)),
-  phoneNumber: mockValidPhoneNumber,
+  phoneNumber: () => faker.helpers.fromRegExp(/\+1[0-9]{10}/),
   stateCode: (options?: { abbreviated?: boolean }) => faker.location.state(options),
   usCongressionalDistrict: () => faker.number.int({ min: 1, max: 20 }).toString(),
   generateReferralId: () => faker.string.uuid().slice(0, 12),
