92a07ef0b75cc6b7eae31cee0e0d3c8d04b32e78
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-01-29 14:05:26 -0300

    refactor: simplify Container and Text components by removing unnecessary wrappers and prop spreading

--- src/utils/web/builder/components/text.builder.tsx
+++ src/utils/web/builder/components/text.builder.tsx
@@ -2,27 +2,29 @@ import { Builder } from '@builder.io/react'
 import sanitizeHtml from 'sanitize-html'
 
 import { BuilderComponentBaseProps } from '@/utils/web/builder'
-import { BuilderComponentAttributes } from '@/utils/web/builder/types'
 import { cn } from '@/utils/web/cn'
-
-interface TextProps {
-  text: string
-}
 
 function transformLi(tagName: string, attribs: Record<string, string>) {
   if (attribs.style && attribs.style.includes('text-indent')) {
+    // Replace text-indent with margin-left because Builder.io applies text-indent to nested lists and
+    // RichText uses tailwind typography prose class which doesn't apply text-ident to the ::marker pseudo-element
+    // So we need to use margin-left instead
     attribs.style = attribs.style.replace('text-indent', 'margin-left')
   }
   return { tagName, attribs }
 }
 
-function Text(props: TextProps & BuilderComponentAttributes) {
-  return (
+interface BuilderTextProps extends BuilderComponentBaseProps {
+  text: string
+}
+
+Builder.registerComponent(
+  ({ text, attributes }: BuilderTextProps) => (
     <div
-      {...props}
-      className={cn('prose max-w-full break-words', props.className)}
+      {...attributes}
+      className={cn('prose max-w-full break-words', attributes?.className)}
       dangerouslySetInnerHTML={{
-        __html: sanitizeHtml(props.text, {
+        __html: sanitizeHtml(text, {
           allowedAttributes: {
             '*': ['style', 'class'],
             a: ['href'],
@@ -32,20 +34,8 @@ function Text(props: TextProps & BuilderComponentAttributes) {
           },
         }),
       }}
+      key={attributes?.key}
     />
-  )
-}
-
-interface BuilderTextProps extends BuilderComponentBaseProps {
-  text: string
-}
-
-Builder.registerComponent(
-  ({ text, attributes }: BuilderTextProps) => (
-    // Replace text-indent with margin-left because Builder.io applies text-indent to nested lists and
-    // RichText uses tailwind typography prose class which doesn't apply text-ident to the ::marker pseudo-element
-    // So we need to use margin-left instead
-    <Text {...attributes} key={attributes?.key} text={text} />
   ),
   {
     name: 'Text',
