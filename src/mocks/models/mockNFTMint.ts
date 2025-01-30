7066ae3da44a4d013524a37df95a17acf6a96e38
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-01-30 11:08:45 -0300

    feat: add tenantId to models for multi-country support
    Add tenantId field with a default value of "US" to various Prisma models including User, UserAction, UserActionOptIn, UserActionEmail, UserActionCall, UserActionDonation, NFTMint, UserActionVotingDay, UserActionViewKeyRaces, and UserActionVoterRegistration.
    
    Update mock generation functions to randomly assign tenantId from supported countries.

--- src/mocks/models/mockNFTMint.ts
+++ src/mocks/models/mockNFTMint.ts
@@ -7,6 +7,7 @@ import { mockCommonDatetimes } from '@/mocks/mockCommonDatetimes'
 import { NFT_SLUG_BACKEND_METADATA } from '@/utils/server/nft/constants'
 import { MOCK_CURRENT_ETH_USD_EXCHANGE_RATE } from '@/utils/shared/exchangeRate'
 import { NFTSlug } from '@/utils/shared/nft'
+import { ORDERED_SUPPORTED_COUNTRIES } from '@/utils/shared/supportedCountries'
 
 export function mockCreateNFTMintInput() {
   const status = faker.helpers.arrayElement(Object.values(NFTMintStatus))
@@ -31,6 +32,7 @@ export function mockCreateNFTMintInput() {
     costAtMintCurrencyCode: NFTCurrency.ETH,
     contractAddress: NFT_SLUG_BACKEND_METADATA[nftSlug].contractAddress,
     costAtMintUsd: costAtMint.times(MOCK_CURRENT_ETH_USD_EXCHANGE_RATE),
+    tenantId: faker.helpers.arrayElement(Object.values(ORDERED_SUPPORTED_COUNTRIES)),
   } satisfies Prisma.NFTMintCreateInput
 }
 export function mockNFTMint(): NFTMint {
