119dbbc21d908f5c1e8e78f10c7b26e5985da9f8
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-01-30 15:06:39 -0300

    feat: add tenantId to mock generation functions for multi-country support

--- src/mocks/models/mockUserActionTweetAtPerson.ts
+++ src/mocks/models/mockUserActionTweetAtPerson.ts
@@ -1,10 +1,13 @@
+import { faker } from '@faker-js/faker'
 import { Prisma, UserActionTweetAtPerson } from '@prisma/client'
 
 import { fakerFields } from '@/mocks/fakerUtils'
+import { ORDERED_SUPPORTED_COUNTRIES } from '@/utils/shared/supportedCountries'
 
 export function mockUserActionTweetAtPersonInput() {
   return {
     recipientDtsiSlug: fakerFields.dtsiSlug(),
+    tenantId: faker.helpers.arrayElement(Object.values(ORDERED_SUPPORTED_COUNTRIES)),
   } satisfies Prisma.UserActionTweetAtPersonCreateInput
 }
 
