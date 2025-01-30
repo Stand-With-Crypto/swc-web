15c27ca300b794e17c72001867e89a558d2569a2
Olavo Parno <olavo.parno@coinbase.com>
2025-01-30 20:18:53 -0300

    refactor: remove hydration check from Builder.io rendering

--- src/components/app/builder/builderComponent.tsx
+++ src/components/app/builder/builderComponent.tsx
@@ -4,24 +4,18 @@ import type { ComponentProps } from 'react'
 import { Builder, BuilderComponent } from '@builder.io/react'
 import { notFound } from 'next/navigation'
 
-import { useHasHydrated } from '@/hooks/useHasHydrated'
 import { useSession } from '@/hooks/useSession'
 import { BuilderState } from '@/utils/web/builder/types'
 
 type BuilderPageProps = ComponentProps<typeof BuilderComponent>
 
 export function RenderBuilderContent(props: BuilderPageProps) {
-  const hasHydrated = useHasHydrated()
   const session = useSession()
 
   const builderData: BuilderState = {
     // This prop should only be used in the context of the Builder.io editor when editing or previewing
     // We should never use it for the actual rendering of the component
     mockIsAuthenticated: session.isLoggedIn,
-  }
-
-  if (!hasHydrated) {
-    return null
   }
 
   if (props.content || Builder.isEditing) {
