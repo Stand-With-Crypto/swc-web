9b3a8823e2144a084830b10c79ccb1bbfd5e1a74
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-02-12 17:12:14 -0300

    Fix TS

--- src/utils/shared/activeUserAction.ts
+++ src/utils/shared/activeUserAction.ts
@@ -12,5 +12,6 @@ const ACTIVE_CLIENT_USER_ACTION_TYPES = [
   UserActionType.VOTER_ATTESTATION,
   UserActionType.VOTING_INFORMATION_RESEARCHED,
   UserActionType.VOTING_DAY,
+  UserActionType.REFER,
 ] as const
 export type ActiveClientUserActionType = (typeof ACTIVE_CLIENT_USER_ACTION_TYPES)[number]
