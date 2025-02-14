db6fe1f074fc864afc5994a17bfb22397975d663
Olavo Parno <olavoparno@gmail.com>
2025-02-14 17:50:07 -0300

    Feat/add poll builder data (#1860)
    * feat: add UserActionPoll and UserActionPollAnswer models to support poll user actions
    
    * chore: include UserActionPollAnswers in full profile info query
    
    * refactor: standardize poll campaign name with DEFAULT_POLL_ID
    
    * refactor: rename `isOther` to `isOtherAnswer` in UserActionPollAnswer model
    
    * feat: add polls data model and validation schema
    
    * feat: add poll action to recent activity row
    
    * chore: add comment for default poll campaign ID
    
    ---------
    
    Co-authored-by: Olavo Parno <olavo.parno@coinbase.com>

---
+++ src/utils/server/builder/models/data/polls.ts
@@ -0,0 +1,74 @@
+import * as Sentry from '@sentry/nextjs'
+import pRetry from 'p-retry'
+
+import { builderSDKClient } from '@/utils/server/builder/builderSDKClient'
+import { BuilderDataModelIdentifiers } from '@/utils/server/builder/models/data/constants'
+import { SWCPoll, zodPollSchemaValidation } from '@/utils/shared/getSWCPolls'
+import { getLogger } from '@/utils/shared/logger'
+import { NEXT_PUBLIC_ENVIRONMENT } from '@/utils/shared/sharedEnv'
+
+const logger = getLogger(`builderIOPolls`)
+
+const LIMIT = 100
+
+async function getAllPollsWithOffset(offset: number) {
+  return await pRetry(
+    () =>
+      builderSDKClient.getAll(BuilderDataModelIdentifiers.POLLS, {
+        query: {
+          ...(NEXT_PUBLIC_ENVIRONMENT === 'production' && { published: 'published' }),
+        },
+        includeUnpublished: NEXT_PUBLIC_ENVIRONMENT !== 'production',
+        cacheSeconds: 60,
+        limit: LIMIT,
+        fields:
+          'id,name,data.maxNumberOptionsSelected,data.pollList,data.pollTitle,data.allowOther,data.multiple',
+        offset,
+      }),
+    {
+      retries: 3,
+      minTimeout: 10000,
+    },
+  )
+}
+
+export async function getPolls() {
+  try {
+    let offset = 0
+
+    const entries = await getAllPollsWithOffset(offset)
+
+    while (entries.length === LIMIT + offset) {
+      offset += entries.length
+      entries.push(...(await getAllPollsWithOffset(offset)))
+    }
+
+    const filteredIncompletePolls = entries
+      .map(entry => {
+        const validEntry = zodPollSchemaValidation.safeParse(entry)
+
+        if (!validEntry.success) {
+          logger.warn('invalid poll entry', {
+            entry,
+            validEntry,
+          })
+
+          return null
+        }
+
+        return validEntry.data
+      })
+      .filter(Boolean) as SWCPoll[]
+
+    if (filteredIncompletePolls.length === 0) return null
+
+    return filteredIncompletePolls
+  } catch (e) {
+    logger.error('error getting all polls:' + e)
+    Sentry.captureException(e, {
+      level: 'error',
+      tags: { domain: 'builder.io', model: 'polls' },
+    })
+    return null
+  }
+}
