c1389070204fb09d318b2dbfb7315f24ab87fa0b
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-02-17 11:08:39 -0300

    Update address mock

--- src/mocks/models/mockAddress.ts
+++ src/mocks/models/mockAddress.ts
@@ -25,18 +25,22 @@ const getDistrict = (stateCode: USStateCode) => {
 export function mockCreateAddressInput() {
   const countryCode = faker.helpers.arrayElement(Object.values(ORDERED_SUPPORTED_COUNTRIES))
 
+  const stateCode = faker.location.state({ abbreviated: true })
+  const stateDistrictCount = US_STATE_CODE_TO_DISTRICT_COUNT_MAP[stateCode as USStateCode]
+  const district = faker.number.int({ min: 1, max: stateDistrictCount })
+
   const partial = {
     googlePlaceId: null,
     streetNumber: faker.location.buildingNumber(),
     route: faker.location.street(),
     subpremise: faker.location.secondaryAddress(),
     locality: faker.location.city(),
-    administrativeAreaLevel1: faker.location.state({ abbreviated: true }),
+    administrativeAreaLevel1: stateCode,
     administrativeAreaLevel2: '',
     postalCode: faker.location.zipCode(),
     postalCodeSuffix: '',
     countryCode: countryCode.toUpperCase(),
-    usCongressionalDistrict: '12',
+    usCongressionalDistrict: district.toString(),
     tenantId: countryCode,
   } satisfies Partial<Prisma.AddressCreateInput>
   return {
