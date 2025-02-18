79ec10c489c2abc274b32201cc60ae9c066d70ee
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-02-18 11:19:58 -0300

    Fix test

--- src/mocks/models/mockAddress.ts
+++ src/mocks/models/mockAddress.ts
@@ -25,11 +25,32 @@ const getDistrict = (stateCode: USStateCode) => {
 export function mockCreateAddressInput() {
   const countryCode = faker.helpers.arrayElement(Object.values(ORDERED_SUPPORTED_COUNTRIES))
 
-  // Faker location.state does not generates DC
+  const partial = {
+    googlePlaceId: null,
+    streetNumber: faker.location.buildingNumber(),
+    route: faker.location.street(),
+    subpremise: faker.location.secondaryAddress(),
+    locality: faker.location.city(),
+    administrativeAreaLevel1: faker.location.state({ abbreviated: true }),
+    administrativeAreaLevel2: '',
+    postalCode: faker.location.zipCode(),
+    postalCodeSuffix: '',
+    countryCode: countryCode.toUpperCase(),
+    usCongressionalDistrict: '12',
+    tenantId: countryCode,
+  } satisfies Partial<Prisma.AddressCreateInput>
+  return {
+    ...partial,
+    formattedDescription: `${partial.streetNumber} ${partial.route}, ${partial.subpremise}, ${partial.locality} ${partial.administrativeAreaLevel1}, ${partial.postalCode} ${partial.countryCode}`,
+  } satisfies Prisma.AddressCreateInput
+}
+
+export function mockCreateAddressInputWithDC() {
+  const countryCode = faker.helpers.arrayElement(Object.values(ORDERED_SUPPORTED_COUNTRIES))
+
   const stateCode =
     faker.helpers.maybe(() => 'DC', { probability: 0.05 }) ||
     faker.location.state({ abbreviated: true })
-
   const district = getDistrict(stateCode as USStateCode)
 
   const partial = {
@@ -38,7 +59,7 @@ export function mockCreateAddressInput() {
     route: faker.location.street(),
     subpremise: faker.location.secondaryAddress(),
     locality: faker.location.city(),
-    administrativeAreaLevel1: stateCode,
+    administrativeAreaLevel1: faker.location.state({ abbreviated: true }),
     administrativeAreaLevel2: '',
     postalCode: faker.location.zipCode(),
     postalCodeSuffix: '',
@@ -51,9 +72,14 @@ export function mockCreateAddressInput() {
     formattedDescription: `${partial.streetNumber} ${partial.route}, ${partial.subpremise}, ${partial.locality} ${partial.administrativeAreaLevel1}, ${partial.postalCode} ${partial.countryCode}`,
   } satisfies Prisma.AddressCreateInput
 }
-export function mockAddress(): Address {
+
+export function mockAddress(includeDC = false): Address {
   return {
-    ...mockCreateAddressInput(),
+    /**
+     * Need to ensure our tests are not affected by the DC change.
+     * ANY address related changes made to the mockCreateAddressInput affects the seed output.
+     */
+    ...(includeDC ? mockCreateAddressInputWithDC() : mockCreateAddressInput()),
     ...mockCommonDatetimes(),
     id: fakerFields.id(),
   }
