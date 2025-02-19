8ad72387cbe7ac54323f350c7d7255f00aa2301f
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-02-19 00:58:13 -0300

    Refactor getRankingData to use zodStateDistrict

--- src/utils/server/districtRankings/constants.ts
+++ src/utils/server/districtRankings/constants.ts
@@ -2,3 +2,5 @@ export const REDIS_KEYS = {
   DISTRICT_ADVOCATES_RANKING: 'district:ranking:advocates',
   DISTRICT_REFERRALS_RANKING: 'district:ranking:referrals',
 } as const
+
+export const CURRENT_DISTRICT_RANKING = REDIS_KEYS.DISTRICT_ADVOCATES_RANKING
