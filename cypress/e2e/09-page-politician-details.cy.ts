0b01c18c5f37b756e95eb48377be9ee5442bfa77
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2025-02-20 15:19:24 -0300

    test: change politicians details test

--- cypress/e2e/09-page-politician-details.cy.ts
+++ cypress/e2e/09-page-politician-details.cy.ts
@@ -1,15 +1,17 @@
 /// <reference types="cypress" />
 
 describe('page - politician details', () => {
-  it.only('page with questionnaire', () => {
+  it('page with questionnaire', () => {
     cy.visit('/politicians/person/adam---schiff')
 
-    cy.get('h2').contains('Candidate questionnaire')
+    cy.get('h2').contains('Candidate questionnaire').should('be.visible')
 
-    cy.get('[data-testid="questionnaire-trigger"]').as('questionnaireTrigger').scrollIntoView()
-    cy.get('@questionnaireTrigger').should('be.visible').click()
+    cy.get('button[data-testid="questionnaire-trigger"]')
+      .as('questionnaireTrigger')
+      .should('be.visible')
+      .click()
 
-    cy.get('div')
+    cy.get('[data-testid="q1ExperienceUsingBlockchainTechnology"]')
       .should(
         'contain',
         'Do you have experience buying, selling, or using blockchain technology assets or investment tools?',
@@ -17,7 +19,7 @@ describe('page - politician details', () => {
       .find('strong')
       .should('contain', 'A: No')
 
-    cy.get('div')
+    cy.get('[data-testid="q2BlockchainWillPlayMajorRoleNextInnoWave"]')
       .should(
         'contain',
         'Do you believe blockchain technology and digital assets, including cryptocurrency like Bitcoin, will play a major role in the next wave of technological innovation globally?',
@@ -25,7 +27,7 @@ describe('page - politician details', () => {
       .find('strong')
       .should('contain', 'A: Yes')
 
-    cy.get('div')
+    cy.get('[data-testid="q3AmerCryptoIsDrivingEconomicGrowth"]')
       .should(
         'contain',
         'Do you believe the American cryptocurrency and digital asset industry is driving economic growth and supporting millions of jobs across the country?',
@@ -33,7 +35,7 @@ describe('page - politician details', () => {
       .find('strong')
       .should('contain', 'A: Yes')
 
-    cy.get('div')
+    cy.get('[data-testid="q4UsCompAtRiskIfDigitalAssetsPushedOverse"]')
       .should(
         'contain',
         'Do you believe US competitiveness and American national security are at risk if the digital asset industry is pushed overseas?',
@@ -41,7 +43,7 @@ describe('page - politician details', () => {
       .find('strong')
       .should('contain', 'A: Yes')
 
-    cy.get('div')
+    cy.get('[data-testid="q5UsModernizeRegulatoryEnvironmentForCrypto"]')
       .should(
         'contain',
         'Do you believe it is important for the United States to modernize the regulatory environment for crypto and digital assets to ensure proper consumer protection while also fostering responsible innovation?',
@@ -49,7 +51,7 @@ describe('page - politician details', () => {
       .find('strong')
       .should('contain', 'A: Yes')
 
-    cy.get('div')
+    cy.get('[data-testid="q6WouldYouVoteInFavorOfLegislation"]')
       .should(
         'contain',
         'If you are currently a Member of Congress or are elected to Congress, would you vote in favor of legislation that creates a comprehensive regulatory framework for digital assets like HR 4763, the “Financial Innovation and Technology for the 21st Century Act”, a bipartisan bill?',
@@ -57,7 +59,7 @@ describe('page - politician details', () => {
       .find('strong')
       .should('contain', 'A: Yes')
 
-    cy.get('div')
+    cy.get('[data-testid="q7VoteInFavorOfLegisToPaymentStablecoins"]')
       .should(
         'contain',
         'If you are currently a Member of Congress or are elected to Congress, would you vote in favor of legislation to create clear rules for payment stablecoins (i.e., digital assets that are redeemable for U.S. dollars 1:1) like HR 4766, “Clarity for Payment Stablecoins Act of 2023”, a bipartisan bill?',
@@ -65,7 +67,7 @@ describe('page - politician details', () => {
       .find('strong')
       .should('contain', 'A: Yes')
 
-    cy.get('div')
+    cy.get('[data-testid="q8ShareAnyOtherOpinionsOnCrypto"]')
       .should(
         'contain',
         'Please share any other positions or opinions that you have on how crypto and digital assets should be regulated?',
