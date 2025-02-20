a0038dea9f8f0d6082f6c6f9c033e76ab6adb7a0
eduardopicolo-cb <167582510+eduardopicolo-cb@users.noreply.github.com>
2025-02-20 12:00:38 -0300

    Trigger REFER action creation on signup (#1862)
    * Create triggerReferralSteps & handlers
    
    * Add triggerReferralSteps to signup flow
    
    * Add test case
    
    * Fix localUser not available inside `after` function
    
    * Create zodReferralId
    
    * Update triggerReferralSteps logic

---
+++ src/actions/actionCreateUserActionReferral.ts
@@ -0,0 +1,134 @@
+'use server'
+import 'server-only'
+
+import { UserActionType } from '@prisma/client'
+import * as Sentry from '@sentry/nextjs'
+import { waitUntil } from '@vercel/functions'
+import { z } from 'zod'
+
+import { getClientUser } from '@/clientModels/clientUser/clientUser'
+import { prismaClient } from '@/utils/server/prismaClient'
+import { getServerAnalytics } from '@/utils/server/serverAnalytics'
+import { zodServerLocalUser } from '@/utils/server/serverLocalUser'
+import { getLogger } from '@/utils/shared/logger'
+import { USER_ACTION_TO_CAMPAIGN_NAME_DEFAULT_MAP } from '@/utils/shared/userActionCampaigns'
+import { withSafeParseWithMetadata } from '@/utils/shared/zod'
+import { zodReferralId } from '@/validation/fields/zodReferrald'
+
+const logger = getLogger(`actionCreateUserActionReferral`)
+
+const zodUserActionReferralInput = z.object({
+  referralId: zodReferralId,
+  userId: z.string().uuid('Invalid user ID format'),
+  /**
+   * Calling the server action from Nextjs `after` may not have the client cookies.
+   * This is why we accept the localUser as an optional parameter.
+   */
+  localUser: zodServerLocalUser.nullable(),
+})
+
+type Input = z.infer<typeof zodUserActionReferralInput>
+
+export async function actionCreateUserActionReferral(input: Input) {
+  logger.info('triggered', input)
+
+  const validatedFields = withSafeParseWithMetadata(zodUserActionReferralInput, input)
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      errorsMetadata: validatedFields.errorsMetadata,
+    }
+  }
+  const { referralId, userId, localUser } = validatedFields.data
+
+  const user = await prismaClient.user.findFirstOrThrow({
+    where: { id: userId },
+    include: {
+      primaryUserCryptoAddress: true,
+      address: true,
+    },
+  })
+  if (!user) {
+    logger.error('user not found', { userId })
+    return {
+      errors: { userId: ['User not found'] },
+    }
+  }
+
+  const analytics = getServerAnalytics({
+    userId: user.id,
+    localUser,
+  })
+  const beforeFinish = () => Promise.all([analytics.flush()])
+
+  const referrer = await prismaClient.user.findFirst({
+    where: { referralId },
+    select: {
+      id: true,
+      userActions: {
+        select: {
+          id: true,
+          actionType: true,
+          campaignName: true,
+          userActionRefer: true,
+        },
+      },
+    },
+  })
+
+  if (!referrer) {
+    logger.info(`no referrer found with referralId: ${validatedFields.data.referralId}`)
+    Sentry.captureMessage(`no referrer found with referralId: ${validatedFields.data.referralId}`, {
+      tags: {
+        domain: 'actionCreateUserActionReferral',
+      },
+      extra: {
+        referralId,
+        userId,
+      },
+      level: 'error',
+    })
+    waitUntil(beforeFinish())
+    return { user: getClientUser(user) }
+  }
+
+  logger.info(`found referrer ${referrer.id}`)
+
+  const existingReferAction = referrer.userActions.find(
+    action => action.actionType === UserActionType.REFER,
+  )
+
+  if (existingReferAction?.userActionRefer) {
+    await prismaClient.userActionRefer.update({
+      where: { id: existingReferAction.id },
+      data: {
+        referralsCount: { increment: 1 },
+      },
+    })
+    logger.info(`incremented referral count for referrer ${referrer.id}`)
+  } else {
+    await prismaClient.userAction.create({
+      data: {
+        userId: referrer.id,
+        actionType: UserActionType.REFER,
+        campaignName: USER_ACTION_TO_CAMPAIGN_NAME_DEFAULT_MAP[UserActionType.REFER],
+        userActionRefer: {
+          create: {
+            referralsCount: 1,
+          },
+        },
+      },
+    })
+    logger.info(`created REFER action for referrer ${referrer.id}`)
+  }
+
+  analytics.trackUserActionCreated({
+    actionType: UserActionType.REFER,
+    campaignName: USER_ACTION_TO_CAMPAIGN_NAME_DEFAULT_MAP[UserActionType.REFER],
+    creationMethod: 'On Site',
+    userState: 'Existing',
+  })
+
+  waitUntil(beforeFinish())
+  return { user: getClientUser(user) }
+}
---
+++ src/utils/server/referral/triggerReferralSteps.ts
@@ -0,0 +1,45 @@
+import * as Sentry from '@sentry/nextjs'
+import { after } from 'next/server'
+
+import { actionCreateUserActionReferral } from '@/actions/actionCreateUserActionReferral'
+import { ServerLocalUser } from '@/utils/server/serverLocalUser'
+import { getLogger } from '@/utils/shared/logger'
+
+const logger = getLogger('triggerReferralSteps')
+
+export function triggerReferralSteps({
+  localUser,
+  searchParams,
+  userId,
+}: {
+  localUser: ServerLocalUser | null
+  searchParams: Record<string, string | undefined>
+  userId: string
+}) {
+  const referralId =
+    searchParams?.utm_campaign ??
+    localUser?.persisted?.initialSearchParams?.utm_campaign ??
+    localUser?.currentSession?.searchParamsOnLoad?.utm_campaign ??
+    ''
+
+  logger.info(`referralId "${referralId}", newUserId "${userId}"`)
+
+  if (!referralId) {
+    logger.error('invalid logic, referral has no referralId')
+    Sentry.captureMessage(
+      'invalid logic, we should only hit this point if the referralId is present',
+      {
+        tags: {
+          domain: 'referral',
+        },
+        extra: { referralId, searchParams, localUser, userId },
+      },
+    )
+    return
+  }
+
+  after(async () => {
+    await actionCreateUserActionReferral({ referralId, userId, localUser })
+    // sendReferralEmail() TODO
+  })
+}
