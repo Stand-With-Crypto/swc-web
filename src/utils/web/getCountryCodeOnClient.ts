f6ef291902b7cc242e316c1a520dfde0dc33a3d8
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-24 22:53:24 -0300

    feat: add browser check in getCountryCodeOnClient function

--- src/utils/web/getCountryCodeOnClient.ts
+++ src/utils/web/getCountryCodeOnClient.ts
@@ -1,12 +1,15 @@
 import * as Sentry from '@sentry/nextjs'
 import Cookies from 'js-cookie'
 
+import { isBrowser } from '@/utils/shared/executionEnvironment'
 import {
   COUNTRY_CODE_REGEX_PATTERN,
   SWC_CURRENT_PAGE_COUNTRY_CODE_COOKIE_NAME,
 } from '@/utils/shared/supportedCountries'
 
 export function getCountryCodeOnClient() {
+  if (!isBrowser) return ''
+
   const countryCode = Cookies.get(SWC_CURRENT_PAGE_COUNTRY_CODE_COOKIE_NAME)
 
   if (!countryCode) {
