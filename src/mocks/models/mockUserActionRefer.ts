1ae7cbfe1f7819404837ddee60cbd03d33740f3d
eduardopicolo-cb <167582510+eduardopicolo-cb@users.noreply.github.com>
2025-02-25 12:59:58 -0300

    Referrals ranking cron job (#1864)
    * WIP: Query ranking data
    
    * Ranking upsert/read functions
    
    * Inngest cron job
    
    * Update address mock
    
    * Improve logging
    
    * Fix mockAddress
    
    * Include DC State in the rankings
    
    * Add address index
    
    * Fix test
    
    * Create zodStateDistrict validation
    
    * Fix getDistrictRanking
    
    * Fix invalid ZSET member key entries
    
    * Refactor getRankingData to use zodStateDistrict
    
    * Remove index
    
    * Move upserter creation to steps
    
    * Improve query performance

--- src/mocks/models/mockUserActionRefer.ts
+++ src/mocks/models/mockUserActionRefer.ts
@@ -6,7 +6,7 @@ import { ORDERED_SUPPORTED_COUNTRIES } from '@/utils/shared/supportedCountries'
 
 export function mockCreateUserActionReferInput() {
   return {
-    referralsCount: faker.number.int({ min: 0, max: 100 }),
+    referralsCount: faker.number.int({ min: 1, max: 100 }),
     tenantId: faker.helpers.arrayElement(Object.values(ORDERED_SUPPORTED_COUNTRIES)),
   } satisfies Prisma.UserActionReferCreateInput
 }
