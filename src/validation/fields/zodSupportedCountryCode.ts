9c7320abd0c2d9bcc66617d93c5b8f15f3e3c59b
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-26 12:23:09 -0300

    feat: transform country code to support UK as 'uk' in validation (#1916)

--- src/validation/fields/zodSupportedCountryCode.ts
+++ src/validation/fields/zodSupportedCountryCode.ts
@@ -2,9 +2,14 @@ import { string } from 'zod'
 
 import { ORDERED_SUPPORTED_COUNTRIES } from '@/utils/shared/supportedCountries'
 
-export const zodSupportedCountryCode = string().refine(
-  value => ORDERED_SUPPORTED_COUNTRIES.includes(value?.toLowerCase()),
-  {
+export const zodSupportedCountryCode = string()
+  .transform(value => {
+    const lowerCaseCountryCode = value?.toLowerCase()
+
+    // TODO(@twistershark): After we start supporting UK, we should return SupportedCountryCodes.UK instead of 'uk'
+    if (lowerCaseCountryCode === 'gb') return 'uk'
+    return lowerCaseCountryCode
+  })
+  .refine(value => ORDERED_SUPPORTED_COUNTRIES.includes(value), {
     message: 'Invalid country code',
-  },
-)
+  })
