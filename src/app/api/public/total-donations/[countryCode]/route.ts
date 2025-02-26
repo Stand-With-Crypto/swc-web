84e10764a3602cbdcd70d251beebbe31c56d0ebb
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-26 10:48:55 -0300

    feat: validate country code in top-level metrics and total donations endpoints

--- src/app/api/public/total-donations/[countryCode]/route.ts
+++ src/app/api/public/total-donations/[countryCode]/route.ts
@@ -3,6 +3,7 @@ import 'server-only'
 import { NextResponse } from 'next/server'
 
 import { getSumDonations } from '@/data/aggregations/getSumDonations'
+import { zodSupportedCountryCode } from '@/validation/fields/zodSupportedCountryCode'
 
 export const revalidate = 30 // 30 seconds
 export const dynamic = 'error'
@@ -15,6 +16,13 @@ interface RequestContext {
 
 export async function GET(_: Request, { params }: RequestContext) {
   const { countryCode } = await params
+
+  const isValid = zodSupportedCountryCode.safeParse(countryCode)
+
+  if (!isValid.success) {
+    return new NextResponse('Invalid country code', { status: 400 })
+  }
+
   const data = await getSumDonations({ countryCode })
   return NextResponse.json(data)
 }
