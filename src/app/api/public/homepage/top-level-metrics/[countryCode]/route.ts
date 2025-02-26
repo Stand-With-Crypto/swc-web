b3d2065486da85b2d2d3e24ccd8b11d68e8eb7ab
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-26 09:01:59 -0300

    feat: create route for top-level metrics with countryCode parameter

--- src/app/api/public/homepage/top-level-metrics/[countryCode]/route.ts
+++ src/app/api/public/homepage/top-level-metrics/[countryCode]/route.ts
@@ -7,7 +7,14 @@ import { getHomepageTopLevelMetrics } from '@/data/pageSpecific/getHomepageData'
 export const revalidate = 30 // 30 seconds
 export const dynamic = 'error'
 
-export async function GET() {
-  const data = await getHomepageTopLevelMetrics()
+interface RequestContext {
+  params: Promise<{
+    countryCode: string
+  }>
+}
+
+export async function GET(_: Request, { params }: RequestContext) {
+  const { countryCode } = await params
+  const data = await getHomepageTopLevelMetrics({ countryCode })
   return NextResponse.json(data)
 }
