c218d1454a9cc9da3e8308aa9c6a09c2dc4302ff
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-02-26 15:32:11 -0300

    fix: add Sentry error tracking to queryDTSIBillDetails

--- src/data/dtsi/queries/queryDTSIBillDetails/index.ts
+++ src/data/dtsi/queries/queryDTSIBillDetails/index.ts
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/nextjs'
+
 import { fetchDTSI } from '@/data/dtsi/fetchDTSI'
 import { DTSI_BillDetailsQuery, DTSI_BillDetailsQueryVariables } from '@/data/dtsi/generated'
 import { dtsiBillDetailsQueryString } from '@/data/dtsi/queries/queryDTSIBillDetails/dtsiBillDetailsQueryString'
@@ -9,4 +11,10 @@ export const queryDTSIBillDetails = (id: string) =>
     id,
   })
     .then(res => res.bill)
-    .catch(() => null)
+    .catch(error => {
+      Sentry.captureException(error, {
+        extra: { id },
+        tags: { domain: 'queryDTSIBillDetails' },
+      })
+      return null
+    })
