2f2a539fbb5cc6c84c555547ae829da4bbb23479
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-26 08:26:34 -0300

    feat: add countryCode parameter to getCountUsers function for tenant-specific user counting

--- src/data/aggregations/getCountUsers.ts
+++ src/data/aggregations/getCountUsers.ts
@@ -3,8 +3,12 @@ import 'server-only'
 import { prismaClient } from '@/utils/server/prismaClient'
 import { NEXT_PUBLIC_ENVIRONMENT } from '@/utils/shared/sharedEnv'
 
-export const getCountUsers = async () => {
-  const count = await prismaClient.user.count()
+interface GetCountUsersProps {
+  countryCode: string
+}
+
+export const getCountUsers = async ({ countryCode }: GetCountUsersProps) => {
+  const count = await prismaClient.user.count({ where: { tenantId: countryCode } })
   if (NEXT_PUBLIC_ENVIRONMENT === 'production') {
     return { count }
   }
