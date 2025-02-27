9f7a4cb08a5242f3db82a9f650e2b95591a521fa
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-27 15:23:01 -0300

    feat: add StaticPageProps type for improved parameter handling in page props

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,6 +9,11 @@ export type PageProps<Params = object> = {
   searchParams?: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
+export type StaticPageProps<Params = object> = {
+  params: Promise<Params>
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
 // ensures that optional fields are at least queried for in graphql requests
 export type PartialButDefined<T> = {
   [P in keyof T]: T[P] | undefined | null
