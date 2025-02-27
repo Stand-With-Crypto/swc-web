c6aa1424c9ec1c5da92949ebdadb4d4030a855fe
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-27 15:20:47 -0300

    feat: add support for British English locale

--- src/utils/shared/supportedLocales.ts
+++ src/utils/shared/supportedLocales.ts
@@ -1,5 +1,9 @@
 export enum SupportedLocale {
   EN_US = 'en-US',
+  EN_GB = 'en-GB',
 }
 export const DEFAULT_LOCALE = SupportedLocale.EN_US
-export const ORDERED_SUPPORTED_LOCALES: readonly SupportedLocale[] = [SupportedLocale.EN_US]
+export const ORDERED_SUPPORTED_LOCALES: readonly SupportedLocale[] = [
+  SupportedLocale.EN_US,
+  SupportedLocale.EN_GB,
+]
