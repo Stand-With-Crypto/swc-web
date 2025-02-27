7cd3907cd0476e27bbbc849d8b1205ada44859bb
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-27 12:14:15 -0300

    feat: replace getCountryCodeOnClient with getCountryCodeForClientAnalytics for improved country code retrieval

--- src/utils/web/getCountryCodeForClientAnalytics.ts
+++ src/utils/web/getCountryCodeForClientAnalytics.ts
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie'
 import { isBrowser } from '@/utils/shared/executionEnvironment'
 import { SWC_CURRENT_PAGE_COUNTRY_CODE_COOKIE_NAME } from '@/utils/shared/supportedCountries'
 
-export function getCountryCodeOnClient() {
+export function getCountryCodeForClientAnalytics() {
   if (!isBrowser) return ''
 
   const countryCode = Cookies.get(SWC_CURRENT_PAGE_COUNTRY_CODE_COOKIE_NAME)
