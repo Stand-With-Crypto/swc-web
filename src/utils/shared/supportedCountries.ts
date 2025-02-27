ac0fd0ef4db333884e20660452104244674b7fa1
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-02-27 15:27:53 -0300

    feat: add support for GB country code and update related mappings

--- src/utils/shared/supportedCountries.ts
+++ src/utils/shared/supportedCountries.ts
@@ -2,18 +2,19 @@ import { SupportedLocale } from '@/utils/shared/supportedLocales'
 
 export enum SupportedCountryCodes {
   US = 'us',
+  GB = 'gb',
 }
 
 export const DEFAULT_SUPPORTED_COUNTRY_CODE = SupportedCountryCodes.US
 export const ORDERED_SUPPORTED_COUNTRIES: readonly SupportedCountryCodes[] = [
   SupportedCountryCodes.US,
+  SupportedCountryCodes.GB,
 ]
 
 export const COUNTRY_CODE_TO_LOCALE: Record<SupportedCountryCodes, SupportedLocale> = {
   [SupportedCountryCodes.US]: SupportedLocale.EN_US,
+  [SupportedCountryCodes.GB]: SupportedLocale.EN_GB,
 }
-
-export const SWC_CURRENT_PAGE_COUNTRY_CODE_COOKIE_NAME = 'SWC_CURRENT_PAGE_COUNTRY_CODE'
 
 // Two lowercase letters (e.g., "us", "uk")
 export const COUNTRY_CODE_REGEX_PATTERN = new RegExp(
