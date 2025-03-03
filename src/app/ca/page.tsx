e38fb66d9d62805e2f6492ac47795ac4c5dcd09e
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2025-03-03 11:33:26 -0300

    feat: add blank pages

---
+++ src/app/ca/page.tsx
@@ -0,0 +1,9 @@
+import { PageTitle } from '@/components/ui/pageTitleText'
+
+export default function GbHomePage() {
+  return (
+    <div className="container">
+      <PageTitle>CA Home Page</PageTitle>
+    </div>
+  )
+}
