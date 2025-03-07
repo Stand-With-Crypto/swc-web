945222c32d18a86dc34b58050c649b4b548d9409
Olavo Parno <olavo.parno@coinbase.com>
2025-03-07 13:04:48 -0300

    feat: add country-specific login content and simplify page layout

--- src/components/app/layout/layout.tsx
+++ src/components/app/layout/layout.tsx
@@ -3,6 +3,7 @@ import NextTopLoader from 'nextjs-toploader'
 
 import { TopLevelClientLogic } from '@/app/[countryCode]/topLevelClientLogic'
 import { CookieConsent } from '@/components/app/cookieConsent'
+import { GoogleTagManager } from '@/components/app/googleTagManager'
 import { NavBarGlobalBanner } from '@/components/app/navbarGlobalBanner'
 import { OverrideGlobalLocalStorage } from '@/components/app/overrideGlobalLocalStorage'
 import { FullHeight } from '@/components/ui/fullHeight'
@@ -13,9 +14,11 @@ import { fontClassName } from '@/utils/web/fonts'
 export function PageLayout({
   children,
   countryCode,
-}: React.PropsWithChildren<{ countryCode: SupportedCountryCodes }>) {
+  shouldRenderGTM,
+}: React.PropsWithChildren<{ countryCode: SupportedCountryCodes; shouldRenderGTM?: boolean }>) {
   return (
     <html lang={COUNTRY_CODE_TO_LOCALE[countryCode]} translate="no">
+      {shouldRenderGTM && <GoogleTagManager />}
       <body className={fontClassName}>
         <OverrideGlobalLocalStorage />
         <NextTopLoader
