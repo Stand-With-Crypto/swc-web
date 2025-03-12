7b422f5d8766bdb0f04cdc65d1b4712a56ac7180
Lucas Bernardi <45859300+LucasdRossi@users.noreply.github.com>
2025-03-12 18:30:07 -0300

    Revert "feat: international homepage redirect (#1948)" (#1999)
    This reverts commit fd2612f756f9acac697aff46b42559936cafbed4.

--- src/utils/server/countryCodeRouter/index.ts
+++ src/utils/server/countryCodeRouter/index.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import {
   COUNTRY_CODE_REGEX_PATTERN,
   DEFAULT_SUPPORTED_COUNTRY_CODE,
+  SWC_CURRENT_PAGE_COUNTRY_CODE_COOKIE_NAME,
 } from '@/utils/shared/supportedCountries'
 
 function extractCountryCode(pathname: string) {
@@ -22,7 +23,7 @@ function extractCountryCode(pathname: string) {
  * /path → /us/path (rewrite if "us" is default)
  * /gb/path → /gb/path (pass through)
  */
-export function obfuscateURLCountryCode(request: NextRequest): NextResponse {
+export function countryCodeRouter(request: NextRequest): NextResponse {
   const { pathname } = request.nextUrl
   const searchParams = request.nextUrl.search
 
@@ -44,4 +45,30 @@ export function obfuscateURLCountryCode(request: NextRequest): NextResponse {
 
   // For all other country codes, keep them visible in the URL
   return NextResponse.next()
+}
+
+/**
+ * Saves the current country code as a cookie
+ * @param request - The request object
+ * @param response - The response object
+ */
+export function saveCurrentCountryCodeAsCookie(request: NextRequest, response: NextResponse) {
+  const { pathname } = request.nextUrl
+  const maybeCountryCode = extractCountryCode(pathname)
+
+  const countryCode = maybeCountryCode ?? DEFAULT_SUPPORTED_COUNTRY_CODE
+
+  const maybeCurrentPageCountryCodeCookie = request.cookies.get(
+    SWC_CURRENT_PAGE_COUNTRY_CODE_COOKIE_NAME,
+  )?.value
+
+  if (!maybeCurrentPageCountryCodeCookie || maybeCurrentPageCountryCodeCookie !== countryCode) {
+    response.cookies.set({
+      name: SWC_CURRENT_PAGE_COUNTRY_CODE_COOKIE_NAME,
+      value: countryCode,
+      httpOnly: false,
+      sameSite: 'lax',
+      secure: true,
+    })
+  }
 }
