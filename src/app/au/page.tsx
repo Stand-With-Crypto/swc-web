e55d8d3e74532842cfa1ed5e8fe6c71bd0169455
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-03-21 15:40:19 -0300

    feat: add au partners home section

--- src/app/au/page.tsx
+++ src/app/au/page.tsx
@@ -1,11 +1,17 @@
 import { AuPageHome } from '@/components/app/pageHome/au'
 import { getHomepageTopLevelMetrics } from '@/data/pageSpecific/getHomepageData'
+import { getPartners } from '@/utils/server/builder/models/data/partners'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 export const revalidate = 60 // 1 minute
 export const dynamic = 'error'
 
+const countryCode = SupportedCountryCodes.AU
+
 export default async function AuHomePage() {
   const topLevelMetrics = await getHomepageTopLevelMetrics()
 
-  return <AuPageHome topLevelMetrics={topLevelMetrics} />
+  const partners = await getPartners({ countryCode })
+
+  return <AuPageHome partners={partners} topLevelMetrics={topLevelMetrics} />
 }
