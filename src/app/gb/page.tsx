e5d3bdd8538283c681951a6444a0d2012477035e
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-03-21 15:29:47 -0300

    feat: add gb partners home section

--- src/app/gb/page.tsx
+++ src/app/gb/page.tsx
@@ -1,5 +1,7 @@
 import { GbPageHome } from '@/components/app/pageHome/gb'
 import { getHomepageTopLevelMetrics } from '@/data/pageSpecific/getHomepageData'
+import { getPartners } from '@/utils/server/builder/models/data/partners'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 export const revalidate = 60 // 1 minute
 export const dynamic = 'error'
@@ -7,5 +9,7 @@ export const dynamic = 'error'
 export default async function GbHomePage() {
   const topLevelMetrics = await getHomepageTopLevelMetrics()
 
-  return <GbPageHome topLevelMetrics={topLevelMetrics} />
+  const partners = await getPartners({ countryCode: SupportedCountryCodes.GB })
+
+  return <GbPageHome partners={partners} topLevelMetrics={topLevelMetrics} />
 }
--- src/components/app/pageHome/common/types.ts
+++ src/components/app/pageHome/common/types.ts
@@ -1,5 +1,7 @@
 import { GetHomepageTopLevelMetricsResponse } from '@/data/pageSpecific/getHomepageData'
+import { SWCPartners } from '@/utils/shared/zod/getSWCPartners'
 
 export interface HomePageProps {
   topLevelMetrics: GetHomepageTopLevelMetricsResponse
+  partners: SWCPartners | null
 }
