54109bafee384b338ea2f7f27f8685868ea62223
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-03-23 18:42:24 -0300

    feat: send founders to CA home

--- src/app/ca/page.tsx
+++ src/app/ca/page.tsx
@@ -1,6 +1,7 @@
 import { CaPageHome } from '@/components/app/pageHome/ca'
 import { getHomepageTopLevelMetrics } from '@/data/pageSpecific/getHomepageData'
 import { getPublicRecentActivity } from '@/data/recentActivity/getPublicRecentActivity'
+import { getFounders } from '@/utils/server/builder/models/data/founders'
 import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 const countryCode = SupportedCountryCodes.CA
@@ -9,13 +10,20 @@ export const revalidate = 60 // 1 minute
 export const dynamic = 'error'
 
 export default async function CaHomePage() {
-  const [topLevelMetrics, recentActivity] = await Promise.all([
+  const [topLevelMetrics, recentActivity, founders] = await Promise.all([
     getHomepageTopLevelMetrics(),
     getPublicRecentActivity({
       limit: 10,
       countryCode,
     }),
+    getFounders({ countryCode }),
   ])
 
-  return <CaPageHome recentActivity={recentActivity} topLevelMetrics={topLevelMetrics} />
+  return (
+    <CaPageHome
+      recentActivity={recentActivity}
+      topLevelMetrics={topLevelMetrics}
+      founders={founders}
+    />
+  )
 }
