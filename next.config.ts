50281e55b1ab8c47c0a37b0d0cbf9e8f03330913
Olavo Parno <olavo.parno@coinbase.com>
2025-03-24 11:37:34 -0300

    Remove redirects for manifesto and mission pages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -656,26 +656,6 @@ const nextConfig: NextConfig = {
         destination: '/vote?utm_source=cb&utm_medium=push&utm_campaign=vote-adv',
         permanent: true,
       },
-      {
-        source: '/manifesto',
-        destination: '/about',
-        permanent: true,
-      },
-      {
-        source: '/:country/manifesto',
-        destination: '/:country/about',
-        permanent: true,
-      },
-      {
-        source: '/mission',
-        destination: '/about',
-        permanent: true,
-      },
-      {
-        source: '/:country/mission',
-        destination: '/:country/about',
-        permanent: true,
-      },
     ]
   },
   async rewrites() {
