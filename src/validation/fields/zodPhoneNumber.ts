12025ded86bc0e0f16c1113d43ad081a90b84a79
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-03-25 13:50:56 -0300

    Add countryCode to phone number validation

--- src/validation/fields/zodPhoneNumber.ts
+++ src/validation/fields/zodPhoneNumber.ts
@@ -1,9 +1,19 @@
-import { literal, string, union } from 'zod'
+import { literal, string, union, z } from 'zod'
 
 import { normalizePhoneNumber, validatePhoneNumber } from '@/utils/shared/phoneNumber'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 export const zodPhoneNumber = string()
   .refine(validatePhoneNumber, 'Please enter a valid phone number')
   .transform(normalizePhoneNumber)
 
 export const zodOptionalEmptyPhoneNumber = union([zodPhoneNumber, literal('')])
+
+export function zodPhoneNumberWithCountryCode(countryCode: SupportedCountryCodes) {
+  return string()
+    .refine(
+      phoneNumber => validatePhoneNumber(phoneNumber, countryCode),
+      'Please enter a valid phone number',
+    )
+    .transform(normalizePhoneNumber)
+}
