76a43450dc5bc29986fe0857cf7395ecb179cc6e
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-25 10:57:14 -0300

    feat: add CA politicians homepage component
    - Introduce the PoliticiansHomepage component for displaying Canadian politicians.
    - Implement metadata generation with a title and description for the page.
    - Query and sort politician data, limiting the display to the top 100 entries.
    - This component enhances the functionality and structure of the Canadian politicians page.

---
+++ src/app/ca/politicians/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from 'next'
+
+import { sortDTSIPersonDataTable } from '@/components/app/dtsiClientPersonDataTable/sortPeople'
+import {
+  CaPagePoliticians,
+  PAGE_POLITICIANS_DESCRIPTION,
+  PAGE_POLITICIANS_TITLE,
+} from '@/components/app/pagePoliticians/ca'
+import { queryDTSIAllPeople } from '@/data/dtsi/queries/queryDTSIAllPeople'
+import { PageProps } from '@/types'
+import { generateMetadataDetails } from '@/utils/server/metadataUtils'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
+
+export const revalidate = 600 // 10 minutes
+export const dynamic = 'error'
+
+type Props = PageProps
+
+export async function generateMetadata(_props: Props): Promise<Metadata> {
+  return generateMetadataDetails({
+    title: PAGE_POLITICIANS_TITLE,
+    description: PAGE_POLITICIANS_DESCRIPTION,
+  })
+}
+
+export default async function PoliticiansHomepage() {
+  const results = await queryDTSIAllPeople({ countryCode: SupportedCountryCodes.CA })
+
+  const politicians = sortDTSIPersonDataTable(results.people).slice(0, 100)
+  return <CaPagePoliticians {...{ politicians }} />
+}
