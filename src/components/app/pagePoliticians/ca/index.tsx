f5cc996c1735c2ce3318eb3cf78a1963c184dfa1
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-25 10:54:27 -0300

    feat: add CA politicians page component
    - Introduce a new component for displaying Canadian politicians, featuring a title and description.
    - Implement a layout with an introduction section and a data table for politicians.
    - Integrate the CaDTSIClientPersonDataTable for displaying politician data.

---
+++ src/components/app/pagePoliticians/ca/index.tsx
@@ -0,0 +1,22 @@
+import { CaDTSIClientPersonDataTable } from '@/components/app/dtsiClientPersonDataTable/ca'
+import { DTSIPersonDataTablePeople } from '@/components/app/dtsiClientPersonDataTable/sortPeople'
+import { PagePoliticiansLayout } from '@/components/app/pagePoliticians/common/layout'
+import { PageSubTitle } from '@/components/ui/pageSubTitle'
+import { PageTitle } from '@/components/ui/pageTitleText'
+
+export const PAGE_POLITICIANS_TITLE = 'Find out where politicians stand on crypto'
+export const PAGE_POLITICIANS_DESCRIPTION = `Crypto drives Canada innovation. Discover the politicians fighting to keep crypto in Canada.`
+
+export function CaPagePoliticians({ politicians }: { politicians: DTSIPersonDataTablePeople }) {
+  return (
+    <PagePoliticiansLayout>
+      <PagePoliticiansLayout.IntroductionSection>
+        <PageTitle>{PAGE_POLITICIANS_TITLE}</PageTitle>
+        <PageSubTitle>{PAGE_POLITICIANS_DESCRIPTION}</PageSubTitle>
+      </PagePoliticiansLayout.IntroductionSection>
+      <PagePoliticiansLayout.PoliticiansTableSection>
+        <CaDTSIClientPersonDataTable initialData={politicians} />
+      </PagePoliticiansLayout.PoliticiansTableSection>
+    </PagePoliticiansLayout>
+  )
+}
