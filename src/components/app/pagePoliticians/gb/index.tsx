6310e929d7b1098e14a7c5a2a7abdefb7f9bcf0e
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-25 10:54:07 -0300

    feat: add GB politicians page component
    - Introduce a new component for displaying British politicians, featuring a title and description.
    - Implement a layout with an introduction section and a data table for politicians.
    - Integrate the GbDTSIClientPersonDataTable for displaying politician data.

---
+++ src/components/app/pagePoliticians/gb/index.tsx
@@ -0,0 +1,22 @@
+import { GbDTSIClientPersonDataTable } from '@/components/app/dtsiClientPersonDataTable/gb'
+import { DTSIPersonDataTablePeople } from '@/components/app/dtsiClientPersonDataTable/sortPeople'
+import { PagePoliticiansLayout } from '@/components/app/pagePoliticians/common/layout'
+import { PageSubTitle } from '@/components/ui/pageSubTitle'
+import { PageTitle } from '@/components/ui/pageTitleText'
+
+export const PAGE_POLITICIANS_TITLE = 'Find out where politicians stand on crypto'
+export const PAGE_POLITICIANS_DESCRIPTION = `Crypto drives British innovation. Discover the politicians fighting to keep crypto in Britain.`
+
+export function GbPagePoliticians({ politicians }: { politicians: DTSIPersonDataTablePeople }) {
+  return (
+    <PagePoliticiansLayout>
+      <PagePoliticiansLayout.IntroductionSection>
+        <PageTitle>{PAGE_POLITICIANS_TITLE}</PageTitle>
+        <PageSubTitle>{PAGE_POLITICIANS_DESCRIPTION}</PageSubTitle>
+      </PagePoliticiansLayout.IntroductionSection>
+      <PagePoliticiansLayout.PoliticiansTableSection>
+        <GbDTSIClientPersonDataTable initialData={politicians} />
+      </PagePoliticiansLayout.PoliticiansTableSection>
+    </PagePoliticiansLayout>
+  )
+}
