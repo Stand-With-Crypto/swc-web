4deef59ddd8eb789a64a07a503e3b71b43b5cfb6
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-25 10:54:38 -0300

    feat: add AU politicians page component
    - Introduce a new component for displaying Australian politicians, featuring a title and description.
    - Implement a layout with an introduction section and a data table for politicians.
    - Integrate the AuDTSIClientPersonDataTable for displaying politician data.

---
+++ src/components/app/pagePoliticians/au/index.tsx
@@ -0,0 +1,22 @@
+import { AuDTSIClientPersonDataTable } from '@/components/app/dtsiClientPersonDataTable/au'
+import { DTSIPersonDataTablePeople } from '@/components/app/dtsiClientPersonDataTable/sortPeople'
+import { PagePoliticiansLayout } from '@/components/app/pagePoliticians/common/layout'
+import { PageSubTitle } from '@/components/ui/pageSubTitle'
+import { PageTitle } from '@/components/ui/pageTitleText'
+
+export const PAGE_POLITICIANS_TITLE = 'Find out where politicians stand on crypto'
+export const PAGE_POLITICIANS_DESCRIPTION = `Crypto drives Australia innovation. Discover the politicians fighting to keep crypto in Australia.`
+
+export function AuPagePoliticians({ politicians }: { politicians: DTSIPersonDataTablePeople }) {
+  return (
+    <PagePoliticiansLayout>
+      <PagePoliticiansLayout.IntroductionSection>
+        <PageTitle>{PAGE_POLITICIANS_TITLE}</PageTitle>
+        <PageSubTitle>{PAGE_POLITICIANS_DESCRIPTION}</PageSubTitle>
+      </PagePoliticiansLayout.IntroductionSection>
+      <PagePoliticiansLayout.PoliticiansTableSection>
+        <AuDTSIClientPersonDataTable initialData={politicians} />
+      </PagePoliticiansLayout.PoliticiansTableSection>
+    </PagePoliticiansLayout>
+  )
+}
