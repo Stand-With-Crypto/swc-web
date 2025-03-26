b6de10b9d2fd0da439ef05d5c5721ddd3295d56a
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-26 10:40:07 -0300

    feat: add default URL to tweet message in x-redirect route

--- src/app/api/public/x-redirect/[state]/route.ts
+++ src/app/api/public/x-redirect/[state]/route.ts
@@ -15,6 +15,7 @@ export async function GET(_: NextRequest, props: { params: Promise<{ state: stri
   const { state } = zodParams.parse(params)
 
   const tweetLink = createTweetLink({
+    url: 'https://standwithcrypto.org',
     message: `${getUSStateNameFromStateCode(state)} just scored a HUGE win for crypto! The state is dropping its staking case against Coinbase, which means YOU can soon stake your crypto freely. Click here to spread the word: https://standwithcrypto.org/tweet/case-dismissed/${state}\n`,
   })
 
