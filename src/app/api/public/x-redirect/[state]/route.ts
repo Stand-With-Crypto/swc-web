34ab185bb50b51b01e5501a92e824805f78c7ce6
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-26 10:38:39 -0300

    fix: update tweet message format in x-redirect route

--- src/app/api/public/x-redirect/[state]/route.ts
+++ src/app/api/public/x-redirect/[state]/route.ts
@@ -10,13 +10,12 @@ const zodParams = z.object({
   state: z.string().length(2),
 })
 
-export async function GET(_request: NextRequest, props: { params: Promise<{ state: string }> }) {
+export async function GET(_: NextRequest, props: { params: Promise<{ state: string }> }) {
   const params = await props.params
   const { state } = zodParams.parse(params)
 
   const tweetLink = createTweetLink({
-    url: 'https://standwithcrypto.org',
-    message: `${getUSStateNameFromStateCode(state)} just scored a HUGE win for crypto! The state is dropping its staking case against Coinbase, which means YOU can soon stake your crypto freely. Repost and spread the word: https://standwithcrypto.org/tweet/case-dismissed/${state}\n\n`,
+    message: `${getUSStateNameFromStateCode(state)} just scored a HUGE win for crypto! The state is dropping its staking case against Coinbase, which means YOU can soon stake your crypto freely. Click here to spread the word: https://standwithcrypto.org/tweet/case-dismissed/${state}\n`,
   })
 
   return NextResponse.redirect(tweetLink)
