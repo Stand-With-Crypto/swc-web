18711cac86f586a9fa2d94c52856ac8292076dcf
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-25 22:33:06 -0300

    feat: creates new route

---
+++ src/app/api/public/x-redirect/[state]/route.ts
@@ -0,0 +1,26 @@
+import 'server-only'
+
+import { NextRequest, NextResponse } from 'next/server'
+import { z } from 'zod'
+
+import { createTweetLink } from '@/utils/web/createTweetLink'
+import { getUSStateNameFromStateCode } from '@/utils/shared/usStateUtils'
+
+export const revalidate = 60 * 60 * 24
+export const dynamic = 'error'
+
+const zodParams = z.object({
+  state: z.string().length(2),
+})
+
+export async function GET(_request: NextRequest, props: { params: Promise<{ state: string }> }) {
+  const params = await props.params
+  const { state } = zodParams.parse(params)
+
+  const tweetLink = createTweetLink({
+    url: 'https://standwithcrypto.org',
+    message: `${getUSStateNameFromStateCode(state)} just scored a HUGE win for crypto! The state is dropping its staking case against Coinbase, which means YOU can soon stake your crypto freely. Repost and spread the word: https://standwithcrypto.org/tweet/case-dismissed/${state}\n\n`,
+  })
+
+  return NextResponse.redirect(tweetLink)
+}
