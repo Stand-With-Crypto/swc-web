c05210badcc0836c5162529b04c6577e5bcda3a8
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-03-26 16:12:12 -0300

    feat: add utility functions for Australian, Canadian, British, and US state/province/country code mappings

---
+++ src/utils/shared/gbCountryUtils.ts
@@ -0,0 +1,30 @@
+export const GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP = {
+  ENG: 'England',
+  NIR: 'Northern Ireland',
+  SCT: 'Scotland',
+  WLS: 'Wales',
+} as const
+
+export type GBCountryCode = keyof typeof GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP
+
+export const getGBCountryNameFromCode = (code: string) => {
+  // @ts-ignore
+  const name: string = GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP[code.toUpperCase()]
+  if (name) {
+    return name
+  }
+  return code
+}
+
+export const getGBCountryCodeFromName = (name: string) => {
+  const code = Object.keys(GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP).find(
+    key =>
+      GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP[
+        key as keyof typeof GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP
+      ] === name,
+  )
+  return code as GBCountryCode | undefined
+}
+
+export const isValidGBCountryCode = (code: string) =>
+  code.toUpperCase() in GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP
