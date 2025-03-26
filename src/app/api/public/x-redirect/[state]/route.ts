38ac037cc53ab8852adc61651cc245c50c927a48
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-26 12:06:09 -0300

    Feat/creates tweet redirect logic (#2057)
    * feat: creates new route
    
    * refactor: clean up imports and maintain zod validation in x-redirect route
    
    * fix: update tweet message format in x-redirect route
    
    * feat: add default URL to tweet message in x-redirect route
    
    * fix: update tweet message to specify state residents in x-redirect route
    
    * fix: add additional newline to tweet message in x-redirect route
    
    * fix: refine tweet message to emphasize state residents and update wording in x-redirect route
    
    * fix: adjust tweet message formatting for clarity in x-redirect route

--- src/app/api/public/x-redirect/[state]/route.ts
+++ src/app/api/public/x-redirect/[state]/route.ts
@@ -13,10 +13,11 @@ const zodParams = z.object({
 export async function GET(_: NextRequest, props: { params: Promise<{ state: string }> }) {
   const params = await props.params
   const { state } = zodParams.parse(params)
+  const parsedState = state.toUpperCase()
 
   const tweetLink = createTweetLink({
     url: 'https://standwithcrypto.org',
-    message: `${getUSStateNameFromStateCode(state)} just scored a HUGE win for crypto! The state is dropping its staking case against Coinbase, which means people in the state can soon stake their crypto freely. Click here to applaud ${state.toUpperCase()} for a job well done: https://standwithcrypto.org/tweet/case-dismissed/${state}\n\n`,
+    message: `Thank you ${getUSStateNameFromStateCode(state)} for dropping the case against @coinbase. Now ${parsedState} users can stake their crypto! We deserve the right to utilize our assets and we are securing that right.\n\nJoin me in applauding ${parsedState} for a job well done: https://standwithcrypto.org/tweet/case-dismissed/${state}\n\n`,
   })
   return NextResponse.redirect(tweetLink)
 }
