30aa07742472e05c520bd325df0b5efeee9f3e1d
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-03-26 11:31:34 -0300

    feat: add international state utils

---
+++ src/utils/shared/auStateUtils.ts
@@ -0,0 +1,38 @@
+export const AU_MAIN_STATE_CODE_TO_DISPLAY_NAME_MAP = {
+  NSW: 'New South Wales',
+  VIC: 'Victoria',
+  QLD: 'Queensland',
+  WA: 'Western Australia',
+  SA: 'South Australia',
+  TAS: 'Tasmania',
+} as const
+
+const AU_TERRITORY_STATE_CODE_TO_DISPLAY_NAME_MAP = {
+  ACT: 'Australian Capital Territory',
+  NT: 'Northern Territory',
+} as const
+
+export const AU_STATE_CODE_TO_DISPLAY_NAME_MAP = {
+  ...AU_MAIN_STATE_CODE_TO_DISPLAY_NAME_MAP,
+  ...AU_TERRITORY_STATE_CODE_TO_DISPLAY_NAME_MAP,
+} as const
+
+export type AUStateCode = keyof typeof AU_STATE_CODE_TO_DISPLAY_NAME_MAP
+
+export const getAUStateNameFromStateCode = (stateCode: string) => {
+  // @ts-ignore
+  const name: string = AU_STATE_CODE_TO_DISPLAY_NAME_MAP[stateCode.toUpperCase()]
+  if (name) {
+    return name
+  }
+  return stateCode
+}
+
+export const getAUStateCodeFromStateName = (stateName: string) => {
+  const stateCode = Object.keys(AU_STATE_CODE_TO_DISPLAY_NAME_MAP).find(
+    key =>
+      AU_STATE_CODE_TO_DISPLAY_NAME_MAP[key as keyof typeof AU_STATE_CODE_TO_DISPLAY_NAME_MAP] ===
+      stateName,
+  )
+  return stateCode as AUStateCode | undefined
+}
---
+++ src/utils/shared/caProvinceUtils.ts
@@ -0,0 +1,45 @@
+export const CA_MAIN_PROVINCES_CODE_TO_DISPLAY_NAME_MAP = {
+  AB: 'Alberta',
+  BC: 'British Columbia',
+  MB: 'Manitoba',
+  NB: 'New Brunswick',
+  NL: 'Newfoundland and Labrador',
+  NS: 'Nova Scotia',
+  ON: 'Ontario',
+  PE: 'Prince Edward Island',
+  QC: 'Quebec',
+  SK: 'Saskatchewan',
+} as const
+
+const CA_TERRITORY_CODE_TO_DISPLAY_NAME_MAP = {
+  NT: 'Northwest Territories',
+  NU: 'Nunavut',
+  YT: 'Yukon',
+} as const
+
+export const CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP = {
+  ...CA_MAIN_PROVINCES_CODE_TO_DISPLAY_NAME_MAP,
+  ...CA_TERRITORY_CODE_TO_DISPLAY_NAME_MAP,
+} as const
+
+export type CAProvinceOrTerritoryCode =
+  keyof typeof CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP
+
+export const getCAProvinceOrTerritoryNameFromCode = (code: string) => {
+  // @ts-ignore
+  const name: string = CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP[code.toUpperCase()]
+  if (name) {
+    return name
+  }
+  return code
+}
+
+export const getCAProvinceOrTerritoryCodeFromName = (provinceOrTerritoryName: string) => {
+  const code = Object.keys(CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP).find(
+    key =>
+      CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP[
+        key as keyof typeof CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP
+      ] === provinceOrTerritoryName,
+  )
+  return code as CAProvinceOrTerritoryCode | undefined
+}
