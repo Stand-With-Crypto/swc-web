90ae6ffdd99065d758da98f5fc8ae0ad6d54dc46
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-27 17:03:02 -0300

    refactor: update politician details page to use common description function and streamline component imports

--- src/app/[countryCode]/politicians/person/[dtsiSlug]/page.tsx
+++ src/app/[countryCode]/politicians/person/[dtsiSlug]/page.tsx
@@ -1,19 +1,15 @@
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
-import { PagePoliticianDetails } from '@/components/app/pagePoliticianDetails'
 import { queryDTSIAllPeopleSlugs } from '@/data/dtsi/queries/queryDTSIAllPeopleSlugs'
-import { DTSIPersonDetails } from '@/data/dtsi/queries/queryDTSIPersonDetails'
 import { PageProps } from '@/types'
 import { dtsiPersonFullName } from '@/utils/dtsi/dtsiPersonUtils'
-import {
-  convertDTSIPersonStanceScoreToLetterGrade,
-  DTSILetterGrade,
-} from '@/utils/dtsi/dtsiStanceScoreUtils'
 import { getQuestionnaire } from '@/utils/server/builder/models/data/questionnaire'
 import { toBool } from '@/utils/shared/toBool'
 
-import { getData } from './getData'
+import { getPoliticianDetailsPageDescription } from '@/components/app/pagePoliticianDetails/common/getPoliticianDetailsPageDescription'
+import { UsPagePoliticianDetails } from '@/components/app/pagePoliticianDetails/us'
+import { getData } from '@/components/app/pagePoliticianDetails/common/getData'
 
 export const revalidate = 86400 // 1 day
 export const dynamic = 'error'
@@ -21,28 +17,6 @@ export const dynamicParams = true
 
 type Props = PageProps<{ dtsiSlug: string }>
 
-const getDescription = (person: DTSIPersonDetails) => {
-  const fullName = dtsiPersonFullName(person)
-  if (!person.stances.length) {
-    return `${fullName} has not made any recent comments about Bitcoin, Ethereum, and cryptocurrency innovation.`
-  }
-  const indication = (() => {
-    switch (convertDTSIPersonStanceScoreToLetterGrade(person)) {
-      case DTSILetterGrade.A:
-        return 'indicated they are very pro-cryptocurrencies'
-      case DTSILetterGrade.B:
-        return 'indicated thy are somewhat pro-cryptocurrencies'
-      case DTSILetterGrade.C:
-      case null:
-        return 'not indicated whether they are for or against cryptocurrencies.'
-      case DTSILetterGrade.D:
-        return 'indicated they are somewhat anti-cryptocurrencies'
-      case DTSILetterGrade.F:
-        return 'indicated they are very anti-cryptocurrencies'
-    }
-  })()
-  return `Based on previous comments, ${fullName} has ${indication}. On this page you can view the tweets, quotes, and other commentary ${fullName} has made about Bitcoin, Ethereum, and cryptocurrency innovation.`
-}
 export async function generateMetadata(props: Props): Promise<Metadata> {
   const person = await getData((await props.params).dtsiSlug)
   if (!person) {
@@ -51,7 +25,7 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   const title = `${dtsiPersonFullName(person)} Crypto Policy Stance`
   return {
     title,
-    description: getDescription(person),
+    description: getPoliticianDetailsPageDescription(person),
   }
 }
 export async function generateStaticParams() {
@@ -77,5 +51,5 @@ export default async function PoliticianDetails(props: Props) {
     notFound()
   }
 
-  return <PagePoliticianDetails {...{ person, countryCode, questionnaire }} />
+  return <UsPagePoliticianDetails {...{ person, countryCode, questionnaire }} />
 }
