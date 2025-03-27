8da0b624420842d86302243fcab5057323fc128c
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-27 12:12:12 -0300

    feat: update queryDTSIHomepagePeople to accept personRoleGroupingOr parameter based on country code for enhanced data retrieval

--- src/data/dtsi/queries/queryDTSIHomepagePeople.ts
+++ src/data/dtsi/queries/queryDTSIHomepagePeople.ts
@@ -1,19 +1,41 @@
 import { fetchDTSI } from '@/data/dtsi/fetchDTSI'
 import { fragmentDTSIPersonCard } from '@/data/dtsi/fragments/fragmentDTSIPersonCard'
 import { DTSI_HomepagePeopleQuery, DTSI_HomepagePeopleQueryVariables } from '@/data/dtsi/generated'
+import { PERSON_ROLE_GROUPINGS_FOR_ALL_PEOPLE_QUERY } from '@/data/dtsi/queries/constants'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 const query = /* GraphQL */ `
-  query HomepagePeople {
-    lowestScores: people(limit: 4, offset: 0, stanceScoreLte: 50) {
+  query HomepagePeople($personRoleGroupingOr: [PersonGrouping!]) {
+    lowestScores: people(
+      limit: 4
+      offset: 0
+      stanceScoreLte: 50
+      personRoleGroupingOr: $personRoleGroupingOr
+    ) {
       ...PersonCard
     }
-    highestScores: people(limit: 4, offset: 0, stanceScoreGte: 51) {
+    highestScores: people(
+      limit: 4
+      offset: 0
+      stanceScoreGte: 51
+      personRoleGroupingOr: $personRoleGroupingOr
+    ) {
       ...PersonCard
     }
   }
   ${fragmentDTSIPersonCard}
 `
 
-export const queryDTSIHomepagePeople = () => {
-  return fetchDTSI<DTSI_HomepagePeopleQuery, DTSI_HomepagePeopleQueryVariables>(query)
+export const queryDTSIHomepagePeople = ({
+  countryCode = SupportedCountryCodes.US,
+}: {
+  countryCode?: string
+} = {}) => {
+  const personRoleGroupingOr =
+    countryCode in PERSON_ROLE_GROUPINGS_FOR_ALL_PEOPLE_QUERY
+      ? PERSON_ROLE_GROUPINGS_FOR_ALL_PEOPLE_QUERY[countryCode as SupportedCountryCodes]
+      : []
+  return fetchDTSI<DTSI_HomepagePeopleQuery, DTSI_HomepagePeopleQueryVariables>(query, {
+    personRoleGroupingOr,
+  })
 }
