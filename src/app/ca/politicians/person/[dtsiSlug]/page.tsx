bfc63efecb4b0b42eda447d7db189567256d5245
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-27 17:03:21 -0300

    feat: create CA politician details page with metadata generation and dynamic slug handling

---
+++ src/app/ca/politicians/person/[dtsiSlug]/page.tsx
@@ -0,0 +1,52 @@
+import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+
+import { getData } from '@/components/app/pagePoliticianDetails/common/getData'
+import { queryDTSIAllPeopleSlugs } from '@/data/dtsi/queries/queryDTSIAllPeopleSlugs'
+import { PageProps } from '@/types'
+import { dtsiPersonFullName } from '@/utils/dtsi/dtsiPersonUtils'
+import { toBool } from '@/utils/shared/toBool'
+import { CaPagePoliticianDetails } from '@/components/app/pagePoliticianDetails/ca'
+import { getPoliticianDetailsPageDescription } from '@/components/app/pagePoliticianDetails/common/getPoliticianDetailsPageDescription'
+
+export const revalidate = 86400 // 1 day
+export const dynamic = 'error'
+export const dynamicParams = true
+
+type Props = PageProps<{ dtsiSlug: string }>
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const person = await getData((await props.params).dtsiSlug)
+  if (!person) {
+    return {}
+  }
+  const title = `${dtsiPersonFullName(person)} Crypto Policy Stance`
+  return {
+    title,
+    description: getPoliticianDetailsPageDescription(person),
+  }
+}
+export async function generateStaticParams() {
+  const slugs = await queryDTSIAllPeopleSlugs().then(x =>
+    x.people.map(({ slug: dtsiSlug }) => ({ dtsiSlug })),
+  )
+  if (toBool(process.env.MINIMIZE_PAGE_PRE_GENERATION)) {
+    return slugs.slice(0, 2)
+  }
+  return slugs
+}
+
+export default async function CaPoliticianDetails(props: Props) {
+  const { dtsiSlug } = await props.params
+  const [person] = await Promise.all([
+    getData(dtsiSlug),
+    // TODO: uncomment this once we have questionnaire data for AU
+    // getQuestionnaire(dtsiSlug),
+  ])
+
+  if (!person) {
+    notFound()
+  }
+
+  return <CaPagePoliticianDetails {...{ person, questionnaire: null }} />
+}
