5552aad79c2220c8454085da6ff320b0ad101db7
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-03-28 14:00:05 -0300

    refactor: fix duplicate export of IntroductionSection in PagePoliticiansLayout for improved clarity

--- src/components/app/pagePoliticians/common/layout.tsx
+++ src/components/app/pagePoliticians/common/layout.tsx
@@ -8,9 +8,10 @@ function IntroductionSection({ children }: { children: React.ReactNode }) {
   )
 }
 
+PagePoliticiansLayout.IntroductionSection = IntroductionSection
+
 function PoliticiansTableSection({ children }: { children: React.ReactNode }) {
   return <section>{children}</section>
 }
 
-PagePoliticiansLayout.IntroductionSection = IntroductionSection
 PagePoliticiansLayout.PoliticiansTableSection = PoliticiansTableSection
