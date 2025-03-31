620ffeb4531b91584d53b523447cb0da28428dd0
Olavo Parno <olavo.parno@coinbase.com>
2025-03-31 18:56:18 -0300

    refactor: add generateStaticParams function for state-specific pages across multiple countries

--- src/app/ca/races/state/[stateCode]/house-of-commons/page.tsx
+++ src/app/ca/races/state/[stateCode]/house-of-commons/page.tsx
@@ -4,8 +4,12 @@ import { CALocationRaceSpecific } from '@/components/app/pageLocationKeyRaces/ca
 import { queryDTSILocationHouseSpecificInformation } from '@/data/dtsi/queries/queryDTSILocationHouseSpecificInformation'
 import { PageProps } from '@/types'
 import { generateMetadataDetails } from '@/utils/server/metadataUtils'
-import { getCAProvinceOrTerritoryNameFromCode } from '@/utils/shared/stateMappings/caProvinceUtils'
+import {
+  CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP,
+  getCAProvinceOrTerritoryNameFromCode,
+} from '@/utils/shared/stateMappings/caProvinceUtils'
 import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
+import { toBool } from '@/utils/shared/toBool'
 import { zodState } from '@/validation/fields/zodState'
 
 export const revalidate = 600 // 10 minutes
@@ -26,10 +30,19 @@ export async function generateMetadata({
   const stateName = getCAProvinceOrTerritoryNameFromCode(validatedStateCode)
   const title = `${stateName} Canadian House of Commons Race`
   const description = `See where politicians running for the Canadian House of Commons in ${stateName} stand on crypto.`
+
   return generateMetadataDetails({
     title,
     description,
   })
+}
+
+export async function generateStaticParams() {
+  return Object.keys(CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP)
+    .slice(0, toBool(process.env.MINIMIZE_PAGE_PRE_GENERATION) ? 1 : 99999)
+    .map(stateCode => ({
+      stateCode: stateCode.toLowerCase(),
+    }))
 }
 
 export default async function LocationHouseOfCommonsSpecificPage({
