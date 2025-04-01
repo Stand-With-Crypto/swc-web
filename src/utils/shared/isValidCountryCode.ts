b67e107a15f2ef0828432d4c8d47e29ba0874914
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-04-01 15:29:43 -0300

    refactor: modify isValidCountryCode function to use user access location for validation instead of user country code

--- src/utils/shared/isValidCountryCode.ts
+++ src/utils/shared/isValidCountryCode.ts
@@ -1,27 +1,24 @@
-import { parseUserCountryCodeCookie } from '@/utils/server/getCountryCode'
 import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 type IsValidCountryCodeArgs = {
   countryCode: string
-  userCountryCode?: string
+  userAccessLocation?: string
   bypassCountryCheck?: boolean
 }
 
 export function isValidCountryCode({
   countryCode,
-  userCountryCode,
+  userAccessLocation,
   bypassCountryCheck,
 }: IsValidCountryCodeArgs) {
   if (bypassCountryCheck) return true
 
-  const parsedExistingCountryCode = parseUserCountryCodeCookie(userCountryCode)
-  // we want to avoid blocking content in situations where a users IP isn't set for some reason
-  if (!parsedExistingCountryCode?.countryCode) return true
-  if (parsedExistingCountryCode.countryCode === countryCode) return true
+  if (userAccessLocation === countryCode) return true
   // there are cases where users in the US are getting blocked if they are close to the US border so we want to be more permissive with Canada/Mexico
   if (
     countryCode === SupportedCountryCodes.US &&
-    ['MX', 'CA'].includes(parsedExistingCountryCode.countryCode)
+    userAccessLocation &&
+    ['MX', 'CA'].includes(userAccessLocation)
   ) {
     return true
   }
