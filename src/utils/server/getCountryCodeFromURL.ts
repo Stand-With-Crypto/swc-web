20fc9f2baf7e20f002bf99edec9f12d52f6bf989
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-04-01 18:22:24 -0300

    refactor: remove getCountryCodeCookie and update getCountryCodeFromURL to use extractCountryCodeFromPathname for improved country code retrieval

--- src/utils/server/getCountryCodeFromURL.ts
+++ src/utils/server/getCountryCodeFromURL.ts
@@ -1,6 +1,6 @@
 import { headers } from 'next/headers'
 
-import { extractCountryCode } from '@/utils/server/obfuscateURLCountryCode'
+import { extractCountryCodeFromPathname } from '@/utils/server/middleware/extractCountryCodeFromPathname'
 import { DEFAULT_SUPPORTED_COUNTRY_CODE } from '@/utils/shared/supportedCountries'
 
 export const getCountryCodeFromURL = async () => {
@@ -9,7 +9,9 @@ export const getCountryCodeFromURL = async () => {
   const referer = headersList.get('referer')
   const xMatchedPath = headersList.get('x-matched-path')
 
-  const countryCode = extractCountryCode(xMatchedPath || '') || extractCountryCode(referer || '')
+  const countryCode =
+    extractCountryCodeFromPathname(xMatchedPath || '') ||
+    extractCountryCodeFromPathname(referer || '')
 
   return countryCode || DEFAULT_SUPPORTED_COUNTRY_CODE
 }
