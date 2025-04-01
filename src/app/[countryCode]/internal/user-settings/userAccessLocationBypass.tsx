3b4076f37d3a6c8aaa08d19c847c7a326f59bb96
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-04-01 16:52:45 -0300

    feat: add UserAccessLocationBypass component for managing user access location overrides

---
+++ src/app/[countryCode]/internal/user-settings/userAccessLocationBypass.tsx
@@ -0,0 +1,94 @@
+import Cookies from 'js-cookie'
+import { toast } from 'sonner'
+
+import { Button } from '@/components/ui/button'
+import { COOKIE_CONSENT_COOKIE_NAME } from '@/utils/shared/cookieConsent'
+import { LOCAL_USER_CURRENT_SESSION_KEY, LOCAL_USER_PERSISTED_KEY } from '@/utils/shared/localUser'
+import {
+  ORDERED_SUPPORTED_COUNTRIES,
+  SupportedCountryCodes,
+  USER_SELECTED_COUNTRY_COOKIE_NAME,
+} from '@/utils/shared/supportedCountries'
+import { USER_SESSION_ID_COOKIE_NAME } from '@/utils/shared/userSessionId'
+import {
+  OVERRIDE_USER_ACCESS_LOCATION_COOKIE_NAME,
+  USER_ACCESS_LOCATION_COOKIE_NAME,
+} from '@/utils/shared/userAccessLocation'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
+export function UserAccessLocationBypass() {
+  const userAccessLocation = Cookies.get(USER_ACCESS_LOCATION_COOKIE_NAME)?.toLowerCase()
+
+  const handleLocationCountryCodeSubmit = async (countryCode: SupportedCountryCodes) => {
+    Cookies.set(OVERRIDE_USER_ACCESS_LOCATION_COOKIE_NAME, countryCode)
+
+    Cookies.remove(USER_ACCESS_LOCATION_COOKIE_NAME)
+    Cookies.remove(COOKIE_CONSENT_COOKIE_NAME)
+    Cookies.remove(LOCAL_USER_PERSISTED_KEY)
+    Cookies.remove(LOCAL_USER_CURRENT_SESSION_KEY)
+    Cookies.remove(USER_SESSION_ID_COOKIE_NAME)
+    Cookies.remove(USER_SELECTED_COUNTRY_COOKIE_NAME)
+
+    toast.success(
+      'IP location country code bypassed. Please, open a new tab to see the changes or refresh the page.',
+      {
+        duration: 10000,
+      },
+    )
+  }
+
+  const handleLocationCountryCodeReset = async () => {
+    Cookies.remove(OVERRIDE_USER_ACCESS_LOCATION_COOKIE_NAME)
+    Cookies.remove(USER_ACCESS_LOCATION_COOKIE_NAME)
+
+    toast.success(
+      'IP location country code bypass removed! Please, open a new tab to see the changes or refresh the page.',
+      {
+        duration: 10000,
+      },
+    )
+  }
+
+  return (
+    <div>
+      <div className="flex items-end gap-4">
+        <strong>User Access Location</strong>
+
+        <Select
+          onValueChange={value => {
+            handleLocationCountryCodeSubmit(value as SupportedCountryCodes)
+          }}
+          value={userAccessLocation}
+        >
+          <SelectTrigger className="w-[195px]">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            {ORDERED_SUPPORTED_COUNTRIES.map(country => (
+              <SelectItem key={country} value={country}>
+                {country}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+
+        <p className="text-sm text-muted-foreground">
+          Changing this will force the defined country code to camouflage your geolocation. It will
+          also delete all necessary cookies to make sure that after the page reload, all logic will
+          be associated with the defined country code.
+        </p>
+      </div>
+      <div className="mt-4 flex gap-4">
+        <Button onClick={handleLocationCountryCodeReset} type="button" variant="secondary">
+          Clear override
+        </Button>
+      </div>
+    </div>
+  )
+}
