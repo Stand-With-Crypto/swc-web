26290175dd1a53f55e17a1deab8dcda66be26268
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-04-02 09:10:48 -0300

    refactor: change router navigation in NavbarCountrySelect from push to replace for improved user experience

--- src/components/app/navbar/navbarCountrySelect.tsx
+++ src/components/app/navbar/navbarCountrySelect.tsx
@@ -90,7 +90,7 @@ export function NavbarCountrySelect() {
               key={option.value}
               onClick={() => {
                 Cookies.set(USER_SELECTED_COUNTRY_COOKIE_NAME, option.value)
-                router.push(getIntlUrls(option.value).home())
+                router.replace(getIntlUrls(option.value).home())
               }}
             >
               <div className="flex items-center gap-2">
