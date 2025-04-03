f6655baac50e58916b52ab5f2d3098308b704687
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2025-04-03 10:57:39 -0300

    feat: remove politicians from navbar on gb

--- next.config.ts
+++ next.config.ts
@@ -684,6 +684,11 @@ const nextConfig: NextConfig = {
         destination: '/gb',
         permanent: true,
       },
+      {
+        source: '/australia',
+        destination: '/au?utm_source=billboard',
+        permanent: true,
+      },
     ]
   },
   async rewrites() {
