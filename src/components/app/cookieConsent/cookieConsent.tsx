c3deebf50eee9b21eee54b907622de5e6e09ece7
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-04-03 14:11:41 -0300

    Lazy load banners

--- src/components/app/cookieConsent/cookieConsent.tsx
+++ src/components/app/cookieConsent/cookieConsent.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useState } from 'react'
 import { useIsPreviewing } from '@builder.io/react'
+import dynamic from 'next/dynamic'
 
 import { CookieConsentAction } from '@/components/app/cookieConsent/common/types'
 import { CookieConsentPermissions } from '@/utils/shared/cookieConsent'
@@ -9,11 +10,23 @@ import { isCypress } from '@/utils/shared/executionEnvironment'
 import { gracefullyError } from '@/utils/shared/gracefullyError'
 import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
-import { AUCookieConsentBanner } from './au/banner'
-import { CACookieConsentBanner } from './ca/banner'
-import { GBCookieConsentBanner } from './gb/banner'
-import { USCookieConsentBanner } from './us/banner'
 import { useCookieConsent } from './useCookieConsent'
+
+const AUCookieConsentBanner = dynamic(() =>
+  import('./au/banner').then(mod => mod.AUCookieConsentBanner),
+)
+
+const CACookieConsentBanner = dynamic(() =>
+  import('./ca/banner').then(mod => mod.CACookieConsentBanner),
+)
+
+const GBCookieConsentBanner = dynamic(() =>
+  import('./gb/banner').then(mod => mod.GBCookieConsentBanner),
+)
+
+const USCookieConsentBanner = dynamic(() =>
+  import('./us/banner').then(mod => mod.USCookieConsentBanner),
+)
 
 interface CookieConsentProps {
   countryCode: SupportedCountryCodes
