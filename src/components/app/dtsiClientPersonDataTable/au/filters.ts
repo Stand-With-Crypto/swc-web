e4a29d17ef19efd5ea971d810120eb3dd9d40b3d
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-04-04 15:17:35 -0300

    fix: update role option from HOUSE_OF_COMMONS to CONGRESS in filters and display name

--- src/components/app/dtsiClientPersonDataTable/au/filters.ts
+++ src/components/app/dtsiClientPersonDataTable/au/filters.ts
@@ -55,7 +55,7 @@ export const getPersonDataTableFilterFns = (): Record<
       return true
     }
 
-    const rolesArray = [ROLE_OPTIONS.HOUSE_OF_COMMONS]
+    const rolesArray = [ROLE_OPTIONS.CONGRESS]
 
     if (rolesArray.includes(filterValue)) {
       return (
@@ -110,14 +110,14 @@ export function getPartyOptionDisplayName(party: string) {
 
 export const ROLE_OPTIONS = {
   ALL: 'All',
-  HOUSE_OF_COMMONS: DTSI_PersonRoleCategory.CONGRESS,
+  CONGRESS: DTSI_PersonRoleCategory.CONGRESS,
   ALL_OTHER: 'ALL_OTHER',
 }
 export function getRoleOptionDisplayName(role: string) {
   switch (role) {
     case ROLE_OPTIONS.ALL_OTHER:
       return 'Other Political Figure'
-    case ROLE_OPTIONS.HOUSE_OF_COMMONS:
+    case ROLE_OPTIONS.CONGRESS:
       return 'House of Reps'
     default:
       return 'All'
