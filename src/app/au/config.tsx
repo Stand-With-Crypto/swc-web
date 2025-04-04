ec00488d77188ea28c555ae18ff00825f2d5e087
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-04-04 17:50:52 -0300

    fix: remove terms of service footer link

--- src/app/au/config.tsx
+++ src/app/au/config.tsx
@@ -52,7 +52,6 @@ export const footerConfig: FooterProps = {
       href: urls.privacyPolicy(),
       text: 'Privacy',
     },
-    { href: urls.termsOfService(), text: 'Terms of service' },
   ],
   socialLinks: [
     {
