5434864fe6eba90001a1a6ea6e81fd1ac568ada7
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-04-08 10:24:02 -0300

    fix: improve subtitle formatting in Australia races page

--- src/app/au/races/page.tsx
+++ src/app/au/races/page.tsx
@@ -38,9 +38,9 @@ export default async function LocationAustraliaPage() {
         </DarkHeroSection.Title>
         <DarkHeroSection.Subtitle>
           View the key races occurring across{' '}
-          {COUNTRY_CODE_TO_DISPLAY_NAME_WITH_PREFIX[countryCode]}
-          that will impact the future of crypto. Learn where politicians stand on crypto to make an
-          informed decision at the ballot box.
+          {COUNTRY_CODE_TO_DISPLAY_NAME_WITH_PREFIX[countryCode]} that will impact the future of
+          crypto. Learn where politicians stand on crypto to make an informed decision at the ballot
+          box.
         </DarkHeroSection.Subtitle>
         {countAdvocates > 1000 && (
           <DarkHeroSection.HighlightedText>
