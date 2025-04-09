e58dcaa4276339d6a1391d2601122f69ceeee8ab
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-04-09 09:37:21 -0300

    Update AU, Ca and GB FollowOnX email

--- src/utils/server/email/templates/ca/followOnXReminder.tsx
+++ src/utils/server/email/templates/ca/followOnXReminder.tsx
@@ -38,12 +38,12 @@ const CAFollowOnXReminderEmail = ({
           src={buildTemplateInternalUrl('/ca/email/x-banner.png', hydratedHrefSearchParams)}
         />
 
-        <Heading gutterBottom="md">Stay up to date on crypto policy in Canada</Heading>
+        <Heading gutterBottom="md">Stay up to date on crypto policy</Heading>
 
         <Text className="text-foreground-muted text-center text-base">
-          We're so grateful for our Stand With Crypto community members in Canada, and we want to
-          make sure you stay connected to everything we're doing. While we strive to keep you up to
-          date here in your inbox, another great way to get the latest news is to{' '}
+          We're so grateful for our Stand With Crypto community members, and we want to make sure
+          you stay connected to everything we're doing. While we strive to keep you up to date here
+          in your inbox, another great way to get the latest news is to{' '}
           <Link className="text-inherit underline" href={CA_SOCIAL_MEDIA_URL.twitter}>
             follow us on X
           </Link>
@@ -51,7 +51,7 @@ const CAFollowOnXReminderEmail = ({
           <br />
           <br />
           X is where you'll get instant updates on events we're hosting, news we're reading, and
-          awesome discussion Spaces that we host with advocates and founders across Canada.
+          awesome discussion Spaces that we host with advocates and founders across the country.
           <br />
           <br />
           <Link className="text-inherit underline" href={CA_SOCIAL_MEDIA_URL.twitter}>
