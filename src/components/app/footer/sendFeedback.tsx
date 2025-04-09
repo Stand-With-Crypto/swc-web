de80de519f3f7b025a4a76e0e0e4234b04365b61
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-04-09 11:34:22 -0300

    refactor: improve SendFeedbackButton layout and remove desktop-specific logic

--- src/components/app/footer/sendFeedback.tsx
+++ src/components/app/footer/sendFeedback.tsx
@@ -10,39 +10,28 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
-import { ExternalLink } from '@/components/ui/link'
 import { useCopyTextToClipboard } from '@/hooks/useCopyTextToClipboard'
 import { useDialog } from '@/hooks/useDialog'
-import { useIsDesktop } from '@/hooks/useIsDesktop'
 
 export function SendFeedbackButton({ href }: { href: string }) {
-  const isDesktop = useIsDesktop()
   const dialogProps = useDialog({ analytics: 'Send-Feedback-Dialog' })
   const [copiedValue, handleCopyToClipboard] = useCopyTextToClipboard()
-  const mobileLink = `mailto:${href}`
-
-  if (!isDesktop) {
-    return (
-      <ExternalLink className="block text-gray-400" href={mobileLink}>
-        Send feedback
-      </ExternalLink>
-    )
-  }
 
   return (
     <Dialog {...dialogProps}>
       <DialogTrigger className="block text-gray-400 hover:underline">Send feedback</DialogTrigger>
-      <DialogContent a11yTitle="Send Feedback">
-        <DialogTitle className="text-center text-2xl">Send Feedback</DialogTitle>
+      <DialogContent a11yTitle="Send Feedback" className="pb-4 pt-0">
         <DialogBody className="flex flex-col justify-between">
-          <p className="py-6 text-center text-muted-foreground">
-            <Balancer>
-              We’d love to hear from you! If you have any thoughts, suggestions, or comments, feel
-              free to share them with us at the email below.
-            </Balancer>
-          </p>
-          <div className="rounded-md bg-muted p-4 text-center font-semibold">{href}</div>
-
+          <div>
+            <DialogTitle className="text-center text-2xl">Send Feedback</DialogTitle>
+            <p className="py-6 text-center text-muted-foreground">
+              <Balancer>
+                We’d love to hear from you! If you have any thoughts, suggestions, or comments, feel
+                free to share them with us at the email below.
+              </Balancer>
+            </p>
+            <div className="rounded-md bg-muted p-4 text-center font-semibold">{href}</div>
+          </div>
           <Button
             className="mt-4"
             onClick={() => handleCopyToClipboard(href)}
