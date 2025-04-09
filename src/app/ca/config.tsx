7d788fdfb41411cddeaa1d28b0bba24c67bb5a12
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-04-08 21:37:36 -0300

    fix: hide CA legal disclaimer & disable CA election email campaign

--- src/app/ca/config.tsx
+++ src/app/ca/config.tsx
@@ -44,7 +44,8 @@ export const footerConfig: FooterProps = {
   countryCode,
   title: 'Join the movement for the future of crypto',
   subtitle: 'Protect the future of crypto in Canada. #standwithcrypto',
-  legalText: 'Authorized by Coinbase Canada Inc, 745 Thurlow Street, Suite 2400, 613-866-4125.',
+  // TODO: uncomment when CBCA is approved
+  // legalText: 'Authorized by Coinbase Canada Inc, 745 Thurlow Street, Suite 2400, 613-866-4125.',
   footerBanner: (
     <div className="pointer-events-none min-w-full select-none">
       <img
