be2fba54e44ceed409705d2b8206c81cd44a5af9
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-04-10 16:04:58 -0300

    feat(gb): add GB user action for emailing MPs and update recent activity messages

--- src/components/app/recentActivityRow/viewKeyPageRecentActivityRow.tsx
+++ src/components/app/recentActivityRow/viewKeyPageRecentActivityRow.tsx
@@ -5,6 +5,7 @@ import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 import { getIntlUrls } from '@/utils/shared/urls'
 import { AUUserActionViewKeyPageCampaignName } from '@/utils/shared/userActionCampaigns/au/auUserActionCampaigns'
 import { CAUserActionViewKeyPageCampaignName } from '@/utils/shared/userActionCampaigns/ca/caUserActionCampaigns'
+import { GBUserActionViewKeyPageCampaignName } from '@/utils/shared/userActionCampaigns/gb/gbUserActionCampaigns'
 import { USUserActionViewKeyPageCampaignName } from '@/utils/shared/userActionCampaigns/us/usUserActionCampaigns'
 
 interface ViewKeyPageRecentActivityRowProps {
@@ -16,6 +17,7 @@ type ViewKeyPageCampaignName =
   | USUserActionViewKeyPageCampaignName
   | CAUserActionViewKeyPageCampaignName
   | AUUserActionViewKeyPageCampaignName
+  | GBUserActionViewKeyPageCampaignName
 
 export function viewKeyPageRecentActivityRow({
   campaignName,
@@ -31,7 +33,7 @@ export function viewKeyPageRecentActivityRow({
     }
   > = {
     [USUserActionViewKeyPageCampaignName.DEFAULT]: {
-      children: <RecentActivityRowMainText>Someone emailed their MP</RecentActivityRowMainText>,
+      children: <RecentActivityRowMainText>Someone viewed a key page</RecentActivityRowMainText>,
     },
     [CAUserActionViewKeyPageCampaignName.CA_Q2_2025_ELECTION]: {
       children: <RecentActivityRowMainText>Someone emailed their MP</RecentActivityRowMainText>,
@@ -48,6 +50,15 @@ export function viewKeyPageRecentActivityRow({
           <Button>Email yours</Button>
         </InternalLink>
       ),
+    },
+    [GBUserActionViewKeyPageCampaignName.NEWMODE_EMAIL_ACTION]: {
+      children: <RecentActivityRowMainText>Someone emailed their MP</RecentActivityRowMainText>,
+      // TODO: uncomment when the debanking action is live
+      // onFocusContent: () => (
+      //   <InternalLink className="block" href={urls.newmodeDebankingAction()}>
+      //     <Button>Email yours</Button>
+      //   </InternalLink>
+      // ),
     },
   }
 
