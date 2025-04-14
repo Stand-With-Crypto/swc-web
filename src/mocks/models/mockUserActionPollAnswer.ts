0d5e475dc56a405d95cfb13a0197b85e435c861b
Olavo Parno <olavo.parno@coinbase.com>
2025-04-14 15:59:56 -0300

    refactor: update user action seeding logic to swap poll and refer actions and clean up mock data

--- src/mocks/models/mockUserActionPollAnswer.ts
+++ src/mocks/models/mockUserActionPollAnswer.ts
@@ -2,21 +2,22 @@ import { faker } from '@faker-js/faker'
 import { Prisma, UserActionPollAnswer } from '@prisma/client'
 
 import { fakerFields } from '@/mocks/fakerUtils'
+import { USUserActionPollCampaignName } from '@/utils/shared/userActionCampaigns/us/usUserActionCampaigns'
 
+const MOCK_POLL_IDS = Object.values(USUserActionPollCampaignName)
 const POLL_ANSWER_OPTIONS = ['Mike Tyson', 'Nick Cage', 'John Cena', 'The Rock']
 
-export function mockCreateUserActionPollAnswerInput(campaignName: string) {
+export function mockCreateUserActionPollAnswerInput() {
   return {
-    userActionCampaignName: campaignName,
     answer: faker.helpers.arrayElement(POLL_ANSWER_OPTIONS),
-    isOtherAnswer: faker.helpers.arrayElement([true, false]),
+    isOtherAnswer: true,
+    userActionCampaignName: faker.helpers.arrayElement(MOCK_POLL_IDS),
   } satisfies Omit<Prisma.UserActionPollAnswerCreateInput, 'userActionPoll'>
 }
 
-export function mockUserActionPollAnswer(campaignName: string): UserActionPollAnswer {
+export function mockUserActionPollAnswer() {
   return {
-    ...mockCreateUserActionPollAnswerInput(campaignName),
+    ...mockCreateUserActionPollAnswerInput(),
     id: fakerFields.id(),
-    userActionPollId: fakerFields.id(),
   }
 }
