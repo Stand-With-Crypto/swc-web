33ccaac02b86fecc6ee6258994427ca969a5a906
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-04-28 22:07:32 -0300

    fix: update help message contact information for SMS support by country

--- src/utils/server/sms/messages.ts
+++ src/utils/server/sms/messages.ts
@@ -21,7 +21,7 @@ const GB_SMS_MESSAGES: SMSMessages = {
   welcomeMessage: `Thanks for subscribing to Stand With Crypto! You can expect news, opportunities to engage, and critical updates on crypto policy. Browse our resources and research where your elected officials stand on crypto at ${fullUrl(`/${SupportedCountryCodes.GB}`)}\n\nMessage & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
   goodbyeMessage: `You have opted out and will no longer receive texts from Stand With Crypto. Text START to receive texts from SWC.`,
   unstopConfirmationMessage: `Thanks for subscribing to Stand With Crypto. Message & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
-  helpMessage: `Reply STOP to unsubscribe. Contact Stand With Crypto Support at info@StandWithCrypto.org`,
+  helpMessage: `Reply STOP to unsubscribe. Contact Stand With Crypto Support at uk@swcinternational.org`,
   bulkWelcomeMessage: `Thank you for being a Stand With Crypto advocate. Message & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
 }
 
@@ -29,7 +29,7 @@ const CA_SMS_MESSAGES: SMSMessages = {
   welcomeMessage: `Thanks for subscribing to Stand With Crypto! You can expect news, opportunities to engage, and critical updates on crypto policy. Browse our resources and research where your elected officials stand on crypto at ${fullUrl(`/${SupportedCountryCodes.CA}`)}\n\nMessage & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
   goodbyeMessage: `You have opted out and will no longer receive texts from Stand With Crypto. Text START to receive texts from SWC.`,
   unstopConfirmationMessage: `Thanks for subscribing to Stand With Crypto. Message & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
-  helpMessage: `Reply STOP to unsubscribe. Contact Stand With Crypto Support at info@StandWithCrypto.org`,
+  helpMessage: `Reply STOP to unsubscribe. Contact Stand With Crypto Support at ca@swcinternational.org`,
   bulkWelcomeMessage: `Thank you for being a Stand With Crypto advocate. Message & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
 }
 
@@ -37,7 +37,7 @@ const AU_SMS_MESSAGES: SMSMessages = {
   welcomeMessage: `Thanks for subscribing to Stand With Crypto! You can expect news, opportunities to engage, and critical updates on crypto policy. Browse our resources and research where your elected officials stand on crypto at ${fullUrl(`/${SupportedCountryCodes.AU}`)}\n\nMessage & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
   goodbyeMessage: `You have opted out and will no longer receive texts from Stand With Crypto. Text START to receive texts from SWC.`,
   unstopConfirmationMessage: `Thanks for subscribing to Stand With Crypto. Message & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
-  helpMessage: `Reply STOP to unsubscribe. Contact Stand With Crypto Support at info@StandWithCrypto.org`,
+  helpMessage: `Reply STOP to unsubscribe. Contact Stand With Crypto Support at au@swcinternational.org`,
   bulkWelcomeMessage: `Thank you for being a Stand With Crypto advocate. Message & data rates may apply. Message frequency varies. Reply HELP for help or STOP to opt out.`,
 }
 
