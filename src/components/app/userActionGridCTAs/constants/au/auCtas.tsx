7746b4689f01e33878dbe3e1855f86bdc2974dc3
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-04-30 09:42:23 -0300

    fix: key races CTA redirecting to email flow (#2241)

--- src/components/app/userActionGridCTAs/constants/au/auCtas.tsx
+++ src/components/app/userActionGridCTAs/constants/au/auCtas.tsx
@@ -90,7 +90,6 @@ export const AU_USER_ACTION_CTAS_FOR_GRID_DISPLAY: UserActionGridCTA = {
     campaignsModalDescription:
       'View the key races occurring across Australia that will impact the future of crypto.',
     image: '/au/actionTypeIcons/view-key-races.png',
-    link: ({ children }) => <Link href={urls.emailDeeplink()}>{children}</Link>,
     campaigns: [
       {
         actionType: UserActionType.VIEW_KEY_RACES,
