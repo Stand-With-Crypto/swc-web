ea37869dcb592699e3968c1eb4d8d293eb97f471
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-05-05 09:38:06 -0300

    feat: add intl politician page CTA

--- src/app/ca/politicians/page.tsx
+++ src/app/ca/politicians/page.tsx
@@ -26,5 +26,5 @@ export default async function PoliticiansHomepage() {
   const results = await queryDTSIAllPeople({ countryCode })
 
   const politicians = sortDTSIPersonDataTable(results.people).slice(0, 100)
-  return <CaPagePoliticians politicians={politicians} />
+  return <CaPagePoliticians countryCode={countryCode} politicians={politicians} />
 }
--- src/components/app/pagePoliticians/au/index.tsx
+++ src/components/app/pagePoliticians/au/index.tsx
@@ -1,18 +1,27 @@
+import { ClientCurrentUserDTSIPersonCardOrCTA } from '@/components/app/clientCurrentUserDTSIPersonCardOrCTA'
 import { AuDTSIClientPersonDataTable } from '@/components/app/dtsiClientPersonDataTable/au'
 import { DTSIPersonDataTablePeople } from '@/components/app/dtsiClientPersonDataTable/common/utils'
 import { PagePoliticiansLayout } from '@/components/app/pagePoliticians/common/layout'
 import { PageSubTitle } from '@/components/ui/pageSubTitle'
 import { PageTitle } from '@/components/ui/pageTitleText'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 export const PAGE_POLITICIANS_TITLE = 'Find out where politicians stand on crypto'
 export const PAGE_POLITICIANS_DESCRIPTION = `Crypto drives Australian innovation. Discover the politicians fighting to keep crypto in Australia.`
 
-export function AuPagePoliticians({ politicians }: { politicians: DTSIPersonDataTablePeople }) {
+export function AuPagePoliticians({
+  politicians,
+  countryCode,
+}: {
+  politicians: DTSIPersonDataTablePeople
+  countryCode: SupportedCountryCodes
+}) {
   return (
     <PagePoliticiansLayout>
       <PagePoliticiansLayout.IntroductionSection>
         <PageTitle>{PAGE_POLITICIANS_TITLE}</PageTitle>
         <PageSubTitle>{PAGE_POLITICIANS_DESCRIPTION}</PageSubTitle>
+        <ClientCurrentUserDTSIPersonCardOrCTA countryCode={countryCode} />
       </PagePoliticiansLayout.IntroductionSection>
       <PagePoliticiansLayout.PoliticiansTableSection>
         <AuDTSIClientPersonDataTable initialData={politicians} />
