55b458e58caba2d3cb0963858287f405b38e9813
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-05-09 10:51:01 -0300

    Revert "feat: add intl politician page CTA"
    This reverts commit ea37869dcb592699e3968c1eb4d8d293eb97f471.

--- src/components/app/pagePoliticians/gb/index.tsx
+++ src/components/app/pagePoliticians/gb/index.tsx
@@ -1,27 +1,18 @@
-import { ClientCurrentUserDTSIPersonCardOrCTA } from '@/components/app/clientCurrentUserDTSIPersonCardOrCTA'
 import { DTSIPersonDataTablePeople } from '@/components/app/dtsiClientPersonDataTable/common/utils'
 import { GbDTSIClientPersonDataTable } from '@/components/app/dtsiClientPersonDataTable/gb'
 import { PagePoliticiansLayout } from '@/components/app/pagePoliticians/common/layout'
 import { PageSubTitle } from '@/components/ui/pageSubTitle'
 import { PageTitle } from '@/components/ui/pageTitleText'
-import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 export const PAGE_POLITICIANS_TITLE = 'Find out where politicians stand on crypto'
 export const PAGE_POLITICIANS_DESCRIPTION = `Crypto drives British innovation. Discover the politicians fighting to keep crypto in the UK.`
 
-export function GbPagePoliticians({
-  politicians,
-  countryCode,
-}: {
-  politicians: DTSIPersonDataTablePeople
-  countryCode: SupportedCountryCodes
-}) {
+export function GbPagePoliticians({ politicians }: { politicians: DTSIPersonDataTablePeople }) {
   return (
     <PagePoliticiansLayout>
       <PagePoliticiansLayout.IntroductionSection>
         <PageTitle>{PAGE_POLITICIANS_TITLE}</PageTitle>
         <PageSubTitle>{PAGE_POLITICIANS_DESCRIPTION}</PageSubTitle>
-        <ClientCurrentUserDTSIPersonCardOrCTA countryCode={countryCode} />
       </PagePoliticiansLayout.IntroductionSection>
       <PagePoliticiansLayout.PoliticiansTableSection>
         <GbDTSIClientPersonDataTable initialData={politicians} />
