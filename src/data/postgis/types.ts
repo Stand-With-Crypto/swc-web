9cf7daa3b30eda3e0b007a347ca9f28f6e2a493d
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-05-09 10:49:14 -0300

    feat: integrate PostGIS for congressional district queries

---
+++ src/data/postgis/types.ts
@@ -0,0 +1,13 @@
+type Params = {
+  latitude: number
+  longitude: number
+}
+
+export interface GetCongressionalDistrictResult {
+  congressionalDistrictName: string
+  stateCode?: string
+}
+
+export type GetCongressionalDistrictQuery = (
+  params: Params,
+) => Promise<GetCongressionalDistrictResult | undefined>
