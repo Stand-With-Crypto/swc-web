bf3e635391a032f94547373bc49c2a1059918e1a
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-05-13 14:46:19 -0300

    fix: update Canada KMZ file reference and adjust makefile commands for consistency

--- bin/swc_civic.mk
+++ bin/swc_civic.mk
@@ -10,7 +10,7 @@ import_us_geojson:
 	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/us_congressional_districts.geojson" -nln us_congressional_district -overwrite
 
 import_ca_kmz:
-	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/FED_CA_2021_EN.kmz" -nlt PROMOTE_TO_MULTI -lco precision=NO -nln ca_electoral_districts -overwrite
+	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/FED_CA_2023_EN.kmz" -nlt PROMOTE_TO_MULTI -lco precision=NO -nln ca_electoral_districts -overwrite
 
 import_gb_geojson:
 	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/uk_parliamentary_constituencies.geojson" -nln uk_parliamentary_constituency -overwrite
@@ -19,7 +19,7 @@ import_au_shp:
 	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/au/AUS_ELB_region.shp" -nln au_federal_electoral_district -nlt MULTIPOLYGON -overwrite
 
 import_swc_civic_data:
-	make import_us_geojson
-	make import_ca_kmz
-	make import_gb_geojson
-	make import_au_shp
+	make import_us_geojson -f bin/swc_civic.mk
+	make import_ca_kmz -f bin/swc_civic.mk
+	make import_gb_geojson -f bin/swc_civic.mk
+	make import_au_shp -f bin/swc_civic.mk
