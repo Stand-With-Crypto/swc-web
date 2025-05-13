7cd6aca26e4eb5a65e4cd52dd2f1a822bb736573
Lucas Pessone <155585835+lucasrmp-cb@users.noreply.github.com>
2025-05-13 14:47:11 -0300

    Postgis config (#2221)
    * wip: makefile with import commands & docker-compose postgis container
    
    * feat: add import_all_data make command
    
    * feat: add intl politician page CTA
    
    * feat: integrate PostGIS for congressional district queries
    
    * Revert "feat: add intl politician page CTA"
    
    This reverts commit ea37869dcb592699e3968c1eb4d8d293eb97f471.
    
    * fix: lint
    
    * fix: rename DB & move swcCivic to utils/server & use prisma instead of pg
    
    * fix: lint
    
    * feat: use env variables on makefile
    
    * fix: use package.json script to generate prisma schema instead of npx
    
    * fix: env.example swc_civic db variable name
    
    * fix: move swc_civic makefile to bin folder
    
    * fix: type definition for wkb_geometry column & add postgis extension to schema
    
    * fix: update Canada KMZ file reference and adjust makefile commands for consistency
    
    ---------
    
    Co-authored-by: Lucas Bernardi <rossi.lucas06@gmail.com>

---
+++ bin/swc_civic.mk
@@ -0,0 +1,25 @@
+include .env
+
+DC_FLAGS = -f dev.docker-compose.yml
+SWC_CIVIC_DATABASE_URL ?= $(SWC_CIVIC_DATABASE_URL)
+
+dc_postgres:
+	docker compose $(DC_FLAGS) exec -ti postgis psql -U postgres --dbname=swc_civic
+
+import_us_geojson:
+	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/us_congressional_districts.geojson" -nln us_congressional_district -overwrite
+
+import_ca_kmz:
+	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/FED_CA_2023_EN.kmz" -nlt PROMOTE_TO_MULTI -lco precision=NO -nln ca_electoral_districts -overwrite
+
+import_gb_geojson:
+	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/uk_parliamentary_constituencies.geojson" -nln uk_parliamentary_constituency -overwrite
+
+import_au_shp:
+	ogr2ogr -f "PostgreSQL" "PG:$(SWC_CIVIC_DATABASE_URL)" "./data/au/AUS_ELB_region.shp" -nln au_federal_electoral_district -nlt MULTIPOLYGON -overwrite
+
+import_swc_civic_data:
+	make import_us_geojson -f bin/swc_civic.mk
+	make import_ca_kmz -f bin/swc_civic.mk
+	make import_gb_geojson -f bin/swc_civic.mk
+	make import_au_shp -f bin/swc_civic.mk
