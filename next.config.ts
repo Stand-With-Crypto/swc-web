7844ef0f6824e51fe55611a41f8818a304c5c2cf
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2025-05-14 10:12:00 -0300

    feat: add shortlink

--- next.config.ts
+++ next.config.ts
@@ -436,6 +436,12 @@ const nextConfig: NextConfig = {
       },
       // SMS shortlinks
       {
+        source: '/s/founders-push/:sessionId*',
+        destination:
+          '/action/email?utm_source=swc&utm_medium=sms&utm_campaign=founders-push-1&sessionId=:sessionId*',
+        permanent: true,
+      },
+      {
         source: '/s/paul-atkins',
         destination: 'https://speak4.app/lp/b901vnot/?ts=1744310543',
         permanent: true,
