79eb2d1281dea39b5d7e753e0acfb35971966933
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-05-15 18:59:11 -0300

    Fix payload size & rate limiting errors

--- src/inngest/functions/sendgridContactsCronJob/config.ts
+++ src/inngest/functions/sendgridContactsCronJob/config.ts
@@ -2,7 +2,7 @@ import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 export const SENDGRID_CONTACTS_API_LIMIT = 30000
 export const SIX_MB_IN_BYTES = 6 * 1024 * 1024
-export const SENDGRID_CONTACTS_API_LIMIT_BYTES = 3600000 // Approx 3.44MiB, server limit is 4MiB (4,194,304 bytes)
+export const SENDGRID_CONTACTS_API_LIMIT_BYTES = 3900000 // Approx 3.72MiB, server limit is 4MiB (4,194,304 bytes)
 export const DB_READ_LIMIT = 100000
 
 /**
