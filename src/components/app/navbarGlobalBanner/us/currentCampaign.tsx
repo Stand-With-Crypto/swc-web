38c24b435e8b989d5a7747183cc12bddee147783
Lucas Pessone <lucasrodrigues.demagalhaespessone@coinbase.com>
2025-05-16 13:58:35 -0300

    feat: add genius banner

--- src/components/app/navbarGlobalBanner/us/currentCampaign.tsx
+++ src/components/app/navbarGlobalBanner/us/currentCampaign.tsx
@@ -1,3 +1,20 @@
+import { CampaignWrapper } from '@/components/app/navbarGlobalBanner/common/campaignWrapper'
+import { useIntlUrls } from '@/hooks/useIntlUrls'
+import { useIsMobile } from '@/hooks/useIsMobile'
+import Link from 'next/link'
+
 export function UsCurrentNavbarGlobalBannerCampaign() {
-  return null
+  const isMobile = useIsMobile()
+  const urls = useIntlUrls()
+  const campaignUrl = urls.emailDeeplink()
+
+  return (
+    <CampaignWrapper url={campaignUrl}>
+      <p>
+        KEY VOTE ALERT IN THE SENATE – VOTE "YES" ON GENIUS ACT
+        {isMobile ? <br /> : <span> – </span>}
+        Find out more <strong>{isMobile ? 'here' : <Link href={campaignUrl}>here</Link>}</strong>
+      </p>
+    </CampaignWrapper>
+  )
 }
