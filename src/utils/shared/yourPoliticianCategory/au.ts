0fe915c07c72c09d9df92eb89988989bf00972b1
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-06-06 15:04:12 -0300

    Add AU house-of-reps category

--- src/utils/shared/yourPoliticianCategory/au.ts
+++ src/utils/shared/yourPoliticianCategory/au.ts
@@ -1,9 +1,12 @@
 import { DTSI_PersonRoleCategory } from '@/data/dtsi/generated'
 import { DTSIPeopleByCongressionalDistrictQueryResult } from '@/data/dtsi/queries/queryDTSIPeopleByCongressionalDistrict'
 
-export type YourPoliticianCategory = 'senate'
+export type YourPoliticianCategory = 'senate' | 'house-of-reps'
 
-export const YOUR_POLITICIAN_CATEGORY_OPTIONS: readonly YourPoliticianCategory[] = ['senate']
+export const YOUR_POLITICIAN_CATEGORY_OPTIONS: readonly YourPoliticianCategory[] = [
+  'senate',
+  'house-of-reps',
+]
 
 export function getYourPoliticianCategoryDisplayName(
   category: YourPoliticianCategory,
@@ -16,6 +19,8 @@ export function getYourPoliticianCategoryDisplayName(
   switch (category) {
     case 'senate':
       return maxCount === 1 ? 'Senator' : 'Senators'
+    case 'house-of-reps':
+      return maxCount === 1 ? 'Representative' : 'Representatives'
   }
 }
 export function getYourPoliticianCategoryShortDisplayName(
@@ -29,6 +34,8 @@ export function getYourPoliticianCategoryShortDisplayName(
   switch (category) {
     case 'senate':
       return maxCount === 1 ? 'Senator' : 'Senators'
+    case 'house-of-reps':
+      return maxCount === 1 ? 'Representative' : 'Representatives'
   }
 }
 
@@ -40,6 +47,10 @@ export function filterDTSIPeopleByPoliticalCategory(category: YourPoliticianCate
       case 'senate':
         return dtsiPeople.filter(
           person => person.primaryRole?.roleCategory === DTSI_PersonRoleCategory.SENATE,
+        )
+      case 'house-of-reps':
+        return dtsiPeople.filter(
+          person => person.primaryRole?.roleCategory === DTSI_PersonRoleCategory.CONGRESS,
         )
       default:
         return dtsiPeople
