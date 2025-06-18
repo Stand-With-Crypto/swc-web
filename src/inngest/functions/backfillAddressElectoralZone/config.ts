46c5bee1b9025487b8525012458554790608a533
eduardopicolo-cb <167582510+eduardopicolo-cb@users.noreply.github.com>
2025-06-18 19:42:49 -0300

    Fix script pagination skipping records (#2403)

--- src/inngest/functions/backfillAddressElectoralZone/config.ts
+++ src/inngest/functions/backfillAddressElectoralZone/config.ts
@@ -1,14 +1,10 @@
 export const BACKFILL_ADDRESS_ELECTORAL_ZONE_BATCH_SIZE = 5000
 export const BATCH_BUFFER = 1.05
 export const BACKFILL_ADDRESS_ELECTORAL_ZONE_RETRY_LIMIT = 5
-export const MINI_BATCH_SIZE = 100
+export const MINI_BATCH_SIZE = 20
 /**
  * This is the maximum number of concurrent "Processor" jobs that can be running at once.
  * This is to prevent the script from overwhelming the database and hitting connection pool limits.
  */
 export const CONCURRENCY_LIMIT = 10
-/**
- * This is to avoid hitting Google's API rate limit.
- */
-export const SUB_BATCH_SIZE = 10
 export const SLEEP_DURATION = 5000
