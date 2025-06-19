e1b78fa9cd90d933b59437f63effb1565cb70f6b
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-06-18 23:12:46 -0300

    Refactor backfill script to use raw query

--- src/inngest/functions/backfillAddressElectoralZone/config.ts
+++ src/inngest/functions/backfillAddressElectoralZone/config.ts
@@ -1,10 +1,9 @@
 export const BACKFILL_ADDRESS_ELECTORAL_ZONE_BATCH_SIZE = 5000
 export const BATCH_BUFFER = 1.05
 export const BACKFILL_ADDRESS_ELECTORAL_ZONE_RETRY_LIMIT = 5
-export const MINI_BATCH_SIZE = 20
+export const MINI_BATCH_SIZE = 100
 /**
  * This is the maximum number of concurrent "Processor" jobs that can be running at once.
  * This is to prevent the script from overwhelming the database and hitting connection pool limits.
  */
-export const CONCURRENCY_LIMIT = 10
-export const SLEEP_DURATION = 5000
+export const CONCURRENCY_LIMIT = 20
