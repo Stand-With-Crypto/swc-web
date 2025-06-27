d0000a6afaf8dddd2b9ad6d210f0cd297b3e3931
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-06-27 16:17:03 -0300

    feat: update branding from 'Stand With Crypto International Ltd' to 'SWC International Ltd'

--- src/components/app/sms/smsOptInConsentText.tsx
+++ src/components/app/sms/smsOptInConsentText.tsx
@@ -5,11 +5,11 @@ const SMS_OPT_IN_CONSENT: Record<SupportedCountryCodes, string> = {
   [SupportedCountryCodes.US]:
     'you consent to receive recurring texts from Stand With Crypto. You can reply STOP to stop receiving texts. Message and data rates may apply.',
   [SupportedCountryCodes.GB]:
-    'I authorize Stand With Crypto International Ltd and its service providers to contact me at this number via text (SMS) for cryptocurrency advocacy purposes. Message and data rates may apply. To opt-out at any time reply "STOP".',
+    'I authorize SWC International Ltd and its service providers to contact me at this number via text (SMS) for cryptocurrency advocacy purposes. Message and data rates may apply. To opt-out at any time reply "STOP".',
   [SupportedCountryCodes.CA]:
-    'I authorize Stand With Crypto International Ltd and its service providers to contact me at this number via text (SMS) for cryptocurrency advocacy purposes. Message and data rates may apply. To opt-out at any time reply "STOP".',
+    'I authorize SWC International Ltd and its service providers to contact me at this number via text (SMS) for cryptocurrency advocacy purposes. Message and data rates may apply. To opt-out at any time reply "STOP".',
   [SupportedCountryCodes.AU]:
-    'I authorize Stand With Crypto International Ltd and its service providers to contact me at this number via text (SMS) for cryptocurrency advocacy purposes. Message and data rates may apply. To opt-out at any time reply "STOP".',
+    'I authorize SWC International Ltd and its service providers to contact me at this number via text (SMS) for cryptocurrency advocacy purposes. Message and data rates may apply. To opt-out at any time reply "STOP".',
 }
 
 export function SMSOptInConsentText({
