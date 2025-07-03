d881fcf65e2607b2878a8bf3f568d317c8b18727
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-07-03 11:18:46 -0300

    Refactor getElectoralZone

--- src/utils/shared/getElectoralZoneFromAddress.ts
+++ src/utils/shared/getElectoralZoneFromAddress.ts
@@ -5,7 +5,7 @@ import { fetchReq } from '@/utils/shared/fetchReq'
 import { getLogger } from '@/utils/shared/logger'
 import { apiUrls, fullUrl } from '@/utils/shared/urls'
 
-export type GetElectoralZoneResult = Awaited<ReturnType<typeof getElectoralZone>>
+export type GetElectoralZoneResult = Awaited<ReturnType<typeof parseCivicApiResults>>
 
 const logger = getLogger('getElectoralZoneFromAddress')
 
@@ -32,7 +32,7 @@ export function maybeGetElectoralZoneFromAddress({
   })
 }
 
-export function getElectoralZoneFromAddressOrPlaceId({
+export async function getElectoralZoneFromAddressOrPlaceId({
   address,
   placeId,
 }: {
@@ -40,10 +40,15 @@ export function getElectoralZoneFromAddressOrPlaceId({
   placeId?: string
 }) {
   const params = { address, placeId }
-  return getElectoralZone(apiUrls.swcCivicElectoralZoneByAddress(params), { params })
+  try {
+    const response = await fetchReq(fullUrl(apiUrls.swcCivicElectoralZoneByAddress(params)))
+    return parseCivicApiResults(response)
+  } catch (error) {
+    return parseCivicApiError(error)
+  }
 }
 
-export function getElectoralZoneFromGeolocation({
+export async function getElectoralZoneFromGeolocation({
   latitude,
   longitude,
 }: {
@@ -51,19 +56,16 @@ export function getElectoralZoneFromGeolocation({
   longitude: number
 }) {
   const params = { latitude, longitude }
-  return getElectoralZone(apiUrls.swcCivicElectoralZoneByGeolocation(params), { params })
+  try {
+    const response = await fetchReq(fullUrl(apiUrls.swcCivicElectoralZoneByGeolocation(params)))
+    return parseCivicApiResults(response)
+  } catch (error) {
+    return parseCivicApiError(error)
+  }
 }
 
-async function getElectoralZone(
-  url: string,
-  {
-    params,
-  }: {
-    params: Record<string, any>
-  },
-) {
+async function parseCivicApiResults(response: Response) {
   try {
-    const response = await fetchReq(fullUrl(url))
     const data = (await response.json()) as ElectoralZone
     if (!data) {
       return {
@@ -72,25 +74,28 @@ async function getElectoralZone(
     }
     return data
   } catch (error) {
-    logger.error('Error fetching electoral zone:', { error, params })
+    return parseCivicApiError(error)
+  }
+}
 
-    if (error instanceof Response) {
-      logger.info('Response error status:', error.status)
-      if (error.status === 404) {
-        logger.info('Electoral zone not found (404)')
-        return {
-          notFoundReason: 'ELECTORAL_ZONE_NOT_FOUND' as const,
-        }
+function parseCivicApiError(error: unknown) {
+  logger.error('Error fetching electoral zone:', { error })
+  if (error instanceof Response) {
+    logger.info('Response error status:', error.status)
+    if (error.status === 404) {
+      logger.info('Electoral zone not found (404)')
+      return {
+        notFoundReason: 'ELECTORAL_ZONE_NOT_FOUND' as const,
       }
-      logger.info('Unexpected response error:', error.status)
     }
+    logger.info('Unexpected response error:', error.status)
+  }
 
-    logger.info(
-      'Unexpected error occurred:',
-      error instanceof Error ? error.message : 'Unknown error type',
-    )
-    return {
-      notFoundReason: 'UNEXPECTED_ERROR' as const,
-    }
+  logger.info(
+    'Unexpected error occurred:',
+    error instanceof Error ? error.message : 'Unknown error type',
+  )
+  return {
+    notFoundReason: 'UNEXPECTED_ERROR' as const,
   }
 }
