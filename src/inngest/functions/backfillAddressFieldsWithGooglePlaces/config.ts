ae21a7257780b4ac2ef9853dad5423a136d623db
Lucas Bernardi <45859300+LucasdRossi@users.noreply.github.com>
2025-07-07 19:39:42 -0300

    fix: use Promise.allSettled to update backfilled addresses (#2472)

--- src/inngest/functions/backfillAddressFieldsWithGooglePlaces/config.ts
+++ src/inngest/functions/backfillAddressFieldsWithGooglePlaces/config.ts
@@ -1,7 +1,7 @@
 export const BACKFILL_ADDRESS_FIELDS_WITH_GOOGLE_PLACES_BATCH_SIZE = 5000
 export const BATCH_BUFFER = 1.05
 export const BACKFILL_ADDRESS_FIELDS_WITH_GOOGLE_PLACES_RETRY_LIMIT = 5
-export const DEFAULT_MINI_BATCH_SIZE = 2500
+export const DEFAULT_MINI_BATCH_SIZE = 600
 /**
  * This is the maximum number of concurrent "Processor" jobs that can be running at once.
  * This is to prevent the script from overwhelming the database and hitting connection pool limits.
