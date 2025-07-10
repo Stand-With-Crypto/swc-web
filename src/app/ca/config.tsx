f419d325c2f3854d5eef1427f823008d23968db8
Lucas Bernardi <45859300+LucasdRossi@users.noreply.github.com>
2025-07-10 16:34:04 -0300

    feat: add Terms of Service links to footer for AU, CA, and GB (#2486)

--- src/app/ca/config.tsx
+++ src/app/ca/config.tsx
@@ -67,6 +67,7 @@ export const footerConfig: FooterProps = {
     </div>
   ),
   links: [
+    { href: urls.termsOfService(), text: 'Terms of service' },
     {
       href: urls.privacyPolicy(),
       text: 'Privacy',
