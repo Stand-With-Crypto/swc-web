a90d0cfb871e1922f0e7da12078a2dd031aa33e9
Theo Nejm <theo.nejm@coinbase.com>
2025-07-17 12:45:32 -0300

    feat: refactor district rankings to support multiple countries
    - Add shared StateCode type supporting US, CA, and AU
    - Split ranking logic into country-specific implementations
    - Update ranking data retrieval and upsertion for international support
    - Remove deprecated constants file

---
+++ src/utils/server/districtRankings/types.ts
@@ -0,0 +1,5 @@
+import { AUStateCode } from '@/utils/shared/stateMappings/auStateUtils'
+import { CAProvinceCode } from '@/utils/shared/stateMappings/caProvinceUtils'
+import { USStateCode } from '@/utils/shared/stateMappings/usStateUtils'
+
+export type StateCode = USStateCode | CAProvinceCode | AUStateCode
