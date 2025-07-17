ba50bbe8e6e55115d710e72be6d00831392f0229
Lucas Bernardi <45859300+LucasdRossi@users.noreply.github.com>
2025-07-17 19:44:18 -0300

    feat: clarity senate email action (#2514)
    * feat: redirect to root deeplink on inactive email campaigns
    
    * feat: add clarity-senate email action
    
    * feat: update US home CTA to new clarity-senate email campaign
    
    * feat: update us global banner text
    
    * fix: remove url from CampaignWrapper
    
    * fix: rename clarity-senate email action file

--- src/components/app/navbarGlobalBanner/us/currentCampaign.tsx
+++ src/components/app/navbarGlobalBanner/us/currentCampaign.tsx
@@ -1,27 +1,11 @@
 'use client'
 
-import Link from 'next/link'
-
 import { CampaignWrapper } from '@/components/app/navbarGlobalBanner/common/campaignWrapper'
-import { getDeeplinkUrlByCampaignName } from '@/components/app/userActionFormEmailCongressperson/getDeeplinkUrl'
-import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
-import { USUserActionEmailCampaignName } from '@/utils/shared/userActionCampaigns/us/usUserActionCampaigns'
 
 export function UsCurrentNavbarGlobalBannerCampaign() {
-  const campaignUrl = getDeeplinkUrlByCampaignName({
-    countryCode: SupportedCountryCodes.US,
-    campaignName: USUserActionEmailCampaignName.CLARITY_GENIUS_ACTS_JUL_17_2025,
-  })
-
   return (
-    <CampaignWrapper url={campaignUrl}>
-      <p>
-        Key Vote Alert – Floor Vote TODAY on The CLARITY Act and The GENIUS Act
-        <span> – </span>
-        <strong>
-          <Link href={campaignUrl}>Email your House Member now</Link>
-        </strong>
-      </p>
+    <CampaignWrapper>
+      <p>Historic Crypto Wins! U.S. House Passes CLARITY and GENIUS Acts</p>
     </CampaignWrapper>
   )
 }
