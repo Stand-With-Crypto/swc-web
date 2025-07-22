f4dac8832bea1a9274aa7a817332b29cac6815d0
Lucas Bernardi <rossi.lucas06@gmail.com>
2025-07-22 14:09:00 -0300

    chore: sort imports

--- src/data/pageSpecific/getAdvocatesMapData.ts
+++ src/data/pageSpecific/getAdvocatesMapData.ts
@@ -1,7 +1,7 @@
 import 'server-only'
 
-import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 import { getTotalAdvocatesPerStateByCountry } from '@/data/aggregations/getTotalAdvocatesPerStateByCountry'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 export async function getAdvocatesMapData({ countryCode }: { countryCode: SupportedCountryCodes }) {
   const results = await getTotalAdvocatesPerStateByCountry(countryCode)
