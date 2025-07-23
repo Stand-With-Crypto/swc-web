a6fc0445468726e226349cbaa9373aa2b855abcd
Theo Nejm <theo.nejm@coinbase.com>
2025-07-23 13:41:16 -0300

    feat: add "Top constituencies" tab for Canada's community page

--- src/app/ca/community/referrals/[[...page]]/page.tsx
+++ src/app/ca/community/referrals/[[...page]]/page.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { flatten, times } from 'lodash-es'
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
-import { CAGetPageData } from '@/components/app/pageCommunity'
 import { CaPageCommunity, PageLeaderboardInferredProps } from '@/components/app/pageCommunity/ca'
-import { CA_RECENT_ACTIVITY_PAGINATION } from '@/components/app/pageCommunity/ca/constants'
+import {
+  CA_COMMUNITY_PAGINATION_DATA,
+  CA_RECENT_ACTIVITY_PAGINATION,
+} from '@/components/app/pageCommunity/ca/constants'
+import { validatePageNum } from '@/components/app/pageCommunity/common/pageValidator'
 import { RecentActivityAndLeaderboardTabs } from '@/components/app/pageHome/ca/recentActivityAndLeaderboardTabs'
 import { PageProps } from '@/types'
+import { getDistrictsLeaderboardData } from '@/utils/server/districtRankings/upsertRankings'
 import { generateMetadataDetails } from '@/utils/server/metadataUtils'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 export const revalidate = 30 // 30 seconds
 export const dynamic = 'error'
@@ -32,17 +38,30 @@ export default async function CaCommunityRecentActivityPage(
   props: CaCommunityRecentActivityPageProps,
 ) {
   const params = await props.params
-  const { offset, pageNum, publicRecentActivity, totalPages } = await CAGetPageData({
-    page: params.page,
-  })
+  const { itemsPerPage } =
+    CA_COMMUNITY_PAGINATION_DATA[RecentActivityAndLeaderboardTabs.TOP_CONSTITUENCIES]
 
-  const dataProps: PageLeaderboardInferredProps = {
-    leaderboardData: undefined,
-    publicRecentActivity,
-    tab: RecentActivityAndLeaderboardTabs.RECENT_ACTIVITY,
+  const { page } = params
+  const pageNum = validatePageNum(page ?? [])
+
+  if (!pageNum) {
+    notFound()
   }
 
-  return (
-    <CaPageCommunity {...dataProps} offset={offset} pageNum={pageNum} totalPages={totalPages} />
-  )
+  const offset = (pageNum - 1) * itemsPerPage
+
+  const commonParams = {
+    limit: itemsPerPage,
+    offset,
+    countryCode: SupportedCountryCodes.CA,
+  }
+
+  const { items: leaderboardData } = await getDistrictsLeaderboardData(commonParams)
+
+  const dataProps: PageLeaderboardInferredProps = {
+    leaderboardData,
+    publicRecentActivity: undefined,
+    tab: RecentActivityAndLeaderboardTabs.TOP_CONSTITUENCIES,
+  }
+  return <CaPageCommunity {...dataProps} offset={offset} pageNum={pageNum} totalPages={undefined} />
 }
