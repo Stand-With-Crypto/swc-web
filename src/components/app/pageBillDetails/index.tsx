03aefec40c5dd0178028aa69b9df521e21ccb2ad
Theo Nejm <54482697+theo-nejm@users.noreply.github.com>
2025-07-29 11:49:42 -0300

    feat: update analysis section (#2552)
    * feat: enhance bill details page with sanitized HTML rendering and related resources
    
    * chore: move text back to builder folder
    
    * chore: use StyledHtmlContent into builder text and refactor component
    
    * fix: change import
    
    ---------
    
    Co-authored-by: Theo Nejm <theo.nejm@coinbase.com>

--- src/components/app/pageBillDetails/index.tsx
+++ src/components/app/pageBillDetails/index.tsx
@@ -1,4 +1,5 @@
 import { orderBy } from 'lodash-es'
+import { FileTextIcon } from 'lucide-react'
 
 import { CryptoSupportHighlight } from '@/components/app/cryptoSupportHighlight'
 import { VotesSection } from '@/components/app/pageBillDetails/votesSection'
@@ -6,6 +7,7 @@ import { FormattedDatetime } from '@/components/ui/formattedDatetime'
 import { ExternalLink } from '@/components/ui/link'
 import { PageSubTitle } from '@/components/ui/pageSubTitle'
 import { PageTitle } from '@/components/ui/pageTitleText'
+import { StyledHtmlContent } from '@/components/ui/styledHtmlContent'
 import { COUNTRY_CODE_TO_LOCALE, SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 import { SWCBill } from '@/utils/shared/zod/getSWCBills'
 
@@ -38,14 +40,28 @@ export function PageBillDetails(props: PageBillDetailsProps) {
       </section>
 
       {bill.analysis && (
-        <section className="space-y-8 text-center">
-          <p className="text-lg font-semibold">Analysis</p>
-
-          <div className="space-y-6 text-center text-fontcolor-muted">
-            {/**
-             * @todo: Replace with the following component <RichTextFormatter richText={bill.analysis} />
-             */}
-            {bill.analysis}
+        <section className="space-y-6 text-center lg:space-y-10">
+          <p className="text-4xl font-bold">Analysis</p>
+          <div className="flex flex-col gap-10 lg:flex-row">
+            <StyledHtmlContent
+              className="w-full space-y-4 text-start text-fontcolor-muted [&_strong]:text-fontcolor-muted"
+              html={bill.analysis}
+            />
+            {bill.relatedUrls.length > 0 && (
+              <div className="flex h-max w-full flex-col gap-6 rounded-3xl border border-muted p-6 lg:w-[478px]">
+                <strong className="text-lg">More Resources</strong>
+                {bill.relatedUrls.map(relatedUrl => (
+                  <ExternalLink
+                    className="flex w-full items-center gap-4 text-start text-primary"
+                    href={relatedUrl.url}
+                    key={relatedUrl.url}
+                  >
+                    <FileTextIcon size={24} />
+                    <span className="flex-1">{relatedUrl.title}</span>
+                  </ExternalLink>
+                ))}
+              </div>
+            )}
           </div>
         </section>
       )}
