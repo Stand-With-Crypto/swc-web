9df7424ec17ce1c10aed40b9590b87f47e2cce41
Theo Nejm <theo.nejm@coinbase.com>
2025-08-08 12:02:32 -0300

    refactor: centralize heading component to reduce duplicated logic

--- src/components/app/pageReferrals/us/heading.tsx
+++ src/components/app/pageReferrals/us/heading.tsx
@@ -1,42 +1,19 @@
-'use client'
+import { PageReferralsHeading } from '@/components/app/pageReferrals/common/heading'
 
-import { PageReferrals } from '@/components/app/pageReferrals/common'
-import { PageSubTitle } from '@/components/ui/pageSubTitle'
-import { PageTitle } from '@/components/ui/pageTitleText'
-import { useHasHydrated } from '@/hooks/useHasHydrated'
-import { useSession } from '@/hooks/useSession'
-
-interface PageReferralsHeadingProps {
+interface UsPageReferralsHeadingProps {
   stateName?: string
 }
 
-export function UsPageReferralsHeading({ stateName }: PageReferralsHeadingProps) {
-  const { isLoggedIn, isLoading } = useSession()
-  const hasHydrated = useHasHydrated()
-
-  if (!isLoggedIn || isLoading || !hasHydrated || stateName) {
-    return (
-      <PageReferrals.Heading>
-        <PageTitle>District Leaderboard</PageTitle>
-        <PageSubTitle>
-          {stateName
-            ? `See which districts in ${stateName} have the most advocates.`
-            : 'See which districts have the most number of Stand With Crypto advocates.'}
-        </PageSubTitle>
-      </PageReferrals.Heading>
-    )
-  }
-
+export function UsPageReferralsHeading({ stateName }: UsPageReferralsHeadingProps) {
   return (
-    <PageReferrals.Heading>
-      <PageTitle>
-        Invite a friend to join
-        <br />
-        <span className="inline-block">Stand With Crypto</span>
-      </PageTitle>
-      <PageSubTitle>
-        Send your friends your unique referral code to encourage them to signup and take action
-      </PageSubTitle>
-    </PageReferrals.Heading>
+    <PageReferralsHeading
+      leaderboardSubtitle={
+        stateName
+          ? `See which districts in ${stateName} have the most advocates.`
+          : 'See which districts have the most number of Stand With Crypto advocates.'
+      }
+      leaderboardTitle="District Leaderboard"
+      stateName={stateName}
+    />
   )
 }
