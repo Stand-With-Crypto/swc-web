1d4e63c34e8e24a8463e752710909d7bedea5252
Theo Nejm <54482697+theo-nejm@users.noreply.github.com>
2025-08-11 16:48:23 -0300

    Fix/refactor: reduce layout shift on referrals and improve code (#2579)
    * refactor: update metadata titles and descriptions for Australia and Canada referrals
    
    * refactor: centralize heading component to reduce duplicated logic
    
    * refactor: centralize leaderboard component to reduce duplicated logic
    
    * refactor: implement centralized location ranking components for AU, CA, and US
    
    * feat: enhance user referral URL component with loading state and session handling
    
    * poc: change base title to 2 lines to validate layout shift
    
    * fix: zeroes layout shift on signed out screen
    
    * refactor: remove back the leaderboard title
    
    * refactor: wrap division ranking components in new wrappers for AU, CA, and US
    
    ---------
    
    Co-authored-by: Theo Nejm <theo.nejm@coinbase.com>

--- src/components/app/pageReferrals/ca/heading.tsx
+++ src/components/app/pageReferrals/ca/heading.tsx
@@ -1,42 +1,19 @@
-'use client'
+import { PageReferralsHeading } from '@/components/app/pageReferrals/common/heading'
 
-import { PageReferrals } from '@/components/app/pageReferrals/common'
-import { PageSubTitle } from '@/components/ui/pageSubTitle'
-import { PageTitle } from '@/components/ui/pageTitleText'
-import { useHasHydrated } from '@/hooks/useHasHydrated'
-import { useSession } from '@/hooks/useSession'
-
-interface PageReferralsHeadingProps {
+interface CaPageReferralsHeadingProps {
   stateName?: string
 }
 
-export function CaPageReferralsHeading({ stateName }: PageReferralsHeadingProps) {
-  const { isLoggedIn, isLoading } = useSession()
-  const hasHydrated = useHasHydrated()
-
-  if (!isLoggedIn || isLoading || !hasHydrated || stateName) {
-    return (
-      <PageReferrals.Heading>
-        <PageTitle>Constituencies Leaderboard</PageTitle>
-        <PageSubTitle>
-          {stateName
-            ? `See which constituencies in ${stateName} have the most advocates.`
-            : 'See which constituencies have the most number of Stand With Crypto advocates.'}
-        </PageSubTitle>
-      </PageReferrals.Heading>
-    )
-  }
-
+export function CaPageReferralsHeading({ stateName }: CaPageReferralsHeadingProps) {
   return (
-    <PageReferrals.Heading>
-      <PageTitle>
-        Invite a friend to join
-        <br />
-        <span className="inline-block">Stand With Crypto</span>
-      </PageTitle>
-      <PageSubTitle>
-        Send your friends your unique referral code to encourage them to signup and take action
-      </PageSubTitle>
-    </PageReferrals.Heading>
+    <PageReferralsHeading
+      leaderboardSubtitle={
+        stateName
+          ? `See which constituencies in ${stateName} have the most advocates.`
+          : 'See which constituencies have the most number of Stand With Crypto advocates.'
+      }
+      leaderboardTitle="Constituencies Leaderboard"
+      stateName={stateName}
+    />
   )
 }
