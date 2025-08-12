bd9824001555093fb7190ededc26abacc71c485b
Luiz Neto <luizneto.ifnmg@gmail.com>
2025-08-12 19:32:47 -0300

    fix: adjust timeline on mobile

--- src/components/ui/timeline/timeline.tsx
+++ src/components/ui/timeline/timeline.tsx
@@ -15,6 +15,7 @@ import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 const {
   TIMELINE_HEIGHT_DESKTOP,
   TIMELINE_HEIGHT_MOBILE,
+  TIMELINE_POINTS_ADJUSTMENT_PERCENT,
   TIMELINE_SPACING_DESKTOP,
   TIMELINE_SPACING_MOBILE,
 } = TIMELINE_CONFIG
@@ -42,46 +43,78 @@ export function Timeline(props: TimelineProps) {
         return a.date.getTime() - b.date.getTime()
       })
 
-    const majorMilestones = plotPoints
-      .filter(plotPoint => plotPoint.isMajorMilestone)
+    const initialMajorMilestones = plotPoints
+      .filter(point => point.isMajorMilestone)
       .map((point, index, points) => {
+        const maxPercent = isMobile ? 100 - TIMELINE_POINTS_ADJUSTMENT_PERCENT : 100
+        const steps = points.length - 1
+
         return {
           ...point,
-          positionPercent: (index * 100) / (points.length - 1),
+          positionPercent: (index * maxPercent) / steps,
         }
       })
 
-    const minorMilestones = plotPoints
-      .filter(plotPoint => !plotPoint.isMajorMilestone)
+    const initialMinorMilestones = plotPoints
+      .filter(point => !point.isMajorMilestone)
       .map(point => {
-        const parentMilestoneIndex = majorMilestones.findLastIndex(majorMilestone =>
-          majorMilestone.date && point.date
-            ? majorMilestone.date.getTime() < point.date.getTime()
-            : false,
-        )
+        const parentMilestoneIndex =
+          initialMajorMilestones.findLastIndex(majorMilestone =>
+            majorMilestone.date && point.date
+              ? majorMilestone.date.getTime() <= point.date.getTime()
+              : false,
+          ) || 0
 
         return {
           ...point,
+          parentMilestone: initialMajorMilestones[parentMilestoneIndex],
           parentMilestoneIndex,
-          parentMilestone: majorMilestones[parentMilestoneIndex],
         }
       })
-      .map((point, _index, points) => {
-        const maxLength = 100 / (majorMilestones.length - 1)
 
-        const siblings = points.filter(
-          ({ parentMilestoneIndex }) => parentMilestoneIndex === point.parentMilestoneIndex,
+    const majorMilestones = initialMajorMilestones
+      .map(point => {
+        const children = initialMinorMilestones.filter(
+          ({ parentMilestone }) => parentMilestone.id === point.id,
         )
-
-        const xPos = maxLength / (siblings.length + 1)
-
-        const index = siblings.findIndex(({ id }) => id === point.id)
+        const weight = isMobile
+          ? (children.length / initialMinorMilestones.length) * TIMELINE_POINTS_ADJUSTMENT_PERCENT
+          : 0
 
         return {
           ...point,
-          positionPercent: point.parentMilestone.positionPercent + xPos * (index + 1),
+          weight,
         }
       })
+      .map((point, index, points) => ({
+        ...point,
+        positionPercent:
+          point.positionPercent +
+          points.slice(0, index).reduce((sum, { weight }) => sum + weight, 0),
+      }))
+
+    const minorMilestones = initialMinorMilestones.map((point, _index, points) => {
+      const parentMilestone = majorMilestones.find(
+        majorMilestone => majorMilestone.id === point.parentMilestone.id,
+      )!
+      const nextMilestone =
+        majorMilestones[point.parentMilestoneIndex + 1] || majorMilestones.at(-1)
+
+      const maxLength = nextMilestone.positionPercent - parentMilestone.positionPercent
+
+      const siblings = points.filter(
+        targetPoint => parentMilestone.id === targetPoint.parentMilestone.id,
+      )
+
+      const xPos = maxLength / (siblings.length + 1)
+
+      const index = siblings.findIndex(({ id }) => id === point.id)
+
+      return {
+        ...point,
+        positionPercent: parentMilestone.positionPercent + xPos * (index + 1),
+      }
+    })
 
     const lastHighlightedMilestone = plotPoints.findLast(point => point.isHighlighted)
     const milestones = [...majorMilestones, ...minorMilestones]
@@ -90,8 +123,12 @@ export function Timeline(props: TimelineProps) {
       : majorMilestones[0]
     const targetPercent = currentMilestone.positionPercent
 
-    return { majorMilestones, minorMilestones, targetPercent }
-  }, [props.plotPoints])
+    return {
+      majorMilestones,
+      minorMilestones,
+      targetPercent,
+    }
+  }, [isMobile, props.plotPoints])
 
   const animation = useProgressAnimation(targetPercent)
 
