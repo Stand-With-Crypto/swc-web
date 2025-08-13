7bcd1eb4396b756908eb702a8f77620b78d2ce44
Nicolas Renard <nicolas.renard@coinbase.com>
2025-08-13 17:36:27 -0300

    refactor: enhance getPageContent and getPageDetails functions with pRetry for improved error handling and retry logic

--- src/utils/server/builder/models/page/utils/getPageContent.ts
+++ src/utils/server/builder/models/page/utils/getPageContent.ts
@@ -1,43 +1,51 @@
 import * as Sentry from '@sentry/nextjs'
+import pRetry from 'p-retry'
 
 import { builderSDKClient } from '@/utils/server/builder/builderSDKClient'
 import { BuilderPageModelIdentifiers } from '@/utils/server/builder/models/page/constants'
 import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
-export function getPageContent(
+export async function getPageContent(
   pageModelName: BuilderPageModelIdentifiers,
   pathname: string,
   countryCode: SupportedCountryCodes,
 ) {
-  return builderSDKClient
-    .get(pageModelName, {
-      query: {
-        data: {
-          countryCode: countryCode.toUpperCase(),
-        },
+  const builderOptions = {
+    query: {
+      data: {
+        countryCode: countryCode.toUpperCase(),
       },
-      userAttributes: {
-        urlPath: pathname,
+    },
+    userAttributes: {
+      urlPath: pathname,
+    },
+    // Set prerender to false to return JSON instead of HTML
+    prerender: false,
+    // Set cachebust to true to get the latest content.
+    // This should only be used to generate static pages.
+    cachebust: true,
+  }
+
+  const content = await pRetry(
+    () => builderSDKClient.get(pageModelName, builderOptions).toPromise(),
+    {
+      retries: 2,
+      minTimeout: 4000,
+    },
+  ).catch(error => {
+    Sentry.captureException(error, {
+      tags: {
+        domain: 'builder.io',
+        model: pageModelName,
+        countryCode,
       },
-      // Set prerender to false to return JSON instead of HTML
-      prerender: false,
-      // Set cachebust to true to get the latest content.
-      // This should only be used to generate static pages.
-      cachebust: true,
+      extra: {
+        pathname,
+      },
+      level: 'error',
     })
-    .toPromise()
-    .catch(error => {
-      Sentry.captureException(error, {
-        tags: {
-          domain: 'builder.io',
-          model: pageModelName,
-          countryCode,
-        },
-        extra: {
-          pathname,
-        },
-        level: 'error',
-      })
-      throw error
-    })
+    throw error
+  })
+
+  return content
 }
