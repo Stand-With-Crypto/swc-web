da0953edd3d449db3e15bba50d062e3814ea3ab2
Paulo Victor <paulovictor.dasilva@coinbase.com>
2025-08-14 17:01:55 -0400

    refactor: streamline getData function in useENS hook by removing unnecessary try-catch and logging

--- src/hooks/useENS.ts
+++ src/hooks/useENS.ts
@@ -1,6 +1,5 @@
 import useSWR from 'swr'
 import { resolveAvatar, resolveName } from 'thirdweb/extensions/ens'
-import { useSocialProfiles } from 'thirdweb/react'
 
 import { useThirdwebAuthUser } from '@/hooks/useAuthUser'
 import { thirdwebClient } from '@/utils/shared/thirdwebClient'
@@ -10,43 +9,27 @@ async function getData(address?: string) {
     return null
   }
 
-  try {
-    const ens = await resolveName({
-      client: thirdwebClient,
-      address,
-    })
+  const ens = await resolveName({
+    client: thirdwebClient,
+    address,
+  })
 
-    console.log('ens', ens, thirdwebClient)
+  const avatarUrl = ens
+    ? await resolveAvatar({
+        client: thirdwebClient,
+        name: ens,
+      })
+    : null
 
-    const avatarUrl = ens
-      ? await resolveAvatar({
-          client: thirdwebClient,
-          name: ens,
-        })
-      : null
-
-    return {
-      ens,
-      avatarUrl,
-    }
-  } catch (error) {
-    console.error(error)
-    return null
+  return {
+    ens,
+    avatarUrl,
   }
 }
 
 export function useENS() {
   const { user } = useThirdwebAuthUser()
   const address = user?.address
-  console.log({ address, thirdwebClient })
-  const { data: socialProfiles } = useSocialProfiles({
-    client: thirdwebClient,
-    address,
-  })
-
-  const correctProfile = socialProfiles?.find(profile => profile.type === 'ens')
-
-  console.log({ correctProfile, socialProfiles })
 
   return useSWR(address, getData, {
     errorRetryCount: 0,
