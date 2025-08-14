264242313fd3596fb934ca63285482f55dd6cb71
Nicolas Renard <nicolas.renard@coinbase.com>
2025-08-14 18:27:28 -0300

    fix: restore maxLength property for text input in PollItem component

--- src/components/app/pagePolls/pollItem.tsx
+++ src/components/app/pagePolls/pollItem.tsx
@@ -92,7 +92,6 @@ export function PollItem({
               className="hidden"
               {...register('answers')}
               disabled={isDisabled}
-              maxLength={FIELD_MAX_LENGTH}
               type="radio"
               value={value}
             />
@@ -116,6 +115,7 @@ export function PollItem({
                 <Input
                   className="w-full rounded-lg border px-4 py-2 focus:border-gray-400 focus:outline-none"
                   disabled={isOtherFieldDisabled}
+                  maxLength={FIELD_MAX_LENGTH}
                   placeholder="Please specify"
                   type="text"
                   {...field}
