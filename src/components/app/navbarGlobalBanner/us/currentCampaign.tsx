52a9df6add7948dcb63bd4f71154b8ad3bb1f3b4
Nicolas Renard <nicolas.renard@coinbase.com>
2025-09-29 10:33:26 -0300

    feat: add USDC Rewards campaign banner and URL generation

--- src/components/app/navbarGlobalBanner/us/currentCampaign.tsx
+++ src/components/app/navbarGlobalBanner/us/currentCampaign.tsx
@@ -1,3 +1,23 @@
+import Link from 'next/link'
+
+import { CampaignWrapper } from '@/components/app/navbarGlobalBanner/common/campaignWrapper'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
+import { getIntlUrls } from '@/utils/shared/urls'
+
+const countryCode = SupportedCountryCodes.US
+const urls = getIntlUrls(countryCode)
+
 export function UsCurrentNavbarGlobalBannerCampaign() {
-  return null
+  const campaignUrl = urls.usdcRewardsSep2025()
+
+  return (
+    <CampaignWrapper>
+      <p>
+        Big Banks are coming after your crypto rewards - Tell Your Senator No - Click{' '}
+        <strong>
+          <Link href={campaignUrl}>here</Link>
+        </strong>{' '}
+      </p>
+    </CampaignWrapper>
+  )
 }
