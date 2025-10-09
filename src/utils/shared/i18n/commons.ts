7aaccbc160dcb6ce26c6627dbba00868fb23a8bf
Theo Nejm <54482697+theo-nejm@users.noreply.github.com>
2025-10-09 14:58:26 -0300

    feat: enhance internationalization in UserActionFormActionUnavailable component (#2772)
    Co-authored-by: Theo Nejm <theo.nejm@coinbase.com>

--- src/utils/shared/i18n/commons.ts
+++ src/utils/shared/i18n/commons.ts
@@ -1,6 +1,7 @@
 import { createI18nMessages } from '@/utils/shared/i18n/createI18nMessages'
 import { mergeI18nMessages } from '@/utils/shared/i18n/mergeI18nMessages'
 import { I18nMessages } from '@/utils/shared/i18n/types'
+import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 //It's made that way to keep the extraction script working
 const i18nMessages = createI18nMessages({
@@ -12,6 +13,11 @@ const i18nMessages = createI18nMessages({
       GB: 'United Kingdom',
       AU: 'Australia',
       EU: 'European Union',
+      USDemonym: 'American',
+      AUDemonym: 'Australian',
+      GBDemonym: 'British',
+      CADemonym: 'Canadian',
+      EUDemonym: 'European',
     },
     fr: {
       territoryDivision: 'État',
@@ -20,6 +26,11 @@ const i18nMessages = createI18nMessages({
       GB: 'Royaume-Uni',
       AU: 'Australie',
       EU: 'Union européenne',
+      USDemonym: 'Américain',
+      AUDemonym: 'Australien',
+      GBDemonym: 'Brit',
+      CADemonym: 'Canadien',
+      EUDemonym: 'Europeen',
     },
     de: {
       territoryDivision: 'Staat',
@@ -28,6 +39,11 @@ const i18nMessages = createI18nMessages({
       GB: 'Vereinigtes Königreich',
       AU: 'Australien',
       EU: 'Europäische Union',
+      USDemonym: 'Amerikanisch',
+      AUDemonym: 'Australisch',
+      GBDemonym: 'Britisch',
+      CADemonym: 'Kanadisch',
+      EUDemonym: 'Europäer',
     },
   },
   messagesOverrides: {
@@ -70,3 +86,19 @@ export const i18nCommonMessages = i18nMessages
 export function withI18nCommons<T extends Record<string, string>>(messages: I18nMessages<T>) {
   return mergeI18nMessages(i18nCommonMessages, messages)
 }
+
+export const COUNTRY_CODE_TO_DEMONYM_KEY = {
+  [SupportedCountryCodes.US]: 'USDemonym',
+  [SupportedCountryCodes.GB]: 'GBDemonym',
+  [SupportedCountryCodes.CA]: 'CADemonym',
+  [SupportedCountryCodes.AU]: 'AUDemonym',
+  [SupportedCountryCodes.EU]: 'EUDemonym',
+} as const
+
+export const COUNTRY_CODE_TO_DISPLAY_NAME_KEY = {
+  [SupportedCountryCodes.US]: 'US',
+  [SupportedCountryCodes.GB]: 'GB',
+  [SupportedCountryCodes.CA]: 'CA',
+  [SupportedCountryCodes.AU]: 'AU',
+  [SupportedCountryCodes.EU]: 'EU',
+} as const
