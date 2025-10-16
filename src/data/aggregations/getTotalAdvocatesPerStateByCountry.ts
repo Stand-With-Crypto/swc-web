f633eddb52fe2c9697b3069654da07265b07cec1
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-10-16 11:50:09 -0300

    refactor(data): extract getStatesForCountry helper to reduce duplication

--- src/data/aggregations/getTotalAdvocatesPerStateByCountry.ts
+++ src/data/aggregations/getTotalAdvocatesPerStateByCountry.ts
@@ -14,63 +14,52 @@ type TotalAdvocatesPerStateQuery = {
   totalAdvocates: bigint
 }[]
 
-const fetchAllFromPrismaByCountry = async (countryCode: SupportedCountryCodes) => {
+function getStatesForCountry(countryCode: SupportedCountryCodes): string[] {
   switch (countryCode) {
-    case SupportedCountryCodes.US: {
-      const states = Object.keys(US_STATE_CODE_TO_DISTRICT_COUNT_MAP)
-      return prismaClient.$queryRaw<TotalAdvocatesPerStateQuery>`
-        SELECT
-          address.administrative_area_level_1 AS state,
-          COUNT(user.id) AS totalAdvocates
-        FROM address
-        JOIN user ON user.address_id = address.id
-        WHERE address.country_code = ${countryCode}
-        AND address.administrative_area_level_1 IN (${Prisma.join(states)})
-        GROUP BY address.administrative_area_level_1;
-      `
-    }
-    case SupportedCountryCodes.CA: {
-      const provinces = Object.keys(CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP)
-      return prismaClient.$queryRaw<TotalAdvocatesPerStateQuery>`
-        SELECT
-          address.administrative_area_level_1 AS state,
-          COUNT(user.id) AS totalAdvocates
-        FROM address
-        JOIN user ON user.address_id = address.id
-        WHERE address.country_code = ${countryCode}
-        AND address.administrative_area_level_1 IN (${Prisma.join(provinces)})
-        GROUP BY address.administrative_area_level_1;
-      `
-    }
-    case SupportedCountryCodes.AU: {
-      const states = Object.keys(AU_STATE_CODE_TO_DISPLAY_NAME_MAP)
-      return prismaClient.$queryRaw<TotalAdvocatesPerStateQuery>`
-        SELECT
-          address.administrative_area_level_1 AS state,
-          COUNT(user.id) AS totalAdvocates
-        FROM address
-        JOIN user ON user.address_id = address.id
-        WHERE address.country_code = ${countryCode}
-        AND address.administrative_area_level_1 IN (${Prisma.join(states)})
-        GROUP BY address.administrative_area_level_1;
-      `
-    }
-    case SupportedCountryCodes.GB: {
-      const countries = Object.keys(GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP)
-      return prismaClient.$queryRaw<TotalAdvocatesPerStateQuery>`
-        SELECT
-          address.swc_civic_administrative_area AS state,
-          COUNT(user.id) AS totalAdvocates
-        FROM address
-        JOIN user ON user.address_id = address.id
-        WHERE address.country_code = ${countryCode}
-        AND address.swc_civic_administrative_area IN (${Prisma.join(countries)})
-        GROUP BY address.swc_civic_administrative_area;
-      `
-    }
+    case SupportedCountryCodes.US:
+      return Object.keys(US_STATE_CODE_TO_DISTRICT_COUNT_MAP)
+    case SupportedCountryCodes.CA:
+      return Object.keys(CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP)
+    case SupportedCountryCodes.AU:
+      return Object.keys(AU_STATE_CODE_TO_DISPLAY_NAME_MAP)
+    case SupportedCountryCodes.GB:
+      return Object.keys(GB_MAIN_COUNTRY_CODE_TO_DISPLAY_NAME_MAP)
     default:
       return []
   }
+}
+
+const fetchAllFromPrismaByCountry = async (countryCode: SupportedCountryCodes) => {
+  const states = getStatesForCountry(countryCode)
+
+  if (states.length === 0) {
+    return []
+  }
+
+  // GB uses a different field for administrative area
+  if (countryCode === SupportedCountryCodes.GB) {
+    return prismaClient.$queryRaw<TotalAdvocatesPerStateQuery>`
+      SELECT
+        address.swc_civic_administrative_area AS state,
+        COUNT(user.id) AS totalAdvocates
+      FROM address
+      JOIN user ON user.address_id = address.id
+      WHERE address.country_code = ${countryCode}
+      AND address.swc_civic_administrative_area IN (${Prisma.join(states)})
+      GROUP BY address.swc_civic_administrative_area;
+    `
+  }
+
+  return prismaClient.$queryRaw<TotalAdvocatesPerStateQuery>`
+    SELECT
+      address.administrative_area_level_1 AS state,
+      COUNT(user.id) AS totalAdvocates
+    FROM address
+    JOIN user ON user.address_id = address.id
+    WHERE address.country_code = ${countryCode}
+    AND address.administrative_area_level_1 IN (${Prisma.join(states)})
+    GROUP BY address.administrative_area_level_1;
+  `
 }
 
 const parseTotalAdvocatesPerState = (totalAdvocatesPerState: TotalAdvocatesPerStateQuery) => {
