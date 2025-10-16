72d2fdfb0073b929f30fe67ff8c9538ccf62c613
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-10-16 11:59:03 -0300

    refactor(data): simplify query logic, keep GB without static list
    - Removed GB from static state list (uses != '' filter as before)
    - Simplified fetchAllFromPrismaByCountry with cleaner switch statement
    - Inline getStatesForCountry call in query for better readability

--- src/data/aggregations/getTotalAdvocatesPerStateByCountry.ts
+++ src/data/aggregations/getTotalAdvocatesPerStateByCountry.ts
@@ -1,12 +1,30 @@
 import 'server-only'
 
+import { Prisma } from '@prisma/client'
+
 import { prismaClient } from '@/utils/server/prismaClient'
+import { AU_STATE_CODE_TO_DISPLAY_NAME_MAP } from '@/utils/shared/stateMappings/auStateUtils'
+import { CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP } from '@/utils/shared/stateMappings/caProvinceUtils'
+import { US_STATE_CODE_TO_DISTRICT_COUNT_MAP } from '@/utils/shared/stateMappings/usStateDistrictUtils'
 import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 
 type TotalAdvocatesPerStateQuery = {
   state: string
   totalAdvocates: bigint
 }[]
+
+function getStatesForCountry(countryCode: SupportedCountryCodes): string[] {
+  switch (countryCode) {
+    case SupportedCountryCodes.US:
+      return Object.keys(US_STATE_CODE_TO_DISTRICT_COUNT_MAP)
+    case SupportedCountryCodes.CA:
+      return Object.keys(CA_PROVINCES_AND_TERRITORIES_CODE_TO_DISPLAY_NAME_MAP)
+    case SupportedCountryCodes.AU:
+      return Object.keys(AU_STATE_CODE_TO_DISPLAY_NAME_MAP)
+    default:
+      return []
+  }
+}
 
 const fetchAllFromPrismaByCountry = async (countryCode: SupportedCountryCodes) => {
   switch (countryCode) {
@@ -28,7 +46,7 @@ const fetchAllFromPrismaByCountry = async (countryCode: SupportedCountryCodes) =
         COUNT(user.id) AS totalAdvocates
       FROM address
       JOIN user ON user.address_id = address.id
-      WHERE address.administrative_area_level_1 != ''
+      WHERE address.administrative_area_level_1 IN (${Prisma.join(getStatesForCountry(countryCode))})
       AND address.country_code = ${countryCode}
       GROUP BY address.administrative_area_level_1;
     `
