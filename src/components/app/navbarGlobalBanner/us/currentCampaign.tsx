3bed2f68b607e587291151c2055e88a922980e72
Nicolas Renard <nicolas.renard@coinbase.com>
2025-10-24 16:16:26 -0300

    refactor: update US current campaign banner and deactivate related email campaign

--- src/components/app/navbarGlobalBanner/us/currentCampaign.tsx
+++ src/components/app/navbarGlobalBanner/us/currentCampaign.tsx
@@ -1,23 +1,3 @@
-import Link from 'next/link'
-
-import { CampaignWrapper } from '@/components/app/navbarGlobalBanner/common/campaignWrapper'
-import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
-import { getIntlUrls } from '@/utils/shared/urls'
-
-const countryCode = SupportedCountryCodes.US
-const urls = getIntlUrls(countryCode)
-
 export function UsCurrentNavbarGlobalBannerCampaign() {
-  const campaignUrl = urls.usdcRewardsSep2025()
-
-  return (
-    <CampaignWrapper>
-      <p>
-        Big Banks are coming after your crypto rewards - Tell Your Senator No - Click{' '}
-        <strong>
-          <Link href={campaignUrl}>here</Link>
-        </strong>{' '}
-      </p>
-    </CampaignWrapper>
-  )
+  return null
 }
