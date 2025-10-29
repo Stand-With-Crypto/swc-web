f65eafa4d365290ba2a23f7a22db4490ab63dccc
Paulo Victor da Silva <paulovictor.dasilva@coinbase.com>
2025-10-29 18:20:16 -0300

    refactor: enhance ESLint configuration with new plugins, improved rules, and global ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,154 +1,196 @@
-import js from '@eslint/js'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths'
+import formatjs from 'eslint-plugin-formatjs'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import nextPlugin from '@next/eslint-plugin-next'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import typescriptParser from '@typescript-eslint/parser'
-import formatjsPlugin from 'eslint-plugin-formatjs'
-import importPlugin from 'eslint-plugin-import'
-import noRelativeImportPathsPlugin from 'eslint-plugin-no-relative-import-paths'
 import reactPlugin from 'eslint-plugin-react'
 import reactHooksPlugin from 'eslint-plugin-react-hooks'
-import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort'
+import importPlugin from 'eslint-plugin-import'
 import storybookPlugin from 'eslint-plugin-storybook'
 
 const sortImports = {
   groups: [
-    // Side effect imports first i.e. import "some-polyfill";
     ['^\\u0000'],
-    // React, react-native, react prefixed imports, all other 3rd party imports
     ['^react$', '^react-native$', '^react', '^@?\\w'],
-    // Shared code imports
     ['^:.*'],
-    // Root level and App specific imports
     [
       '^@/(actions|app|bin|clientModels|components|data|hooks|inngest|intl|mocks|pages|staticContent|types|utils|validation)(/.*)?',
     ],
-    // Parent imports. Put `..` last.
     ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-    // Other relative imports. Put same-folder imports and `.` last.
     ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
   ],
 }
 
-export default [
-  {
-    ignores: [
-      '**/**/*.generated.ts',
-      '*.js',
-      '**/generated/**',
-      'node_modules/**',
-      'src/data/dtsi/generated.ts',
-      'src/data/dtsi/introspection.json',
-    ],
-  },
+export default defineConfig(
+  [
+    {
+      files: ['**/*.{ts,tsx}'],
+      ignores: ['.storybook/**/*.{ts,tsx}'],
+      plugins: {
+        '@typescript-eslint': typescriptEslint,
+        '@next/next': nextPlugin,
+        react: reactPlugin,
+        'react-hooks': reactHooksPlugin,
+        import: importPlugin,
+        'no-relative-import-paths': noRelativeImportPaths,
+        formatjs,
+        'simple-import-sort': simpleImportSort,
+      },
 
-  js.configs.recommended,
+      languageOptions: {
+        parser: typescriptParser,
+        parserOptions: {
+          project: './tsconfig.json',
+          tsconfigRootDir: import.meta.dirname,
+        },
+      },
 
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: typescriptParser,
-      parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: import.meta.dirname,
+      rules: {
+        ...typescriptEslint.configs.recommended.rules,
+        ...nextPlugin.configs.recommended.rules,
+        ...nextPlugin.configs['core-web-vitals'].rules,
+        ...reactPlugin.configs.recommended.rules,
+
+        // UPGRADE TO NEXTJS 16: These are disabled because new versions of these rules are more strict and would require a lot of changes on the codebase.
+        'react/prop-types': 'off',
+        'react-hooks/set-state-in-effect': 'off',
+        'react-hooks/immutability': 'off',
+        'react/no-unknown-property': 'off',
+        /***********/
+
+        eqeqeq: ['error', 'always'],
+        'formatjs/enforce-description': ['error', 'literal'],
+        'formatjs/no-offset': 'error',
+        'formatjs/enforce-default-message': ['error', 'literal'],
+
+        '@typescript-eslint/no-shadow': [
+          'error',
+          {
+            ignoreOnInitialization: true,
+          },
+        ],
+
+        '@typescript-eslint/restrict-template-expressions': [
+          'error',
+          {
+            allowAny: true,
+            allowNumber: true,
+            allowBoolean: false,
+            allowNullish: false,
+          },
+        ],
+
+        'no-empty': [
+          'error',
+          {
+            allowEmptyCatch: true,
+          },
+        ],
+
+        'react/jsx-sort-props': 'warn',
+        'react/no-unescaped-entities': 'off',
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-floating-promises': 'error',
+
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+          },
+        ],
+
+        '@typescript-eslint/no-namespace': 'off',
+
+        'no-relative-import-paths/no-relative-import-paths': [
+          'error',
+          {
+            allowSameFolder: true,
+          },
+        ],
+
+        'no-restricted-imports': [
+          'error',
+          {
+            paths: [
+              {
+                name: 'lodash',
+                message: 'Please use lodash-es instead.',
+              },
+              {
+                name: 'react-use',
+                importNames: ['useEffectOnce'],
+                message: 'Please use the version of this hook defined in `@/hooks`',
+              },
+            ],
+          },
+        ],
+
+        'react-hooks/exhaustive-deps': [
+          'warn',
+          {
+            additionalHooks: 'useLoadingCallback',
+          },
+        ],
+
+        'simple-import-sort/imports': ['warn', sortImports],
+        'simple-import-sort/exports': 'warn',
+        'import/first': 'warn',
+        'import/newline-after-import': 'warn',
+        curly: ['error', 'multi-line'],
+        'import/no-duplicates': 'error',
+
+        'import/no-restricted-paths': [
+          'error',
+          {
+            zones: [
+              {
+                target: './cypress',
+                from: './src',
+              },
+            ],
+          },
+        ],
+
+        'react/no-unstable-nested-components': [
+          'error',
+          {
+            allowAsProps: true,
+          },
+        ],
+
+        '@typescript-eslint/consistent-type-definitions': 'error',
+      },
+
+      settings: {
+        react: {
+          version: 'detect',
+        },
       },
     },
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      formatjs: formatjsPlugin,
-      import: importPlugin,
-      'no-relative-import-paths': noRelativeImportPathsPlugin,
-      '@next/next': nextPlugin,
-      react: reactPlugin,
-      'react-hooks': reactHooksPlugin,
-      'simple-import-sort': simpleImportSortPlugin,
-    },
-    rules: {
-      eqeqeq: ['error', 'always'],
-      'no-empty': ['error', { allowEmptyCatch: true }],
-      curly: ['error', 'multi-line'],
 
-      ...typescriptEslint.configs.recommended.rules,
-      '@typescript-eslint/no-shadow': ['error', { ignoreOnInitialization: true }],
-      '@typescript-eslint/restrict-template-expressions': [
-        'error',
-        { allowAny: true, allowNumber: true, allowBoolean: false, allowNullish: false },
-      ],
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-        },
-      ],
-      '@typescript-eslint/no-namespace': 'off',
-      '@typescript-eslint/consistent-type-definitions': 'error',
-
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-
-      ...reactPlugin.configs.recommended.rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-
-      'formatjs/enforce-description': ['error', 'literal'],
-      'formatjs/no-offset': 'error',
-      'formatjs/enforce-default-message': ['error', 'literal'],
-
-      'react/jsx-sort-props': 'warn',
-      'react/no-unescaped-entities': 'off',
-      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
-      'react-hooks/exhaustive-deps': [
-        'warn',
-        {
-          additionalHooks: 'useLoadingCallback',
-        },
-      ],
-
-      'no-relative-import-paths/no-relative-import-paths': ['error', { allowSameFolder: true }],
-      'no-restricted-imports': [
-        'error',
-        {
-          paths: [
-            {
-              name: 'lodash',
-              message: 'Please use lodash-es instead.',
-            },
-            {
-              name: 'react-use',
-              importNames: ['useEffectOnce'],
-              message: 'Please use the version of this hook defined in `@/hooks`',
-            },
-          ],
-        },
-      ],
-      'simple-import-sort/imports': ['warn', sortImports],
-      'simple-import-sort/exports': 'warn',
-      'import/first': 'warn',
-      'import/newline-after-import': 'warn',
-      'import/no-duplicates': 'error',
-      'import/no-restricted-paths': [
-        'error',
-        {
-          zones: [{ target: './cypress', from: './src' }],
-        },
-      ],
-    },
-    settings: {
-      react: {
-        version: 'detect',
+    {
+      files: ['**/*.stories.{ts,tsx}'],
+      plugins: {
+        storybook: storybookPlugin,
+      },
+      rules: {
+        ...storybookPlugin.configs.recommended.rules,
       },
     },
-  },
-
-  {
-    files: ['**/*.stories.{ts,tsx}'],
-    plugins: {
-      storybook: storybookPlugin,
-    },
-    rules: {
-      ...storybookPlugin.configs.recommended.rules,
-    },
-  },
-]
+  ],
+  globalIgnores([
+    '**/**/*.generated.ts',
+    '**/*.js',
+    '**/generated/**/*',
+    '.next/**',
+    'node_modules/**',
+    'src/data/dtsi/generated.ts',
+    'src/data/dtsi/introspection.json',
+    '.storybook/**',
+  ]),
+)
