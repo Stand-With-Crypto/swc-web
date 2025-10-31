38fb5f97ba11f0e3adcd6051301eff1044fa6ea5
eduardopicolo-cb <eduardo.npicolo@gmail.com>
2025-10-31 18:45:54 -0300

    Auth gate deeplink

--- src/components/app/userActionFormLetter/homepageDialogDeeplinkWrapper.tsx
+++ src/components/app/userActionFormLetter/homepageDialogDeeplinkWrapper.tsx
@@ -5,12 +5,14 @@ import { UserActionType } from '@prisma/client'
 import { notFound } from 'next/navigation'
 
 import { LoginDialogWrapper } from '@/components/app/authentication/loginDialogWrapper'
+import { ThirdwebLoginContent } from '@/components/app/authentication/thirdwebLoginContent'
 import { GeoGate } from '@/components/app/geoGate'
 import { UserActionFormActionUnavailable } from '@/components/app/userActionFormCommon/actionUnavailable'
 import { UserActionFormLetter } from '@/components/app/userActionFormLetter'
 import { ANALYTICS_NAME_USER_ACTION_FORM_LETTER } from '@/components/app/userActionFormLetter/common/constants'
 import { UserActionFormLetterSkeleton } from '@/components/app/userActionFormLetter/common/skeleton'
 import { getUserActionCTAsByCountry } from '@/components/app/userActionGridCTAs/constants/ctas'
+import { dialogContentPaddingStyles } from '@/components/ui/dialog/styles'
 import { trackDialogOpen } from '@/components/ui/dialog/trackDialogOpen'
 import { useApiResponseForUserFullProfileInfo } from '@/hooks/useApiResponseForUserFullProfileInfo'
 import { useEncodedInitialValuesQueryParam } from '@/hooks/useEncodedInitialValuesQueryParam'
@@ -18,6 +20,7 @@ import { usePreventOverscroll } from '@/hooks/usePreventOverscroll'
 import { SupportedCountryCodes } from '@/utils/shared/supportedCountries'
 import { getIntlUrls } from '@/utils/shared/urls'
 import { AUUserActionLetterCampaignName } from '@/utils/shared/userActionCampaigns/au/auUserActionCampaigns'
+import { cn } from '@/utils/web/cn'
 
 interface UserActionFormLetterDeeplinkWrapperProps {
   countryCode: SupportedCountryCodes
@@ -66,14 +69,15 @@ function UserActionFormLetterDeeplinkWrapperContent(
     )
   }
 
-  // return <UserActionFormLetter {...props} initialValues={initialValues} user={user} />
   return (
     <LoginDialogWrapper
       authenticatedContent={
         <UserActionFormLetter {...props} initialValues={initialValues} user={user} />
       }
     >
-      <UserActionFormLetter {...props} initialValues={initialValues} user={user} />
+      <div className={cn(dialogContentPaddingStyles)}>
+        <ThirdwebLoginContent />
+      </div>
     </LoginDialogWrapper>
   )
 }
